bc9d5a2a1f1ca4dbf8d680127370f6a3db9544c3
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-06-27 18:50:29 -0600

    Create multiply.js

---
+++ Kata/multiply.js
@@ -0,0 +1,10 @@
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  return a * b;
+}
+
+// function multiply(a, b){
+//   let c = a * b;
+//   return c;
+// }
