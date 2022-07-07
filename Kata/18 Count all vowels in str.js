ebec44a4b939dd9c6d7748b309dea28a6185910f
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-07 16:13:59 -0600

    Update 18 Count all vowels in str.js

--- Kata/18 Count all vowels in str.js
+++ Kata/18 Count all vowels in str.js
@@ -14,3 +14,18 @@ function getCount(str) {
 }
 
 console.log(getCount("abracadabra")); // 5
+
+
+function getCount(str) {
+  var vowelsCount = 0;
+  var vowels = ["a","e","i","o","u"];
+  for(var i = 0;i < str.length;i++){
+    for(var j=0;j<vowels.length;j++){
+      if(str[i] === vowels[j]){
+        vowelsCount++;
+      }
+    }
+  }
+  
+  return vowelsCount;
+}
