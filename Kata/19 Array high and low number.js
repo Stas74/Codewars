83768b7dabd72730b3e0393106c5dd2e0fcb6ffd
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-07 16:57:29 -0600

    Update 19 Array high and low number.js

--- Kata/19 Array high and low number.js
+++ Kata/19 Array high and low number.js
@@ -24,3 +24,17 @@ function highAndLow(numbers){
 
 console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")); // "42 -9"
 console.log(highAndLow("1 2 3")); // "3 1"
+
+
+
+function highAndLow(numbers){
+  numbers = numbers.split(' ').map(Number);
+  return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+}
+
+
+function highAndLow(numbers){
+  numbers = numbers.split(' ');
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
+
