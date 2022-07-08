399ad9b6f4d453f09fc12b6af1ed50c4a52dee2d
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-08 15:29:19 -0600

    Create 25 Return boolean string.js

---
+++ Kata/25 Return boolean string.js
@@ -0,0 +1,12 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+  //your code here
+  return true == b ? "true" : "false";
+}
+
+console.log(booleanToString(true));  // "true"
+console.log(booleanToString(false));  // "false"
+
