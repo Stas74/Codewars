7453ffc7b0f284cd8727c766c111b1bb894c0080
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-08 16:01:58 -0600

    Update 27 Sum of numbers of array.js

--- Kata/27 Sum of numbers of array.js
+++ Kata/27 Sum of numbers of array.js
@@ -1,4 +1,5 @@
-// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. 
+// If the array does not contain any numbers then you should return 0.
 
 // Examples
 // Input: [1, 5.2, 4, 0, -1]
@@ -29,3 +30,11 @@ function sum(numbers) {
 
 console.log(sum([])); // 0
 console.log(sum([1, 5.2, 4, 0, -1])); // 9.2
+
+
+
+function sum(numbers) {
+  return numbers.reduce((a, b) => a + b, 0);
+}
+
+
