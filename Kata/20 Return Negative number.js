f47d6d6b71c7043b4d2c5774d48e54874b3720d3
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-07 18:42:26 -0600

    Create 20 Return Negative number.js

---
+++ Kata/20 Return Negative number.js
@@ -0,0 +1,24 @@
+// In this simple assignment you are given a number and have to make it negative. 
+// But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(num) {
+  // Code?
+  if (num <= 0) {
+    return num;
+  }  else {
+    return num - num * 2;
+  }
+}
+
+console.log(makeNegative(42)) // -42
+console.log(makeNegative(0)) // 0
+console.log(makeNegative(-12)) // -12
