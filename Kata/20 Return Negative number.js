5c01decd4c4f94ec13c5d4dd0b11f137e983676a
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-07 18:43:34 -0600

    Update 20 Return Negative number.js

--- Kata/20 Return Negative number.js
+++ Kata/20 Return Negative number.js
@@ -22,3 +22,27 @@ function makeNegative(num) {
 console.log(makeNegative(42)) // -42
 console.log(makeNegative(0)) // 0
 console.log(makeNegative(-12)) // -12
+
+
+// --------------------------------------------------------
+function makeNegative(num) {
+  return -Math.abs(num);
+}
+
+
+
+function makeNegative(num) {
+  return num < 0 ? num : -num;
+}
+
+
+
+function makeNegative(num) {
+  if ( num > 0 ) {
+        return -num;
+    }
+    else {
+        return num;
+    }
+}
+
