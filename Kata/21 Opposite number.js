377fa50e60e4d8802bb6f6fd6bfd867779f35444
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-07 19:20:20 -0600

    Create 21 Opposite number.js

---
+++ Kata/21 Opposite number.js
@@ -0,0 +1,18 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  //your code here
+  return -number;
+}
+
+
+
+console.log(opposite(1))  // -1
+console.log(opposite(-1))  // 1
+
