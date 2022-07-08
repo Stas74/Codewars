9f2329098dfcb9ea89ce48583bd260efc3c74d89
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-08 13:50:07 -0600

    Create 22 String to upper case.js

---
+++ Kata/22 String to upper case.js
@@ -0,0 +1,9 @@
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+  // Code here
+  return str.toUpperCase();
+}
+
+console.log(makeUpperCase("hello")) // "HELLO"
+
