7a9be33d83b009175fc882180d9df331e64087bb
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-11 13:37:54 -0600

    Update 31 Reversed sequence array.js

--- Kata/31 Reversed sequence array.js
+++ Kata/31 Reversed sequence array.js
@@ -13,3 +13,20 @@ const reverseSeq = (n) => {
 
 console.log(reverseSeq(5)); // [5, 4, 3, 2, 1]
 
+
+// ----------------------------------------------------
+
+const reverseSeq = n => {
+  return Array(n).fill(0).map((e, i) => n - i );
+};
+
+
+const reverseSeq = length => Array.from({length}, () => length--)
+
+
+const reverseSeq = num => {
+  return new Array(num)
+      .fill()
+      .map((d, i) => i + 1)
+      .reverse()
+};
