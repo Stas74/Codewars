18a7d93f4e918e00785d35479f9d0f23bedb17ac
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-11 15:45:45 -0600

    Create 33 Sum of Numbers.js

---
+++ Kata/33 Sum of Numbers.js
@@ -0,0 +1,34 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and
+// including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+function getSum(a, b) {
+  let summ = 0;
+  if (b > a) {
+    while (b >= a) {
+      summ += a;
+      a++;
+    }
+  } else {
+    while (a >= b) {
+      summ += b;
+      b++;
+    }
+  }
+  return summ;
+}
+
+console.log(getSum(0, -1)); // -1
+console.log(getSum(0, 1)); // 1
+console.log(getSum(1, 1)); // 1
+
+// --------------------------------------
