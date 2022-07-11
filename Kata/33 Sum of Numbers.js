d01fbb16de40cc94791a53b56f204ba06cfeceb5
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-11 15:49:07 -0600

    Update 33 Sum of Numbers.js

--- Kata/33 Sum of Numbers.js
+++ Kata/33 Sum of Numbers.js
@@ -31,4 +31,35 @@ console.log(getSum(0, -1)); // -1
 console.log(getSum(0, 1)); // 1
 console.log(getSum(1, 1)); // 1
 
+
 // --------------------------------------
+
+
+const GetSum = (a, b) => {
+  let min = Math.min(a, b),
+      max = Math.max(a, b);
+  return (max - min + 1) * (min + max) / 2;
+}
+
+
+function GetSum( a,b )
+{
+   if (a == b) return a;
+   else if (a < b) return a + GetSum(a+1, b);
+   else return a + GetSum(a-1,b);
+}
+
+
+function GetSum(a,b)
+{
+  return (Math.abs(a - b) + 1) * (a+b) / 2;
+}
+
+
+function GetSum( a,b ) {
+   var result = 0;
+   var bigger = a > b ? a : b;
+   var smaller = a > b ? b : a;
+   for (var i = smaller; i <= bigger; i++) { result += i }
+   return result
+}
