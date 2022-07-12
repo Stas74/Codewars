ec0ebfab6616b4c71bbdc81b5dd494ef13962795
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-11 18:30:05 -0600

    Update 35 Rock Paper Scissors.js

--- Kata/35 Rock Paper Scissors.js
+++ Kata/35 Rock Paper Scissors.js
@@ -29,3 +29,44 @@ const rps = (p1, p2) => {
 console.log(rps("rock", "scissors")); // 1
 console.log(rps("scissors", "rock")); // 2
 console.log(rps("rock", "rock")); // -
+
+
+// -----------------------------------------------------
+
+const rps = (p1, p2) => {
+  if (p1 === p2) return "Draw!";
+  var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+  if (p2 === rules[p1]) {
+    return "Player 1 won!";
+  }
+  else {
+    return "Player 2 won!";
+  }
+};
+
+
+const rps = (p1, p2) => {
+  if(p1 === p2) {
+    return 'Draw!'
+  }; 
+  return `Player ${/rockscissors|scissorspaper|paperrock/.test(p1+p2)? 1 : 2} won!`;
+}
+
+
+const rps = (p1, p2) => {
+  var map = {
+    'rock': 'scissors',
+    'scissors': 'paper',
+    'paper': 'rock'
+  };
+  
+  if (p1 == p2) {
+    return 'Draw!';
+  } else {
+    return 'Player ' + (map[p1] == p2 ? 1 : 2) + ' won!';
+  }
+};
+
+
+rps=(a,b)=>a==b?'Draw!':`Player ${/rp|ps|sr/.test(a[0]+b[0])?2:1} won!`;
+
