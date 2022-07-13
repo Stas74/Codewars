52b1ac0cb736948822aaec337e3ed9b02af2b510
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-13 16:29:43 -0600

    Create 45 Isograms(unique letters).js

---
+++ Kata/45 Isograms(unique letters).js
@@ -0,0 +1,22 @@
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+// Implement a function that determines whether a string that contains only letters is an isogram. 
+// Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
+
+function isIsogram(str){
+  let flag = true;
+  str.toLowerCase().split('').map(function (a, i, w) {
+    return w.indexOf(a) == w.lastIndexOf(a) ? true : flag = false;
+  })
+  return flag;
+}
+
+console.log(isIsogram("Dermatoglyphics")); // true
+console.log(isIsogram("isogram")); // true
+console.log(isIsogram("aba")); // false
+console.log(isIsogram("moOse")); // false
