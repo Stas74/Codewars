fb5dd5969ed3463d9e09dca0ab7bd9677e69cd5f
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-13 16:36:04 -0600

    Update 45 Isograms(unique letters).js

--- Kata/45 Isograms(unique letters).js
+++ Kata/45 Isograms(unique letters).js
@@ -20,3 +20,38 @@ console.log(isIsogram("Dermatoglyphics")); // true
 console.log(isIsogram("isogram")); // true
 console.log(isIsogram("aba")); // false
 console.log(isIsogram("moOse")); // false
+
+// -------------------------------------------------
+
+
+function isIsogram(str){
+  return new Set(str.toUpperCase()).size == str.length;
+}
+
+
+function isIsogram(str){ 
+  return !/(\w).*\1/i.test(str)
+}
+
+
+function isIsogram(str){
+  return !str.match(/([a-z]).*\1/i);
+}
+
+
+function isIsogram(str)  {
+  // Downcase everything, split on letters.
+  str_lc = str.toLowerCase();
+  var letters = str_lc.split('');
+  
+  // Utility: checks whether the given "value" at "index" is actually
+  // first encountered at this "index" position within a string ("self").
+  // Expected to be used with Array#filter.
+  var check4uniqueness = function(value, index, self) { 
+    return self.indexOf(value) === index;
+  }
+  
+  // An isogram string contains no duplicate, hence must have the same
+  // length than the array composed of unique letters from that string.
+  return letters.filter(check4uniqueness).length == str.length;
+}
