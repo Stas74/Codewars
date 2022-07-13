f39971a8b32e35a1caa4907d4b9e20783d5c8e35
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-13 14:11:37 -0600

    Create 43 Alphabet Position.js

---
+++ Kata/43 Alphabet Position.js
@@ -0,0 +1,26 @@
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+
+
+function alphabetPosition(text) {
+  let arr = [];
+  text = text.toLowerCase();
+  for (let i = 0; i < text.length; i++) {
+    if (96 < text.charCodeAt(i) && text.charCodeAt(i) < 123) {
+      arr.push(text.charCodeAt(i) - 96)
+    }
+  }
+  return arr.join(' ');
+}
+
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+console.log(alphabetPosition("The narwhal bacons at midnight.")); // "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
