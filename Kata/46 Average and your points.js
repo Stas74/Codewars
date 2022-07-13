443dc2a254d4bc02949eb968c7a798f906e3cad5
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-13 17:12:42 -0600

    Update 46 Average and your points.js

--- Kata/46 Average and your points.js
+++ Kata/46 Average and your points.js
@@ -18,4 +18,12 @@ function betterThanAverage(classPoints, yourPoints) {
 console.log(betterThanAverage([2, 3], 5)); // true
 console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)); // true
 console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)); // false
-v
+
+
+
+function betterThanAverage(classPoints, yourPoints) {
+  return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
+}
+
+
+
