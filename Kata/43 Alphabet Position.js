42e52dd0dc3451114eacf88f2b9c233cdbe7d983
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-13 14:17:03 -0600

    Update 43 Alphabet Position.js

--- Kata/43 Alphabet Position.js
+++ Kata/43 Alphabet Position.js
@@ -24,3 +24,22 @@ function alphabetPosition(text) {
 
 console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
 console.log(alphabetPosition("The narwhal bacons at midnight.")); // "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
+
+
+// ----------------------------------------------------------------------------------
+
+function alphabetPosition(text) {
+  return text
+    .toUpperCase()
+    .match(/[a-z]/gi)
+    .map( (c) => c.charCodeAt() - 64)
+    .join(' ');
+}
+
+
+function alphabetPosition(text) {
+  return text.match(/[a-zA-Z]/g).map( (el) => el.toLowerCase().charCodeAt() - 96 ).join(' ');
+}
+
+
+let alphabetPosition = (text) => text.toUpperCase().replace(/[^A-Z]/g, '').split('').map(ch => ch.charCodeAt(0) - 64).join(' ');
