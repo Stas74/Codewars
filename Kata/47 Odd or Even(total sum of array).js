8c0707be05c360358a0049ebfb627aeffc099647
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-14 15:02:46 -0600

    Create 47 Odd or Even(total sum of array).js

---
+++ Kata/47 Odd or Even(total sum of array).js
@@ -0,0 +1,27 @@
+// Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+
+function oddOrEven(array) {
+  // let total = array.reduce((a, b) => a + b, 0)
+  // return total % 2 === 0 ? "even" : "odd";
+  return array.reduce((a, b) => a + b, 0) % 2 === 0 ? "even" : "odd";
+}
+
+console.log(oddOrEven([0])); // even
+console.log(oddOrEven([1])); // odd
+console.log(oddOrEven([])); // even
+console.log(oddOrEven([-1023, -1, 3])); // odd
