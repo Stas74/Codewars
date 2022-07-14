46f56cd0c90a11a0da60ecf6b4bce7c68b3476c9
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-14 17:58:52 -0600

    Update 49 Number is square(squared root).js

--- Kata/49 Number is square(squared root).js
+++ Kata/49 Number is square(squared root).js
@@ -31,3 +31,9 @@ console.log(isSquare(-1)); // false
 console.log(isSquare(0)); // true
 console.log(isSquare(3)); // false
 console.log(isSquare(4)); // true
+
+
+
+function isSquare(n) {
+  return Math.sqrt(n) % 1 === 0;
+}
