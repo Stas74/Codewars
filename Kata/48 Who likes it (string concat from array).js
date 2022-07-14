db8ff40fbbb8092f4c92d907b2cafdfa554835c0
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-14 17:45:10 -0600

    Update 48 Who likes it (string concat from array).js

--- Kata/48 Who likes it (string concat from array).js
+++ Kata/48 Who likes it (string concat from array).js
@@ -30,3 +30,46 @@ console.log(likes([])); // no one likes this
 console.log(likes(["Peter"])); // 'Peter likes this'
 console.log(likes(["Jacob", "Alex"])); // 'Jacob and Alex like this'
 console.log(likes(["Alex", "Jacob", "Mark", "Max"])); // 'Alex, Jacob and 2 others like this'
+
+// -------------------------------------------------------
+
+
+function likes(names) {
+  names = names || [];
+  switch(names.length){
+    case 0: return 'no one likes this'; break;
+    case 1: return names[0] + ' likes this'; break;
+    case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+    case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+    default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+  }
+  
+  
+  
+  function likes(names) {
+  return {
+    0: 'no one likes this',
+    1: `${names[0]} likes this`, 
+    2: `${names[0]} and ${names[1]} like this`, 
+    3: `${names[0]}, ${names[1]} and ${names[2]} like this`, 
+    4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`, 
+  }[Math.min(4, names.length)]
+}
+}
+
+
+function likes (names) {
+  var templates = [
+    'no one likes this',
+    '{name} likes this',
+    '{name} and {name} like this',
+    '{name}, {name} and {name} like this',
+    '{name}, {name} and {n} others like this'
+  ];
+  var idx = Math.min(names.length, 4);
+  
+  return templates[idx].replace(/{name}|{n}/g, function (val) {
+    return val === '{name}' ? names.shift() : names.length;
+  });
+}
+
