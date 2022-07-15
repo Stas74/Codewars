2506895473a82dca57cf89082ba4acdd0ea10f61
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-15 15:36:21 -0600

    Update 55 Sum of two lowest positive integers of array.js

--- Kata/55 Sum of two lowest positive integers of array.js
+++ Kata/55 Sum of two lowest positive integers of array.js
@@ -17,3 +17,15 @@ console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22])); // 13
 console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43])); // 6
 
 console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7])); // 10
+
+
+
+
+function sumTwoSmallestNumbers(numbers) {  
+  var [ a, b ] = numbers.sort((a, b) => a - b)
+  return a + b
+}
+
+
+
+const sumTwoSmallestNumbers = numbers => numbers.sort((x, y) => x - y).slice(0, 2).reduce((x, y) => x + y);
