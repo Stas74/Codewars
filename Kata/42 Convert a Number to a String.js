fc20c6e4fa2314e6de2d0ff14b377470948bad8e
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-14 18:25:57 -0600

    Create 42 Convert a Number to a String.js

---
+++ Kata/42 Convert a Number to a String.js
@@ -0,0 +1,13 @@
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+function numberToString(num) {
+  return ""+num;
+}
+
+console.log(numberToString(67)) // "67"
