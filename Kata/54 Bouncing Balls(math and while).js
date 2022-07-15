5e4ded792082e3a09fa72e00162240f36cca99e0
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-15 15:26:25 -0600

    Update 54 Bouncing Balls(math and while).js

--- Kata/54 Bouncing Balls(math and while).js
+++ Kata/54 Bouncing Balls(math and while).js
@@ -43,3 +43,28 @@ console.log(bouncingBall(3.0, 0.66, 1.5)); // 3
 console.log(bouncingBall(30.0, 0.66, 1.5)); // 15
 
 console.log(bouncingBall(3.0, 1.0, 1.5)); // -1
+
+
+// ------------------------------------------------
+
+
+function bouncingBall(h,  bounce,  window) {
+  var rebounds = -1;
+  if (bounce > 0 && bounce < 1) while (h > window) rebounds+=2, h *= bounce;
+  return rebounds;
+}
+
+
+
+function bouncingBall(h,  bounce,  window) {
+  if(h <= 0 || bounce <= 0 || bounce >= 1 || window >= h){
+    return -1;
+  }
+
+  var newHeight = h * bounce;
+  return bouncingBall(newHeight, bounce, window) + 2;
+}
+
+
+
+const bouncingBall=(h, b, w)=>(b>=1 || h<w || b<=0) ? -1 : Math.ceil(Math.log(w/h)/Math.log(b))*2-1;
