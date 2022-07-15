ca74df3fcbbb92df27c1e3fed3485e72570ef0ff
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-14 18:10:25 -0600

    Create 50 Convert number to reversed array of digits.js

---
+++ Kata/50 Convert number to reversed array of digits.js
@@ -0,0 +1,19 @@
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number
+// within an array in reverse order.
+
+// Example(Input => Output):
+// 348597 => [7,9,5,8,4,3]
+// 0 => [0]
+
+function digitize(n) {
+  return n
+    .toString()
+    .split("")
+    .reverse()
+    .map((x) => +x);
+}
+
+console.log(digitize(35231)); // [1,3,2,5,3]
+console.log(digitize(0)); // [0]
+
