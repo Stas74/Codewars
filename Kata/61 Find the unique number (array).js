a04fa13e108943797a11b827145a6c6db1f61489
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-18 14:21:05 -0600

    Update 61 Find the unique number (array).js

--- Kata/61 Find the unique number (array).js
+++ Kata/61 Find the unique number (array).js
@@ -21,3 +21,15 @@ function findUniq(arr) {
 console.log(findUniq([1, 0, 0])); // 1
 console.log(findUniq([1, 1, 1, 2, 1, 1])); // 2
 console.log(findUniq([3, 10, 3, 3, 3])); // 10
+
+
+
+function findUniq(arr) {
+  arr.sort((a,b)=>a-b);
+  return arr[0]==arr[1]?arr.pop():arr[0]
+}
+
+
+function findUniq(arr) {
+  return arr.find(n => arr.indexOf(n) === arr.lastIndexOf(n));
+}
