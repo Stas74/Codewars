e861698359a1afcd85220fbf6adc0f97dbed51b2
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-18 17:40:50 -0600

    Update 63 Regex validate PIN code.js

--- Kata/63 Regex validate PIN code.js
+++ Kata/63 Regex validate PIN code.js
@@ -16,3 +16,47 @@ console.log(validatePIN("1")); // false
 console.log(validatePIN("-1.234")); // false
 console.log(validatePIN("a234")); // false
 console.log(validatePIN("098765")); // true
+
+
+
+function validatePIN (pin) {
+  
+  var pinlen = pin.length;
+  var isCorrectLength = (pinlen == 4 || pinlen == 6);
+  var hasOnlyNumbers = pin.match(/^\d+$/);
+    
+  if(isCorrectLength && hasOnlyNumbers){
+    return true;
+  }  
+  return false;
+}
+
+
+
+function validatePIN (pin) {
+  //return true or false
+  var n = pin.length;
+  if( n != 4 && n != 6)
+      return false;
+  for (var i in pin)
+      if (pin[i] > '9' || pin[i] < '0')
+          return false;
+  return true;
+}
+
+
+
+/*
+
+  ^ = start of a line, 
+  \d = [0-9], {4} specifies 4 digits, 
+  {6} specifies 6 digits, $ - end of a line
+  
+  first alternative ^\d{4}$ - equivalent to [0-9], matches exactly 4 digits.
+  second alternative ^\d{6}$ - "", matches previous token exactly 6 digits.
+  
+   use RegEx .test to search for these characters stored.
+   
+*/ 
+
+const validatePIN = pin => /^\d{4}$|^\d{6}$/.test(pin);
