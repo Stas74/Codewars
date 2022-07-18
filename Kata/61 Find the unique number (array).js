256b875ffacb589d59771e0393e97a4b73d7a736
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-18 14:14:45 -0600

    Create 61 Find the unique number (array).js

---
+++ Kata/61 Find the unique number (array).js
@@ -0,0 +1,23 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+function findUniq(arr) {
+  let finObj = {};
+  for (let elem of arr) {
+    finObj[elem] ? (finObj[elem] += 1) : (finObj[elem] = 1);
+  }
+  for (let obj in finObj) {
+    if (finObj[obj] === 1) {
+      return +obj; // convert string to number
+    }
+  }
+}
+
+console.log(findUniq([1, 0, 0])); // 1
+console.log(findUniq([1, 1, 1, 2, 1, 1])); // 2
+console.log(findUniq([3, 10, 3, 3, 3])); // 10
