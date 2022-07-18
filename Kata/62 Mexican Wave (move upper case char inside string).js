b47776756d0c9f9f8b3a5963b8bf3e7009f59342
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-18 16:26:52 -0600

    Create 62 Mexican Wave (move upper case char inside string).js

---
+++ Kata/62 Mexican Wave (move upper case char inside string).js
@@ -0,0 +1,29 @@
+// Task
+// In this simple Kata your task is to create a function that turns a string into a Mexican Wave.
+// You will be passed a string and you must return that string in an array
+// where an uppercase letter is a person standing up.
+
+// Rules
+//  1.  The input string will always be lower case but maybe empty.
+
+//  2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+
+//  Example
+// wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+function wave(str) {
+  let finArr = [];
+  // let word = str;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " ") {
+      continue;
+    } else {
+      // let word = str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1, str.length);
+      finArr.push(str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1, str.length));
+    }
+  }
+  return finArr;
+}
+
+console.log(wave("hello")); // ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+console.log(wave("au to")); // [ 'Au to', 'aU to', 'au To', 'au tO' ]
