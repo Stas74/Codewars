b94a8c2c733f6b957a30c757f888d02cd75bc5b5
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-19 14:21:28 -0600

    Update and rename 59 Sort the odd(array).js to 69 Sort the odd(array).js

--- Kata/69 Sort the odd(array).js
+++ Kata/69 Sort the odd(array).js
@@ -13,3 +13,30 @@ function sortArray(array) {
 }
 
 console.log(sortArray([5, 3, 2, 8, 1, 4])); // [1, 3, 2, 8, 5, 4]
+
+
+
+// ????
+
+function sortArray(array) {
+  var odds = [];
+  //loop, if it's odd, push to odds array
+  for (var i = 0; i < array.length; ++i) {
+    if (array[i]%2 !== 0) {
+      odds.push(array[i]);
+    }
+  }
+  //sort odds from smallest to largest
+  odds.sort(function(a,b){
+    return a-b;
+  });
+  
+  //loop through array, replace any odd values with sorted odd values
+  for (var j = 0; j < array.length; ++j) {
+    if (array[j]%2 !== 0) {
+      array[j] = odds.shift();
+    }
+  }
+  
+ return array;
+}
