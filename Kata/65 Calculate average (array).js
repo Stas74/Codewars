8d00e09d7968b8c10e7678ad9143c603eabfab06
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-18 18:44:30 -0600

    Update 65 Calculate average (array).js

--- Kata/65 Calculate average (array).js
+++ Kata/65 Calculate average (array).js
@@ -14,3 +14,21 @@ console.log(find_average([1,2,3,4])); // 2.5
 
 
 
+
+var find_average = (array) => {  
+  return array.length === 0 ? 0 : array.reduce((acc, ind)=> acc + ind, 0)/array.length
+}
+
+
+function find_average(array) {
+  if (array.length === 0) {
+  return 0;
+  }
+  var result = 0;
+  for (i=0; i<array.length; i++) {
+    result +=array[i];
+  }
+  return result/array.length;
+}
+
+
