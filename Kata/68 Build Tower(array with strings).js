f7b1cca5867fcbd61a75b7634a8d4931996dfdb4
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-19 13:40:14 -0600

    Update 68 Build Tower(array with strings).js

--- Kata/68 Build Tower(array with strings).js
+++ Kata/68 Build Tower(array with strings).js
@@ -35,3 +35,28 @@ function towerBuilder(nFloors) {
 console.log(towerBuilder(1)); // ["*"]
 console.log(towerBuilder(2)); // [" * ","***"]
 console.log(towerBuilder(5)); // console.log(towerBuilder(2));
+
+
+
+
+
+function towerBuilder(n) {
+  return Array.from({length: n}, function(v, k) {
+    const spaces = ' '.repeat(n - k - 1);
+    return spaces + '*'.repeat(k + k + 1) + spaces;
+  });
+}
+
+
+
+function towerBuilder(nFloors) {
+  var tower = [];
+  for (var i = 0; i < nFloors; i++) {
+    tower.push(" ".repeat(nFloors - i - 1)
+             + "*".repeat((i * 2)+ 1)
+             + " ".repeat(nFloors - i - 1));
+  }
+  return tower;
+}
+
+
