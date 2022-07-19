eae705dd299bc972e807bee7da206314489c3384
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-18 18:59:17 -0600

    Update 66 Find the first non-consecutive number (array).js

--- Kata/66 Find the first non-consecutive number (array).js
+++ Kata/66 Find the first non-consecutive number (array).js
@@ -31,3 +31,25 @@ function firstNonConsecutive(arr) {
 }
 
 console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])); // 6
+
+
+
+function firstNonConsecutive(arr) {
+  for (let i = 0; i < arr.length - 1; ++i) {
+    if (arr[i] + 1 !== arr[i + 1]) {
+      return arr[i + 1]
+    }
+  }
+  return null
+}
+
+
+
+function firstNonConsecutive (arr) {
+    let result = arr.find((val, index) => val !== index + arr[0]);
+
+    return (Number.isInteger(result)) ? result : null;
+}
+
+
+firstNonConsecutive = arr => arr.length == 1 ? null : arr[0] + 1 != arr[1] ? arr[1] : firstNonConsecutive(arr.slice(1)) 
