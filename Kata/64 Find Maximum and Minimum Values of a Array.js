2a34d0a85f85ba54c40ba74f2ca32d78f56185f1
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-18 18:21:48 -0600

    Update 64 Find Maximum and Minimum Values of a Array.js

--- Kata/64 Find Maximum and Minimum Values of a Array.js
+++ Kata/64 Find Maximum and Minimum Values of a Array.js
@@ -24,3 +24,20 @@ var max = function (list) {
 
 console.log(min([-52, 56, 30, 29, -54, 0, -110])); // -110
 console.log(max([4, 6, 2, 1, 9, 63, -134, 566])); // 566
+
+
+
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
+
+
+
+var min = function(list){
+    list.sort((a, b) => (a - b));
+    return list[0];
+}
+
+var max = function(list){
+    list.sort((a, b) => (b - a));  // !!!!
+    return list[0];
+}
