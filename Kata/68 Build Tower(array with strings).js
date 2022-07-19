c538deba21a499b3f0cf087df86235f8dfa96f73
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-19 13:38:39 -0600

    Create 68 Build Tower(array with strings).js

---
+++ Kata/68 Build Tower(array with strings).js
@@ -0,0 +1,37 @@
+// Build Tower
+// Build a pyramid-shaped tower given a positive integer number of floors.
+// A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ",
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ",
+//   "    ***    ",
+//   "   *****   ",
+//   "  *******  ",
+//   " ********* ",
+//   "***********"
+// ]
+
+function towerBuilder(nFloors) {
+  let space,
+    star,
+    tower = [];
+  for (let i = 1; i <= nFloors; i++) {
+    space = " ".repeat(nFloors - i);
+    star = "*".repeat(2 * i - 1);
+    tower.push(`${space}${star}${space}`);
+  }
+  return tower;
+}
+
+console.log(towerBuilder(1)); // ["*"]
+console.log(towerBuilder(2)); // [" * ","***"]
+console.log(towerBuilder(5)); // console.log(towerBuilder(2));
