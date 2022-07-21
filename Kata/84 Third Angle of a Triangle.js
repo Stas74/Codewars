bd5cb54ba5ccad09df2ce767cbdef00e06e114ce
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-21 14:57:09 -0600

    Update 84 Third Angle of a Triangle.js

--- Kata/84 Third Angle of a Triangle.js
+++ Kata/84 Third Angle of a Triangle.js
@@ -9,3 +9,14 @@ function otherAngle(a, b) {
 
 
 console.log(otherAngle(30, 60)); // 90
+
+
+
+
+function otherAngle(a, b) {
+  if(a<0 || b<0) //ensure no negative angles
+    return 0;
+  if((a+b)>=180) //ensure 2 angles don't sum up to 180
+    return 0;
+  return 180 - a - b; //return missing angle
+}
