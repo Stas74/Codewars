99969c9512b3e5291e1f640f799a28457363ea16
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-21 14:50:14 -0600

    Update 83 L1 - Set Alarm(Booleans).js

--- Kata/83 L1 - Set Alarm(Booleans).js
+++ Kata/83 L1 - Set Alarm(Booleans).js
@@ -20,3 +20,11 @@ function setAlarm(employed, vacation){
 console.log(setAlarm(true, true)); // "false"
 console.log(setAlarm(false, true)); // "false"
 console.log(setAlarm(true, false)); // "true"
+
+
+
+function setAlarm(employed, vacation){
+  return employed > vacation
+}
+
+
