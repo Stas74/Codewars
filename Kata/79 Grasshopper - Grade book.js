5e09174f73701a22316f86eedf2024f792ff2128
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-20 18:42:47 -0600

    Update 79 Grasshopper - Grade book.js

--- Kata/79 Grasshopper - Grade book.js
+++ Kata/79 Grasshopper - Grade book.js
@@ -29,3 +29,30 @@ function getGrade(s1, s2, s3) {
 console.log(getGrade(95, 90, 93)); // "A"
 console.log(getGrade(65, 70, 59)); // "C"
 console.log(getGrade(48, 55, 52)); // "F"
+
+
+
+
+function getGrade (s1, s2, s3) {
+  var s = (s1 + s2 + s3) / 3
+  return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F"
+}
+
+
+
+const grades = { A: 90, B: 80, C: 70, D: 60, F: 0 };
+const sum    = (sum, item)  => sum + item;
+const mean   = (...numbers) => numbers.reduce(sum, 0) / numbers.length;
+
+function getGrade () {
+  const score = mean(...arguments);
+  
+  for (key in grades) {
+    if (!grades.hasOwnProperty(key)) continue; // Never do a for-in without this
+    if (score >= grades[key]) return key; 
+  }
+}
+
+
+
+var getGrade=(a,b,c)=>'FFFFFFDCBAA'.charAt((a+b+c)/3/10);
