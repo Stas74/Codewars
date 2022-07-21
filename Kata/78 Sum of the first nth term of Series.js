adfdf902d29f51ff0291a5ec895e80ed8d2411b8
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-20 18:18:28 -0600

    Create 78 Sum of the first nth term of Series.js

---
+++ Kata/78 Sum of the first nth term of Series.js
@@ -0,0 +1,36 @@
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+function SeriesSum(n) {
+  if (n === 0) {
+    return "0.00";
+  } else if (n === 1) {
+    return "1.00";
+  } else {
+    let total = 1.0;
+    let lower = 4;
+    while (n !== 1) {
+      total += 1 / lower;
+      lower += 3;
+      n--;
+    }
+    return "" + total.toFixed(2);
+  }
+}
+
+console.log(SeriesSum(2)); // "1.25"
+console.log(SeriesSum(3)); // "1.39"
+console.log(SeriesSum(4)); // "1.49"
+console.log(SeriesSum(5)); // "1.57"
