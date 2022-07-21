d8d4f842d10d7b48f266dc54563978b43dedf1c5
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-20 18:27:18 -0600

    Update 78 Sum of the first nth term of Series.js

--- Kata/78 Sum of the first nth term of Series.js
+++ Kata/78 Sum of the first nth term of Series.js
@@ -34,3 +34,20 @@ console.log(SeriesSum(2)); // "1.25"
 console.log(SeriesSum(3)); // "1.39"
 console.log(SeriesSum(4)); // "1.49"
 console.log(SeriesSum(5)); // "1.57"
+
+
+
+
+function SeriesSum(n) {
+  for (var s = 0, i = 0; i < n; i++) {
+    s += 1 / (1 + i * 3);
+  }
+
+  return s.toFixed(2);
+}
+
+
+
+function SeriesSum(n, s = 0) {
+    return n ? SeriesSum(n - 1, s + 1 / (3 * n - 2)) : s.toFixed(2)
+}
