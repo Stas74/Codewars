0916ae27c8b62df4855ff3e8f545afe58f662622
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-21 15:47:29 -0600

    Update 87 Keep up the hoop.js

--- Kata/87 Keep up the hoop.js
+++ Kata/87 Keep up the hoop.js
@@ -15,3 +15,20 @@ function hoopCount(n) {
 
 console.log(hoopCount(3)); // "Keep at it until you get it"
 console.log(hoopCount(11)); // "Great, now move on to tricks"
+
+
+
+
+function hoopCount (n) {
+   let times = n;
+   let error = 'Invalid input';
+   let congrats = 'Great, now move on to tricks';
+   let encourage = 'Keep at it until you get it';
+   if (times <= 9) {
+     return encourage;
+   } else if (times >= 10) {
+     return congrats;
+   } else {
+   return error;
+   }
+}
