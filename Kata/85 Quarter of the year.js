ce2bd2ff4d1e1de3926ac883c625ff2158b26588
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-21 15:10:35 -0600

    Update 85 Quarter of the year.js

--- Kata/85 Quarter of the year.js
+++ Kata/85 Quarter of the year.js
@@ -13,3 +13,50 @@ const quarterOf = (month) => {
 console.log(quarterOf(3)); // 1
 console.log(quarterOf(8)); // 3
 console.log(quarterOf(11)); // 4
+
+
+
+const quarterOf = (month, quarter = 4) => {
+  switch (month) {
+    case 1:
+    case 2:
+    case 3:
+      quarter = 1;
+      break;
+    case 4:
+    case 5:
+    case 6:
+      quarter = 2;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      quarter = 3;
+      break;
+  }
+  return quarter;
+};
+
+
+const quarterOf = (month) => {
+  if (month <= 3) {
+    quarter = 1; }
+  else if (month <=6) {
+    quarter = 2; }
+  else if (month <=9) {
+    quarter = 3; }
+  else if (month <=12) {
+    quarter = 4; }
+  else {
+    console.log('month is not valid')
+  }
+  
+  return quarter  
+}
+
+
+
+const quarterOf = (month) => {
+  return month < 4 ? 1 : month < 7 ? 2 : month < 10 ? 3 : 4;
+  
+}
