3cbea0a5b23684cbf194ac0b7758e57dce4b5208
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-25 14:38:41 -0600

    Update 88 Sum of odd numbers.js

--- Kata/88 Sum of odd numbers.js
+++ Kata/88 Sum of odd numbers.js
@@ -32,3 +32,29 @@ function rowSumOddNumbers(n) {
 console.log(rowSumOddNumbers(1)); // 1
 console.log(rowSumOddNumbers(2)); // 8
 console.log(rowSumOddNumbers(42)); // 74088
+
+
+
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
+
+
+function rowSumOddNumbers(n)
+{
+  /* The rows' start numbers are Hogben's centered polygonal numbers:
+     1, 3, 7, 13, 21, 31, 43 = b[n] = n^2 - n + 1.
+     <https://oeis.org/A002061>
+     
+     The sum of one row is given by:
+     s[n] = n^2 + n(b[n] - 1).
+     <https://www.quora.com/What-is-the-sum-of-n-consecutive-odd-integers/answer/Xavier-Dectot>
+     
+     Inline b[n]:
+     s[n] = n^2 + n(n^2 - n + 1 - 1)
+          = n^2 + n(n^2 - n)
+          = n^2 + n^3 - n^2
+          = n^3
+     ... oh. */
+  return n * n * n;
+}
