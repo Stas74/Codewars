004bbd0614f01b097f15dc69ca09c9847591f3bc
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-25 15:16:12 -0600

    Update 89 Directions Reduction.js

--- Kata/89 Directions Reduction.js
+++ Kata/89 Directions Reduction.js
@@ -72,3 +72,43 @@ function dirReduc(arr){
 console.log(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])); // ["WEST"]
 console.log(dirReduc(["NORTH", "WEST", "SOUTH", "EAST"])); // ["NORTH", "WEST", "SOUTH", "EAST"]
 console.log(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"])); // []
+
+
+
+
+function dirReduc(plan) {
+  var opposite = {
+    'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'};
+  return plan.reduce(function(dirs, dir){
+      if (dirs[dirs.length - 1] === opposite[dir])
+        dirs.pop();
+      else
+        dirs.push(dir);
+      return dirs;
+    }, []);
+}
+
+
+
+function dirReduc(arr) {
+  var str = arr.join(''), pattern = /NORTHSOUTH|EASTWEST|SOUTHNORTH|WESTEAST/;
+  while (pattern.test(str)) str = str.replace(pattern,'');
+  return str.match(/(NORTH|SOUTH|EAST|WEST)/g)||[];
+}
+
+
+
+function dirReduc(arr){
+  var count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "WEST" && arr[i+1] === "EAST" ||
+        arr[i] === "EAST" && arr[i+1] === "WEST" ||
+        arr[i] === "NORTH" && arr[i+1] === "SOUTH" ||
+        arr[i] === "SOUTH" && arr[i+1] === "NORTH") {
+        arr.splice(i, 2);
+        count++;
+        i--;
+    }
+  }
+  return count === 0 ? arr : dirReduc(arr);
+}
