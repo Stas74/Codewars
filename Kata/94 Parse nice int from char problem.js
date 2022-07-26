93a84f6a4764689e82d709dcd372066aad19603a
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-25 18:52:01 -0600

    Update 94 Parse nice int from char problem.js

--- Kata/94 Parse nice int from char problem.js
+++ Kata/94 Parse nice int from char problem.js
@@ -15,5 +15,11 @@ function getAge(inputString) {
   return +inputString[0]
 }
 
-
 console.log(getAge("4 years old")); // 4
+
+
+
+function getAge(inputString){
+  return parseInt(inputString);
+}
+
