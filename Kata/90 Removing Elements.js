ce5819c3f580618df65f9bf7979fb3754fc6521e
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-25 18:10:36 -0600

    Update 90 Removing Elements.js

--- Kata/90 Removing Elements.js
+++ Kata/90 Removing Elements.js
@@ -18,3 +18,28 @@ function removeEveryOther(arr) {
 
 console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // [1, 3, 5, 7, 9]
 console.log(removeEveryOther(["Hello", "Goodbye", "Hello Again"])); // ['Hello', 'Hello Again']
+
+
+
+function removeEveryOther(arr){
+  return arr.filter(function(elem, index) {
+    return index % 2 === 0;
+  });
+}
+
+
+
+const removeEveryOther = arr => arr.filter((_, i) => !(i % 2));
+
+
+
+function removeEveryOther(arr){
+  //your code here
+  for (var i = 1; i < arr.length;i++){
+      arr.splice(i,1);
+  }
+  return arr;
+}
+
+
+
