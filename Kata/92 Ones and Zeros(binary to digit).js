f58d85591ea83286ecd8e92771b4ac171bc2cd51
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-25 18:33:54 -0600

    Update 92 Ones and Zeros(binary to digit).js

--- Kata/92 Ones and Zeros(binary to digit).js
+++ Kata/92 Ones and Zeros(binary to digit).js
@@ -22,3 +22,37 @@ const binaryArrayToNumber = arr => {
 console.log(binaryArrayToNumber([0,0,0,1]));  // 1
 console.log(binaryArrayToNumber([0,0,1,0]));  // 2
 console.log(binaryArrayToNumber([1,1,1,1]));  // 15
+
+
+
+
+function binaryArrayToNumber(arr) {
+  return arr.reduce( (a, b) => a << 1 | b );
+}
+
+
+const binaryArrayToNumber = arr => {
+  return arr.reduce((total, cur) => (total = total * 2 + cur), 0);
+}
+
+
+
+const binaryArrayToNumber = arr => {
+  // your code
+  //array recieved as argument
+  //reverse array to begin at first digit
+  //loop through array
+  //first digit signifies number of 1s| second signifies 2s| 3rd signifies 4s| 4th signifies 8s|
+  //add numbers to variable | return variable
+  
+  arr.reverse();
+  var count = 0;
+  
+  for(var i = 0; i < arr.length; i++){
+      if(arr[i] === 1){
+        count += Math.pow(2,i);
+      }  
+  }
+    
+    return count;  
+};
