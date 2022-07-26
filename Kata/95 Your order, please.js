9866d562f3e7cfdbd27255564135e4a0bdb97ff9
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-26 13:34:34 -0600

    Update 95 Your order, please.js

--- Kata/95 Your order, please.js
+++ Kata/95 Your order, please.js
@@ -33,3 +33,45 @@ function order(words) {
 console.log(order("is2 Thi1s T4est 3a")); // "Thi1s is2 3a T4est"
 console.log(order("4of Fo1r pe6ople g3ood th5e the2")); // "Fo1r the2 g3ood 4of th5e pe6ople"
 console.log(order("")); // ""
+
+
+
+
+function order(words){  
+  return words.split(' ').sort(function(a, b){
+      return a.match(/\d/) - b.match(/\d/);
+   }).join(' ');
+}    
+
+
+
+
+function order(words){
+  var array = words.split(' ');
+  var sortedArray = [];
+  for(i = 0; i <= array.length; i++) {
+    for(j = 0; j < array.length; j++) {
+      if(array[j].indexOf(i) >= 0) {
+        sortedArray.push(array[j]);
+      }
+    }
+  }
+  return sortedArray.join(' ');
+}
+
+
+
+
+function order(words){
+  let arr = words.split(' ')
+  let r = []
+
+  arr.forEach(word=>{
+    let x = word.split('')
+    let num = x.find(el=>parseInt(el))
+    r.push([word,parseInt(num)]) 
+  })
+
+  r.sort((a,b)=>a[1]-b[1]).map(x=> x.splice(1,1))
+  return r.join(' ')
+}
