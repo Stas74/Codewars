1aaa016925987956a0b0e5a34930424e0b8e0f6e
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-27 17:01:18 -0600

    Update 101 Beginner - Reduce but Grow.js

--- Kata/101 Beginner - Reduce but Grow.js
+++ Kata/101 Beginner - Reduce but Grow.js
@@ -12,3 +12,9 @@ function grow(x) {
 console.log(grow([1, 2, 3])); // 6
 console.log(grow([4, 1, 1, 1, 4])); // 16
 console.log(grow([2, 2, 2, 2, 2, 2])); // 64
+
+
+
+const grow=x=>eval(x.join("*"))
+
+
