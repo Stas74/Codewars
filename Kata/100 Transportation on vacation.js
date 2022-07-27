73d5116c5d8c165da632cd38fb763b11635a60ff
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-27 16:54:21 -0600

    Update 100 Transportation on vacation.js

--- Kata/100 Transportation on vacation.js
+++ Kata/100 Transportation on vacation.js
@@ -20,3 +20,33 @@ console.log(rentalCarCost(1)); // 40
 console.log(rentalCarCost(2)); // 80
 console.log(rentalCarCost(4)); // 140
 console.log(rentalCarCost(8)); // 270
+
+
+
+const rentalCarCost = d => d * 40 - ((d > 6) ? 50 : ((d > 2) ? 20 : 0));
+
+function rentalCarCost(d) {
+  return d * 40 - (d >= 7 ? 50 : (d >= 3 ? 20 : 0));
+}
+
+
+
+function baseCost(days, rate) {
+  return days * rate;
+}
+
+function discountRate(days) {
+  if ( days >= 7 ) {
+    return 50;
+  }
+  else if ( days >= 3 ) {
+    return 20;
+  }
+  else {
+    return 0;
+  }
+}
+
+function rentalCarCost(days) {
+  return baseCost(days, 40) - discountRate(days);
+}
