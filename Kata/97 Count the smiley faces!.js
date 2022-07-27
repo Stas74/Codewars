a4dbecc46d07e457a492c938acedd3df6aefb8d8
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-27 15:09:15 -0600

    Update 97 Count the smiley faces!.js

--- Kata/97 Count the smiley faces!.js
+++ Kata/97 Count the smiley faces!.js
@@ -31,3 +31,29 @@ console.log(countSmileys([])); // 0
 console.log(countSmileys([":D", ":~)", ";~D", ":)"])); // 4
 console.log(countSmileys([":)", ":(", ":D", ":O", ":;"])); // 2
 console.log(countSmileys([";]", ":[", ";*", ":$", ";-D"])); // 1
+
+
+
+
+function countSmileys(arr) {
+  return arr.filter(x => /^[:;][-~]?[)D]$/.test(x)).length;
+}
+
+
+const countSmileys = ss => ss.reduce((a, s) => a + /^[:;][-~]?[D)]$/.test(s), 0);
+
+
+function countSmileys(arr) {
+  var smileys = [":)",";)",":-)",";-)",";~)",":~)",":D",";D",":-D",":~D",";-D",";~D"];
+  var count = 0;
+
+  for (var i=0; i<arr.length; i++){
+    for (var j=0; j<smileys.length; j++){
+      if (arr[i]===smileys[j]){
+        count++;
+      }
+    }
+  }
+return count;
+}
+
