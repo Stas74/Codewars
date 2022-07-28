a871a483eaecb31a1fc5b4aa9380a8570d9803d0
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-28 15:11:00 -0600

    Update 104 Grasshopper - Messi goals function.js

--- Kata/104 Grasshopper - Messi goals function.js
+++ Kata/104 Grasshopper - Messi goals function.js
@@ -22,3 +22,15 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
 
 console.log(goals(0, 0, 0)); // 0
 console.log(goals(43, 10, 5)); // 58
+
+
+
+const goals = (...a) => a.reduce((s, v) => s + v, 0);
+
+const goals = (...goalsArray) => goalsArray.reduce((a, b) => a + b);
+
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  let goals = [ laLigaGoals, copaDelReyGoals, championsLeagueGoals ];
+  return goals.reduce( ( a, b ) => a + b ,0);
+}
