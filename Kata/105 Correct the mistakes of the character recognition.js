6c06b2095b99010a479d2863be3f993890e3c1d9
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-28 15:52:11 -0600

    Update 105 Correct the mistakes of the character recognition.js

--- Kata/105 Correct the mistakes of the character recognition.js
+++ Kata/105 Correct the mistakes of the character recognition.js
@@ -23,3 +23,19 @@ function correct(string) {
 
 console.log(correct("L0ND0N")); // "LONDON"
 console.log(correct("DUBL1N")); // "DUBLIN"
+
+
+
+correct = s => s.replace(/0/g,'O').replace(/1/g,'I').replace(/5/g,'S')
+
+
+
+const corrections = {
+  '5': 'S',
+  '0': 'O',
+  '1': 'I',
+};
+
+const correct = string => (
+  string.replace(/[501]/g, character => corrections[character])
+);
