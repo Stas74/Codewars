80730ef78bccc54178c61571cad0bc15423eb39b
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-28 14:45:16 -0600

    Update 102 Will there be enough space.js

--- Kata/102 Will there be enough space.js
+++ Kata/102 Will there be enough space.js
@@ -22,3 +22,19 @@ function enough(cap, on, wait) {
 console.log(enough(10, 5, 5)); // 0
 console.log(enough(100, 60, 50)); // 10
 console.log(enough(20, 5, 5)); // 0
+
+
+
+
+
+function enough(cap, on, wait) {
+  return Math.max(wait + on - cap, 0);
+}
+
+
+
+function enough(cap, on, wait) {
+  return (on+wait > cap) ? on+wait-cap : 0;
+}
+
+
