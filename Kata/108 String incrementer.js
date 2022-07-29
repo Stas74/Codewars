b0aea7b9d7d1700e18f79ba6740d161bb4e6b0ef
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-07-29 14:17:32 -0600

    Update 108 String incrementer.js

--- Kata/108 String incrementer.js
+++ Kata/108 String incrementer.js
@@ -43,3 +43,23 @@ function incrementString(str) {
 console.log(incrementString("foobar000")); // "foobar001"
 console.log(incrementString("foo")); // "foo1"
 console.log(incrementString("foobar99")); // "foobar100"
+
+
+
+
+function incrementString(input) {
+  if(isNaN(parseInt(input[input.length - 1]))) return input + '1';
+  return input.replace(/(0*)([0-9]+$)/, function(match, p1, p2) {
+    var up = parseInt(p2) + 1;
+    return up.toString().length > p2.length ? p1.slice(0, -1) + up : p1 + up;
+  });
+}
+
+
+
+let incrementString = str => str.replace(/([0-8]|\d?9+)?$/, (e) => e ? + e + 1 : 1)
+
+
+const incrementString = s => s.replace(/[0-8]?9*$/, m => String(++m));
+
+
