962e3c5c11364a528b4a1697c5afd86359ef497c
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-01 13:17:12 -0600

    Create 106 Invert values.js

---
+++ Kata/106 Invert values.js
@@ -0,0 +1,17 @@
+// Invert values
+
+// Given a set of numbers, return the additive inverse of each.
+// Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+
+function invert(array) {
+  // array = array.map(x => -1 * x)
+  return array.map(el => -1 * el)
+}
+
+console.log(invert([1, 2, 3, 4, 5])); // [-1,-2,-3,-4,-5]
+console.log(invert([1, -2, 3, -4, 5])); // [-1,2,-3,4,-5]
+console.log(invert([])); // []
