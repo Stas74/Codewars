80a297e99cee5a139da58f0b652446e1cb90039f
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-01 12:47:49 -0600

    Update 115 Thinkful - Logic Drills Traffic light.js

--- Kata/115 Thinkful - Logic Drills Traffic light.js
+++ Kata/115 Thinkful - Logic Drills Traffic light.js
@@ -21,3 +21,21 @@ function updateLight(current) {
 console.log(updateLight("green")); // "yellow"
 console.log(updateLight("yellow")); // "red"
 console.log(updateLight("red")); // "green"
+
+
+
+
+
+function updateLight(current) {
+  
+  return current === 'yellow' ? 'red' : current === 'green' ? 'yellow' : 'green';
+
+}
+
+
+
+const updateLight = current => ({
+  green: 'yellow',
+  yellow: 'red',
+  red: 'green',
+})[current]
