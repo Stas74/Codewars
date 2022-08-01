ae2ac9be01b06307ab344f8a5a21f1755b8295b6
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-01 13:20:58 -0600

    Create 117 Function 3 - multiplying two numbers.js

---
+++ Kata/117 Function 3 - multiplying two numbers.js
@@ -0,0 +1,10 @@
+// Function 3 - multiplying two numbers
+
+// Implement a function which multiplies two numbers.
+
+function multiply(a, b) {  
+  return a * b;
+}
+
+console.log(multiply(2, 3)); // 6
+
