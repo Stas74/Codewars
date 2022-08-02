f12261622f78b8ad3e27b2fc8a22ce0bd8cce6ad
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-02 15:45:10 -0600

    Update 120 Is it a palindrome.js

--- Kata/120 Is it a palindrome.js
+++ Kata/120 Is it a palindrome.js
@@ -10,3 +10,27 @@ console.log(isPalindrome("a")); //  true
 console.log(isPalindrome("Aba")); //  true
 
 console.log(isPalindrome("hello")); //  false
+
+
+
+
+
+function isPalindrome(x) {
+  x = x.toLowerCase()
+  let a = 0
+  let b = x.length
+  while (a < b) {
+    if (x[a] != x[b-1])
+      return false
+    a++
+    b--
+  }
+  return true
+}
+
+
+
+
+function isPalindrome(x, str = x.toLowerCase()) {
+  return str === [...str].reverse().join('')
+} 
