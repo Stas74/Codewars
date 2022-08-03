30a25d289a20e92bdd0ad8583ead75b5476b8fbb
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-03 14:44:58 -0600

    Create 124 Testing 1-2-3.js

---
+++ Kata/124 Testing 1-2-3.js
@@ -0,0 +1,23 @@
+// Testing 1-2-3
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+var number = function (array) {
+  // const finArr = [];
+  // for (let i = 0; i < array.length; i++) {
+  //   finArr.push([i + 1] + ": " + array[i]) ;
+  // }
+  return array.map((x, i) => i + 1 + ": " + x);
+};
+
+console.log(number([])); //  []
+console.log(number(["a", "b", "c"])); //  ["1: a", "2: b", "3: c"]
