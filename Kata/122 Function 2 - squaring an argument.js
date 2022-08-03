e2c2919d1e4bb2546b5c26d19a03859277b35946
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-03 11:45:18 -0600

    Create 122 Function 2 - squaring an argument.js

---
+++ Kata/122 Function 2 - squaring an argument.js
@@ -0,0 +1,9 @@
+// Function 2 - squaring an argument
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+const square = (n) => {
+  return n * n;
+};
+
+console.log(square(3)); //  9
