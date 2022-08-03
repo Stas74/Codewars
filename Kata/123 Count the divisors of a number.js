4452a7d21b5718ae50c21faac27bf895071e0fd9
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-03 11:56:58 -0600

    Create 123 Count the divisors of a number.js

---
+++ Kata/123 Count the divisors of a number.js
@@ -0,0 +1,25 @@
+// Count the divisors of a number
+
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+// Examples (input --> output)
+// 4 --> 3 (1, 2, 4)
+// 5 --> 2 (1, 5)
+// 12 --> 6 (1, 2, 3, 4, 6, 12)
+// 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+
+function getDivisorsCnt(n) {
+  let counter = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+console.log(getDivisorsCnt(1)); //  1
+console.log(getDivisorsCnt(10)); //  4
+console.log(getDivisorsCnt(11)); //  2
