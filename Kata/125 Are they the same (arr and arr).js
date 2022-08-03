7db2f39566d2d144f9abd7dd5034abff0712278d
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-03 16:34:16 -0600

    Update 125 Are they the same (arr and arr).js

--- Kata/125 Are they the same (arr and arr).js
+++ Kata/125 Are they the same (arr and arr).js
@@ -10,7 +10,8 @@
 // a = [121, 144, 19, 161, 19, 144, 19, 11]
 // b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
 // comp(a, b) returns true because in b 121 is the square of 11, 14641
-// is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+// is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. 
+// It gets obvious if we write b's elements in terms of squares:
 
 // a = [121, 144, 19, 161, 19, 144, 19, 11]
 // b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
@@ -63,3 +64,20 @@ console.log(
     [ 1, 31 ]
   )
 ); //  false
+
+
+
+
+
+function comp(array1, array2) {
+  if(array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
+  return array1.map(v => v * v).every((v, i) => v == array2[i]);
+}
+
+
+
+function comp(a, b) {
+  return !!a && !!b && a.map(x => x*x).sort().join() == b.sort().join();
+}
+
