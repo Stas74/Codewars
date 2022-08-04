2a2c00e1976219e276d895992aa445f52cc38295
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-04 13:00:45 -0600

    Create 117 Double Char.js

---
+++ Kata/117 Double Char.js
@@ -0,0 +1,20 @@
+// Double Char
+
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+
+
+function doubleChar(str) {
+  let finStr = "";
+  for (let char of str) {
+    finStr += char + char
+  }
+  return finStr
+}
+
+console.log(doubleChar("abcd")) // "aabbccdd"
