e94431c21cd9e27d98861d0a386b6d4055a87119
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-04 15:01:48 -0600

    Create 130 Remove anchor from URL.js

---
+++ Kata/130 Remove anchor from URL.js
@@ -0,0 +1,15 @@
+// Remove anchor from URL
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+
+function removeUrlAnchor(url) {
+  return url.indexOf("#") > 0 ? url.slice(0, url.indexOf("#")) : url;
+}
+
+
+console.log(removeUrlAnchor("www.codewars.com#about")); // 'www.codewars.com'
