842c9364d9bc4792abe62d4315e25335c3e2c711
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-04 15:29:37 -0600

    Update 131 Count characters in your string (obj).js

--- Kata/131 Count characters in your string (obj).js
+++ Kata/131 Count characters in your string (obj).js
@@ -15,3 +15,24 @@ function count(string) {
 
 console.log(count("aba")); // { a: 2, b: 1 }
 console.log(count("")); // {}
+
+
+
+
+
+function count (string) {  
+  var count = {};
+  string.split('').forEach(function(s) {
+     count[s] ? count[s]++ : count[s] = 1;
+  });
+  return count;
+}
+
+
+function count (string) {
+  return string.split('').reduce(function(counts,char){
+    counts[char] = (counts[char]||0) + 1;
+    return counts;
+  },{});
+}
+
