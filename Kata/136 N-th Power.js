6a0ea5eaf56bbbfb4ca6ea3c6944268b3c2ad741
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-05 16:01:07 -0600

    Update 136 N-th Power.js

--- Kata/136 N-th Power.js
+++ Kata/136 N-th Power.js
@@ -19,3 +19,12 @@ function index(array, n) {
 
 console.log(index([1, 2, 3, 4], 2)); // 9
 console.log(index([1, 3, 10, 100], 3)); // 1000000
+
+
+
+
+function index(array, n){
+ 
+  return array[n] ** n || -1;
+}
+
