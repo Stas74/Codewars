c486045faac6f3b0e5a905b4681059a59e274a1d
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-05 13:51:10 -0600

    Create 132 Two fighters, one winner.js

---
+++ Kata/132 Two fighters, one winner.js
@@ -0,0 +1,55 @@
+// Two fighters, one winner.
+
+// Create a function that returns the name of the winner in a fight between two fighters.
+
+// Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+// Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+// Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+// Example:
+//   declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+
+//   Lew attacks Harry; Harry now has 3 health.
+//   Harry attacks Lew; Lew now has 6 health.
+//   Lew attacks Harry; Harry now has 1 health.
+//   Harry attacks Lew; Lew now has 2 health.
+//   Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+
+function Fighter(name, health, damagePerAttack) {
+  this.name = name;
+  this.health = health;
+  this.damagePerAttack = damagePerAttack;
+  this.toString = function () {
+    return this.name;
+  };
+}
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  let i = 0;
+  if (firstAttacker === fighter1.name) {
+    i = 1;
+  }
+  for (i; fighter1.health > 0 && fighter2.health > 0; i++) {
+    if (i % 2 === 0) {
+      fighter1.health -= fighter2.damagePerAttack;
+    } else {
+      fighter2.health -= fighter1.damagePerAttack;
+    }
+  }
+  if (fighter1.health > 0) {
+    return fighter1.name;
+  } else {
+    return fighter2.name;
+  }
+}
+
+console.log(
+  declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew")
+); // "Lew"
+
+console.log(
+  declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry")
+); // "Harry"
+
