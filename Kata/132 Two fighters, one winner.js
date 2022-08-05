242b6e28ed7ef3696009fca984dccbb0a35b90e0
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-05 13:52:16 -0600

    Update 132 Two fighters, one winner.js

--- Kata/132 Two fighters, one winner.js
+++ Kata/132 Two fighters, one winner.js
@@ -53,3 +53,33 @@ console.log(
   declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry")
 ); // "Harry"
 
+
+
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  while (fighter1.health > 0 && fighter2.health > 0) {
+    fighter2.health -= fighter1.damagePerAttack;
+    fighter1.health -= fighter2.damagePerAttack;
+  }
+  
+  if (fighter1.health <= 0 && fighter2.health <= 0)
+    return firstAttacker;
+  else if (fighter1.health <= 0)
+    return fighter2.name;
+  else
+    return fighter1.name;
+}
+
+
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  var fac1 = Math.ceil( fighter1.health / fighter2.damagePerAttack );
+  var fac2 = Math.ceil( fighter2.health / fighter1.damagePerAttack );
+  if(fac1 < fac2) {
+    return fighter2.name;
+  } else if(fac2 < fac1) {
+    return fighter1.name;
+  } else {
+    return firstAttacker;
+  }
+}
