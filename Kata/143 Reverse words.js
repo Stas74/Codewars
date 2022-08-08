6efd4b1374ef60b10490d90c9b622975bc992d02
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-08 15:12:37 -0600

    Create 143 Reverse words.js

---
+++ Kata/143 Reverse words.js
@@ -0,0 +1,16 @@
+// Reverse words
+
+// Complete the function that accepts a string parameter, and reverses each word in the string. 
+// All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+
+function reverseWords(str) {
+  return str.split(' ').map(word => word.split('').reverse().join('')).join(' ')
+}
+
+console.log(reverseWords('apple')); // 'elppa'
+console.log(reverseWords('double  spaced  words')); // 'elbuod  decaps  sdrow'
