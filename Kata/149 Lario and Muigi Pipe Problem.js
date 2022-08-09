c4f293110e9e892735265d09264cec7d49367849
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-09 14:27:50 -0600

    Update 149 Lario and Muigi Pipe Problem.js

--- Kata/149 Lario and Muigi Pipe Problem.js
+++ Kata/149 Lario and Muigi Pipe Problem.js
@@ -29,3 +29,12 @@ function pipeFix(numbers){
 console.log(pipeFix([1,2,3,5,6,8,9])); // [1,2,3,4,5,6,7,8,9]
 console.log(pipeFix([6,9])); // [6,7,8,9]
 console.log(pipeFix([-1,4])); // [-1,0,1,2,3,4]
+
+
+
+
+const pipeFix = (numbers) =>
+  [...Array(numbers[numbers.length - 1] - numbers[0] + 1)].map((_, idx) => numbers[0] + idx);
+
+
+let pipeFix = nums => Array.from({ length: nums.pop() - nums[0] + 1 }, (v, i) => i + nums[0]);
