c81171454c39f387988bd6df531ef18f91137e5d
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-09 12:54:53 -0600

    Update 145 Exclusive or (xor) Logical Operator.js

--- Kata/145 Exclusive or (xor) Logical Operator.js
+++ Kata/145 Exclusive or (xor) Logical Operator.js
@@ -24,3 +24,12 @@ function xor(a, b) {
 console.log(xor(false, false)); // false
 console.log(xor(false, true)); // true
 console.log(xor(true, true)); // false
+
+
+
+function xor(a, b) {
+  return a != b;
+}
+
+
+const xor = (a, b) => !!(a ^ b)
