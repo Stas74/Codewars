fa630f06ccbbae5f1f4fd77b5bc65acdf493aeaf
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-14 15:57:02 -0600

    Create 156 Sort Numbers.js

---
+++ Kata/156 Sort Numbers.js
@@ -0,0 +1,16 @@
+// Sort Numbers
+
+// Finish the solution so that it sorts the passed in array of numbers. 
+// If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+
+function solution(nums){
+  return nums ? nums.sort((a, b) => {return a - b}) : [];
+}
+
+console.log(solution([1,2,3,10,5])); // [1,2,3,5,10]
+console.log(solution(null)); // []
