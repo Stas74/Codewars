01f7bb1aad066037cef9c289c98960d00004d305
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-14 16:12:53 -0600

    Update 157 Super Duper Easy.js

--- Kata/157 Super Duper Easy.js
+++ Kata/157 Super Duper Easy.js
@@ -9,3 +9,23 @@ function problem(x){
 
 console.log(problem("hello")); // "Error"
 console.log(problem(5)); // 256
+
+
+
+const problem = x => typeof x === 'string' ? 'Error' : x * 50 + 6;
+
+
+const problem = x =>
+  Number.isFinite(x) ? x * 50 + 6 : `Error`;
+
+
+const problem = x => x === +x ? (x * 50) + 6 :  'Error' 
+
+
+function problem(x){
+if(isNaN(x) | x === ""){
+ return "Error";
+} else {
+ return(x * 50)+6
+ }
+}
