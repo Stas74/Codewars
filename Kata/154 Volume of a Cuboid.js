efc9881f766731fdca310800bcf7fd2f4bb336c1
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-14 14:58:07 -0600

    Create 154 Volume of a Cuboid.js

---
+++ Kata/154 Volume of a Cuboid.js
@@ -0,0 +1,11 @@
+// Volume of a Cuboid
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: 
+// the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+function getVolumeOfCuboid(length, width, height) {
+  return length * width * height
+}
+
+console.log(getVolumeOfCuboid(1,2,2)); // 4
+console.log(getVolumeOfCuboid(6.3,2,5)); // 63
