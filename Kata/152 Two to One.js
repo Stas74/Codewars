1bc8170c4c5f7bd45fd193fc0514079de990d35d
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-14 14:23:17 -0600

    Create 152 Two to One.js

---
+++ Kata/152 Two to One.js
@@ -0,0 +1,26 @@
+// Two to One
+
+// Take 2 strings s1 and s2 including only letters from a to z.
+// Return a new sorted string, the longest possible, containing distinct letters -
+// each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+  return (s1 + s2)
+    .split("")
+    .filter((element, index) => {
+      return (s1 + s2).split("").indexOf(element) === index;
+    })
+    .sort()
+    .join("");
+}
+
+console.log(longest("aretheyhere", "yestheyarehere")); // "aehrsty"
+console.log(longest("inmanylanguages", "theresapairoffunctions")); // "acefghilmnoprstuy"
