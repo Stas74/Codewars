a4fc50ec1585dab4f5e9dae144a831a70945b9e8
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-14 14:51:41 -0600

    Update 153 Hex to Decimal.js

--- Kata/153 Hex to Decimal.js
+++ Kata/153 Hex to Decimal.js
@@ -9,3 +9,25 @@ function hexToDec(hexString){
 console.log(hexToDec("1")); // 1
 console.log(hexToDec("a")); // 10
 console.log(hexToDec("10")); // 16
+
+
+
+function hexToDec(hexString){
+  let hexVal = {
+    a: 10,
+    b: 11,
+    c: 12,
+    d: 13,
+    e: 14,
+    f: 15,
+  },  clone = hexString.split('').map(x => x.toLowerCase()), isNeg = false;
+  if(clone[0] === '-') {
+    isNeg = true;
+    clone.splice(0,1);
+  }
+  let output = Number(clone
+    .reverse()
+    .map((e,i)=> hexVal[e] ? hexVal[e]*Math.pow(16, i) : e*Math.pow(16, i))
+    .reduce((a,b) => a+b), 10);
+  return isNeg ? output * -1 : output;
+}
