5b760e3627cd3e8550d1727a67b9fc574c8c57c4
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-15 15:33:48 -0600

    Create 162 Simple multiplication.js

---
+++ Kata/162 Simple multiplication.js
@@ -0,0 +1,13 @@
+// Simple multiplication
+
+/*
+This kata is about multiplying a given number by eight if it is an even number 
+and by nine otherwise.
+*/
+
+function simpleMultiplication(number) {
+  return number % 2 == 0 ? number * 8 : number * 9; 
+}
+
+console.log(simpleMultiplication(5)) // 45
+console.log(simpleMultiplication(8)) // 64
