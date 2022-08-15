87c81c52295a6c3f9994c6bff6a0a74cd5ea5486
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-15 16:12:14 -0600

    Update 165 Cat years, Dog years.js

--- Kata/165 Cat years, Dog years.js
+++ Kata/165 Cat years, Dog years.js
@@ -30,3 +30,27 @@ var humanYearsCatYearsDogYears = function(humanYears) {
 console.log(humanYearsCatYearsDogYears(1)) // [1,15,15]
 console.log(humanYearsCatYearsDogYears(2)) // [2,24,24]
 console.log(humanYearsCatYearsDogYears(10)) // [10,56,64]
+
+
+
+
+
+const humanYearsCatYearsDogYears = humanYears => [
+  humanYears,
+  ( humanYears - 1 ? 16 : 11 ) + 4 * humanYears,
+  ( humanYears - 1 ? 14 : 10 ) + 5 * humanYears,
+];
+
+
+
+function humanYearsCatYearsDogYears(humanYears) {
+  switch (humanYears) {
+    case 1:
+      return [1, 15, 15]
+    case 2:
+      return [2, 24, 24];
+    default:
+      return [humanYears, (humanYears - 2) * 4 + 24, (humanYears - 2) * 5 + 24 ];
+  }
+  
+}
