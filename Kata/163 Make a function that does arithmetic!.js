58428609d2ba3fce6d896268bdb932d433f261a1
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-15 15:49:43 -0600

    Update 163 Make a function that does arithmetic!.js

--- Kata/163 Make a function that does arithmetic!.js
+++ Kata/163 Make a function that does arithmetic!.js
@@ -23,3 +23,35 @@ function arithmetic(a, b, operator){
 
 console.log(arithmetic(1, 2, "add")) // 3
 console.log(arithmetic(8, 2, "subtract")) // 6
+
+
+
+function arithmetic(a, b, operator){
+  switch(operator) {
+    case 'add':
+      return a + b;
+    case 'subtract':
+      return a - b;
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      return a / b;
+  }
+}
+
+
+
+const arithmetic = (a, b, operator) => ({
+  'add'     : a + b,
+  'subtract': a - b,
+  'multiply': a * b,
+  'divide'  : a / b
+}[operator]);
+
+
+
+
+function arithmetic(a, b, operator){
+  optable = { "add":"+", "subtract": "-", "multiply": "*" , "divide":"/"};
+  return eval(a + optable[operator] + b); 
+}
