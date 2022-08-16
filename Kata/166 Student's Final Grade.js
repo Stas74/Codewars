75f1918826628a157524bef3da8b24023da5b455
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-16 11:47:49 -0600

    Update 166 Student's Final Grade.js

--- Kata/166 Student's Final Grade.js
+++ Kata/166 Student's Final Grade.js
@@ -43,5 +43,23 @@ function finalGrade (exam, projects) {
 
 console.log(finalGrade(100, 12)) // 100
 console.log(finalGrade(85, 5)) // 90
-console.log(finalGrade(50, 2)) // 100
 
+
+
+
+
+function finalGrade (exam, projects) {
+  if(exam > 90 || projects > 10) return 100;
+  if(exam > 75 & projects >= 5) return 90;
+  if(exam > 50 & projects >= 2) return 75;
+  return 0;
+}
+
+
+const finalGrade = (exam, projects) => {
+  return (
+    exam > 90 || projects > 10 ? 100 :
+    exam > 75 && projects >= 5 ? 90 :
+    exam > 50 && projects >= 2 ? 75 : 0
+  )
+}
