aa9f3aa4b8bf0c42ffd16a699f3576b9e4ae06e8
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-16 14:31:18 -0600

    Update 170 Highest Scoring Word.js

--- Kata/170 Highest Scoring Word.js
+++ Kata/170 Highest Scoring Word.js
@@ -28,3 +28,36 @@ function high(x){
 
 console.log(high('man i need a taxi up to ubud')) // 'taxi'
 console.log(high('take me to semynak')) // 'semynak'
+
+
+
+
+function high(s){
+  let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
+  return s.split(' ')[as.indexOf(Math.max(...as))];
+}
+
+
+
+function high(words) {
+
+  const alpha = ' abcdefghijklmnopqrstuvwxyz';
+  const score = word => word.split('').reduce((a, b) => a + alpha.indexOf(b), 0);
+
+  return words
+    .split(' ')
+    .map((word, pos) => ({ word: word, pos: pos, score: score(word) }))
+    .sort((a, b) => a.score - b.score || b.pos - a.pos)
+    .pop()
+    .word;
+    
+}
+
+
+function high(x){
+  const alpha = 'abcdefghijklmnopqrstuvwxyz'
+  const words = x.split(' ')
+  const scores = words.map(x => [...x].map(y => alpha.indexOf(y) + 1)).map(x => x.reduce((a,b) => a + b,0))
+  
+  return words[scores.indexOf(Math.max(...scores))]
+}
