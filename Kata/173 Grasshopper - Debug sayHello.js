0e048b9645147051aed0f1421030e05cfa3d7ed6
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-17 12:56:41 -0600

    Create 173 Grasshopper - Debug sayHello.js

---
+++ Kata/173 Grasshopper - Debug sayHello.js
@@ -0,0 +1,19 @@
+// Grasshopper - Debug sayHello
+
+/*
+Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone 
+as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+*/
+
+function sayHello(name) {
+  return `Hello, ${name}`
+}
+
+
+console.log(sayHello('Mr. Spock')) // 'Hello, Mr. Spock'
+console.log(sayHello('Captain Kirk')) // 'Hello, Captain Kirk'
