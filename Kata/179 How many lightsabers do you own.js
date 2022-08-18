8886b63cfc4622b16e267bb930a93d481f427317
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-18 13:16:43 -0600

    Update 179 How many lightsabers do you own.js

--- Kata/179 How many lightsabers do you own.js
+++ Kata/179 How many lightsabers do you own.js
@@ -22,3 +22,8 @@ function howManyLightsabersDoYouOwn(name) {
 
 console.log(howManyLightsabersDoYouOwn("Adam"))  // 0
 console.log(howManyLightsabersDoYouOwn("Zach"))  // 18
+
+
+
+const howManyLightsabersDoYouOwn = name =>
+  (name === `Zach`) * 18;
