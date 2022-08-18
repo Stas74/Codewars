05434aba7c37ae66e2a26ea8fb0d74ff8605f00a
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-18 14:29:36 -0600

    Create 184 Anagram Detection.js

---
+++ Kata/184 Anagram Detection.js
@@ -0,0 +1,33 @@
+// Anagram Detection
+
+/*
+An anagram is the result of rearranging the letters of a word to produce a new word.
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"
+*/
+
+
+var isAnagram = function(test, original) {
+  if (test.length !== original.length) {
+    return false
+  }
+  let testArr = test.toLowerCase().split('').sort()
+  let originalArr = original.toLowerCase().split('').sort()
+  for (let i = 0; i < testArr.length; i++) {
+    if (testArr[i] !== originalArr[i]) {
+      return false
+    }    
+  }
+  return true
+};
+
+console.log(isAnagram("foefet", "toffee"))  // true
+console.log(isAnagram("Buckethead", "DeathCubeK"))  // true
+console.log(isAnagram("apple", "pale"))  // false
