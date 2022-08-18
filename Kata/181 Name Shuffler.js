dd6488f426ec24d59880e86d39201ce9ad9e6731
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-18 13:29:18 -0600

    Create 181 Name Shuffler.js

---
+++ Kata/181 Name Shuffler.js
@@ -0,0 +1,17 @@
+// Name Shuffler
+
+/*
+Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"
+*/
+
+function nameShuffler(str){
+  return str.split(" ").reverse().join(" ")
+}
+
+
+console.log(nameShuffler('john McClane'))  // 'McClane john'
+console.log(nameShuffler('tom jerry'))  // 'jerry tom'
