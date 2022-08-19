9261babb49d4fe327319f82251cb247a1406be4f
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-19 16:43:43 -0600

    Update 191 Flatten and sort an array.js

--- Kata/191 Flatten and sort an array.js
+++ Kata/191 Flatten and sort an array.js
@@ -22,3 +22,19 @@ function flattenAndSort(array) {
 console.log(flattenAndSort([]))  // []
 console.log(flattenAndSort([[], [1]]))  // [1]
 console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))  // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+
+function flattenAndSort(array) {
+  return [].concat(...array).sort((a,b) => a - b);
+}
+
+
+
+function flattenAndSort(array) {
+  return array
+    .reduce((result, current) => [...result, ...current],[])
+    .sort((a, b) => a - b)
+    ;
+}
