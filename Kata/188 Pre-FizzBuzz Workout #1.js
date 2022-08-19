4360f46a8efac324052444caade4545f0c9319a7
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-19 13:28:15 -0600

    Create 188 Pre-FizzBuzz Workout #1.js

---
+++ Kata/188 Pre-FizzBuzz Workout #1.js
@@ -0,0 +1,23 @@
+// Pre-FizzBuzz Workout #1
+
+/*
+This is the first step to understanding FizzBuzz.
+
+Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+
+Your expected output is an array of positive integers from 1 to n (inclusive).
+
+Your job is to write an algorithm that gets you from the input to the output.
+*/
+
+
+function preFizz(n) {
+  const finArr = [];
+  for (let i = 1; i <= n; i++) {
+    finArr.push(i)    
+  }
+  return finArr;
+}
+
+console.log(preFizz(3))  // [1,2,3]
+console.log(preFizz(4))  // [1,2,3,4]
