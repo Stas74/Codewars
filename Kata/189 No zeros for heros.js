873b6c71a255ea8a8c9dbd978eecfa50903d0c03
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-19 16:15:44 -0600

    Create 189 No zeros for heros.js

---
+++ Kata/189 No zeros for heros.js
@@ -0,0 +1,28 @@
+// No zeros for heros
+
+/*
+Numbers ending with zeros are boring.
+
+They might be fun in your world, but not here.
+
+Get rid of them. Only the ending ones.
+
+1450 -> 145
+960000 -> 96
+1050 -> 105
+-1050 -> -105
+Zero alone is fine, don't worry about it. Poor guy anyway
+*/
+
+
+function noBoringZeros(n) { 
+  // return +n.toString().replace(/0/gi, '')  
+  while (n % 10 === 0 && n !== 0) {
+    n /= 10;
+  }
+  return n  
+}
+
+console.log(noBoringZeros(1450))  // 145
+console.log(noBoringZeros(960000))  // 96
+console.log(noBoringZeros(0))  // 0
