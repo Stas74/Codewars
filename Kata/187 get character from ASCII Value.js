121ec63145c930a265d22afb2df75071c4da68e9
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-19 13:14:59 -0600

    Create 187 get character from ASCII Value.js

---
+++ Kata/187 get character from ASCII Value.js
@@ -0,0 +1,20 @@
+// get character from ASCII Value
+
+/*
+Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65)
+
+should return:
+'A'
+*/
+
+
+function getChar(c){
+  return String.fromCharCode(c)
+}
+
+console.log(getChar(55))  // 7
+console.log(getChar(65))  // A
