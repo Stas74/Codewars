80653ccb60c03db652a6143909a8e5753488a360
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-22 15:03:36 -0600

    Update 197 Training JS #4 Basic data types--Array.js

--- Kata/197 Training JS #4 Basic data types--Array.js
+++ Kata/197 Training JS #4 Basic data types--Array.js
@@ -59,3 +59,15 @@ console.log(getFirst([1,2,3]))  // 1
 console.log(getLast([1,2,3]))  // 3
 console.log(pushElement([1,2,3]))  // length 4
 console.log(popElement([1,2,3]))  // length 2
+
+
+
+const getLength = array => array.length;
+
+const getFirst = array => array[0];
+
+const getLast = array => array[array.length - 1];
+
+const pushElement = array => array.concat(0);
+
+const popElement = array => array.splice(0, array.length - 1);
