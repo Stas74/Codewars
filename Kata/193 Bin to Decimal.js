d9495794d32f353ce0a295292a5a9447b83b6f67
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-22 13:47:35 -0600

    Create 193 Bin to Decimal.js

---
+++ Kata/193 Bin to Decimal.js
@@ -0,0 +1,14 @@
+// Bin to Decimal
+
+/*
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
+
+
+function binToDec(bin){
+  return parseInt(bin, 2)
+}
+
+console.log(binToDec("1"))  // 1
+console.log(binToDec("0"))  // 0
+console.log(binToDec("1001001"))  // 73
