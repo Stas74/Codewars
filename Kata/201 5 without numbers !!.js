88fa5fa718efc4e2cb12c41ec1464f753667030f
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-23 14:01:53 -0600

    Create 201 5 without numbers !!.js

---
+++ Kata/201 5 without numbers !!.js
@@ -0,0 +1,15 @@
+// 5 without numbers !!
+
+/*
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+*/
+
+function unusualFive() {
+  return "aaaaa".length
+}
+
+console.log(unusualFive()); // 5
+
+// should not use any banned characters ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/")
