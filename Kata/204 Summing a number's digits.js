e1e86ca9e4c1a43e2a8b824d37a2b1487fdde146
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-23 15:08:10 -0600

    Update 204 Summing a number's digits.js

--- Kata/204 Summing a number's digits.js
+++ Kata/204 Summing a number's digits.js
@@ -19,3 +19,13 @@ function sumDigits(number) {
 console.log(sumDigits(10)) // 1
 console.log(sumDigits(99)) // 18
 console.log(sumDigits(-32)) // 5
+
+
+
+
+function sumDigits(number) {
+  return Math.abs(number).toString().split('').reduce(function(a,b){return +a + +b}, 0);
+}
+
+
+const sumDigits = n => [...Math.abs(n)+''].reduce((s,v)=>s+(+v),0)
