aedbc41ba9f4c10a9f353eb110c0a105e2843d1c
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-24 15:49:09 -0600

    Create 211 Hello, Name or World!.js

---
+++ Kata/211 Hello, Name or World!.js
@@ -0,0 +1,23 @@
+// Hello, Name or World!
+
+/*
+Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given 
+(or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+Examples:
+
+* With `name` = "john"  => return "Hello, John!"
+* With `name` = "aliCE" => return "Hello, Alice!"
+* With `name` not given 
+  or `name` = ""        => return "Hello, World!"
+*/
+
+function hello(name) {
+  return name ? `Hello, ${name.slice(0, 1).toUpperCase()+ name.slice(1).toLowerCase()}!` : "Hello, World!";  
+}
+
+console.log(hello('alice'))  // "Hello, Alice!"
+console.log(hello())  // "Hello, World!"
+console.log(hello(''))  // "Hello, World!"
