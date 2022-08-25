531d40f6001c8f945df4e8f27c2cf8dcb40a2989
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-25 12:08:33 -0600

    Create 212 Convert to Binary.js

---
+++ Kata/212 Convert to Binary.js
@@ -0,0 +1,18 @@
+// Convert to Binary
+
+/*
+Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+to_binary(1)   should return 1 
+to_binary(5)  should return 101 
+to_binary(11)  should return 1011
+
+*/
+
+function toBinary(n){
+  return +n.toString(2);
+}
+
+console.log(toBinary(1))  // 1
+console.log(toBinary(2))  // 10
+console.log(toBinary(11))  // 1011
