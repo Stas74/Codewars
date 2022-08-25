c0bb531be99d95dbad792ac4859b130c1f0b5563
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-25 12:36:30 -0600

    Update 213 Total amount of points.js

--- Kata/213 Total amount of points.js
+++ Kata/213 Total amount of points.js
@@ -28,3 +28,38 @@ function points(games) {
 }
 
 console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]))  // 1
+
+
+
+const points = g => g.reduce((a, [x, _, y]) => a + (x > y ? 3 : x == y), 0)
+
+
+const points=games=>games.reduce((output,current)=>{
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+  },0)
+
+
+
+
+function points(games) {
+  return games.reduce((output,current)=>{
+    let x = parseInt(current[0]);
+    let y = parseInt(current[2]);
+    let value= x>y ? 3 : x===y ? 1 : 0;
+    return output+value;
+  },0)
+}
+
+
+
+function points(games) {
+  let total = 0;
+  games.map(game => {
+    if (game[0] === game[2]) {
+      total += 1;
+    } else if (game[0] > game[2]) {
+      total += 3;
+    }
+  });
+  return total;
+}
