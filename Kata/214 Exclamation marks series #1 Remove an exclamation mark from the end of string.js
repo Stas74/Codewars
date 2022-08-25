03379f6e92f814b21a5336e89234d66144375f83
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-25 12:46:20 -0600

    Create 214 Exclamation marks series #1 Remove an exclamation mark from the end of string.js

---
+++ Kata/214 Exclamation marks series #1 Remove an exclamation mark from the end of string.js
@@ -0,0 +1,24 @@
+// Exclamation marks series #1: Remove an exclamation mark from the end of string
+
+/*
+Remove an exclamation mark from the end of a string. 
+For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi!!"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
+
+function remove(string) {
+  return string[string.length - 1] === "!"
+    ? string.slice(0, string.length - 1)
+    : string;
+}
+
+console.log(remove("Hi!")); // "Hi"
+console.log(remove("Hi!!!")); // "Hi!!"
+console.log(remove("Hi")); // "Hi"
