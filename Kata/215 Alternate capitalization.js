d421db335af21aaa5b1f19b539d160206640e432
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-25 14:17:46 -0600

    Update 215 Alternate capitalization.js

--- Kata/215 Alternate capitalization.js
+++ Kata/215 Alternate capitalization.js
@@ -15,3 +15,36 @@ function capitalize(s){
 };
 
 console.log(capitalize("abcdef")); // ['AbCdEf', 'aBcDeF']
+
+
+
+function capitalize(s){
+  return [0,1].map(r=>[...s].map((c,i)=>i%2===r?c.toUpperCase():c).join(''));
+};
+
+
+
+function capitalize(s){
+  return [[...s].map((x,i) => i % 2 == 0 ? x.toUpperCase() : x).join(''),
+          [...s].map((x,i) => i % 2 != 0 ? x.toUpperCase() : x).join('')]
+}
+
+
+
+function capitalize(s) {
+  const S = s.toUpperCase()
+  let a = ""
+  let b = ""
+  for (let i = 0; i < s.length; ++i) {
+    if (i & 1) {
+      a += s[i]
+      b += S[i]
+    } else {
+      a += S[i]
+      b += s[i]
+    }
+  }
+  return [a, b]
+}
+
+
