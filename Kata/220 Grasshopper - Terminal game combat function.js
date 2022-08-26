65255b4ea048653356eeadfc06b705563d8c9b6b
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-26 15:21:15 -0600

    Create 220 Grasshopper - Terminal game combat function.js

---
+++ Kata/220 Grasshopper - Terminal game combat function.js
@@ -0,0 +1,13 @@
+// Grasshopper - Terminal game combat function
+
+/*
+Create a combat function that takes the player's current health and the amount of damage recieved, 
+and returns the player's new health. Health can't be less than 0.
+*/
+
+function combat(health, damage) {
+  return health - damage >= 0 ? health - damage : 0;
+}
+
+console.log(combat(100, 5)); // 95
+console.log(combat(20, 30)); // 0  // "Health cannot go below 0"
