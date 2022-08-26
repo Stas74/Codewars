7ec4a6a04560e633ce12a5f2b0d39881f6663fb4
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-26 15:34:14 -0600

    Update 221 Merge two sorted arrays into one.js

--- Kata/221 Merge two sorted arrays into one.js
+++ Kata/221 Merge two sorted arrays into one.js
@@ -18,3 +18,19 @@ function mergeArrays(arr1, arr2) {
 
 console.log((mergeArrays([1,2,3,4], [5,6,7,8]))); // [1,2,3,4,5,6,7,8]
 console.log(mergeArrays([1,3,5,7,9,11,12], [1,2,3,4,5,10,12])); // [1,2,3,4,5,7,9,10,11,12]
+
+
+
+
+function mergeArrays(arr1, arr2) {
+  return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
+}
+
+
+
+function mergeArrays(arr1, arr2) {
+  return arr1
+    .filter((item) => !arr2.includes(item))
+    .concat(arr2)
+    .sort((a, b) => a - b)
+}
