4a8ab3f2c0c2443bff1432403dc08643d4ec146b
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-26 15:00:19 -0600

    Update 218 List Filtering.js

--- Kata/218 List Filtering.js
+++ Kata/218 List Filtering.js
@@ -17,3 +17,11 @@ function filter_list(l) {
 console.log(filter_list([1, 2, "a", "b"])); // [1,2]
 console.log(filter_list([1,'a','b',0,15])); // [1,0,15]
 console.log(filter_list([1,2,'aasf','1','123',123])); // [1,2,123]
+
+
+
+
+function filter_list(l) {
+  return l.filter(Number.isInteger);
+}
+
