8f9878235bfbb35654cef392205dd13bdd6d7427
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-29 13:08:08 -0600

    Create 226 Sum of angles.js

---
+++ Kata/226 Sum of angles.js
@@ -0,0 +1,15 @@
+// Sum of angles
+
+/*
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+*/
+
+function angle(n) {
+  return 180 * (n - 2)  
+}
+
+// The sum of the interior angles, in degrees, of a regular polygon is given by the formula 180(n – 2),
+// where n is the number of sides
+
+console.log(angle(3)) // 180
+console.log(angle(4)) // 360
