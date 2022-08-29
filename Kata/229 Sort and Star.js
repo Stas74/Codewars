26b680822555cb42a9927e89f3bd2fef382586fb
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-29 15:31:55 -0600

    Update 229 Sort and Star.js

--- Kata/229 Sort and Star.js
+++ Kata/229 Sort and Star.js
@@ -17,3 +17,7 @@ console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", 
 // 'b***i***t***c***o***i***n'
 console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"])) 
 // 'a***r***e'
+
+
+
+const twoSort = s => [...s.sort()[0]].join('***');
