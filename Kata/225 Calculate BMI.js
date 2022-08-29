a133c2f2d0576c46d7e9b87e7ce8d79d45638914
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-29 12:58:45 -0600

    Create 225 Calculate BMI.js

---
+++ Kata/225 Calculate BMI.js
@@ -0,0 +1,24 @@
+// Calculate BMI
+
+/*
+Write function bmi that calculates body mass index (bmi = weight / height ** 2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+*/
+
+function bmi(weight, height) {
+  let index = weight / height ** 2
+  return index <= 18.5 ? "Underweight" : index <= 25.0 ? "Normal" : index <= 30.0 ? "Overweight" : "Obese";
+}
+
+
+console.log(bmi(80, 1.80)) // Normal
+
+
+
