fac95baff80177428f242450df4b3980e19250b7
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-30 17:55:22 -0600

    Update 234 simple calculator.js

--- Kata/234 simple calculator.js
+++ Kata/234 simple calculator.js
@@ -33,3 +33,23 @@ function calculator(a,b,sign){
 console.log(calculator(1,2,"+")) // 3
 console.log(calculator(1,2,"-")) // -1
 console.log(calculator(6,2,"$")) // "unknown value"
+
+
+
+
+function calculator(a,b,sign) {
+  try
+    { return eval(a+sign+b); }
+  catch(e)
+    { return "unknown value"; }
+}
+
+
+
+
+function calculator(a,b,sign) {
+  try
+    { return eval(a+sign+b); }
+  catch(e)
+    { return "unknown value"; }
+}
