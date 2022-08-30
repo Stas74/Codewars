c4a8bdbde2944d46b5fa8238411dbbd838d76f19
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-08-30 13:56:50 -0600

    Update 232 L1 Bartender, drinks.js

--- Kata/232 L1 Bartender, drinks.js
+++ Kata/232 L1 Bartender, drinks.js
@@ -35,3 +35,49 @@ function getDrinkByProfession(param){
 console.log(getDrinkByProfession("jabrOni")) // "Patron Tequila"
 console.log(getDrinkByProfession("prOgramMer")) //  "Hipster Craft Beer"
 console.log(getDrinkByProfession("Man")) //  "Beer"
+
+
+
+
+const drinks = {
+  "jabroni": "Patron Tequila",
+  "school counselor": "Anything with Alcohol",
+  "programmer": "Hipster Craft Beer",
+  "bike gang member": "Moonshine",
+  "politician": "Your tax dollars",
+  "rapper": "Cristal"
+}
+
+const getDrinkByProfession = profession => drinks[profession.toLowerCase()] || "Beer"
+
+
+
+function getDrinkByProfession(param){
+  let map = new Map([
+      ["jabroni", "Patron Tequila"],
+      ["school counselor", "Anything with Alcohol"],
+      ["programmer", "Hipster Craft Beer"],
+      ["bike gang member", "Moonshine"],
+      ["politician", "Your tax dollars"],
+      ["rapper", "Cristal"]
+      ]);
+      
+   let normal = param.toLowerCase();
+   return map.has(normal) ? map.get(normal) : "Beer";
+}
+
+
+
+function getDrinkByProfession(param) {
+  param = param.toLowerCase();
+  
+  switch(param) {
+    case "jabroni": return "Patron Tequila";
+    case "school counselor": return "Anything with Alcohol";
+    case "programmer": return "Hipster Craft Beer";
+    case "bike gang member": return "Moonshine";
+    case "politician": return "Your tax dollars";
+    case "rapper": return "Cristal";
+    default: return "Beer";
+  }
+}
