30a541a998d1591c498415e188baf7eecd9797af
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-01 11:34:48 -0600

    Update 244 Add Length.js

--- Kata/244 Add Length.js
+++ Kata/244 Add Length.js
@@ -21,3 +21,11 @@ function addLength(str) {
 
 console.log(addLength('apple ban')); // ["apple 5", "ban 3"]
 console.log(addLength('you will win')); // ["you 3", "will 4", "win 3"]
+
+
+
+function addLength(str){
+  return str.split(" ").map(s => `${s} ${s.length}`)
+}
+
+
