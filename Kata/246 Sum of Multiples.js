149f1017a0d45a47bcc7612bf3807f68e1311807
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-01 12:04:16 -0600

    Create 246 Sum of Multiples.js

---
+++ Kata/246 Sum of Multiples.js
@@ -0,0 +1,28 @@
+// Sum of Multiples
+
+/*
+Find the sum of all multiples of n below m
+
+Keep in Mind
+n and m are natural numbers (positive integers)
+m is excluded from the multiples
+
+Examples:
+
+sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+sumMul(4, -7)  ==> "INVALID"
+*/
+
+function sumMul(n,m){
+  let total = 0;
+  for (let i = n; i < m; i += n) {
+    total += i    
+  }
+  return total === 0 ? "INVALID" : total;
+}
+
+console.log(sumMul(0, 0)); // "INVALID"
+console.log(sumMul(2, 9)); // 20
+console.log(sumMul(4, -7)); // "INVALID"
