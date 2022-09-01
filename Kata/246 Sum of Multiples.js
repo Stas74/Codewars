b147d54c88b3fb35454e001b5ca276459f903b0c
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-01 12:05:27 -0600

    Update 246 Sum of Multiples.js

--- Kata/246 Sum of Multiples.js
+++ Kata/246 Sum of Multiples.js
@@ -26,3 +26,11 @@ function sumMul(n,m){
 console.log(sumMul(0, 0)); // "INVALID"
 console.log(sumMul(2, 9)); // 20
 console.log(sumMul(4, -7)); // "INVALID"
+
+
+
+function sumMul(n,m){
+  return m<=n ? "INVALID" : Math.floor(m/n)*(Math.floor(m/n)+1)*(n/2);
+}
+
+
