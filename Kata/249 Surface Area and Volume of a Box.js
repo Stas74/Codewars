c8ee27f9cfbf6589ba6bee9dd2f541340232e2fc
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-01 15:44:28 -0600

    Update 249 Surface Area and Volume of a Box.js

--- Kata/249 Surface Area and Volume of a Box.js
+++ Kata/249 Surface Area and Volume of a Box.js
@@ -4,7 +4,6 @@
 Write a function that returns the total surface area and volume of a box as an array: [area, volume]
 */
 
-// checkExam = (x, y) => (x = y.reduce((s, e, i) => s + (e === x[i] ? 4 : e === '' ? 0 : -1), 0)) > 0 ? x : 0;
 
 function getSize(width, height, depth) { 
   return [2 * height * width + 2 * height * depth + 2 * depth * width, width * height * depth]
