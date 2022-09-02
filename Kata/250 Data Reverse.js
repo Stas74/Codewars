119f614ac28a79aa8613af092649e169a774acb5
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-02 17:40:13 -0600

    Create 250 Data Reverse.js

---
+++ Kata/250 Data Reverse.js
@@ -0,0 +1,36 @@
+// Data Reverse
+
+/*
+A stream of data is received and needs to be reversed.
+
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+
+10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+The total number of bits will always be a multiple of 8.
+
+The data is given in an array as such:
+
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+*/
+
+function dataReverse(data) {
+  if (data.length === 0) {return []}
+  let finArr = [];
+  for (let i = 0; i < data.length; i += 8) {
+    finArr.unshift(data.slice(i, i + 8));
+  }
+  return finArr.flat()
+}
+
+console.log(
+  dataReverse([
+    1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+    0, 1, 0, 1, 0, 1, 0,
+  ])
+);
+// [1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
