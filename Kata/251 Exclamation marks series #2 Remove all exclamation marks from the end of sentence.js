6c058db94f9d694ec0309f35636713370f56dfb0
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-02 17:54:17 -0600

    Create 251 Exclamation marks series #2 Remove all exclamation marks from the end of sentence.js

---
+++ Kata/251 Exclamation marks series #2 Remove all exclamation marks from the end of sentence.js
@@ -0,0 +1,26 @@
+// Exclamation marks series #2 Remove all exclamation marks from the end of sentence
+
+/*
+Remove all exclamation marks from the end of sentence.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
+
+function remove (string) {  
+  while (string[string.length - 1] === "!") {
+    string = string.slice(0, -1)
+  }
+  return string
+}
+// return string[string.length - 1] === "!" ? string.slice(0, -1) : string;
+
+console.log(remove("Hi!"))  // "Hi"
+console.log(remove("Hi! Hi!"))  // "Hi! Hi"
+console.log(remove("Hi"))  // "Hi"
+
