ed58d71ab231e5cae52f29ef2e0206960a24322c
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-03 12:01:15 -0600

    Update 259 Speed Control.js

--- Kata/259 Speed Control.js
+++ Kata/259 Speed Control.js
@@ -37,3 +37,44 @@ function gps(s, x) {
 
 
 console.log(gps(15, [0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25]))  // 74
+
+
+
+
+const gps = (s, x) => Math.floor(3600 * x.slice(1).reduce((m, d, i) => Math.max(m, d - x[i]), 0) / s);
+
+
+function gps(s, x) {
+  return Math.max(...x.slice(1).map((a, i) => (a - x[i]) / s * 3600)) | 0
+}
+
+
+/**
+ * @param {Number} a Start
+ * @param {Number} b End
+ * @param {Number} s Speed
+ * @returns {Number} Speed on section from a to b
+ */
+const getSpeed = (a, b, s) => Math.floor((b - a) / s * 60 * 60);
+
+/**
+ * @param {Number} s Speed
+ * @param {Number[]} x X-axis values
+ * @returns {Number[]} Array of speed values on passed sections
+ */
+const reduceRecords = (s, x) => x.reduce(
+  (p, c, i, a) => i === 0 ? p : p.concat(getSpeed(a[i-1], c, s)),
+  []
+);
+
+const gps = (s, x) => x.length < 2 ? 0 : Math.max(...(reduceRecords(s, x)));
+
+/**
+ * Hint:
+ *
+ * arr.reduce and arr.concat is native array methods
+ * "a ? b : c" is ternary operator
+ * => is arrow functions from ECMAScript-2015
+ * const is ECMAScript-2015 feature
+ * func(...arr) is spread operator from ECMAScript-2015
+ */
