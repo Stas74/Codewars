40d859f9c87a9b9dc7ba29a0b4ef2ee7fb20e107
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-02 18:30:36 -0600

    Create 256 Enumerable Magic - Does My List Include This.js

---
+++ Kata/256 Enumerable Magic - Does My List Include This.js
@@ -0,0 +1,13 @@
+// Enumerable Magic - Does My List Include This?
+
+/*
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+
+function include(arr, item){
+  return arr.includes(item)
+}
+
+
+console.log(include([1,2,3,4], 3))  // 'true'
+console.log(include([1,2,4,5], 3))  // 'false'
