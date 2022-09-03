e0e118e3e3f65377b270fa61ba78d5c461589c82
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-02 18:22:21 -0600

    Create 255 Define a card suit.js

---
+++ Kata/255 Define a card suit.js
@@ -0,0 +1,23 @@
+// Define a card suit
+
+/*
+You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+*/
+
+function defineSuit(card) {
+  if (card.slice(-1) === '♠') return 'spades'
+  if (card.slice(-1) === '♦') return 'diamonds'
+  if (card.slice(-1) === '♥') return 'hearts'
+  if (card.slice(-1) === '♣') return 'clubs'
+}
+
+
+console.log(defineSuit('Q♠'))  // 'spades'
+console.log(defineSuit('J♥'))  // 'hearts'
