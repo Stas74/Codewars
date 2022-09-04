46b50d88d9cd21aa21a760411173b3d5da03109a
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-04 15:38:03 -0600

    Update 261 Training JS #8 Conditional statement--switch.js

--- Kata/261 Training JS #8 Conditional statement--switch.js
+++ Kata/261 Training JS #8 Conditional statement--switch.js
@@ -95,3 +95,26 @@ function howManydays(month){
 
 
 console.log(howManydays(1))  // 31
+
+
+
+
+function howManydays(month){
+  switch (month){
+     case 2: return 28
+     case 4:
+     case 6:
+     case 9:
+     case 11: return 30
+  }
+  return 31
+}
+
+
+function howManydays(month){
+  switch ( month ){
+    case 2 : return 28
+    case 4: case 6: case 9: case 11: return 30
+    default: return 31
+  }
+}
