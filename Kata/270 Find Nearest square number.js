0450ce1933e7dba4bfaaf8b0c05419d144308c5b
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-06 15:14:36 -0600

    Update 270 Find Nearest square number.js

--- Kata/270 Find Nearest square number.js
+++ Kata/270 Find Nearest square number.js
@@ -12,3 +12,9 @@ console.log(nearestSq(1));  // 1
 console.log(nearestSq(2));  // 1
 console.log(nearestSq(10));  // 9
 console.log(nearestSq(111));  // 121
+
+
+
+function nearestSq(n){
+    return Math.round(n**0.5)**2
+}
