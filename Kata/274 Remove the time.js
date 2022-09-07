05194b8205fff2f6e018e1fab8175d56a8ab0641
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-07 13:56:56 -0600

    Update 274 Remove the time.js

--- Kata/274 Remove the time.js
+++ Kata/274 Remove the time.js
@@ -22,3 +22,14 @@ function shortenToDate(longDate) {
 console.log(shortenToDate("Friday May 2, 9am")); // "Friday May 2"
 console.log(shortenToDate("Tuesday January 29, 10pm")); // "Tuesday January 29"
 console.log(shortenToDate("Monday December 25, 10pm")); // "Monday December 25"
+
+
+
+
+function shortenToDate(longDate) {
+  return longDate.slice(0, longDate.indexOf(","));
+}
+
+
+
+const shortenToDate =longDate => longDate.replace(/,.+$/,'');
