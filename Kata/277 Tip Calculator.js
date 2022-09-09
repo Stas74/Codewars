8f3f8a239a0885507ebd4e3541ecb249482af786
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-09 11:06:32 -0600

    Update 277 Tip Calculator.js

--- Kata/277 Tip Calculator.js
+++ Kata/277 Tip Calculator.js
@@ -33,3 +33,37 @@ function calculateTip(amount, rating) {
 console.log(calculateTip(20, "Excellent"))  // 4
 console.log(calculateTip(26.95, "good"))  // 3
 console.log(calculateTip(26.95, "Noo"))  // "Rating not recognised"
+
+
+
+
+
+const TIPS = {
+  "terrible": 0.0,
+  "poor": 0.05,
+  "good": 0.1,
+  "great": 0.15,
+  "excellent": 0.2
+};
+
+const calculateTip = (amount, rating) => {
+  rating = rating.toLowerCase();
+  
+  return rating in TIPS ? Math.ceil(TIPS[rating] * amount) : "Rating not recognised";
+};
+
+
+
+
+function calculateTip(amount, rating) {
+  switch(rating.toLowerCase()){
+    case "terrible":return 0;
+    case "poor":return Math.ceil(amount * 0.05);
+    case "good":return Math.ceil(amount * 0.1);
+    case "great":return Math.ceil(amount * 0.15);
+    case "excellent":return Math.ceil(amount * 0.2);
+    default:return "Rating not recognised";
+  }
+}
+
+
