f3bd7d4eb463e78091ea359763569fb87abd92c2
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-12 19:01:10 -0600

    Update 280 Kata Example Twist.js

--- Kata/280 Kata Example Twist.js
+++ Kata/280 Kata Example Twist.js
@@ -8,3 +8,9 @@ Add the value "codewars" to the array websites/Websites 1,000 times.
 
 // add the value "codewars" to the websites array 1,000 times
 var websites = new Array(1000).fill("codewars")
+
+
+
+var websites = [];
+while (websites.length < 1000) websites.push("codewars")
+
