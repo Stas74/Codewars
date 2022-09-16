4b30c2292cb92616ddc3067fb9714ce6095173eb
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-16 14:26:26 -0600

    Update 289 UEFA EURO 2016.js

--- Kata/289 UEFA EURO 2016.js
+++ Kata/289 UEFA EURO 2016.js
@@ -21,3 +21,31 @@ function uefaEuro2016(teams, scores){
 console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0]))  // "At match Germany - Ukraine, Germany won!"
 console.log(uefaEuro2016(['Belgium', 'Italy'], [0, 2]))  // "At match Belgium - Italy, Italy won!"
 console.log(uefaEuro2016(['Portugal', 'Iceland'], [1, 1]))  // "At match Portugal - Iceland, teams played draw."
+
+
+
+
+
+const uefaEuro2016 = (c, s) => `At match ${c[0]} - ${c[1]}, ${s[0] == s[1] ? "commands played draw." : (s[0] > s[1] ? c[0] : c[1]) + ' won!'}`;
+
+
+function uefaEuro2016(commands, scores){
+  // your code...
+  if (scores[0] == scores[1])
+    return `At match ${commands[0]} - ${commands[1]}, commands played draw.`;
+  if (scores[0] < scores[1])
+    return `At match ${commands[0]} - ${commands[1]}, ${commands[1]} won!`;
+  return `At match ${commands[0]} - ${commands[1]}, ${commands[0]} won!`;
+}
+
+
+
+function uefaEuro2016(teams, scores) {
+    const [team1, team2] = teams
+    const [score1, score2] = scores
+    const winner = score1 > score2 ? team1 : team2
+
+    return score1 === score2
+        ? `At match ${team1} - ${team2}, teams played draw.`
+        : `At match ${team1} - ${team2}, ${winner} won!`
+}
