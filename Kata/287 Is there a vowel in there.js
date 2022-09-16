ca90c3b0befdfa93ed8f9df1afbb851c08621054
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-16 13:47:24 -0600

    Update 287 Is there a vowel in there.js

--- Kata/287 Is there a vowel in there.js
+++ Kata/287 Is there a vowel in there.js
@@ -17,3 +17,26 @@ console.log(isVow([118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120
 // [118,"u",120,121,"u",98,122,"a",120,106,104,116,113,114,113,120,106]
 console.log(isVow([101,121,110,113,113,103,121,121,101,107,103]))
  // ["e",121,110,113,113,103,121,121,"e",107,103]
+
+
+
+function isVow(a){
+  for (var i=0, l=a.length; i<l; ++i)
+  {
+    var char = String.fromCharCode(a[i])
+    if ('aeiou'.indexOf(char) !== -1)
+    a[i] = char;
+  }
+  
+  return a;
+}
+
+
+
+const isVow = a => a.map(x=>'aeiou'.includes(y=String.fromCharCode(x)) ? y : x)
+
+
+
+const v={"97":"a","101":"e","105":"i","111":"o","117":"u"}
+const isVow=a=>a.map(x=>v[x]||x)
+
