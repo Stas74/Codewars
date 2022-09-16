29b2719809c8561b4523026f7d7dd12e9907e9a3
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-16 15:51:11 -0600

    Create 291 Find out whether the shape is a cube.js

---
+++ Kata/291 Find out whether the shape is a cube.js
@@ -0,0 +1,25 @@
+// Find out whether the shape is a cube
+
+/*
+To find the volume (centimeters cubed) of a cuboid you use the formula:
+
+volume = Length * Width * Height
+
+But someone forgot to use proper record keeping, so we only have the volume, and the length of a single side!
+
+It's up to you to find out whether the cuboid has equal sides (= is a cube).
+
+Return true if the cuboid could have equal sides, return false otherwise.
+
+Return false for invalid numbers too (e.g volume or side is less than or equal to 0).
+
+Note: the sides must be integers
+*/
+
+var cubeChecker = function(volume, side){
+  return volume <= 0 ? false : side === Math.cbrt(volume);
+};
+
+console.log(cubeChecker(56.3, 1))  // false
+console.log(cubeChecker(-1, 2))  // false
+console.log(cubeChecker(125, 5))  // true
