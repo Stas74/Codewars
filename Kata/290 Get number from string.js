349f6a07f49b4312af918cc96b611f0a1c81e01b
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-16 15:42:33 -0600

    Update 290 Get number from string.js

--- Kata/290 Get number from string.js
+++ Kata/290 Get number from string.js
@@ -11,3 +11,19 @@ function getNumberFromString(s) {
 console.log(getNumberFromString("1"))  // 1
 console.log(getNumberFromString("123"))  // 123
 console.log(getNumberFromString("this is number: 7"))  // 7
+
+
+
+function getNumberFromString(s) {
+  return Number(s.replace(/\D/g, ""));
+}
+
+
+
+function getNumberFromString(s) {
+  return Number(s.split('').filter(a => a <= '9' && a >= '0').join(''))
+}
+
+
+
+const getNumberFromString = (s) => parseInt(s.split("").filter(x => "0123456789".includes(x)).join(""))
