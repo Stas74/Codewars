7478eb224c8cbeb474f0f09dfd07a2cd0b8f10fa
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-19 16:43:32 -0600

    Update 293 Array Helpers.js

--- Kata/293 Array Helpers.js
+++ Kata/293 Array Helpers.js
@@ -1,6 +1,7 @@
 // Array Helpers
 /*
-This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+This kata is designed to test your ability to extend the functionality of built-in classes. 
+In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
 
 Explanation:
 
@@ -51,3 +52,16 @@ console.log(numbers.sum()); // 15
 console.log(numbers.average()); // 3
 console.log(numbers.even()); // [2, 4]
 console.log(numbers.odd()); // [1, 3, 5]
+
+
+
+
+
+Array.prototype.square  = function () { return this.map(function(n) { return n*n; }); }
+Array.prototype.cube    = function () { return this.map(function(n) { return n*n*n; }); }
+Array.prototype.average = function () { return this.sum() / this.length; }
+Array.prototype.sum     = function () { return this.reduce(function(a, b) { return a + b; }, 0); }
+Array.prototype.even    = function () { return this.filter(function(item) { return 0 == item % 2; }); }
+Array.prototype.odd     = function () { return this.filter(function(item) { return 0 != item % 2; }); }
+
+
