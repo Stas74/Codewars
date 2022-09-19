9f9afcff99dadd6f49b86013fedf0f5a0319a1da
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-19 16:00:56 -0600

    Update 292 Help the bookseller !.js

--- Kata/292 Help the bookseller !.js
+++ Kata/292 Help the bookseller !.js
@@ -52,3 +52,36 @@ function stockList(listOfArt, listOfCat){
 
 console.log(stockList(["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"], ["A", "B", "C", "D"]));  
 // "(A : 0) - (B : 1290) - (C : 515) - (D : 600)"
+
+
+
+
+const stockList = (listOfArt, listOfCat) =>
+  listOfArt.length ? listOfCat.map(val => `(${val} : ${listOfArt.reduce((pre, v) => pre + (v[0] === val) * v.split(` `)[1], 0)})`).join(` - `) : ``;
+
+
+
+function stockList(listOfArt, listOfCat) {
+  var qs = {};
+  if (!listOfArt.length) return '';
+
+  listOfArt.forEach(function(art) {
+    var cat = art[0];
+    qs[cat] = (qs[cat] | 0) + +art.split(' ')[1];
+  });
+
+  return listOfCat.map(function(c) {
+    return '(' + c + ' : ' + (qs[c] | 0) + ')';  
+  }).join(' - ');  
+}
+
+
+
+
+function stockList(listOfArt, listOfCat) {
+  if (!listOfArt.length || !listOfCat.length) return ''
+  return listOfCat.map(w => {
+    const s = listOfArt.reduce((a, b) => a + (b.charAt(0) === w ? +b.split(' ')[1] : 0), 0)
+    return `(${w} : ${s})`
+  }).join(' - ')
+}
