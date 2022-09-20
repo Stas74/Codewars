ea553eaff561d0d4f1cef689926685d6f656c82c
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-20 17:30:53 -0600

    Update 297 Price of Mangoes.js

--- Kata/297 Price of Mangoes.js
+++ Kata/297 Price of Mangoes.js
@@ -17,3 +17,15 @@ function mango(quantity, price){
 console.log(mango(3, 3))  // 6
 console.log(mango(9, 5))  // 30
 console.log(mango(5, 3))  // 12
+
+
+
+
+function mango(quantity, price){
+  return price * (quantity - Math.floor(quantity / 3));
+}
+
+
+function mango(quantity, price){
+  return price * (quantity - Math.floor(quantity / 3));
+}
