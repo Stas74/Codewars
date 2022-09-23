626c4d5e2b6b615220fb73ac8c5e73f5e0a0f724
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-23 12:22:39 -0600

    Update 302 Divide and Conquer.js

--- Kata/302 Divide and Conquer.js
+++ Kata/302 Divide and Conquer.js
@@ -17,3 +17,26 @@ function divCon(x) {
   
 console.log(divCon([9, 3, '7', '3']))  // 2
 console.log(divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7]))  // 14
+
+
+
+
+function divCon(x){
+  return x.reduce((acc, cur) => typeof cur === 'number'? acc + cur : acc - Number(cur),0)
+}
+
+
+
+function divCon(x){
+  let numValue = 0;
+  let strValue = 0;
+  
+  for(let i = 0; i < x.length; i++) {
+    if(typeof(x[i]) === 'string' ) {
+      strValue += parseFloat(x[i]);
+    } else if(typeof(x[i]) === 'number' ) {
+      numValue += x[i];
+    }
+  }
+  return numValue - strValue;
+}
