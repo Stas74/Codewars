165aa723028cb6ce09f4dc21aee45daf7f96bc23
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-23 16:12:04 -0600

    Create 308 Is it a number.js

---
+++ Kata/308 Is it a number.js
@@ -0,0 +1,26 @@
+// Is it a number?
+
+/*
+Given a string s, write a method (function) that will return true if its 
+a valid single integer or floating number or false if its not.
+
+Valid examples, should return true:
+
+isDigit("3")
+isDigit("  3  ")
+isDigit("-3.23")
+
+should return false:
+
+isDigit("3-4")
+isDigit("  3   5")
+isDigit("3 5")
+isDigit("zero")
+*/
+
+function isDigit(s) {
+  return s == parseFloat(s)
+}
+
+console.log(isDigit("s2324"))  // false
+console.log(isDigit("-234.4"))  // true
