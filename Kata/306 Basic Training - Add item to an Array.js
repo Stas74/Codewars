3e103627a8ce493c1100148c9f7b3b3de9a6c3bc
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-23 15:10:36 -0600

    Create 306 Basic Training - Add item to an Array.js

---
+++ Kata/306 Basic Training - Add item to an Array.js
@@ -0,0 +1,15 @@
+// Basic Training: Add item to an Array
+
+/*
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+*/
+
+var websites = [];
+// add the value "codewars" to the already defined websites array
+
+websites.push("codewars")
