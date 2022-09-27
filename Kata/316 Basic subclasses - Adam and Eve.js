3d794996d6c885b0a2f94a5f8c714af44886ec42
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-27 15:12:24 -0600

    Update 316 Basic subclasses - Adam and Eve.js

--- Kata/316 Basic subclasses - Adam and Eve.js
+++ Kata/316 Basic subclasses - Adam and Eve.js
@@ -57,3 +57,19 @@ class God{
   }
   Woman.prototype = Object.create(Human.prototype)
 
+
+
+
+
+
+class God{
+/**
+ * @returns Human[]
+ */
+  static create(){
+    return [new Man, new Woman];
+  }
+}
+class Human{}
+class Man extends Human{}
+class Woman extends Human{}
