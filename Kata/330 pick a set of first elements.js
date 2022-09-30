f26f74427c9c7fb2354af47039e28aa5e7883185
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-30 13:45:12 -0600

    Create 330 pick a set of first elements.js

---
+++ Kata/330 pick a set of first elements.js
@@ -0,0 +1,24 @@
+// pick a set of first elements
+
+/*
+Write a function to get the first element(s) of a sequence. 
+Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+If n == 0 return an empty sequence []
+
+Examples
+var arr = ['a', 'b', 'c', 'd', 'e'];
+first(arr) //=> ['a'];
+first(arr, 2) //=> ['a', 'b']
+first(arr, 3) //=> ['a', 'b', 'c'];
+first(arr, 0) //=> [];
+*/
+
+function first(arr, n) {
+  return n == undefined ? arr.slice(0, 1) : arr.slice(0, n)
+}
+
+var arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(first(arr))  // ['a']
+console.log(first(arr, 2))  // ['a', 'b']
+console.log(first(arr, 0))  // []
