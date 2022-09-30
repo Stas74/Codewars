50cab3279c6930756f25e51dbd862271ce728154
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-30 11:48:02 -0600

    Create 327 Fundamentals - Return.js

---
+++ Kata/327 Fundamentals - Return.js
@@ -0,0 +1,53 @@
+// Fundamentals: Return
+
+/*
+Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+Please use the following function names:
+
+addition = add
+
+multiply = multiply
+
+division = divide (both integer and float divisions are accepted)
+
+modulus = mod
+
+exponential = exponent
+
+subtraction = subt
+
+Note: All math operations will be: a (operation) b
+*/
+
+function add(a,b){
+  return a + b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function exponent(a,b){
+    return a % b
+}
+   
+function mod(a,b){
+  return Math.pow(a, b)
+}
+    
+function subt(a,b){
+    return a - b
+}
+
+
+console.log(add(1,2)) // 3
+console.log(multiply(1,2)) // 2
+console.log(divide(2,1)) // 2
+console.log(mod(1,2)) // 1
+console.log(exponent(1,2)) // 1
+console.log(subt(1,2)) // -1
