28c29828e59e9d41da227bd8c9970d9533d8517e
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-30 13:33:49 -0600

    Update 329 Training JS #9  loop statement --while and do..while.js

--- Kata/329 Training JS #9  loop statement --while and do..while.js
+++ Kata/329 Training JS #9  loop statement --while and do..while.js
@@ -60,3 +60,14 @@ function padIt(str, n) {
 console.log(padIt("a",1))  // "*a"
 console.log(padIt("a",3))  // "**a*"
 console.log(padIt("a",5))  // "***a**"
+
+
+
+
+function padIt(str, n) {
+  while (n > 0) {
+    str = n-- % 2 ? '*' + str : str + '*';
+  }
+  return str;
+}
+
