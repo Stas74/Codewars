881a17e2572a14b01f43b5ac5c7490d703203104
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-09-30 11:12:17 -0600

    Create 325 Maximum Triplet Sum (Array Series #7).js

---
+++ Kata/325 Maximum Triplet Sum (Array Series #7).js
@@ -0,0 +1,43 @@
+// Maximum Triplet Sum (Array Series #7)
+
+/*
+Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+
+Notes :
+Array/list size is at least 3 .
+
+Array/list numbers could be a mixture of positives , negatives and zeros .
+
+Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+
+Input >> Output Examples
+
+1- maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+
+Explanation:
+As the triplet that maximize the sum {6,8,3} in order , their sum is (17)
+
+Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+2- maxTriSum ({2,1,8,0,6,4,8,6,2,4}) ==> return (18)
+
+Explanation:
+As the triplet that maximize the sum {8, 6, 4} in order , their sum is (18) ,
+
+Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+3- maxTriSum ({-7,12,-7,29,-5,0,-7,0,0,29}) ==> return (41)
+
+Explanation:
+As the triplet that maximize the sum {12 , 29 , 0} in order , their sum is (41) ,
+
+Note : duplications are not included when summing , (i.e) the numbers added only once .
+*/
+
+function maxTriSum(numbers){  
+  return [...new Set(numbers)].sort((a, b) => a - b).slice(-3).reduce((prev, cur) => prev + cur, 0)
+}
+
+console.log(maxTriSum([3,2,6,8,2,3])); // 17
+console.log(maxTriSum([2,1,8,0,6,4,8,6,2,4])); // 18
+console.log(maxTriSum([2,9,13,10,5,2,9,5])); // 32
