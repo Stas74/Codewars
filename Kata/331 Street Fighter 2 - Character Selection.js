5f3bdb3cde15b821639d36447f7a1acb0ba981cf
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-03 13:38:57 -0600

    Update 331 Street Fighter 2 - Character Selection.js

--- Kata/331 Street Fighter 2 - Character Selection.js
+++ Kata/331 Street Fighter 2 - Character Selection.js
@@ -108,3 +108,52 @@ let fighters = [
 ];
 
 console.log(streetFighterSelection(fighters, [0,0], moves))  // ['Ryu', 'Vega', 'Ryu', 'Vega', 'Balrog']
+
+
+
+
+function streetFighterSelection(fighters, position, moves){
+  var result = [];
+  
+  moves.forEach(function(move) {
+    if (move === "up") {
+      position[0] = 0;
+    }
+    else if (move === "down") {
+      position[0] = 1;
+    }
+    else if (move === "right") {
+      position[1] = (position[1] === 5) ? 0 : position[1] + 1;
+    }
+    else if (move === "left") {
+      position[1] = (position[1] === 0) ? 5 : position[1] - 1;
+    }
+    
+    result.push(fighters[position[0]][position[1]]);
+  });
+  
+  return result;
+}
+
+
+
+
+function streetFighterSelection(fighters, position, moves){
+
+  const boundY = fighters.length - 1;
+  const boundX = fighters[0].length - 1;
+
+  let [y, x] = position;
+
+  const controls = {
+    right: () => x === boundX ? x = 0 : x++,
+    left: () => x ? x-- : x = boundX,
+    up: () => y ? y-- : y,
+    down: () => y < boundY ? y++ : y
+  };
+  
+  return moves.map(move => {
+    controls[move]();
+    return fighters[y][x];
+  });
+}
