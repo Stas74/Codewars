9986550937a57215346c9d7f19048302e674f7b6
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-03 13:59:58 -0600

    Update 332 esreveR.js

--- Kata/332 esreveR.js
+++ Kata/332 esreveR.js
@@ -19,3 +19,23 @@ reverse = function(array) {
 
 console.log(reverse([1,2,3]))  // [3,2,1]
 console.log(reverse([1,null,14,"two"]))  // ["two",14,null,1]
+
+
+
+reverse = function(array) {
+  return array.map((c, i) => array[array.length - 1 - i]);
+}
+
+
+
+reverse = function(array) {
+  return array.reduceRight(function(a, v){ return a.push(v), a }, []);
+}
+
+
+
+function reverse(array) {
+  return array.reduce(function(a, b) {
+    return [b].concat(a)
+  }, []);
+}
