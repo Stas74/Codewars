2499486afe2bd82de04a3f53aadc73bb029c38a8
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-03 14:42:00 -0600

    Create 334 Exclamation marks series #4 - Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string.js

---
+++ Kata/334 Exclamation marks series #4 - Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string.js
@@ -0,0 +1,29 @@
+// Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string
+
+
+/*
+Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. 
+For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+Examples
+remove("Hi!") === "Hi!"
+remove("Hi!!!") === "Hi!"
+remove("!Hi") === "Hi!"
+remove("!Hi!") === "Hi!"
+remove("Hi! Hi!") === "Hi Hi!"
+remove("Hi") === "Hi!"
+*/
+
+
+function remove (string) {
+  let finStr = "";
+  for (let char of string) {
+    if (char !== "!") {
+      finStr += char;
+    }
+  }
+  return finStr + "!"
+}
+
+console.log(remove("Hi!"))  // "Hi!"
+console.log(remove("Hi! Hi!"))  // "Hi Hi!"
