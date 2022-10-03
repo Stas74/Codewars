3c0acfa9d30b5ef9aa389408cd96743bf5587cfe
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-03 14:35:39 -0600

    Update 333 Word a10n (abbreviation).js

--- Kata/333 Word a10n (abbreviation).js
+++ Kata/333 Word a10n (abbreviation).js
@@ -2,11 +2,13 @@
 
 
 /*
-The word i18n is a common abbreviation of internationalization in the developer community, used instead of typing the whole word and trying to spell it correctly. Similarly, a11y is an abbreviation of accessibility.
+The word i18n is a common abbreviation of internationalization in the developer community, used instead of typing the whole word and trying to spell it correctly. 
+Similarly, a11y is an abbreviation of accessibility.
 
 Write a function that takes a string and turns any and all "words" (see below) within that string of length 4 or greater into an abbreviation, following these rules:
 
-A "word" is a sequence of alphabetical characters. By this definition, any other character like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two words (eg. "elephant" and "ride").
+A "word" is a sequence of alphabetical characters. By this definition, any other character like a space or hyphen (eg. "elephant-ride") 
+will split up a series of letters into two words (eg. "elephant" and "ride").
 The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
 Example
 abbreviate("elephant-rides are really fun!")
@@ -31,3 +33,22 @@ function abbreviate(string) {
 console.log(abbreviate("internationalization"))  // "i18n"
 console.log(abbreviate("accessibility"))  // "a11y"
 console.log(abbreviate("elephant-ride"))  // "e6t-r2e"
+
+
+
+
+var find = /[a-z]{4,}/gi;
+function replace(match) { return match[0] + (match.length - 2) + match[match.length - 1]; }
+
+function abbreviate(string) {
+  return string.replace(find, replace);
+}
+
+
+
+
+function abbreviate(string) {
+  return string.replace(/\w{4,}/g, function(word) {
+    return word[0] + (word.length - 2) + word.slice(-1);
+  });
+}
