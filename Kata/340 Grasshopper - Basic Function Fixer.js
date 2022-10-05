869f2ad5df99ddd7467974dadcbad32bae5b7a96
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-05 12:26:39 -0600

    Update 340 Grasshopper - Basic Function Fixer.js

--- Kata/340 Grasshopper - Basic Function Fixer.js
+++ Kata/340 Grasshopper - Basic Function Fixer.js
@@ -22,3 +22,14 @@ const addFive = (num) => {
 console.log(addFive(5)); // 10
 console.log(addFive(0)); // 5
 console.log(addFive(-5)); // 0
+
+
+
+
+function addFive(num) {
+  return num + 5;
+}
+
+
+const addFive = n => n + 5;
+const addFive = (num) => num + 5
