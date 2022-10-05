4c65eb74f991b2b2daf554739479022693f6bc41
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-05 12:45:26 -0600

    Update 342 Fix your code before the garden dies!.js

--- Kata/342 Fix your code before the garden dies!.js
+++ Kata/342 Fix your code before the garden dies!.js
@@ -29,3 +29,19 @@ function rainAmount(mm) {
 
 console.log(rainAmount(100)); // "Your plant has had more than enough water for today!"
 console.log(rainAmount(39)); // "You need to give your plant 1mm of water"
+
+
+
+
+
+function rainAmount(mm){
+  if (mm < 40)
+    return "You need to give your plant " + (40 - mm) + "mm of water"
+  return "Your plant has had more than enough water for today!" 
+}
+
+
+function rainAmount(mm){
+    return mm < 40 ? `You need to give your plant ${40 - mm}mm of water` : "Your plant has had more than enough water for today!"
+}
+
