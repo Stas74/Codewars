9ce2147924f422a9e33ed9559bcba0386819b42e
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-05 12:24:52 -0600

    Create 340 Grasshopper - Basic Function Fixer.js

---
+++ Kata/340 Grasshopper - Basic Function Fixer.js
@@ -0,0 +1,24 @@
+// Grasshopper - Basic Function Fixer
+
+/*
+Fix the function
+I created this function to add five to any number that was passed in to it and return the new value. 
+It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+
+function addFive(num) {
+  var total = num + 5
+  return num
+}
+
+*/
+
+const addFive = (num) => {
+  const total = num + 5;
+  return total;
+}
+
+console.log(addFive(5)); // 10
+console.log(addFive(0)); // 5
+console.log(addFive(-5)); // 0
