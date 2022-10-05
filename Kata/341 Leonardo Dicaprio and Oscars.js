9f44399a86176bfedb1220bf2e200ac3ef8d33ae
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-05 12:35:46 -0600

    Update 341 Leonardo Dicaprio and Oscars.js

--- Kata/341 Leonardo Dicaprio and Oscars.js
+++ Kata/341 Leonardo Dicaprio and Oscars.js
@@ -23,3 +23,33 @@ console.log(leo(89)); // "Leo got one already!"
 console.log(leo(88)); // "Leo finally won the oscar! Leo is happy"
 console.log(leo(87)); // "When will you give Leo an Oscar?"
 console.log(leo(86)); // "Not even for Wolf of wallstreet?!"
+
+
+
+
+function leo(oscar){
+  switch (oscar){
+    case 88:
+      return "Leo finally won the oscar! Leo is happy";
+      break;
+    case 86:
+      return  "Not even for Wolf of wallstreet?!";
+      break;
+    default:
+      if(oscar<88) return "When will you give Leo an Oscar?";
+      else return  "Leo got one already!";
+  }
+}
+
+
+function leo(oscar){
+  switch(true){
+    case oscar>88: return 'Leo got one already!'; break;
+    case oscar==88: return 'Leo finally won the oscar! Leo is happy'; break;
+    case oscar==86: return 'Not even for Wolf of wallstreet?!'; break;
+    default: return 'When will you give Leo an Oscar?'
+  }
+}
+
+
+
