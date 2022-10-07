aa2957598104971766ec41beba532153bd598899
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-07 12:48:21 -0600

    Create 345 Tidy Number (Special Numbers Series #9).js

---
+++ Kata/345 Tidy Number (Special Numbers Series #9).js
@@ -0,0 +1,23 @@
+// Tidy Number (Special Numbers Series #9)
+
+/*
+A Tidy number is a number whose digits are in non-decreasing order.
+
+Task
+Given a number, Find if it is Tidy or not .
+*/
+
+function tidyNumber(n) {
+  return (
+    String(n) ===
+    String(n)
+      .split("")
+      .sort((a, b) => a - b)
+      .join("")
+  );
+}
+
+console.log(tidyNumber(12)); // true
+console.log(tidyNumber(102)); // false
+console.log(tidyNumber(2789)); // true
+console.log(tidyNumber(9672)); // false
