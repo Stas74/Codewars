db9607d9c10dddf3659a905565fb3be135276e95
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-07 15:21:15 -0600

    Create 348 get ascii value of character.js

---
+++ Kata/348 get ascii value of character.js
@@ -0,0 +1,16 @@
+// get ascii value of character
+
+/*
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/
+*/
+
+function getASCII(c){
+  return c.charCodeAt()
+}
+
+
+console.log(getASCII('A')) // 65
+console.log(getASCII(' ')) // 32
+console.log(getASCII('!')) // 33
