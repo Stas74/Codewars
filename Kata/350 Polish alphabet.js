54491fedd75ab0ed72ca27206eeb7f8dc018a8fc
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-10 14:32:27 -0600

    Update 350 Polish alphabet.js

--- Kata/350 Polish alphabet.js
+++ Kata/350 Polish alphabet.js
@@ -30,3 +30,54 @@ function correctPolishLetters (string) {
 console.log(correctPolishLetters("Jędrzej Błądziński")) // "Jedrzej Bladzinski"
 console.log(correctPolishLetters("Lech Wałęsa")) // "Lech Walesa"
 console.log(correctPolishLetters("Maria Skłodowska-Curie")) // "Maria Sklodowska-Curie"
+
+
+
+
+function correctPolishLetters (string) {
+  var dict = {'ą':'a','ć':'c','ę':'e','ł':'l','ń':'n','ó':'o','ś':'s','ź':'z','ż':'z'};
+  return string.replace(/[ąćęłńóśźż]/g, match => dict[match]);
+}
+
+
+
+
+
+var polishLetters = {
+    "ą": "a",
+    "ć": "c",
+    "ę": "e",
+    "ł": "l",
+    "ń": "n",
+    "ó": "o",
+    "ś": "s",
+    "ź": "z",
+    "ż": "z",
+};
+
+function correctPolishLetters (string) {
+  return string.split('').map((c) => polishLetters[c] || c).join("");
+}
+
+
+
+
+function correctPolishLetters (string) {
+  return string
+  .replace(/ł/g, 'l')
+  .replace(/ą/g, 'a')
+  .replace(/ć/g, 'c')
+  .replace(/ę/g, 'e')
+  .replace(/ń/g, 'n')
+  .replace(/ó/g, 'o')
+  .replace(/ś/g, 's')
+  .replace(/ź/g, 'z')
+  .replace(/ż/g, 'z')
+}
+
+
+
+
+function correctPolishLetters(s) {
+  return s.replace(/[ąćęłńóśźż]/g, c => "acelnoszz"["ąćęłńóśźż".indexOf(c)])
+}
