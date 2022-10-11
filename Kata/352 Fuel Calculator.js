037a6bebd53bd359f9962912a85590513c628e81
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-11 12:47:10 -0600

    Create 352 Fuel Calculator.js

---
+++ Kata/352 Fuel Calculator.js
@@ -0,0 +1,28 @@
+// Fuel Calculator
+
+/*
+In this kata you will have to write a function that takes litres and pricePerLitre (in dollar) as arguments.
+
+Purchases of 2 or more litres get a discount of 5 cents per litre, 
+purchases of 4 or more litres get a discount of 10 cents per litre, and so on every two litres, 
+up to a maximum discount of 25 cents per litre. 
+But total discount per litre cannot be more than 25 cents. 
+Return the total cost rounded to 2 decimal places. 
+Also you can guess that there will not be negative or non-numeric inputs.
+
+Good Luck!
+
+Note
+1 Dollar = 100 Cents
+*/
+
+function fuelPrice(litres, pricePerLitre) {
+  let discount = litres >= 10 ? 0.25 : litres >= 8 ? 0.2 : litres >= 6 ? 0.15 : litres >= 4 ? 0.1 : litres >= 2 ? 0.05 : 0;
+  return +(litres * pricePerLitre - litres * discount).toFixed(2);
+}
+
+
+
+console.log(fuelPrice(5, 1.23)); // 5.65
+console.log(fuelPrice(8, 2.5)); // 18.40
+console.log(fuelPrice(5, 5.6)); // 27.50
