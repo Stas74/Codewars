1ac70bf2122216d00c1f7b78b1eac5fc6f8cdd8c
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-11 13:02:57 -0600

    Create 353 Filter the number.js

---
+++ Kata/353 Filter the number.js
@@ -0,0 +1,23 @@
+// Filter the number
+
+/*
+Oh, no! The number has been mixed up with the text. 
+Your goal is to retrieve the number from the text, can you return the number back to its original state?
+
+Task
+Your task is to return a number from a string.
+
+Details
+You will be given a string of numbers and letters mixed up, 
+you have to return all the numbers in that string in the order they occur.
+*/
+
+var filterString = function(value) {
+  return +value.replace(/[a-z]/g, "")
+}
+
+
+
+console.log(filterString("123")); // 123
+console.log(filterString("a1b2c3")); // 123
+console.log(filterString("aa1bb2cc3dd")); // 123
