16cb48d9f10574018dbb048e4d967381591d5c6e
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-11 12:51:11 -0600

    Update 352 Fuel Calculator.js

--- Kata/352 Fuel Calculator.js
+++ Kata/352 Fuel Calculator.js
@@ -26,3 +26,33 @@ function fuelPrice(litres, pricePerLitre) {
 console.log(fuelPrice(5, 1.23)); // 5.65
 console.log(fuelPrice(8, 2.5)); // 18.40
 console.log(fuelPrice(5, 5.6)); // 27.50
+
+
+
+
+function fuelPrice(litres, pricePerLiter) {
+  for (var i = 2; i <= 10; i +=2) { //discount loop
+    if (litres >= i) {
+      pricePerLiter -= 0.05;
+    }
+  }
+  return Math.round(litres * pricePerLiter * 100) / 100;;
+}
+
+
+
+function fuelPrice(litres, pricePerLiter) {
+  var discount = Math.min(Math.floor(litres/2) * 0.05, 0.25);
+  var price = litres * (pricePerLiter - discount);
+  return Math.round(price*100) / 100;
+}
+
+
+
+function fuelPrice(l, p) {
+  return Math.round(100 * l * Math.max(p - 0.05 * ~~(l / 2), p - 0.25)) / 100;
+}
+
+
+const fuelPrice = (litres, pricePerLiter) =>
+  +(litres * (pricePerLiter - Math.min((litres / 2 ^ 0) * 0.05, 0.25))).toFixed(2);
