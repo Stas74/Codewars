607f424f1b6a4ff8ae5d88ba761c6ff34bbccd57
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-11 13:04:11 -0600

    Update 353 Filter the number.js

--- Kata/353 Filter the number.js
+++ Kata/353 Filter the number.js
@@ -12,12 +12,24 @@ You will be given a string of numbers and letters mixed up,
 you have to return all the numbers in that string in the order they occur.
 */
 
+
 var filterString = function(value) {
   return +value.replace(/[a-z]/g, "")
 }
 
 
-
 console.log(filterString("123")); // 123
 console.log(filterString("a1b2c3")); // 123
 console.log(filterString("aa1bb2cc3dd")); // 123
+
+
+
+var FilterString = function(value) {
+  return parseInt(value.replace(/[^\d]/g, ""))
+}
+
+
+var FilterString = function(value) {
+  return +value.split('').filter(n => !isNaN(n)).join('');
+}
+
