a3dcb02cacda663bed5b915a4e09dbbe5753053e
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-12 15:33:47 -0600

    Update 358 Product Of Maximums Of Array (Array Series #2).js

--- Kata/358 Product Of Maximums Of Array (Array Series #2).js
+++ Kata/358 Product Of Maximums Of Array (Array Series #2).js
@@ -45,3 +45,12 @@ function maxProduct(numbers, size){
 
 console.log(maxProduct([4,3,5], 2)); // 20
 console.log(maxProduct([10,8,7,9], 3)); // 720
+
+
+
+
+function maxProduct(numbers, size){
+  return numbers.sort((a, b) => b - a).slice(0, size).reduce((a, b) => a * b);
+}
+
+
