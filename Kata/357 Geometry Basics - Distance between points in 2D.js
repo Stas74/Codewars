35d33ae3a369b3782fa3810ba37149ae410f15b4
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-12 15:18:20 -0600

    Create 357 Geometry Basics - Distance between points in 2D.js

---
+++ Kata/357 Geometry Basics - Distance between points in 2D.js
@@ -0,0 +1,18 @@
+// Geometry Basics: Distance between points in 2D
+
+/*
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Point objects have x and y attributes (X and Y in C#) attributes.
+
+Write a function calculating distance between Point a and Point b.
+
+Tests round answers to 6 decimal places.
+*/
+
+function distanceBetweenPoints(a, b) {
+  return Math.hypot(a.x - b.x, a.y - b.y)
+}
+
+console.log(distanceBetweenPoints(new Point(3, 3), new Point(3, 3))); // 0
+console.log(distanceBetweenPoints(new Point(1, 6), new Point(4, 2))); // 5
