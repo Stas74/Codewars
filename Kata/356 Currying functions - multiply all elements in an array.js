e642e94f1b94a29648fd1cd088ceb29fd7c394d5
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-12 14:25:14 -0600

    Update 356 Currying functions - multiply all elements in an array.js

--- Kata/356 Currying functions - multiply all elements in an array.js
+++ Kata/356 Currying functions - multiply all elements in an array.js
@@ -19,3 +19,14 @@ const multiplyAll = arr => index => {
 }
 
 console.log(multiplyAll([1, 2, 3])(2)); // [2, 4, 6]
+
+
+
+
+function multiplyAll(arr) {
+  return function(n) {
+    return arr.map(x => x * n);
+  }
+}
+
+
