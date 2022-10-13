4ad59a477812de31b0919293716205c84660478c
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-13 12:22:22 -0600

    Update 360 Sleigh Authentication.js

--- Kata/360 Sleigh Authentication.js
+++ Kata/360 Sleigh Authentication.js
@@ -28,3 +28,14 @@ Sleigh.prototype.authenticate = function(name, password) {
 };
 
 
+
+
+function Sleigh() {
+  this.name = "Santa Claus";
+  this.password = "Ho Ho Ho!";
+}
+
+Sleigh.prototype.authenticate = function(name, password) {
+  return this.name == name && this.password == password;
+};
+
