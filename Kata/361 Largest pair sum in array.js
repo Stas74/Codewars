0f1749a0e41a70a3fcb9bf143f9be2d94942b836
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-13 12:30:34 -0600

    Create 361 Largest pair sum in array.js

---
+++ Kata/361 Largest pair sum in array.js
@@ -0,0 +1,19 @@
+// Largest pair sum in array
+
+/*
+Given a sequence of numbers, find the largest pair sum in the sequence.
+
+For example
+
+[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+Input sequence contains minimum two elements and every element is an integer.
+*/
+
+function largestPairSum (numbers) {
+  return numbers.sort( (a, b) => b - a).slice(0, 2).reduce((prev, cur) => prev + cur);
+}
+
+
+console.log(largestPairSum([10,14,2,23,19])) // 42
+console.log(largestPairSum([99, 2, 2, 23, 19])) // 122
