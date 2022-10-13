dc5afe57c6c9ed37575c37ee8b2cf02b25760de4
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-13 11:41:59 -0600

    Create 359 Simple remove duplicates.js

---
+++ Kata/359 Simple remove duplicates.js
@@ -0,0 +1,21 @@
+// Simple remove duplicates
+
+/*
+Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+Example:
+For input: [3, 4, 4, 3, 6, 3]
+
+remove the 3 at index 0
+remove the 4 at index 1
+remove the 3 at index 3
+
+Expected output: [4, 6, 3]
+*/
+
+function solve(arr) {
+  return [...new Set(arr.reverse())].reverse()
+}
+
+console.log(solve([3,4,4,3,6,3])); // [4,6,3]
+console.log(solve([1,1,4,5,1,2,1])); // [4,5,2,1]
