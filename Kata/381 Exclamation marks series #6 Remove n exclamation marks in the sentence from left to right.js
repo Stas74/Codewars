8a8bfd27c71e8daffd4ed784c17f00ac57bb979a
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-20 15:15:49 -0600

    Create 381 Exclamation marks series #6 Remove n exclamation marks in the sentence from left to right.js

---
+++ Kata/381 Exclamation marks series #6 Remove n exclamation marks in the sentence from left to right.js
@@ -0,0 +1,25 @@
+// Exclamation marks series #6 Remove n exclamation marks in the sentence from left to right
+
+/*
+Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+Examples
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+*/
+
+function remove(s, n) {
+ return n > 0 ? remove(s.replace("!", ""), n - 1) : s
+}
+
+console.log(remove("Hi!", 1)); // "Hi"
+console.log(remove("Hi!!!", 1)); // "Hi!!"
