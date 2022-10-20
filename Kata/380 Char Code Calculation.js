b889bf58aaa9571fcfe0b93fc6b58bce39ccde05
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-20 13:58:02 -0600

    Update 380 Char Code Calculation.js

--- Kata/380 Char Code Calculation.js
+++ Kata/380 Char Code Calculation.js
@@ -27,3 +27,29 @@ function calc(x){
 
 console.log(calc('abcdef')) // 6
 console.log(calc('aaaaaddddr')) // 30
+
+
+
+
+function calc(x){
+  let sum = n => [...n].reduce((a,b) => +a + +b);
+  let total1 = x.replace(/./g, x => x.charCodeAt(0));
+  let total2 = total1.replace(/7/g,'1');
+  return sum(total1) - sum(total2);
+}
+
+
+const calc = x =>
+  x.replace(/./g, val => val.charCodeAt()).replace(/[^7]/g, ``).length * 6;
+
+
+function calc(x){
+  return x
+    .split('')
+    .map(c => c.charCodeAt(0))
+    .join('')
+    .split('')
+    .map(Number)
+    .filter(x => x === 7)
+    .length * 6
+}
