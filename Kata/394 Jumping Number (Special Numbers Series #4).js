1b9741fe99c885ac179edf27db11f075cb0ab6cc
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-26 15:07:35 -0600

    Create 394 Jumping Number (Special Numbers Series #4).js

---
+++ Kata/394 Jumping Number (Special Numbers Series #4).js
@@ -0,0 +1,21 @@
+// Jumping Number (Special Numbers Series #4)
+
+/*
+Jumping number is the number that All adjacent digits in it differ by 1.
+
+Task
+Given a number, Find if it is Jumping or not .
+*/
+
+function jumpingNumber(n) {
+  let arr = String(n).split("").map((el => +el)); 
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (Math.abs(arr[i] - arr[i + 1]) != 1) {
+      return "Not!!"
+    }      
+  }
+  return "Jumping!!"
+}
+
+console.log(jumpingNumber(8987)); // "Jumping!!"
+console.log(jumpingNumber(8981)); // "Not!!"
