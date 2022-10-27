c31b607d9ae494095aa389fc741d30509a249648
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-27 11:04:55 -0600

    Update 398 Longest vowel chain.js

--- Kata/398 Longest vowel chain.js
+++ Kata/398 Longest vowel chain.js
@@ -14,3 +14,27 @@ function solve(s) {
 console.log(solve("codewarriors")); // 2
 console.log(solve("suoidea")); // 3
 console.log(solve("chrononhotonthuooaos")); // 5
+
+
+
+
+const solve = s => s.split(/[^aeiou]/).reduce((s,n)=> Math.max(s,n.length),0);
+
+
+
+function solve(s) {
+  let cur = 0
+  let max = 0
+  for (let i = 0; i < s.length; ++i) {
+    if ("aeiou".includes(s[i])) {
+      cur++
+      if (cur > max) {
+        max = cur
+      }
+    } else {
+      cur = 0
+    }
+  }
+  return max
+}
+
