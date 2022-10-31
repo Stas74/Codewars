140434110d77863c1ebfa55f365b15d0cd5a49cc
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-10-31 17:48:27 -0600

    Update 403 Equal Sides Of An Array.js

--- Kata/403 Equal Sides Of An Array.js
+++ Kata/403 Equal Sides Of An Array.js
@@ -51,3 +51,14 @@ function findEvenIndex(arr) {
 console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1])); // 3
 console.log(findEvenIndex([1, 100, 50, -51, 1, 1])); // 1
 console.log(findEvenIndex([1, 2, 3, 4, 5, 6])); // -1
+
+
+
+const sum = (a, from, to) => a.slice(from, to).reduce((a, b) => a + b, 0)
+const findEvenIndex = a => a.findIndex((el, i) => sum(a, 0, i) === sum(a, i + 1));
+
+
+
+function findEvenIndex(arr) {
+  return arr.findIndex((e,i,a)=> a.slice(0,i).reduce((p,c)=>p+c,0)==a.slice(i+1).reduce((p,c)=>p+c,0));
+}
