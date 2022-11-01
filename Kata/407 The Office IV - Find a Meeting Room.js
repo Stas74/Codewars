a67ff007a33d02e7153fdde30e2ace1f2bc07357
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-01 11:19:33 -0600

    Create 407 The Office IV - Find a Meeting Room.js

---
+++ Kata/407 The Office IV - Find a Meeting Room.js
@@ -0,0 +1,22 @@
+// The Office IV - Find a Meeting Room
+
+/*
+Your job at E-Corp is both boring and difficult. 
+It isn't made any easier by the fact that everyone constantly wants to have a meeting with you, 
+and that the meeting rooms are always taken!
+
+In this kata, you will be given an array. Each value represents a meeting room. Your job? 
+Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
+
+'X' --> busy
+'O' --> empty
+If all rooms are busy, return "None available!"
+*/
+
+function meeting(x) {
+  return x.includes("O") ? x.findIndex((val) => val == "O") : "None available!";
+}
+
+console.log(meeting(["X", "O", "X"])); // 1
+console.log(meeting(["O", "X", "X", "X", "X"])); // 0
+console.log(meeting(["X", "X", "X", "X", "X"])); // 'None available!'
