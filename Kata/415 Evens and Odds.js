f7d98baae7d29d181e5cc2aeee404fb78504ba6b
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-03 20:10:44 -0600

    Create 415 Evens and Odds.js

---
+++ Kata/415 Evens and Odds.js
@@ -0,0 +1,16 @@
+// Evens and Odds
+
+/*
+This kata is about converting numbers to their binary or hexadecimal representation:
+
+If a number is even, convert it to binary.
+If a number is odd, convert it to hex.
+Numbers will be positive. The hexadecimal string should be lowercased.
+*/
+
+function evensAndOdds(num){
+	return num % 2 != 0 ? num.toString(16) : num.toString(2)
+}
+
+console.log(evensAndOdds(2)); // 10
+console.log(evensAndOdds(47)); // 2f
