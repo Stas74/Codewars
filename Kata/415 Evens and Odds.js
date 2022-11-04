c4c89948fadd146e58a72555b75d326ffa9cd760
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-03 20:11:55 -0600

    Update 415 Evens and Odds.js

--- Kata/415 Evens and Odds.js
+++ Kata/415 Evens and Odds.js
@@ -14,3 +14,12 @@ function evensAndOdds(num){
 
 console.log(evensAndOdds(2)); // 10
 console.log(evensAndOdds(47)); // 2f
+
+
+
+const evensAndOdds = n => n.toString(n % 2 ? 16 : 2);
+
+
+function evensAndOdds(num){
+  return (num & 1) ? num.toString(16) : num.toString(2);
+}
