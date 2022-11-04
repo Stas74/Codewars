655b353901ea61577e992f7ee978fdd3157878fa
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-04 14:12:29 -0600

    Update 417 Word values.js

--- Kata/417 Word values.js
+++ Kata/417 Word values.js
@@ -24,3 +24,18 @@ function wordValue(a) {
 
 console.log(wordValue(["codewars","abc","xyz"])); // [88,12,225]
 console.log(wordValue(["abc abc","abc abc","abc","abc"])); // [12,24,18,24]
+
+
+
+function wordValue(a) {
+  return a.map((s, i) => (i + 1) * s.split``.reduce((s, n) => s + 1 + 'abcdefghijklmnopqrstuvwxyz'.indexOf(n), 0));
+}
+
+
+function wordValue(a) {
+  return a.map(
+    (s, i) => (i + 1) * [...s].reduce((a, c) => a + (c.charCodeAt(0) % 32), 0)
+  );
+}
+
+
