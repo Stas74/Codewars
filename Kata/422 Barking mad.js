9e3ef7862518ccff9c3ce8106827ca7db23cb6d1
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-07 12:54:08 -0700

    Update 422 Barking mad.js

--- Kata/422 Barking mad.js
+++ Kata/422 Barking mad.js
@@ -26,3 +26,36 @@ scoobydoo.bark = function() {
 
 console.log(snoopy.bark()); // "Woof"
 console.log(scoobydoo.bark()); // "Woof"
+
+
+
+
+
+class Dog {
+    constructor(breed) {
+        this.breed = breed;
+    }
+    
+    bark() {
+        return "Woof";
+    }
+}
+
+let snoopy = new Dog("Beagle");
+let scoobydoo = new Dog("Great Dane");
+
+
+
+
+
+function Dog (breed) {
+  this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+Dog.prototype.bark = function() {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
