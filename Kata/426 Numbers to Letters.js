456058c7ffd4d1b51cea5b400825a5182ac73b61
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-08 11:07:15 -0700

    Update 426 Numbers to Letters.js

--- Kata/426 Numbers to Letters.js
+++ Kata/426 Numbers to Letters.js
@@ -16,3 +16,22 @@ function switcher(x) {
 console.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8'])); // "codewars"
 console.log(switcher(['25','7','8','4','14','23','8','25','23','29','16','16','4'])); // "btswmdsbd"
 console.log(switcher(['4', '24'])); // "wc"
+
+
+
+const switcher=x=>x.reduce((a, b)=> a + " ?!abcdefghijklmnopqrstuvwxyz"[29-b],"")
+
+
+switcher= x => x.map(i => '_zyxwvutsrqponmlkjihgfedcba!? '[i]).join``
+
+function switcher(x)
+{
+  return x.map(a => 
+    {
+      if(a == 27) return "!";
+      if(a == 28) return "?";
+      if(a == 29) return " ";
+      return String.fromCharCode(97 + 26 - parseInt(a));
+      
+    }).join('');
+}
