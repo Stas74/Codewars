377d198847fb1e8723e4811efacf8c1549a955c1
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-10 13:08:30 -0700

    Create 436 Strong Number (Special Numbers Series #2).js

---
+++ Kata/436 Strong Number (Special Numbers Series #2).js
@@ -0,0 +1,40 @@
+// Strong Number (Special Numbers Series #2)
+
+/*
+Strong number is the number that the sum of the factorial of its digits is equal to number itself.
+
+For example, 145 is strong, since 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Task
+Given a number, Find if it is Strong or not and return either "STRONG!!!!" or "Not Strong !!".
+
+Notes
+Number passed is always Positive.
+Return the result as String
+Input >> Output Examples
+strong_num(1) ==> return "STRONG!!!!"
+Since, the sum of its digits' factorial (1) is equal to number itself, then its a Strong.
+
+strong_num(123) ==> return "Not Strong !!"
+Since the sum of its digits' factorial of 1! + 2! + 3! = 9 is not equal to number itself, then it's Not Strong .
+
+strong_num(2)  ==>  return "STRONG!!!!"
+Since the sum of its digits' factorial of 2! = 2 is equal to number itself, then its a Strong.
+
+strong_num(150) ==> return "Not Strong !!"
+Since the sum of its digits' factorial of 1! + 5! + 0! = 122 is not equal to number itself, Then it's Not Strong .
+*/
+
+function factorial(num) {
+  return num != 0 ? num * factorial(num - 1) : 1;
+}
+
+function strong(n) {
+  return String(n).split("").map(el => factorial(el)).reduce((prev, cur) => prev + cur, 0) == n ? "STRONG!!!!" : "Not Strong !!";
+}
+
+console.log(strong(1)); // "STRONG!!!!"
+console.log(strong(145)); // "STRONG!!!!"
+console.log(strong(7)); // "Not Strong !!"
+console.log(strong(93)); // "Not Strong !!"
+console.log(strong(104)); // "Not Strong !!"
