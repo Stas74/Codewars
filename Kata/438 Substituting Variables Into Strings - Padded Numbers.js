b466ae34a61772f35440c6de482a9969f3419c63
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-10 14:21:48 -0700

    Create 438 Substituting Variables Into Strings - Padded Numbers.js

---
+++ Kata/438 Substituting Variables Into Strings - Padded Numbers.js
@@ -0,0 +1,18 @@
+// Substituting Variables Into Strings: Padded Numbers
+
+/*
+Complete the solution so that it returns a formatted string. 
+The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+Example:
+
+solution(5) // should return "Value is 00005"
+*/
+
+function solution(value){
+  return `Value is ${String(value).padStart(5, "0")}`
+}
+
+console.log(solution(5)); // "Value is 00005"
+console.log(solution(1204)); // "Value is 01204"
+console.log(solution(0)); // "Value is 00000"
