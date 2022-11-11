9e6b67e725268abf4981661f817070aa5175afc7
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-11 11:18:10 -0700

    Update 440 ToLeetSpeak.js

--- Kata/440 ToLeetSpeak.js
+++ Kata/440 ToLeetSpeak.js
@@ -49,3 +49,21 @@ function toLeetSpeak(str) {
 console.log(toLeetSpeak("LEET")); // "1337"
 console.log(toLeetSpeak("CODEWARS")); // "(0D3W@R$"
 console.log(toLeetSpeak("HELLO WORLD")); // "#3110 W0R1D"
+
+
+
+
+const toLeetSpeak = s => s.replace(/[ABCEGHILOSTZ]/g, c => D[c]);
+const D = {
+  A: '@', B: '8', C: '(', E: '3',
+  G: '6', H: '#', I: '!', L: '1',
+  O: '0', S: '$', T: '7', Z: '2'
+};
+
+
+
+var translation = { A : '@',  B : '8',  C : '(',  E : '3', G : '6', H : '#',  I : '!',  L : '1',  O : '0',  S : '$',  T : '7',  Z : '2'
+}
+function toLeetSpeak(str) {
+  return str.split('').map( c => translation[c] || c).join('');
+}
