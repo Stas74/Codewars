4492c4baf5a5447aa127bee5d4604440020e627b
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-12 11:29:51 -0700

    Update 444 Holiday III - Fire on the boat.js

--- Kata/444 Holiday III - Fire on the boat.js
+++ Kata/444 Holiday III - Fire on the boat.js
@@ -17,3 +17,10 @@ function fireFight(s){
 console.log(fireFight("Mast Deck Engine Water Fire")); // "Mast Deck Engine Water ~~"
 console.log(fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain")); 
 // "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain"
+
+
+
+const fireFight = s =>
+  s.replace(/Fire/g, `~~`);
+
+
