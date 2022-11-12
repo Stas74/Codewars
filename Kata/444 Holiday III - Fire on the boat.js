3bd73e135b2cc621f5b94e8f03aa50b301ad6d88
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-12 11:27:05 -0700

    Create 444 Holiday III - Fire on the boat.js

---
+++ Kata/444 Holiday III - Fire on the boat.js
@@ -0,0 +1,19 @@
+// Holiday III - Fire on the boat
+
+/*
+Enjoying your holiday, you head out on a scuba diving trip!
+
+Disaster!! The boat has caught fire!!
+
+You will be provided a string that lists many boat related items. 
+If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". 
+Then return the string.
+*/
+
+function fireFight(s){
+  return s.split(" ").map(el => el == "Fire" ? "~~" : el).join(" ")
+}
+
+console.log(fireFight("Mast Deck Engine Water Fire")); // "Mast Deck Engine Water ~~"
+console.log(fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain")); 
+// "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain"
