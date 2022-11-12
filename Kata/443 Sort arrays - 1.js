87968e87258909ab042fd666c0c2c50875f1515c
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-12 10:56:02 -0700

    Create 443 Sort arrays - 1.js

---
+++ Kata/443 Sort arrays - 1.js
@@ -0,0 +1,12 @@
+// Sort arrays - 1
+
+/*
+Just a simple sorting usage. 
+Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+*/
+
+sortme = function( names ){
+  return names.sort()
+}
+
+console.log(sortme(['one', 'two', 'three' ])); // ["one", "three", "two"]
