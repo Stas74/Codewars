44554f9f05ed0596e0568389b673571530a4787a
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-12 12:01:41 -0700

    Create 446 Series of integers from m to n.js

---
+++ Kata/446 Series of integers from m to n.js
@@ -0,0 +1,23 @@
+// Series of integers from m to n
+
+/*
+Write a function that accepts two arguments and generates a sequence containing 
+the integers from the first argument to the second inclusive.
+
+Input
+Pair of integers greater than or equal to 0. The second argument will always be greater than or equal to the first.
+
+Example
+generateIntegers(2, 5) // --> [2, 3, 4, 5]
+*/
+
+function generateIntegers(m, n) {
+  let finArr = [];
+  while (m <= n) {
+    finArr.push(m);
+    m++;
+  }
+  return finArr;
+}
+
+console.log(generateIntegers(2, 5)); // [2, 3, 4, 5]
