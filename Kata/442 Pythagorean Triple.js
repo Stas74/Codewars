81582f48ea0e0c6a9658024d5637ea1a76946add
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-12 10:42:19 -0700

    Create 442 Pythagorean Triple.js

---
+++ Kata/442 Pythagorean Triple.js
@@ -0,0 +1,27 @@
+// Pythagorean Triple
+
+/*
+Given an array of 3 non-negative integers a, b and c, determine if they form a pythagorean triple.
+
+A pythagorean triple is formed when:
+
+c2 = a2 + b2
+where c is the largest value of a, b, c.
+
+For example: a = 3, b = 4, c = 5 forms a pythagorean triple, because 52 = 32 + 42
+
+Return Values
+1 if a, b and c form a pythagorean triple
+0 if a, b and c do not form a pythagorean triple
+For Python: return True or False
+For JavaScript: return true or false
+*/
+
+function isPythagoreanTriple(integers) {
+  integers.sort((a,b) => (a - b))
+  return integers[2] ** 2 == integers[0] ** 2 + integers[1] ** 2
+}
+
+console.log(isPythagoreanTriple([3, 4, 5])); // true
+console.log(isPythagoreanTriple([3, 5, 9])); // false
+console.log(isPythagoreanTriple([72, 78, 30])); // true
