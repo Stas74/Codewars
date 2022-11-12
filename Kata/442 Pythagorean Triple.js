d6703d5bb45d6f23dfc48c151aa061e16d4e5c55
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-12 10:44:27 -0700

    Update 442 Pythagorean Triple.js

--- Kata/442 Pythagorean Triple.js
+++ Kata/442 Pythagorean Triple.js
@@ -25,3 +25,13 @@ function isPythagoreanTriple(integers) {
 console.log(isPythagoreanTriple([3, 4, 5])); // true
 console.log(isPythagoreanTriple([3, 5, 9])); // false
 console.log(isPythagoreanTriple([72, 78, 30])); // true
+
+
+
+function isPythagoreanTriple(nums) {
+  const [a,b,c] = nums.sort((a,b) => a - b)
+
+  return a**2 + b**2 === c**2
+}
+
+
