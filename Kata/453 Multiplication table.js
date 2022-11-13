df2d41be40ac7666ebf914388c5f9541a1d29836
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-13 12:34:17 -0700

    Update 453 Multiplication table.js

--- Kata/453 Multiplication table.js
+++ Kata/453 Multiplication table.js
@@ -24,3 +24,25 @@ multiplicationTable = function (size) {
 };
 
 console.log(multiplicationTable(3)); // [[1,2,3], [2,4,6], [3,6,9]]
+
+
+
+
+
+multiplicationTable = function(size) {
+
+  return Array.apply(null, new Array(size)).map(function(val, i) {
+    return Array.apply(null, new Array(size)).map(function(val, j) {
+      return (i+1) * (j+1);
+    });
+  });
+}
+
+
+
+multiplicationTable = function(n) {
+  return Array.from({length:n},(_,i)=>Array.from({length:n},(_,j)=>(i+1)*(j+1)))
+}
+
+
+const multiplicationTable = s => Array(s).fill().map((a, i) => Array(s).fill().map((e, j) => (1 + i) * (1 + j)))
