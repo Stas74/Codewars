1ce3787f0b11c472ab1cda78927f8992e29fc667
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-13 10:26:36 -0700

    Update 450 Training JS #17 Methods of String object--indexOf(), lastIndexOf() and search().js

--- Kata/450 Training JS #17 Methods of String object--indexOf(), lastIndexOf() and search().js
+++ Kata/450 Training JS #17 Methods of String object--indexOf(), lastIndexOf() and search().js
@@ -91,3 +91,8 @@ function firstToLast(str, c) {
 console.log(firstToLast("ababc", "a")); // 2
 console.log(firstToLast("ababc", "c")); // 0
 console.log(firstToLast("ababc", "d")); // -1
+
+
+
+const firstToLast = (str, c) =>
+  str.search(c) < 0 ? -1 : str.lastIndexOf(c) - str.indexOf(c);
