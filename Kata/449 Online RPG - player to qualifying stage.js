54b009ac8ea273a153a7ca4bcc2f6d7b1e24fb67
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-13 10:06:12 -0700

    Update 449 Online RPG - player to qualifying stage.js

--- Kata/449 Online RPG - player to qualifying stage.js
+++ Kata/449 Online RPG - player to qualifying stage.js
@@ -22,3 +22,8 @@ function playerRankUp (points) {
 
 console.log(playerRankUp(64)); // false
 console.log(playerRankUp(101)); // "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+
+
+
+const playerRankUp = points =>
+  points >= 100 && `Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.`;
