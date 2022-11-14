00d2cd8ed30b240f25b6d443caf2a3d8b62aea7d
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-14 12:11:04 -0700

    Update 455 Take an Arrow to the knee, Functionally.js

--- Kata/455 Take an Arrow to the knee, Functionally.js
+++ Kata/455 Take an Arrow to the knee, Functionally.js
@@ -20,3 +20,6 @@ var ArrowFunc = function(arr) {
 
 console.log(ArrowFunc([84,101,115,116])); // 'Test'
 console.log(ArrowFunc([70,85,83,32,82,79,72,32,68,65,72])); // 'FUS ROH DAH'
+
+
+const ArrowFunc = (arr) => String.fromCharCode(...arr);
