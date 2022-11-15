7ff535fa265446651248bc2ca87c47af14b0b611
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-15 14:14:41 -0700

    Update 458 Training JS #15 Methods of Number object--toFixed(), toExponential() and toPrecision().js

--- Kata/458 Training JS #15 Methods of Number object--toFixed(), toExponential() and toPrecision().js
+++ Kata/458 Training JS #15 Methods of Number object--toFixed(), toExponential() and toPrecision().js
@@ -104,3 +104,9 @@ function howManySmaller(arr, n) {
 console.log(howManySmaller([1.234, 1.235, 1.228], 1.24)); // 2
 console.log(howManySmaller([1.1888, 1.1868, 1.1838], 1.19)); // 1
 console.log(howManySmaller([3.1288, 3.1212, 3.1205], 3.1212)); // 2
+
+
+
+
+const howManySmaller = (arr, n) => arr.reduce((a, b) => a + (b.toFixed(2) < n), 0);
+
