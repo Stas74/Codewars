647deb124edda6f60cf8f994ab628fd1d03e44e5
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-15 14:27:49 -0700

    Create 459  Age Range Compatibility Equation.js

---
+++ Kata/459  Age Range Compatibility Equation.js
@@ -0,0 +1,44 @@
+// Age Range Compatibility Equation
+
+/*
+Everybody knows the classic "half your age plus seven" dating rule that a lot of people follow (including myself). 
+It's the 'recommended' age range in which to date someone.
+
+age / 2 + 7 = Min
+(age - 7) * 2 = Max
+
+minimum age <= your age <= maximum age #Task
+
+Given an integer (1 <= n <= 100) representing a person's age, return their minimum and maximum age range.
+
+This equation doesn't work when the age <= 14, so use this equation instead:
+
+min = age - 0.10 * age
+max = age + 0.10 * age
+
+You should floor all your answers so that an integer is given instead of a float (which doesn't represent age). 
+Return your answer in the form [min]-[max]
+
+##Examples:
+
+age = 27   =>   20-40
+age = 5    =>   4-5
+age = 17   =>   15-20
+
+*/
+
+function datingRange(age) {
+  if (age <= 14) {
+    let minAge = age - 0.1 * age;
+    let maxAge = age + 0.1 * age;
+    return `${Math.floor(minAge)}-${Math.floor(maxAge)}`;
+  } else {
+    let minAge = age / 2 + 7;
+    let maxAge = (age - 7) * 2;
+    return `${Math.floor(minAge)}-${Math.floor(maxAge)}`;
+  }
+}
+
+console.log(datingRange(17)); // "15-20"
+console.log(datingRange(40)); // "27-66"
+console.log(datingRange(15)); // "14-16"
