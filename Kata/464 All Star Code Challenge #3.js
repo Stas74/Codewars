d8e755bd86b7d89048b732754da91f2ad55d869f
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-17 12:01:38 -0700

    Create 464 All Star Code Challenge #3.js

---
+++ Kata/464 All Star Code Challenge #3.js
@@ -0,0 +1,16 @@
+// All Star Code Challenge #3
+
+/*
+Create a function, called removeVowels (or remove_vowels), 
+that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
+
+removeVowels("drake") // => "drk"
+removeVowels("aeiou") // => ""
+*/
+
+var removeVowels = function (str) {
+  return str.replace(/[aeiou]/g, "");
+};
+
+console.log(removeVowels("drake")); // "drk"
+console.log(removeVowels("aeiou")); // ""
