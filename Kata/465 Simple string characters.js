76ee0344240e95730a95c33c486f4b8d0a02d13a
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-17 14:08:30 -0700

    Update 465 Simple string characters.js

--- Kata/465 Simple string characters.js
+++ Kata/465 Simple string characters.js
@@ -22,3 +22,52 @@ function solve(s) {
 
 console.log(solve("aAbBcC")); // [3,3,0,0]
 console.log(solve("Codewars@codewars123.com")); // [1,18,3,2]
+
+
+
+const solve = x => {
+  let u = x.match(/[A-Z]/g)||[]
+  let d = x.match(/[a-z]/g)||[]
+  let n = x.match(/[0-9]/g)||[]
+  let s = x.match(/[^A-Z0-9]/gi)||[]   // !!!!!!
+  return [u.length, d.length, n.length, s.length]
+}
+
+
+
+
+
+const solve = (string) => (
+  Object.values([...string].reduce((acc, char) => {
+    if (/[A-Z]/.test(char)) {
+      acc.upper++;
+    } else if (/[a-z]/.test(char)) {
+      acc.lower++;
+    } else if (/[0-9]/.test(char)) {
+      acc.number++;
+    } else {
+      acc.other++;
+    }
+    return acc;
+  }, { upper: 0, lower: 0, number: 0, other: 0 }))
+);
+
+
+
+function solve(s){
+  let a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let b = "abcdefghijklmnopqrstuvwxyz";
+  let c = '1234567890';
+  let up = 0;
+  let low = 0;
+  let num = 0;
+  let char = 0;
+  s = s.split('');
+  for(let i = 0; i < s.length; i++){
+    if(a.includes(s[i])){up++};
+    if(b.includes(s[i])){low++};
+    if(c.includes(s[i])){num++};
+    if(!a.includes(s[i]) && !b.includes(s[i]) && !c.includes(s[i])){char++};
+  }
+  return [up,low,num,char];
+}
