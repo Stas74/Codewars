bb88800a7ac89059d6f36d1e68c3471ff3d0d6c5
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-21 13:01:23 -0700

    Update 477 Safen User Input Part I - htmlspecialchars.js

--- Kata/477 Safen User Input Part I - htmlspecialchars.js
+++ Kata/477 Safen User Input Part I - htmlspecialchars.js
@@ -1,7 +1,13 @@
 // Safen User Input Part I - htmlspecialchars
 
 /*
-You are a(n) novice/average/experienced/professional/world-famous Web Developer (choose one) who owns a(n) simple/clean/slick/beautiful/complicated/professional/business website (choose one or more) which contains form fields so visitors can send emails or leave a comment on your website with ease. However, with ease comes danger. Every now and then, a hacker visits your website and attempts to compromise it through the use of XSS (Cross Site Scripting). This is done by injecting script tags into the website through form fields which may contain malicious code (e.g. a redirection to a malicious website that steals personal information).
+You are a(n) novice/average/experienced/professional/world-famous Web Developer (choose one) who owns 
+a(n) simple/clean/slick/beautiful/complicated/professional/business website (choose one or more) 
+which contains form fields so visitors can send emails or leave a comment on your website with ease. 
+However, with ease comes danger. Every now and then, a hacker visits your website and attempts 
+to compromise it through the use of XSS (Cross Site Scripting). 
+This is done by injecting script tags into the website through form fields which may contain malicious code 
+(e.g. a redirection to a malicious website that steals personal information).
 
 Mission
 Your mission is to implement a function that converts the following potentially harmful characters:
@@ -20,3 +26,31 @@ function htmlspecialchars(formData) {
 
 console.log(htmlspecialchars("<h2>Hello World</h2>")); // "&lt;h2&gt;Hello World&lt;/h2&gt;"
 console.log(htmlspecialchars("Hello, how would you & I fare?")); // "Hello, how would you &amp; I fare?"
+
+
+
+function htmlspecialchars(formData) {
+  return formData.replace(/&/g, "&amp;")
+                 .replace(/"/g, "&quot;")
+                 .replace(/</g, "&lt;")
+                 .replace(/>/g, "&gt;");
+}
+
+
+function htmlspecialchars(formData) {
+  return formData.replace(/[<>"&]/g, ch => ({"<":"&lt;",">":"&gt;","\"":"&quot;","&":"&amp;"}[ch]));
+}
+
+
+
+function htmlspecialchars(formData) {
+    const dict = {
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        '&': '&amp;',
+    };
+    return formData.replace(/[<>"&]/g, char => dict[char]);
+}
+
+
