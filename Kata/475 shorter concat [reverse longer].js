2e094387c0a2aafc41dab655402a1ae73948c460
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-21 11:30:57 -0700

    Create 475 shorter concat [reverse longer].js

---
+++ Kata/475 shorter concat [reverse longer].js
@@ -0,0 +1,19 @@
+// shorter concat [reverse longer]
+
+/*
+Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+
+In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+
+Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+If a and b have the same length treat a as the longer producing b+reverse(a)+b
+
+*/
+
+function shorter_reverse_longer(a,b){
+  return b.length <= a.length ? `${b}${a.split("").reverse().join("")}${b}` : `${a}${b.split("").reverse().join("")}${a}`
+}
+
+console.log(shorter_reverse_longer("first", "abcde")); // "abcdetsrifabcde"
+console.log(shorter_reverse_longer("hello", "bau")); // "bauollehbau"
+console.log(shorter_reverse_longer("fghi", "abcde")); // "fghiedcbafghi"
