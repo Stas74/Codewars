74ad9bd01a1eca62cad6240e58b78b4dc91c2251
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-20 18:07:03 -0700

    Create 474 Sort Out The Men From Boys.js

---
+++ Kata/474 Sort Out The Men From Boys.js
@@ -0,0 +1,28 @@
+// Sort Out The Men From Boys
+
+/*
+Given an array/list [] of n integers , Separate The even numbers from the odds , or Separate the men from the boys
+
+Notes
+Return an array/list where Even numbers come first then odds
+
+Since , Men are stronger than Boys , Then Even numbers in ascending order While odds in descending .
+
+Array/list size is at least 4 .
+
+Array/list numbers could be a mixture of positives , negatives .
+
+Have no fear , It is guaranteed that no Zeroes will exists
+Repetition of numbers in the array/list could occur , So (duplications are not included when separating).
+
+*/
+
+function menFromBoys(arr) {
+  let even = [...new Set(arr)].filter((el) => el % 2 == 0).sort((a, b) => a - b);
+  let odd = [...new Set(arr)].filter((el) => el % 2 != 0).sort((a, b) => b - a);
+  return even.concat(odd);
+}
+
+console.log(menFromBoys([7, 3, 14, 17])); // [14,17,7,3]
+console.log(menFromBoys([2, 43, 95, 90, 37])); // [2,90,95,43,37]
+console.log(menFromBoys([20, 33, 50, 34, 43, 46])); // [20,34,46,50,43,33]
