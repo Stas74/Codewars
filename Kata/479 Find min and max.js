6b8fa4db1db1e4b5385e5d64960d7a234072fcf1
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-22 11:59:53 -0700

    Update 479 Find min and max.js

--- Kata/479 Find min and max.js
+++ Kata/479 Find min and max.js
@@ -12,3 +12,11 @@ function getMinMax(arr) {
 console.log(getMinMax([1])); // [1, 1]
 console.log(getMinMax([1, 2])); // [1, 2]
 console.log(getMinMax([2, 1])); // [1, 2]
+
+
+
+function getMinMax(arr){
+  return [Math.min(...arr),Math.max(...arr)];
+};
+
+
