124d818ce6d8cea4c27cdd3b57fb478d57bb6000
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-23 12:41:02 -0700

    Create 482 Number Of Occurrences.js

---
+++ Kata/482 Number Of Occurrences.js
@@ -0,0 +1,20 @@
+// Number Of Occurrences
+
+/*
+Write a function that returns the number of occurrences of an element in an array.
+
+Examples
+var arr = [0, 1, 2, 2, 3];
+arr.numberOfOccurrences(0) === 1;
+arr.numberOfOccurrences(4) === 0;
+arr.numberOfOccurrences(2) === 2;
+arr.numberOfOccurrences("a") === 0;
+*/
+
+Array.prototype.numberOfOccurrences = function (num) {
+  return this.filter((el) => el == num).length;
+};
+
+var arr = [0, 4, 4, 3];
+
+console.log(arr.numberOfOccurrences(4)); //  2
