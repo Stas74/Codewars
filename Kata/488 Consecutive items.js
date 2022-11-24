168b120dc5526996ceac45750c87e4a0bb4a699c
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-24 13:37:03 -0700

    Update 488 Consecutive items.js

--- Kata/488 Consecutive items.js
+++ Kata/488 Consecutive items.js
@@ -20,3 +20,25 @@ function consecutive(arr, a, b) {
 console.log(consecutive([1, 3, 5, 7], 3, 7)); //  false
 console.log(consecutive([1, 3, 5, 7], 3, 1)); //  true
 console.log(consecutive([1, 6, 9, -3, 4, -78, 0], -3, 4)); //  true
+
+
+
+
+function consecutive(arr, a, b) {
+  return Math.abs(arr.indexOf(a) - arr.indexOf(b)) == 1;
+}
+
+
+
+const consecutive = (arr, a, b) =>
+  new RegExp(`${a},${b}|${b},${a}`).test(arr);
+
+
+
+
+function consecutive(arr, a, b, indx = arr.indexOf(a)) {
+  if( arr[indx+1] === b || arr[indx-1] === b){
+    return true
+  }
+  return false
+}
