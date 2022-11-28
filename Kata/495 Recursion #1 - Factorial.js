c9fd7635ea39c24d03309d442fec3ed1fb8f073a
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-28 13:50:15 -0700

    Create 495 Recursion #1 - Factorial.js

---
+++ Kata/495 Recursion #1 - Factorial.js
@@ -0,0 +1,28 @@
+// Recursion #1 - Factorial
+
+/*
+##1 - Factorial
+
+In mathematics, the factorial of a non-negative integer n, denoted by n!, 
+is the product of all positive integers less than or equal to n. For example,
+
+5! = 5 * 4 * 3 * 2 * 1 = 120.
+
+The value of 0! is 1.
+
+#Your task
+
+You have to create the function factorial that receives n and returns n!. You have to use recursion.
+*/
+
+const factorial = (n) => {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
+
+console.log(factorial(0)); //  1
+console.log(factorial(2)); //  2
+console.log(factorial(3)); //  6
