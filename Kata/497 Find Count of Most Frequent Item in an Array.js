b37f5d3c30c28e928fe215f3233e63103769d19a
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-29 13:47:07 -0700

    Update 497 Find Count of Most Frequent Item in an Array.js

--- Kata/497 Find Count of Most Frequent Item in an Array.js
+++ Kata/497 Find Count of Most Frequent Item in an Array.js
@@ -22,3 +22,27 @@ function mostFrequentItemCount(collection) {
 console.log(mostFrequentItemCount([3, -1, -1])); //  2
 console.log(mostFrequentItemCount([3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3])); //  5
 console.log(mostFrequentItemCount([])); //  0
+
+
+
+function mostFrequentItemCount(c) {
+  return c.length ? Math.max(...c.map(x=>c.filter(y=>y==x).length)) : 0;
+}
+
+
+function mostFrequentItemCount(collection) {
+  let frequency = {}, max = 0;
+  for(var v in collection) {
+    frequency[collection[v]] = (frequency[collection[v]] || 0) + 1;
+    if(frequency[collection[v]] > max) max = frequency[collection[v]];      
+  }
+  return max;
+}
+
+
+
+const _ = require('lodash');
+const mostFrequentItemCount = collection => 
+  collection.length 
+    ? _(collection).countBy().values().max().valueOf() 
+    : 0;
