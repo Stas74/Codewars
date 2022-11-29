8a9c397147295a8a44ae500a8bdf305acd94cf42
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-29 14:26:48 -0700

    Create 499 Digits explosion.js

---
+++ Kata/499 Digits explosion.js
@@ -0,0 +1,23 @@
+// Digits explosion
+
+/*
+Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+Examples
+
+explode("312")
+should return :
+"333122"
+
+explode("102269")
+should return :
+"12222666666999999999"
+*/
+
+function explode(s) {
+  return s.split("").map(el => el = el.repeat(+el)).join("")
+}
+
+console.log(explode("312")); // "333122"
+console.log(explode("123")); // "122333"
+console.log(explode("00")); // ""
