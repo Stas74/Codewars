788765ae079efde63d1b03d16a343d769780bad7
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-11-30 13:51:54 -0700

    Update 502 Bumps in the Road.js

--- Kata/502 Bumps in the Road.js
+++ Kata/502 Bumps in the Road.js
@@ -16,3 +16,14 @@ function bump(x) {
 console.log(bump("n")); //  "Woohoo!"
 console.log(bump("__nn_nnnn__n_n___n____nn__nnn")); //  "Woohoo!"
 console.log(bump("_nnnnnnn_n__n______nn__nn_nnn")); //  "Car Dead"
+
+
+
+const bump = x => x.split('n').length > 16 ? "Car Dead" : "Woohoo!"
+
+
+function bump(x){
+  return (x.match(/n/g) || []).length > 15 ? "Car Dead" : "Woohoo!";
+}
+
+
