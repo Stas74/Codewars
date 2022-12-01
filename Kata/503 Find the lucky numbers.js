519fbfb0c78f4318292993d65df2c9e4a1579a49
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-01 10:20:23 -0700

    Create 503 Find the lucky numbers.js

---
+++ Kata/503 Find the lucky numbers.js
@@ -0,0 +1,19 @@
+// Find the lucky numbers
+
+/*
+Write a function filterLucky/filter_lucky() that accepts a list of integers and 
+filters the list to only include the elements that contain the digit 7.
+
+For example,
+
+ghci> filterLucky [1,2,3,4,5,6,7,68,69,70,15,17]
+[7,70,17]
+Don't worry about bad input, you will always receive a finite list of integers.
+*/
+
+var filterLucky = (x) => {
+  return x.filter(el => String(el).includes("7"));
+};
+
+console.log(filterLucky([1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17])); //  [7,70,17]
+console.log(filterLucky([71, 9907, 69])); //  [71,9907]
