b49e6d62c9dd69e0a1061532d5290d304606391b
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-02 13:40:45 -0700

    Create 508 Exclamation marks series #13 - Count the number of exclamation marks and question marks, return the product.js

---
+++ Kata/508 Exclamation marks series #13 - Count the number of exclamation marks and question marks, return the product.js
@@ -0,0 +1,25 @@
+// Exclamation marks series #13 - Count the number of exclamation marks and question marks, return the product
+
+/*
+Count the number of exclamation marks and question marks, return the product.
+
+Examples
+Product("") == 0
+product("!") == 0
+Product("!ab? ?") == 2
+Product("!!") == 0
+Product("!??") == 2
+Product("!???") == 3
+Product("!!!??") == 6
+Product("!!!???") == 9
+Product("!???!!") == 9
+Product("!????!!!?") == 20
+*/
+
+function product(string) {
+  return string.split("").filter(e => e == "!").length * string.split("").filter(e => e == "?").length
+}
+
+console.log(product("!!")); //  0
+console.log(product("!??")); //  2
+console.log(product("!!!??")); //  6
