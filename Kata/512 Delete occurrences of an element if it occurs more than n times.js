7777cf1cdacfa00df4e7314d5896957dd970d971
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-05 14:55:18 -0700

    Update 512 Delete occurrences of an element if it occurs more than n times.js

--- Kata/512 Delete occurrences of an element if it occurs more than n times.js
+++ Kata/512 Delete occurrences of an element if it occurs more than n times.js
@@ -33,3 +33,19 @@ function deleteNth(arr, n) {
 
 console.log(deleteNth([20, 37, 20, 21], 1)); //  [20,37,21]
 console.log(deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3)); //  [1, 1, 3, 3, 7, 2, 2, 2]
+
+
+
+
+function deleteNth(arr,x) {
+  var cache = {};
+  return arr.filter(function(n) {
+    cache[n] = (cache[n]||0) + 1;
+    return cache[n] <= x;
+  });
+}
+
+
+
+
+const deleteNth = (arr,x) => arr.filter((e,i)=> arr.slice(0,i).filter(f=>f== e).length < x );
