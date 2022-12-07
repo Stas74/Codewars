036e36263ee53790b759f7d78cc501f73e69e0c3
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-07 15:58:30 -0700

    Create 520 Formatting decimal places #1.js

---
+++ Kata/520 Formatting decimal places #1.js
@@ -0,0 +1,24 @@
+// Formatting decimal places #1
+
+/*
+Each floating-point number should be formatted that only the first two decimal places are returned. 
+You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Don't round the numbers! Just cut them after two decimal places!
+
+Right examples:  
+32.8493 is 32.84  
+14.3286 is 14.32
+
+Incorrect examples (e.g. if you round the numbers):  
+32.8493 is 32.85  
+14.3286 is 14.33
+*/
+
+function twoDecimalPlaces(number) {
+  const dotPos = String(number).indexOf(".");
+  return +String(number).slice(0, dotPos + 3);
+}
+
+console.log(twoDecimalPlaces(10.1289767789)); //  10.12
+console.log(twoDecimalPlaces(-7488.83485834983)); //  -7488.83
