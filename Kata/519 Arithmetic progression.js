866a31108eec15571a469018e9dcd9424f2fc4c8
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-07 15:28:06 -0700

    Create 519 Arithmetic progression.js

---
+++ Kata/519 Arithmetic progression.js
@@ -0,0 +1,27 @@
+// Arithmetic progression
+
+/*
+In your class, you have started lessons about arithmetic progression. 
+Since you are also a programmer, you have decided to write a function that will return 
+the first n elements of the sequence with the given common difference d and first element a. 
+Note that the difference may be zero!
+
+The result should be a string of numbers, separated by comma and space.
+
+Example
+# first element: 1, difference: 2, how many: 5
+arithmetic_sequence_elements(1, 2, 5) == "1, 3, 5, 7, 9"
+*/
+
+function arithmeticSequenceElements(a, d, n) {
+  const finArr = [a];
+  for (let i = 1; i < n; i++) {
+    a = finArr[i - 1];
+    finArr.push(a + d);
+  }
+  return finArr.join(", ");
+}
+
+console.log(arithmeticSequenceElements(1, 2, 5)); //  "1, 3, 5, 7, 9"
+console.log(arithmeticSequenceElements(1, 0, 5)); //  "1, 1, 1, 1, 1"
+console.log(arithmeticSequenceElements(1, -3, 10)); //  "1, -2, -5, -8, -11, -14, -17, -20, -23, -26
