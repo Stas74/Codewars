abbb5a3df4c778381fc7498221ad3773e0959463
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-07 15:30:08 -0700

    Update 519 Arithmetic progression.js

--- Kata/519 Arithmetic progression.js
+++ Kata/519 Arithmetic progression.js
@@ -25,3 +25,23 @@ function arithmeticSequenceElements(a, d, n) {
 console.log(arithmeticSequenceElements(1, 2, 5)); //  "1, 3, 5, 7, 9"
 console.log(arithmeticSequenceElements(1, 0, 5)); //  "1, 1, 1, 1, 1"
 console.log(arithmeticSequenceElements(1, -3, 10)); //  "1, -2, -5, -8, -11, -14, -17, -20, -23, -26
+
+
+
+
+function arithmeticSequenceElements(a,r,n) {
+  var ret = [a]
+  while (--n) ret.push(a+=r);
+  return ret.join(', ')
+}
+
+
+
+function arithmeticSequenceElements(a, r, n) {
+  return Array.from({length: n}, (_, i) => a + r * i).join(', ');
+}
+
+
+
+const arithmeticSequenceElements = (a, r, n) =>
+  [...Array(n)].map((_, idx) => a + r * idx).join(`, `);
