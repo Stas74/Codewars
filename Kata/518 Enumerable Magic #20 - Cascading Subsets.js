7fe3e448331eb582b1237b784a4453b28bea09a1
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-07 15:08:26 -0700

    Update 518 Enumerable Magic #20 - Cascading Subsets.js

--- Kata/518 Enumerable Magic #20 - Cascading Subsets.js
+++ Kata/518 Enumerable Magic #20 - Cascading Subsets.js
@@ -28,3 +28,38 @@ console.log(eachCons(lst, 2)); //  [[3,5], [5,8], [8,13]]
 console.log(eachCons(lst, 3)); //  [[3,5,8], [5,8,13]]
 console.log(eachCons([], 3)); // []
 
+
+
+
+function eachCons(array, n) {
+  let res = [];
+  
+  for(let i = 0; i < array.length; i++){
+    res.push(array.slice(i,i + n));
+  }
+	
+  return res.filter(e => e.length === n);
+}
+
+
+
+function eachCons(array, n) {
+  let cascadingSubset = [];
+  for( i = 0; i <= array.length - n; i++){
+    cascadingSubset.push(array.slice(i, i + n ));
+  }
+	return cascadingSubset;
+}
+
+
+
+function eachCons(array, n) {
+  const newArr = [];
+  for (let a=n, i=0; a <= array.length; i++, a++){
+    newArr.push(array.slice(i,a))
+  }
+  return newArr
+}
+
+
+const eachCons = (l,n) => [...Array(Math.max(0,l.length-n+1)).keys()].map(i=>l.slice(i,i+n));
