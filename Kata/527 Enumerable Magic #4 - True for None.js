03e128e66079932824c7500e9aa85a9e429e2d65
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-09 13:44:03 -0700

    Create 527 Enumerable Magic #4 - True for None.js

---
+++ Kata/527 Enumerable Magic #4 - True for None.js
@@ -0,0 +1,16 @@
+// Enumerable Magic #4 - True for None?
+
+/*
+Create a method none? (JS none) that accepts an array and a block (JS: a function), 
+and returns true if the block (/function) returns true for none of the items in the array. 
+An empty list should return true.
+*/
+
+function none(arr, fun) {
+  return arr.filter(el => fun(el)).length == 0 ? true : false;
+}
+
+console.log(none([1,2,3,4,5], function(item){ return item > 5 })); // true
+console.log(none([1,2,3,4,5], function(item){ return item > 4 })); // false
+
+
