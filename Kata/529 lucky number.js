de43407266cba50184cbd366fa528b410dd830a8
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-11 10:59:07 -0700

    Create 529 lucky number.js

---
+++ Kata/529 lucky number.js
@@ -0,0 +1,18 @@
+// lucky number
+
+/*
+Write a function to find if a number is lucky or not. 
+If the sum of all digits is 0 or multiple of 9 then the number is lucky.
+
+1892376 => 1+8+9+2+3+7+6 = 36. 36 is divisible by 9, hence number is lucky.
+
+Function will return true for lucky numbers and false for others.
+*/
+
+function isLucky(n) {  
+  return n == 0 ? true : String(n).split("").reduce((prev, cur) => prev + (+cur), 0) % 9  == 0 ? true : false;
+}
+
+console.log(isLucky(1892376)) // true
+console.log(isLucky(189237)) // false
+
