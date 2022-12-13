7ecb894ff54217a23ad7b9b6b5faed26fcf84f0c
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-13 15:27:36 -0700

    Update 534  Nice Array.js

--- Kata/534  Nice Array.js
+++ Kata/534  Nice Array.js
@@ -33,3 +33,22 @@ function isNice(arr) {
 
 console.log(isNice([2, 10, 9, 3])); // true
 console.log(isNice([3, 4, 5, 7])); // false
+
+
+
+function isNice(arr){
+  return !!arr.length && arr.every(x => arr.some(y => y === x - 1 ||  y === x + 1));
+}
+
+
+const isNice = arr => Boolean(arr.length) && arr.every(e=> arr.includes(e-1) || arr.includes(e+1));
+
+
+function isNice(arr){
+    var counter = 0
+    for(var i = 0; i < arr.length; i++) {
+        if (arr.includes(arr[i] - 1) || arr.includes(arr[i] + 1)) counter ++
+    }
+    if (counter == arr.length && arr.length != 0) return true
+    return false
+}
