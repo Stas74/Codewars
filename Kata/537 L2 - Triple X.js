583c4a43a8188f4d850a5a125cd5c990aaa92903
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-14 11:00:06 -0700

    Update 537 L2 - Triple X.js

--- Kata/537 L2 - Triple X.js
+++ Kata/537 L2 - Triple X.js
@@ -21,3 +21,15 @@ console.log(tripleX("abraxxxas")); // true
 console.log(tripleX("xoxotrololololololoxxx")); // false
 console.log(tripleX("soft kitty, warm kitty, xxxxx")); // true
 console.log(tripleX("softx kitty, warm kitty, xxxxx")); // false
+
+
+
+
+const tripleX = str => /^[^x]*xxx/.test(str);
+
+
+function tripleX(str){
+  return str.substr(str.indexOf('x')+1,2) === 'xx';
+}
+
+
