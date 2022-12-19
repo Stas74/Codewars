b3df1b951a92e5b1bfbd4861971a9de5abde1195
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-19 16:23:00 -0700

    Create Enumerable Magic #2 - True for Any.js

---
+++ Kata/Enumerable Magic #2 - True for Any.js
@@ -0,0 +1,14 @@
+// Enumerable Magic #2 - True for Any?
+
+/*
+Create an any? (JS: any) function that accepts an array and a block (JS: function), 
+and returns true if the block (/function) returns true for any item in the array. 
+If the array is empty, the function should return false.
+*/
+
+function any(arr, fun){
+  return arr.map(el => fun(el)).includes(true);
+}
+
+console.log(any([1,2,3,4], function(v,i) {return v > 3})); // true
+console.log(any([1,2,3,4], function(v,i) {return v > 4})); // false
