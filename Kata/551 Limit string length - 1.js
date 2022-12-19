8408aa802834c1cf136cbf5892b93bc00135a92f
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-19 15:51:59 -0700

    Create 551 Limit string length - 1.js

---
+++ Kata/551 Limit string length - 1.js
@@ -0,0 +1,20 @@
+// Limit string length - 1
+
+/*
+The function must return the truncated version of the given string up to the given limit followed by "..." 
+if the result is shorter than the original. Return the same string if nothing was truncated.
+
+Example:
+
+solution('Testing String', 3) --> 'Tes...'
+solution('Testing String', 8) --> 'Testing ...'
+solution('Test', 8)           --> 'Test'
+*/
+
+function solution(string, limit) {
+  return limit >= string.length ? string : string.slice(0, limit) + "...";
+}
+
+console.log(solution("Testing String", 3)); // 'Tes...'
+console.log(solution("Testing String", 8)); // 'Testing ...'
+console.log(solution("Test", 8)); // 'Test'
