23c9534bf625222e2af21ac52ce7559054b779a5
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-20 12:45:52 -0700

    Update 553 Find all occurrences of an element in an array.js

--- Kata/553 Find all occurrences of an element in an array.js
+++ Kata/553 Find all occurrences of an element in an array.js
@@ -20,3 +20,19 @@ const findAll = (array, n) => {
 console.log(findAll([6, 9, 3, 4, 3, 82, 11], 3)); // [2, 4]
 console.log(findAll([10, 16, 20, 6, 14, 11, 20, 2, 17, 16, 14], 16)); // [1, 9]
 console.log(findAll([20, 20, 10, 13, 15, 2, 7, 2, 20, 3, 18, 2, 3, 2, 16, 10, 9, 9, 7, 5, 15, 5], 20)); // [0, 1, 8]
+
+
+
+const findAll = (array, n) => {
+  let a = []; 
+  array.forEach( (v,i) => {if (v===n) {a.push(i)} } )
+  return a
+}
+
+
+
+const findAll = (nums, find) => nums
+  .reduce((acc, num, index) => num === find ? [...acc, index] : acc, [])
+
+
+const findAll = (arr, n) => arr.map((el, i) => el === n ? i : 'str').filter(e => e !== 'str')
