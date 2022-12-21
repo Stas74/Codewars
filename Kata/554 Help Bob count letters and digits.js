882076ba713870891b6ba6deb9cecb6fd32f235a
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-21 16:21:18 -0700

    Update 554 Help Bob count letters and digits.js

--- Kata/554 Help Bob count letters and digits.js
+++ Kata/554 Help Bob count letters and digits.js
@@ -21,3 +21,22 @@ function countLettersAndDigits(input) {
 
 console.log(countLettersAndDigits("hel2!lo")); // 6
 console.log(countLettersAndDigits("n!!_ice!!123")); // 7
+
+
+
+
+function countLettersAndDigits(s) {
+  return s.replace(/[^a-z\d]/gi,"").length;
+}
+
+
+const countLettersAndDigits = input =>
+  input.replace(/[\W_]/g, ``).length;
+
+
+
+function countLettersAndDigits(input) {
+  return (input.match(/[a-z0-9]/gi) || []).length
+}
+
+
