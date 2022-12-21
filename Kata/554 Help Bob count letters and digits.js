1648ccefcfdc7473c4bfacbff86a0b261a8140ab
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-21 16:18:09 -0700

    Create 554 Help Bob count letters and digits.js

---
+++ Kata/554 Help Bob count letters and digits.js
@@ -0,0 +1,23 @@
+// Help Bob count letters and digits.
+
+/*
+Bob is a lazy man.
+
+He needs you to create a method that can determine how many letters 
+(both uppercase and lowercase ASCII letters) and digits are in a given string.
+
+Example:
+
+"hel2!lo" --> 6
+
+"wicked .. !" --> 6
+
+"!?..A" --> 1
+*/
+
+function countLettersAndDigits(input) {
+  return input.replace(/[^a-z0-9]/gi, "").length;
+}
+
+console.log(countLettersAndDigits("hel2!lo")); // 6
+console.log(countLettersAndDigits("n!!_ice!!123")); // 7
