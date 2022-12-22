e2b077eebb098fcec5d7930f33ecb382e1215f18
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-21 17:02:37 -0700

    Update 556 What's up next.js

--- Kata/556 What's up next.js
+++ Kata/556 What's up next.js
@@ -35,3 +35,27 @@ console.log(nextItem([1, 2, 3, 4, 5, 6, 7, 8], 5)); // 6
 console.log(nextItem(["a", "b", "c"], "d")); // undefined
 console.log(nextItem("testing", "t")); // "e"
 console.log(nextItem("testing", "a")); // undefined
+
+
+
+function nextItem(xs, item) {
+  var found = false;
+  for (var x of xs) {
+    if (found) return x;
+    if (x == item) found = true;
+  }
+  return undefined;
+}
+
+
+
+
+const nextItem = (xs, item) => {
+  const iter = xs[Symbol.iterator]();
+  for (let el of iter) {
+    if (el === item) return iter.next().value;
+  }
+};
+
+
+
