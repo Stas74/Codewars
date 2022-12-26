56170cfeb6ffcd32d934dd124e3247e92f4e5d40
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-26 11:19:13 -0700

    Create 564 Unscrambled eggs.js

---
+++ Kata/564 Unscrambled eggs.js
@@ -0,0 +1,17 @@
+// Unscrambled eggs
+
+/*
+The string given to your function has had an "egg" inserted directly after each consonant. 
+You need to return the string before it became eggcoded.
+
+Example
+unscrambleEggs("Beggegeggineggneggeregg"); => "Beginner"
+//             "B---eg---in---n---er---"
+*/
+
+function unscrambleEggs(word) {
+  return word.replace(/egg/gi, "");
+}
+
+console.log(unscrambleEggs("ceggodegge heggeregge")); // "code here"
+console.log(unscrambleEggs("FeggUNegg KeggATeggA")); // "FUN KATA"
