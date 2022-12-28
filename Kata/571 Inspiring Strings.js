f280e8d0817f88672f74062d4e1f5ea8156d8b55
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-28 15:32:28 -0700

    Update 571 Inspiring Strings.js

--- Kata/571 Inspiring Strings.js
+++ Kata/571 Inspiring Strings.js
@@ -20,3 +20,17 @@ function longestWord(stringOfWords) {
 console.log(longestWord("a b c d e fgh")); // "fgh"
 console.log(longestWord("one two three")); // "three"
 console.log(longestWord("red blue grey")); // "grey"
+
+
+
+let longestWord = s => s.split(' ').reduceRight((a,b)=>(b.length>a.length) ? b : a);
+
+
+function longestWord(str){
+  return str.split(' ').sort((b, a) => b.length - a.length).pop();
+}
+
+
+const longestWord = a => a.split(' ').reduce((s,n)=> s.length > n.length ? s : n, '')
+
+
