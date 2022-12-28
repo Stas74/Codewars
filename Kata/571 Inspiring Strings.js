544a0c30b1ed491b739662f2af7090837f7f3387
Stas74 <88213143+Stas74@users.noreply.github.com>
2022-12-28 15:28:15 -0700

    Create 571 Inspiring Strings.js

---
+++ Kata/571 Inspiring Strings.js
@@ -0,0 +1,22 @@
+// Inspiring Strings
+
+/*
+When given a string of space separated words, return the word with the longest length. 
+If there are multiple words with the longest length, return the last instance of the word with the longest length.
+
+Example:
+
+'red white blue' //returns string value of white
+
+'red blue gold' //returns gold
+*/
+
+function longestWord(stringOfWords) { 
+  const strArr = stringOfWords.split(" ");
+  const maxLength = Math.max(...strArr.map((el) => el.length));
+  return strArr.reverse().filter(el => el.length == maxLength)[0];
+}
+
+console.log(longestWord("a b c d e fgh")); // "fgh"
+console.log(longestWord("one two three")); // "three"
+console.log(longestWord("red blue grey")); // "grey"
