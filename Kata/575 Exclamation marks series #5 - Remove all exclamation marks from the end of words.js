f689cade22af206dbeb09978f3ae6646c090f7b8
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-03 13:17:53 -0700

    Create 575 Exclamation marks series #5 - Remove all exclamation marks from the end of words.js

---
+++ Kata/575 Exclamation marks series #5 - Remove all exclamation marks from the end of words.js
@@ -0,0 +1,22 @@
+// Exclamation marks series #5: Remove all exclamation marks from the end of words
+
+/*
+Remove all exclamation marks from the end of words. Words are separated by a single space. 
+There are no exclamation marks within a word.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi Hi"
+remove("!!!Hi !!hi!!! !hi") === "!!!Hi !!hi !hi"
+*/
+
+function remove (string) {
+  return string.split(" ").map(el => el.replace(/!+$/, '')).join(" ");
+}
+
+console.log(remove("Hi!!!")); // "Hi"
+console.log(remove("!Hi!")); // "!Hi"
+console.log(remove("Hi! Hi!")); // "Hi Hi"
