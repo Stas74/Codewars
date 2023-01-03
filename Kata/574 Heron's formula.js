bb12e86ade551c5cc85bbc0a08a99921b9815613
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-03 12:47:02 -0700

    Create 574 Heron's formula.js

---
+++ Kata/574 Heron's formula.js
@@ -0,0 +1,29 @@
+// Heron's formula
+
+/*
+Write function heron which calculates the area of a triangle with sides a, b, and c (x, y, z in COBOL).
+
+Heron's formula:
+
+s∗(s−a)∗(s−b)∗(s−c)\sqrt{s * (s - a) * (s - b) * (s - c)} 
+s∗(s−a)∗(s−b)∗(s−c)
+​
+ 
+where
+
+s=a+b+c2s = \frac{a + b + c} 2s= 
+2
+a+b+c
+​
+ 
+Output should have 2 digits precision.
+*/
+
+
+function heron(a, b, c) {
+  let s = (a + b + c) / 2;
+  return (s * (s - a) * (s - b) * (s - c)) ** 0.5;
+}
+
+console.log(heron(3, 4, 5)); // 6
+console.log(heron(6, 8, 10)); // 24
