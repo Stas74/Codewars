598289befc31289cb4d0408ff5951b6ee4073eff
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-04 15:07:40 -0700

    Update 578 Salesman's Travel.js

--- Kata/578 Salesman's Travel.js
+++ Kata/578 Salesman's Travel.js
@@ -47,19 +47,33 @@ console.log(travel(r, "NY 56432")) // "NY 56432:High Street Pollocksville/786"
 console.log(travel(r, "NY 5643")) // "NY 5643:/"
 
 
-// const ad = "123 Main Street St. Louisville OH 43071,432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432,"
-//   + "54 Holy Grail Street Niagara Town ZP 32908,3200 Main Rd. Bern AE 56210,1 Gordon St. Atlanta RE 13000,"
-//   + "10 Pussy Cat Rd. Chicago EX 34342,10 Gordon St. Atlanta RE 13000,58 Gordon Road Atlanta RE 13000,"
-//   + "22 Tokyo Av. Tedmondville SW 43098,674 Paris bd. Abbeville AA 45521,10 Surta Alley Goodtown GG 30654,"
-//   + "45 Holy Grail Al. Niagara Town ZP 32908,320 Main Al. Bern AE 56210,14 Gordon Park Atlanta RE 13000,"
-//   + "100 Pussy Cat Rd. Chicago EX 34342,2 Gordon St. Atlanta RE 13000,5 Gordon Road Atlanta RE 13000,"
-//   + "2200 Tokyo Av. Tedmondville SW 43098,67 Paris St. Abbeville AA 45521,11 Surta Avenue Goodtown GG 30654,"
-//   + "45 Holy Grail Al. Niagara Town ZP 32918,320 Main Al. Bern AE 56215,14 Gordon Park Atlanta RE 13200,"
-//   + "100 Pussy Cat Rd. Chicago EX 34345,2 Gordon St. Atlanta RE 13222,5 Gordon Road Atlanta RE 13001,"
-//   + "2200 Tokyo Av. Tedmondville SW 43198,67 Paris St. Abbeville AA 45522,11 Surta Avenue Goodville GG 30655,"
-//   + "2222 Tokyo Av. Tedmondville SW 43198,670 Paris St. Abbeville AA 45522,114 Surta Avenue Goodville GG 30655,"
-//   + "2 Holy Grail Street Niagara Town ZP 32908,3 Main Rd. Bern AE 56210,77 Gordon St. Atlanta RE 13000"
 
 
-// console.log(travel(ad, "AA 45522")); // "AA 45522:Paris St. Abbeville,Paris St. Abbeville/67,670"
-// console.log(travel(ad, "EX 34345")); // "EX 34345:Pussy Cat Rd. Chicago/100"
+function travel(r, zipcode) {
+    const addressList = r.split(",");
+
+    const streets = [];
+    const numbers = [];
+
+    addressList.forEach(address => {
+        let zip = address.substring(address.length - 8, address.length);
+        if (zip === zipcode) {
+            let number = address.substring(0, address.indexOf(" ") + 1);
+            let street = address.substring(address.indexOf(" "), address.length -8);
+            
+            streets.push(street.trim());
+            numbers.push(number.trim());
+        }
+    });
+    return `${zipcode}:${streets.join(",")}/${numbers.join(",")}`;
+}
+
+
+function travel(r, zipcode) {
+  re = RegExp('(\\d+)\\s+(.+)\\s+'+zipcode+'$')
+  streets = r.split(',').map(x=>x.match(re)).filter(x=>x)
+  return zipcode+':'+streets.map(x=>x[2])+'/'+streets.map(x=>x[1])
+}
+
+
+
