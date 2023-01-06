33a75569c2c3bc1bd18b88a617dd8fd10fd69e8e
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-06 12:27:42 -0700

    Update 583 Largest Elements.js

--- Kata/583 Largest Elements.js
+++ Kata/583 Largest Elements.js
@@ -16,3 +16,13 @@ function largest(n, xs) {
 console.log(largest(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1])); // [9, 10]
 console.log(largest(3, [5, 1, 5, 2, 3, 1, 2, 3, 5])); // [5, 5, 5]
 console.log(largest(0,[1,2,3,4,8,7,6,5])); // []
+
+
+
+
+function largest(n,xs){
+  return xs.sort(function(a, b) {return a - b;}).slice(xs.length-n);
+}
+
+
+
