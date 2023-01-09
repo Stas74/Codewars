174868c7ff550395bd1027dd6ca4a181500513cf
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-09 13:41:09 -0700

    Update 586 Merge two arrays.js

--- Kata/586 Merge two arrays.js
+++ Kata/586 Merge two arrays.js
@@ -26,3 +26,45 @@ function mergeArrays(a, b) {
 
 console.log(mergeArrays([1, 2, 3, 4, 5, 6, 7, 8], ["a", "b", "c", "d", "e"])); // [1, "a", 2, "b", 3, "c", 4, "d", 5, "e", 6, 7, 8]
 console.log(mergeArrays(["a", "b", "c", "d", "e"], [1, 2, 3, 4, 5])); // ['a', 1, 'b', 2, 'c', 3, 'd', 4, 'e', 5]
+
+
+
+function mergeArrays(a, b) {
+  // your code here
+  var returnArray = [];
+  var counter = 0;
+  while (a[counter] || b[counter] ){
+    if(a[counter]){
+      returnArray.push(a[counter]);
+    }
+    if(b[counter]){
+      returnArray.push(b[counter]);
+    }
+    counter++;    
+   }
+   return returnArray;
+}
+
+
+
+function mergeArrays(a, b) {
+  var answer = [];  
+  for (i = 0; i < Math.max(a.length, b.length); i++){
+    if (i < a.length) {answer.push(a[i]);}
+    if (i < b.length) {answer.push(b[i]);}
+  }  
+  return answer;
+}
+
+
+
+const _ = require('lodash')
+
+function mergeArrays(a, b) {
+  return _.compact(_.flatten(_.zip(a, b)))
+}
+
+
+
+
+const mergeArrays = ( $ , $2) => [...Array($.length + $2.length)].reduce((acc, el, i) => acc.concat( $[i],$2[i]), []).filter(e => e)
