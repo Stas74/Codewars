5ffe107f1666c067f7c094fbeb065b416424d5ca
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-10 15:10:52 -0700

    Create 593 Reverse list.js

---
+++ Kata/593 Reverse list.js
@@ -0,0 +1,11 @@
+// Reverse list
+
+/*
+Write reverseList function that simply reverses lists.
+*/
+
+function reverseList(arr) {
+  return arr.reverse();
+}
+
+console.log(reverseList([1,2,3])); // [3,2,1]
