faec3553c1d3c06289d0c2fa35abc488a2174a0f
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-10 15:04:52 -0700

    Update 592 Insert dashes.js

--- Kata/592 Insert dashes.js
+++ Kata/592 Insert dashes.js
@@ -24,3 +24,37 @@ function insertDash(num) {
 console.log(insertDash(454793)); // '4547-9-3'
 console.log(insertDash(123456)); // '123456'
 console.log(insertDash(1003567)); // '1003-567'
+
+
+
+function insertDash(num) {
+   return num.toString().replace(/[13579](?=[13579])/g, "$&-");
+}
+
+function insertDash(num) {
+   return String(num).replace(/([13579])(?=[13579])/g, '$1-');
+}
+
+
+
+function insertDash(num) {
+   return num.toString().split('').reduce((acc, c, i, arr) => {
+     if (i > 0 && Number(arr[i]) % 2 !== 0 && Number(arr[i - 1]) % 2 !== 0)
+       acc = acc + '-';
+     
+     return acc + c;
+   }, '');
+}
+
+
+
+function insertDash(num) {
+   //code me
+ num = num.toString().split('')  
+  for(let i = 0; i < num.length; i++){
+    if (num[i]%2 > 0 && num[i +1]%2 > 0){
+      num[i] = num[i] + '-'
+    }
+  } 
+  return num.join('')  
+}
