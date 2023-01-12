8dee651a88afca914ef00cf69b24c8551a501083
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-12 16:04:28 -0700

    Create 601 Frequency sequence.js

---
+++ Kata/601 Frequency sequence.js
@@ -0,0 +1,24 @@
+// Frequency sequence
+
+/*
+Your task is to return an output string that translates an input string s by replacing each character in s 
+with a number representing the number of times that character occurs in s 
+and separating each number with the sep character(s).
+
+Example (s, sep --> Output)
+
+"hello world", "-" --> "1-1-3-3-2-1-1-2-1-3-1"
+"19999999"   , ":" --> "1:7:7:7:7:7:7:7"
+"^^^**$"     , "x" --> "3x3x3x2x2x1"  
+*/
+
+function freqSeq(str, sep) {
+  let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    obj[str[i]] ? obj[str[i]] += 1 : obj[str[i]] = 1;
+  }
+  return str.split("").map(el => obj[el]).join(sep);
+}
+
+console.log(freqSeq('hello world', '-')); // '1-1-3-3-2-1-1-2-1-3-1'
+console.log(freqSeq('19999999',    ':')); // '1:7:7:7:7:7:7:7'
