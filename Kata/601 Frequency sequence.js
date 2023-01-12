73df1d7134b8c360bb2db9c47769b3c5e1a97ff5
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-12 16:11:09 -0700

    Update 601 Frequency sequence.js

--- Kata/601 Frequency sequence.js
+++ Kata/601 Frequency sequence.js
@@ -22,3 +22,29 @@ function freqSeq(str, sep) {
 
 console.log(freqSeq('hello world', '-')); // '1-1-3-3-2-1-1-2-1-3-1'
 console.log(freqSeq('19999999',    ':')); // '1:7:7:7:7:7:7:7'
+
+
+
+function freqSeq(str, sep) {
+  return str.split('').map((v, i, arr) => arr.filter(vi => vi === v).length).join(sep);
+}
+
+
+
+function freqSeq(str, sep) {
+  let letters = str.split("");
+  var count = letters.reduce(function(acc, l) {
+    acc[l] = acc[l] ? acc[l] + 1 : 1;
+    return acc;
+  }, {});
+  return letters.map(l => count[l]).join(sep);
+}
+
+
+
+const freqSeq = (s, sep) => s.split("").map(i => s.split(i).length - 1).join(sep)
+
+
+
+const freqSeq = (str, sep) =>
+  [...str].map(val => --str.split(val).length).join(sep);
