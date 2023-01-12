df78881273c556b4b30bac46ad6b2cf259510612
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-12 12:24:28 -0700

    Update 598 How many consecutive numbers are needed.js

--- Kata/598 How many consecutive numbers are needed.js
+++ Kata/598 How many consecutive numbers are needed.js
@@ -15,3 +15,14 @@ function consecutive(arr) {
 
 console.log(consecutive([4, 8, 6])); // 2
 console.log(consecutive([1, 2, 3, 4])); // 0
+
+
+
+const consecutive = arr => arr.length ? Math.max(...arr) - Math.min(...arr) - arr.length + 1 : 0;
+
+
+
+const consecutive = arr =>
+  Math.max(...arr) - Math.min(...arr) - arr.length + 1 | 0;
+
+
