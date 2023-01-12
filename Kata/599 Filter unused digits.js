18877626f9d5865aeb90cfdb90a784b7561fae04
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-12 14:35:43 -0700

    Update 599 Filter unused digits.js

--- Kata/599 Filter unused digits.js
+++ Kata/599 Filter unused digits.js
@@ -18,3 +18,39 @@ function unusedDigits() {
 
 console.log(unusedDigits(12, 34, 56, 78)); // "09"
 console.log(unusedDigits(2015, 8, 26)); // "3479"
+
+
+
+function unusedDigits(...args){
+  return "0123456789".replace(new RegExp('['+args.join('')+']','g'), '')
+}
+
+
+const unusedDigits = (...arr) => {
+	var digits = arr.join();
+	return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+		.filter(x => !digits.includes(x))
+		.join('');
+};
+
+
+
+function unusedDigits(...args) {
+  // Given: varying # of integer arguments
+  // define the parameter
+  // create a variable   
+  
+  const numbers = [0,1,2,3,4,5,6,7,8,9];
+  
+  // change the arguments from integer into a string   
+  //join 
+  let numStr = args.join('');  
+  
+  // filter out those that are not present in any of the arguments
+  // Return: a sorted string of digits
+  return numbers.filter(element => !numStr.includes(element)).join('')
+}
+
+
+
+unusedDigits=(...a)=>'0123456789'.replace(new RegExp('['+a+']','g'),'')
