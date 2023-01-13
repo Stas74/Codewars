a9afb815c59c720195a7b79bf3e89a2f06ff79cb
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-13 14:02:25 -0700

    Update 604 Grasshopper - Terminal Game Turn Function.js

--- Kata/604 Grasshopper - Terminal Game Turn Function.js
+++ Kata/604 Grasshopper - Terminal Game Turn Function.js
@@ -30,3 +30,23 @@ function doTurn() {
 
 console.log(duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3, "5"])); // [4, 3, 1]
 console.log(duplicates([0, 1, 2, 3, 4, 5])); // []
+
+
+
+
+const doTurn = () => {
+  rollDice()
+  move()
+  combat()
+  getCoins()
+  buyHealth()
+  printStatus()
+}
+
+
+
+function doTurn () {
+  return [rollDice, move, combat, getCoins, buyHealth, printStatus].map( $ => $() )
+}
+
+
