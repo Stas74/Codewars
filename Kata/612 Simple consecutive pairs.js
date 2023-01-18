596bdb12ca911a3d017ad3deee0603d1be1b66d7
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-18 14:38:47 -0700

    Update 612 Simple consecutive pairs.js

--- Kata/612 Simple consecutive pairs.js
+++ Kata/612 Simple consecutive pairs.js
@@ -25,3 +25,27 @@ function pairs(ar) {
 console.log(pairs([1, 2, 5, 8, -4, -3, 7, 6, 5])); // 3
 console.log(pairs([21, 20, 22, 40, 39, -56, 30, -55, 95, 94])); // 2
 console.log(pairs([81, 44, 80, 26, 12, 27, -34, 37, -35])); // 0
+
+
+
+function pairs(array, count = 0) {
+  for (let i = 0; i < array.length; i += 2)
+    if (Math.abs(array[i] - array[i + 1]) === 1)
+      count += 1;
+  return count;
+}
+
+
+
+const pairs = ar =>
+  ar.reduce((pre, val, idx) => pre + (++idx % 2 && Math.abs(val - ar[idx]) === 1), 0);
+
+
+
+const _ = require('lodash');
+function pairs(xs) {
+  return _(xs).chunk(2).filter(([x, y]) => Math.abs(x - y) == 1).size();
+}
+
+
+
