1b51f1e3a8fd41a02d28b92b0c8acff2d137fa31
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-19 13:44:56 -0700

    Update 616 Improving Math.round(x).js

--- Kata/616 Improving Math.round(x).js
+++ Kata/616 Improving Math.round(x).js
@@ -30,3 +30,12 @@ console.log(Math.roundTo(3.1415926535, 4)); // 3.1416
 console.log(Math.roundTo(4, 5)); // 4
 console.log(Math.roundTo(5.3035802, 3)); // 5.304
 
+
+
+
+Math.roundTo = (number, precision) => Number(number.toFixed(precision));
+
+
+Math.roundTo = function (number, precision) {
+  return Math.round(number * Math.pow(10, precision)) / Math.pow(10, precision);
+}
