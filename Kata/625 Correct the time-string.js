604916b2bc91700142c04d78a5db02e3340358df
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-24 15:33:59 -0700

    Update 625 Correct the time-string.js

--- Kata/625 Correct the time-string.js
+++ Kata/625 Correct the time-string.js
@@ -45,3 +45,42 @@ console.log(timeCorrect("09:10:01")); // "09:10:01"
 console.log(timeCorrect("1170:10")); // null
 console.log(timeCorrect("0a:1c:22")); // null
 console.log(timeCorrect("52:01:01")); // "04:01:01"
+
+
+
+function timeCorrect(str) {
+	const date = new Date(); 
+  if (str == '') return str;
+	if (!/^\d{2}\:\d{2}\:\d{2}$/g.test(str)) return null;
+  date.setUTCHours(...str.split(':'));
+  return date.toLocaleTimeString('en', {hour12: false});
+}
+
+
+
+function timeCorrect(timestring) {
+  if (!timestring)
+    return timestring
+    
+  if (!/^(\d\d:){2}\d\d$/.test(timestring))
+    return null
+    
+  return (new Date(0,0,0,...timestring.split(':'))).toString().slice(16,24)
+}
+
+
+
+function timeCorrect(ts) {
+  if (!ts) return ts
+  if (!/\d\d:\d\d:\d\d/.test(ts)) return null
+  var rs=(ts.slice(0,2)*3600)+(ts.slice(3,5)*60)+(+ts.slice(-2))
+  return `${("0"+~~(rs/3600)%24).slice(-2)}:${("0"+~~(rs%3600/60)).slice(-2)}:${("0"+rs%3600%60).slice(-2)}`  
+}
+
+
+
+const timeCorrect = timestring =>
+  !timestring ? timestring : /^\d{2}:\d{2}:\d{2}$/.test(timestring) ? new Date(new Date().setHours(...timestring.split(`:`))).toTimeString().slice(0, 8) : null;
+
+
+
