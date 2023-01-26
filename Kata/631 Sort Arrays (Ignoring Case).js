cf5cb6ea190460498530c179be90422896fd6058
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-26 15:52:13 -0700

    Update 631 Sort Arrays (Ignoring Case).js

--- Kata/631 Sort Arrays (Ignoring Case).js
+++ Kata/631 Sort Arrays (Ignoring Case).js
@@ -15,3 +15,23 @@ const sortme = function (names) {
 
 console.log(sortme(["Hello", "there", "I'm", "fine"])); // ["fine", "Hello", "I'm", "there"]
 console.log(sortme(["C", "d", "a", "B"])); // ["a", "B", "C", "d"]
+
+
+
+const sortme = names => names.slice().sort((a, b) => a.localeCompare(b));
+
+
+var sortme = function( names ){
+   return names.sort((a,b) => { return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1 });
+}
+
+
+const compareIgnoreCase = (s1,s2) => s1.localeCompare(s2, {sensitivity: 'accent'})
+    , sortme = a => a.sort(compareIgnoreCase)
+    ;
+
+
+const sortme = (names) =>
+  names.sort((a, b) => new Intl.Collator("en", { sensitivity: "base" }).compare(a, b));
+
+
