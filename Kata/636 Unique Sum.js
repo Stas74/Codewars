88d56c46e5b06fd461454e1ad9f193768dbd804d
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-27 14:57:29 -0700

    Create 636 Unique Sum.js

---
+++ Kata/636 Unique Sum.js
@@ -0,0 +1,24 @@
+// Unique Sum
+
+/*
+Given a list of integers values, your job is to return the sum of the values; however, 
+if the same integer value appears multiple times in the list, you can only count it once in your sum.
+
+For example:
+
+[ 1, 2, 3] ==> 6
+
+[ 1, 3, 8, 1, 8] ==> 12
+
+[ -1, -1, 5, 2, -7] ==> -1
+
+[] ==> null
+*/
+
+function uniqueSum(lst) {
+  return lst.length === 0 ? null : [...new Set(lst)].reduce((prev, cur) => prev + cur, 0);
+}
+
+console.log(uniqueSum([1, 2, 3])); // 6
+console.log(uniqueSum([1, 3, 8, 1, 8])); // 12
+console.log(uniqueSum([])); // null
