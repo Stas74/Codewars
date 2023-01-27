b5c3fc3e9a07a409cbeed8da1f49e1b211d69472
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-27 13:50:10 -0700

    Create 634 Geometry Basics - Circle Area in 2D.js

---
+++ Kata/634 Geometry Basics - Circle Area in 2D.js
@@ -0,0 +1,48 @@
+// Geometry Basics: Circle Area in 2D
+
+/*
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Write the function circleArea/CircleArea which takes in a Circle object and calculates the area of that circle.
+The Circle class can be seen below:
+
+// Represents a Circle where center is a Point and radius is a Number
+class Circle {
+  constructor(center, radius) { 
+    this.center = center; 
+    this.radius = radius;
+  }
+}
+And the Point class can be seen below:
+
+// Represents a Point where x and y are Numbers
+class Point {
+  constructor (x,y) { 
+    this.x = x;
+    this.y = y; 
+  }
+}
+Tests round answers to 6 decimal places.
+*/
+
+// Represents a Circle where center is a Point and radius is a Number
+class Circle {
+  constructor(center, radius) { 
+    this.center = center; 
+    this.radius = radius;
+  }
+}
+
+// Represents a Point where x and y are Numbers
+class Point {
+  constructor (x,y) { 
+    this.x = x;
+    this.y = y; 
+  }
+}
+
+function circleArea(circle) {
+  return (circle.radius ** 2 * Math.PI).toFixed(6);
+}
+
+console.log(circleArea( new Circle( new Point(10, 10), 30))); // 2827.433388
