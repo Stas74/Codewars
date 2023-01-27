7192664e3d839537aa9a5d59444e0bca70d78e62
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-27 16:26:29 -0700

    Update 637 Find all pairs.js

--- Kata/637 Find all pairs.js
+++ Kata/637 Find all pairs.js
@@ -29,3 +29,48 @@ function duplicates(array) {
 console.log(duplicates([1, 2, 5, 6, 5, 2])); // 2
 console.log(duplicates([1, 2, 2, 20, 6, 20, 2, 6, 2])); // 4
 console.log(duplicates([0, 0, 0, 0, 0, 0, 0])); // 3
+
+
+
+
+function duplicates(array) {
+  let res = 0, odd = new Set();
+  for (let x of array) {
+    if (odd.delete(x))
+      res++;
+    else
+      odd.add(x);
+  }
+  return res;
+  
+  
+  
+function duplicates(array) {
+  const newArray = array.sort((a, b) => a - b);
+  if (newArray.length <= 1) return 0;
+  let count = 0;
+  for (let i = 0; i < newArray.length; i++) {
+    if (newArray[i] == newArray[i + 1]) {
+      count++;
+      i++;
+    }
+  }
+  return count;
+}
+
+
+  
+const duplicates = a => (a.sort((x,y)=>x-y).join('_').match(/(?<=_|^)(\d+)_\1(?=_|$)/g)||[]).length
+
+
+
+const duplicates = array => {
+  let numberOf = array.reduce((acc,c) => (acc[c] = (acc[c] ||0) + 1, acc), {});
+  return Object.values(numberOf).reduce((acc,c) => acc + Math.floor(c / 2), 0);
+};
+  
+ 
+  
+const duplicates = array =>
+  array.reduce((pre, val) => pre[0].delete(val) ? [pre[0], ++pre[1]] : [pre[0].add(val), pre[1]], [new Set(), 0])[1];
+  
