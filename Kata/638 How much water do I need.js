6f08470eaa9ad2cdfbe1576d37e3036544527000
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-30 12:44:27 -0700

    Update 638 How much water do I need.js

--- Kata/638 How much water do I need.js
+++ Kata/638 How much water do I need.js
@@ -28,3 +28,24 @@ console.log(howMuchWater(10, 10, 21)); // 'Too much clothes'
 console.log(howMuchWater(10, 10, 2)); // 'Not enough clothes'
 console.log(howMuchWater(10, 11, 20)); // 23.58
 console.log(howMuchWater(50, 15, 29)); // 189.87
+
+
+
+
+const howMuchWater = (water, load, clothes) =>
+  clothes < load ? `Not enough clothes` : clothes > 2 * load ? `Too much clothes` : Math.round(water * 1.1 ** (clothes - load) * 1e2) / 1e2;
+
+
+
+const howMuchWater = (water, load, clothes) =>
+   clothes > 2 * load ? 'Too much clothes' 
+  : clothes < load ? 'Not enough clothes' 
+  : Math.round(water * Math.pow(1.1, clothes-load)*100)/100;    
+
+
+
+function howMuchWater(L,X,N){
+  if (N > 2 * X) return "Too much clothes";
+  if (N < X) return "Not enough clothes";
+  return +(L * 1.1 ** (N - X)).toFixed(2);
+}
