f86141bb1cb2bbdcd44f913b3c2f7105fde1ca98
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-31 13:27:16 -0700

    Update 642 Driving Licence.js

--- Kata/642 Driving Licence.js
+++ Kata/642 Driving Licence.js
@@ -61,3 +61,58 @@ function driver(data) {
 console.log(driver(["John", "James", "Smith", "01-Jan-2000", "M"])); // "SMITH001010JJ9AA"
 console.log(driver(["Johanna", "", "Gibbs", "13-Dec-1981", "F"])); // "GIBBS862131J99AA"
 console.log(driver(["Andrew", "Robert", "Lee", "02-September-1981", "M"])); // "LEE99809021AR9AA"
+
+
+
+
+function driver(data) {
+  var name = (data[2].slice(0,5).toUpperCase() +'99999').slice(0,5);    // 5
+  var dob = new Date(data[3]);
+  var dec = (dob.getYear().toString().length==2 ? dob.getYear().toString()[0] : dob.getYear().toString()[1]);  // 1
+  var month = (data[4] == "M" ? ("0" + (dob.getMonth()+1).toString()).slice(-2) : dob.getMonth()+51);
+  var day = ("0" + dob.getDate().toString()).slice(-2);
+  var yearDig = dob.getYear().toString().slice(-1);
+  var inits = data[0][0] + (data[1]=="" ? "9" : data[1][0])
+  var arb1 = "9"
+  var arb2 = "AA"
+  return name + dec + month + day + yearDig + inits + arb1 + arb2
+}
+
+
+
+function driver(data) {
+  var birthDay = new Date(data[3]);
+  
+  return (data[2].toUpperCase()+"99999").slice(0,5)                              //1-5
+          +  data[3].slice(-2,-1)                                                //6
+          +  ("0"+(birthDay.getMonth() + (data[4] == "M" ? 1 : 51))).slice(-2)   //7-8
+          +  ("0"+birthDay.getDate()).slice(-2)                                  //9-10
+          +  data[3].slice(-1)                                                   //11
+          +  data[0].slice(0,1)+(data[1]+"9").slice(0,1)                         //12-13
+          +  "9AA";                                                              //14-16
+}
+
+
+
+const driver = data =>
+  data[2].slice(0, 5).toUpperCase().padEnd(5, 9) +
+  data[3].slice(-2,-1) +
+  `${new Date(data[3]).getMonth() + (data[4] === `F` ? 51 : 1)}`.padStart(2, 0) +
+  `${new Date(data[3]).getDate()}`.padStart(2, 0) +
+  data[3].slice(-1) +
+  data[0][0] + (data[1][0] || 9) +
+  `9AA`;
+
+
+
+function driver([ forename, middle_name, surname, str_birth_date, sex ]) {
+
+  let birth_date = new Date(str_birth_date).toJSON();
+
+  return `${surname}99999`.slice(0, 5).toUpperCase()
+       + birth_date.replace(/^..(.)(.).(.)(.).(..).*$/, (_, y3, y4, m1, m2, dd) =>
+           y3 + (+m1 + (sex === 'M' ? 0 : 5)) + m2 + dd + y4)
+       + forename[0] + (middle_name ? middle_name[0] : '9')
+       + `9AA`;
+
+}
