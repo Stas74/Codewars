46746dbafb63bf02ab1dfd44ac13b47e4a12efc1
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-30 20:28:24 -0700

    Update 640 Split The Bill.js

--- Kata/640 Split The Bill.js
+++ Kata/640 Split The Bill.js
@@ -42,3 +42,35 @@ function splitTheBill(x) {
 console.log(splitTheBill({ A: 20, B: 15, C: 10 })); // {A: 5, B: 0, C: -5}
 console.log(splitTheBill({ A: 40, B: 25, X: 10 })); // {A: 15, B: 0, X: -15}
 console.log(splitTheBill({ A: 40, B: 25, C: 10, D: 153, E: 58 })); // { A: -17.2, B: -32.2, C: -47.2, D: 95.8, E: 0.8 }
+
+
+
+
+const splitTheBill = x => {
+  let vals = Object.values(x),
+      avg  = vals.reduce((s, v) => s + v) / vals.length;
+  return Object.keys(x).reduce((y, v) => (y[v] = +(x[v] - avg).toFixed(2), y), {});
+}
+
+
+
+function splitTheBill(x) {
+  // Instantiate a blank object to return
+  let finalObject = {};
+  // Figure out how many people there are in the group
+  const totalNumberOfPeople = Object.keys(x).length;
+  // Figure out the total cost of everyones payments together
+  const totalCost = Object.values(x).reduce((sum, b) => sum + b, 0)
+  // Split the bill equally to figure out how much people paid before being owed / owing.
+  const valuePerPerson = totalCost / totalNumberOfPeople;
+  // minus the value per person const from the amount each person spent so that we can see if they spent more or less than the value intended
+  // and put that in the object with their name key
+  const finalAmounts = Object.entries(x).forEach(([person, value]) => {
+    const amountOwed = value - valuePerPerson;
+    finalObject[person] = Math.round(amountOwed * 100) / 100;
+  })
+  return finalObject;
+}
+
+
+
