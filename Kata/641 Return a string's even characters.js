f86369055fb40b0522c19f865c2123c4d402ac70
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-01-31 11:53:58 -0700

    Create 641 Return a string's even characters.js

---
+++ Kata/641 Return a string's even characters.js
@@ -0,0 +1,20 @@
+// Return a string's even characters
+
+/*
+Write a function that returns a sequence (index begins with 1) of all the even characters from a string. 
+If the string is smaller than two characters or longer than 100 characters, the function should return "invalid string".
+
+For example:
+
+"abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
+"a"             --> "invalid string"
+*/
+
+function evenChars(string) {
+  return string.length < 2 || string.length > 100 ? "invalid string" : 
+  string.split("").filter((el, idx) => {if (idx % 2 !== 0) {return el}})
+}
+
+console.log(evenChars("1234")); // ["2", "4"]
+console.log(evenChars("a")); // "invalid string"
+console.log(evenChars("abcdefghijklm")); // ["b", "d", "f", "h", "j", "l"]
