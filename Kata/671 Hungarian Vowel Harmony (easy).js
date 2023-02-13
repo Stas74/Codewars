757f3165707df168d7edba80ac45eeed6940d62b
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-02-13 13:54:39 -0700

    Update 671 Hungarian Vowel Harmony (easy).js

--- Kata/671 Hungarian Vowel Harmony (easy).js
+++ Kata/671 Hungarian Vowel Harmony (easy).js
@@ -35,3 +35,35 @@ console.log(dative("ablak")); // "ablaknak"
 console.log(dative("tükör")); // "tükörnek"
 console.log(dative("keret")); // "keretnek"
 
+
+
+
+function dative(word){
+  var str = word;
+  var nekArr = ["e","é","i","í","ö","ő","ü","ű"];
+  var nakArr = ["a","á","o","ó","u","ú"];
+  for(var i=str.length-1;i>=0;i--){
+    if(nekArr.indexOf(str[i])!==-1) return str + "nek";
+    if(nakArr.indexOf(str[i])!==-1) return str + "nak";
+  }
+}
+
+
+
+const dative = word =>
+  `${word}n${/[eéiíöőüű][^eéiíöőüűaáoóuú]*$/.test(word) ? `e` : `a`}k`;
+
+
+
+const dative = s => s + ('aáoóuú'.includes(s.replace(/[^aáoóuúeéiíöőüű]/g, '').slice(-1)) ? 'nak' : 'nek');
+
+
+
+function dative(word) {
+  let lastVowel = word.replace(/[^eéiíöőüűaáoóuú]/gi, '').slice(-1)
+  return 'eéiíöőüű'.match(lastVowel) ? word + 'nek' : word + 'nak'
+}
+
+
+
+let dative = w => 'eéiíöőüű'.includes(w.match(/[eéiíöőüűaáoóuú][^eéiíöőüűaáoóuú]+$/)[0][0]) ? w+'nek' : w+'nak'
