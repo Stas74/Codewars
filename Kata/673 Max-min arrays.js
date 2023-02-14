7a2b82cd24d726801b8fc811d6d9b48acec550cc
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-02-14 15:32:00 -0700

    Update 673 Max-min arrays.js

--- Kata/673 Max-min arrays.js
+++ Kata/673 Max-min arrays.js
@@ -16,3 +16,32 @@ function solve(arr) {
 }
 
 console.log(solve([15, 11, 10, 7, 12])); // [15,7,12,10,11]
+
+
+
+function solve(arr){
+  var minMax = [];
+  arr.sort((a,b)=>a-b);
+  for(var i = 0, j = arr.length-1; i <= j; i++,j--){
+    if(i != j) minMax.push(arr[j]), minMax.push(arr[i]);
+    else minMax.push(arr[i]);
+  }
+  return minMax;
+}
+
+
+
+const solve = arr =>
+  [...Array(arr.sort((a, b) => a - b).length)].map((_, idx) => idx % 2 ? arr.shift() : arr.pop());
+
+
+
+function solve(arr, ar = arr.sort((a, b)=> b - a)){
+  let ind = Math.ceil(arr.length/2)
+  arr = ar.slice(0, ind)
+  ar = ar.slice(ind).reverse()
+  return [].concat(...arr.map((_,i)=> ar[i] ? [arr[i], ar[i]] : [arr[i]]))
+};
+
+
+
