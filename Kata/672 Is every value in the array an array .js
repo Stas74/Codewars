8c9f3021d916d6d78bc72a093b0899832e5492b9
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-02-14 13:35:23 -0700

    Update 672 Is every value in the array an array .js

--- Kata/672 Is every value in the array an array .js
+++ Kata/672 Is every value in the array an array .js
@@ -19,3 +19,32 @@ console.log(arrCheck([])); // true
 console.log(arrCheck([["string"]])); // true
 console.log(arrCheck([[], {}])); // false
 console.log(arrCheck([[1], [2], [3]])); // true
+
+
+
+function arrCheck(value){
+  let count = 0;
+  for(let i = 0; i < value.length; i++){
+  if(Array.isArray(value[i]) == true) {count++}  
+}
+  return (count === value.length);
+}
+
+
+
+const arrCheck = value => Array.isArray(value) && value.every(Array.isArray);
+
+
+
+var arrCheck = function(array) {
+  var def = true;
+  for(var i = 0; i < array.length; i++) {
+    def = array[i] instanceof Array ? def : false;
+  }
+  
+  return def;
+};
+
+
+
+const arrCheck = a => a.every(x=>x.pop)
