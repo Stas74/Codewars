c62ae27e8b59e81b6da0ac2c2c6d1471006c55b2
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-02-14 15:30:28 -0700

    Create 673 Max-min arrays.js

---
+++ Kata/673 Max-min arrays.js
@@ -0,0 +1,18 @@
+// Max-min arrays
+
+/*
+In this Kata, you will be given an array of unique elements, 
+and your task is to rearrange the values so that the first max value is followed by the first minimum, 
+followed by second max value then second min value, etc.
+
+For example:
+
+solve([15,11,10,7,12]) = [15,7,12,10,11]
+The first max is 15 and the first min is 7. The second max is 12 and the second min is 10 and so on.
+*/
+
+function solve(arr) {  
+  return [...Array(arr.sort((a, b) => a - b).length)].map((el, idx) => idx % 2 ?  arr.shift(el) : arr.pop(el));
+}
+
+console.log(solve([15, 11, 10, 7, 12])); // [15,7,12,10,11]
