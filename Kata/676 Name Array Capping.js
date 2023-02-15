e8876d752f39e4cc9a74753860bc269126f27918
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-02-15 10:42:11 -0700

    Update 676 Name Array Capping.js

--- Kata/676 Name Array Capping.js
+++ Kata/676 Name Array Capping.js
@@ -14,3 +14,23 @@ function capMe(names) {
 }
 
 console.log(capMe(['KARLY', 'DANIEL', 'KELSEY'])) // returns ['Karly', 'Daniel', 'Kelsey']
+
+
+
+function capMe(names) {
+  return names.map(function (n) { return n.charAt(0).toUpperCase() + n.substring(1).toLowerCase(); });
+}
+
+
+function capMe(names) {
+    return names.map(function(name) {return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();});
+}
+
+
+function capMe(names) {
+  return names.map(n => n[0].toUpperCase() + n.slice(1).toLowerCase());
+}
+
+
+const capMe = names =>
+  names.map(val => val[0].toUpperCase() + val.slice(1).toLowerCase());
