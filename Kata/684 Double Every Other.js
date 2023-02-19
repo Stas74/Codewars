d4207b0d808c52ae7b57ca01656ced3e510bd633
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-02-19 11:20:35 -0700

    Update 684 Double Every Other.js

--- Kata/684 Double Every Other.js
+++ Kata/684 Double Every Other.js
@@ -18,3 +18,23 @@ function doubleEveryOther(a) {
 }
 
 console.log(doubleEveryOther([1, 2, 3, 4])); // [1,4,3,8]
+
+
+const doubleEveryOther = a =>
+  a.map((val, idx) => val * (idx % 2 + 1));
+
+
+function doubleEveryOther(a) {
+  for(let i = 1; i < a.length; i += 2) {
+    a[i] *= 2;
+  }
+  return a;
+}
+
+
+
+const doubleEveryOther = (a) => a.map((x, i) => i % 2 === 0 ? x : x + x);
+
+
+
+const doubleEveryOther = (a) => a.map((x, i) => i % 2 === 0 ? x : x + x);
