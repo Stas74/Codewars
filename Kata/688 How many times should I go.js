fcc9560f9f35968f78ae8fbcc64fe890c911d1e0
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-02-20 16:12:26 -0700

    Create 688 How many times should I go.js

---
+++ Kata/688 How many times should I go.js
@@ -0,0 +1,16 @@
+// How many times should I go?
+
+/*
+Lot of museum allow you to be a member, for a certain amount amount_by_year you can have unlimitted acces to the museum.
+
+In this kata you should complete a function in order to know after how many visit it will be better to take an annual pass. 
+The function take 2 arguments annual_price and individual_price.
+*/
+
+function howManyTimes(annualPrice, individualPrice) {
+  return Math.ceil(annualPrice / individualPrice);
+}
+
+console.log(howManyTimes(40, 15)); // 3
+console.log(howManyTimes(30, 10)); // 3
+console.log(howManyTimes(80, 15)); // 6
