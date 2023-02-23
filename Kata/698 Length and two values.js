7441ed1d4bcc55e56e565fad348b20ea5c24dc39
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-02-23 15:51:44 -0700

    Update 698 Length and two values.js

--- Kata/698 Length and two values.js
+++ Kata/698 Length and two values.js
@@ -20,3 +20,37 @@ function alternate(n, firstValue, secondValue) {
 
 console.log(alternate(5, true, false)); // [true, false, true, false, true]
 console.log(alternate(6, "blue", "red")); // ['blue', 'red', 'blue', 'red', 'blue', 'red']
+
+
+
+function alternate(n, firstValue, secondValue){
+  const array = [];
+  for (let i = 0; i < n; i++) {
+    array.push(i % 2 === 0 ? firstValue : secondValue)
+  }
+  return array;
+}
+
+
+function alternate(n, firstValue, secondValue){
+  return Array.from({length: n}, (_, i) => i % 2 === 0 ? firstValue : secondValue);
+}
+
+
+const alternate = (n, a, b) => n ? [a, ...alternate(n-1, b, a)] : [];
+
+
+function alternate(n, firstValue, secondValue) {
+  let arr = [];
+  while (arr.length < n) {
+    arr.push(firstValue, secondValue);
+  }
+  if (arr.length > n) {
+    arr.pop();
+  }
+  return arr;
+}
+
+
+
+alternate=(n, f, s)=>[...Array(n)].map((_, i) => i & 1 ? s : f)
