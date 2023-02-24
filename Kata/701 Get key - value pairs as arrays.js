a13f9510e03b6315baca3611bb7a286eebdfa542
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-02-24 15:47:31 -0700

    Create 701 Get key - value pairs as arrays.js

---
+++ Kata/701 Get key - value pairs as arrays.js
@@ -0,0 +1,19 @@
+// Get key - value pairs as arrays
+
+/*
+Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+
+Example:
+
+keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]]
+Style Points (JS/CoffeeScript only): 
+This kata only tests for data that uses object literal notation (simple objects). 
+For extra style, can you get your method to check for objects that extend their prototype?
+*/
+
+function keysAndValues(data) {
+  return [Object.keys(data), Object.values(data)];
+}
+
+console.log(keysAndValues({ a: 1, b: 2, c: 3 })); //[['a', 'b', 'c'], [1, 2, 3]]
+console.log(keysAndValues({ 1: "a", 2: "b", 3: "c" })); //[['1', '2', '3'], ['a', 'b', 'c']]
