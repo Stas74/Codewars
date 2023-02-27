d5ff1f31a852fb434b38e062b30fc5181944f842
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-02-27 14:54:55 -0700

    Update 705 Find the calculation type.js

--- Kata/705 Find the calculation type.js
+++ Kata/705 Find the calculation type.js
@@ -28,3 +28,25 @@ console.log(calcType(1, 2, 3)); // "addition"
 console.log(calcType(10, 4, 40)); // "multiplication"
 console.log(calcType(10, 5, 5)); // "subtraction"
 console.log(calcType(9, 5, 1.8)); // "division"
+
+
+
+const calcType = (a, b, res) =>
+  ({[a + b]: `addition`,
+    [a - b]: `subtraction`,
+    [a * b]: `multiplication`,
+    [a / b]: `division`
+  })[res];
+
+
+const calcType = (a, b, res) => 
+  a + b === res ? 'addition' :
+  a - b === res ? 'subtraction' :
+  a * b === res ? 'multiplication' :
+  a / b === res ? 'division' : 
+  null;
+
+
+calcType=(Q,S,R) => Q+S-R ? Q-S-R ? Q*S-R ? 'division': 'multiplication' : 'subtraction' : 'addition';
+
+
