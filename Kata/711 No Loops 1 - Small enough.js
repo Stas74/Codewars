43ca9dc568c9c6fce297e4554bdfcd1fd7676c8e
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-01 14:14:52 -0700

    Update 711 No Loops 1 - Small enough.js

--- Kata/711 No Loops 1 - Small enough.js
+++ Kata/711 No Loops 1 - Small enough.js
@@ -3,7 +3,9 @@
 /*
 *** No Loops Allowed ***
 
-You will be given an array (a) and a limit value (limit). You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+You will be given an array (a) and a limit value (limit). 
+You must check that all values in the array are below or equal to the limit value. 
+If they are, return true. Else, return false.
 
 You can assume all values in the array are numbers.
 
@@ -18,3 +20,17 @@ console.log(smallEnough([66, 101], 200)); // true
 console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100)); // false
 console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)); // true
 
+
+const smallEnough = (a, limit) => Math.max(...a) <= limit
+
+
+function smallEnough(a, limit){
+  a.sort((c, d) => d - c)
+  return a[0] <= limit
+}
+
+
+const smallEnough = (a, limit) => a.map( num => num > limit ).reduce((a, b) => a + b , 0) != 0 ? false : true;
+
+
+smallEnough=([a,...b], c) => b.length ? c >= a && smallEnough(b,c) : c >= a
