a373844849eda625f8d25beec414337f7b8dfdbe
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-02 14:37:31 -0700

    Update 714 Help the Elite Squad of Brazilian forces BOPE.js

--- Kata/714 Help the Elite Squad of Brazilian forces BOPE.js
+++ Kata/714 Help the Elite Squad of Brazilian forces BOPE.js
@@ -35,3 +35,39 @@ function magNumber(info) {
 console.log(magNumber(["PT92", 6])); // 2
 console.log(magNumber(["M4A1", 8])); // 1
 console.log(magNumber(["PSG1", 31])); // 19
+
+
+
+magNumber = info => Math.ceil(info[1] * 3 / {PT92: 17, M4A1: 30, M16A2: 30, PSG1: 5}[info[0]])
+
+
+const magNumber = (info) => {
+  const [gun, street] = info;
+  let magzines = 0;
+  switch(gun){
+      case('PT92'):
+      magzines = Math.ceil((street * 3) / 17);
+      break;
+      case('M4A1'):
+      magzines = Math.ceil((street * 3) / 30);
+      break;
+      case('M16A2'):
+      magzines = Math.ceil((street * 3) / 30);
+      break;
+      case('PSG1'):
+      magzines = Math.ceil((street * 3) / 5);
+      break;
+  }
+  return magzines;
+}
+
+
+function magNumber(info){
+  const dict = new Map([
+    ["PT92", 17],
+    ["M4A1", 30],
+    ["M16A2", 30],
+    ["PSG1", 5]
+  ]);
+  return Math.ceil(info[1] * 3 / dict.get(info[0]));
+}
