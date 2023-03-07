f62a112ed7b10d14021d04c55f4ad38312176874
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-07 14:50:10 -0700

    Update 722 Is your period late.js

--- Kata/722 Is your period late.js
+++ Kata/722 Is your period late.js
@@ -20,3 +20,15 @@ function periodIsLate(last, today, cycleLength) {
 
 console.log(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 35)); // false
 console.log(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 28)); // true
+
+
+
+function periodIsLate(last, today, cycleLength) {
+  return (today - last) / 86400000 > cycleLength
+}
+
+
+function periodIsLate(last, today, c){
+  return (last.setDate(last.getDate() + c),last < today)
+}
+
