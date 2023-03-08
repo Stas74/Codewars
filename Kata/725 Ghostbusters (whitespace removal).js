5b26fb7fa4cbe287ba75f0a89c3d5e52ffa72273
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-08 14:44:40 -0700

    Create 725 Ghostbusters (whitespace removal).js

---
+++ Kata/725 Ghostbusters (whitespace removal).js
@@ -0,0 +1,27 @@
+// Ghostbusters (whitespace removal)
+
+/*
+Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
+
+In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+
+So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+
+Example:
+
+ghostBusters("Sky scra per");
+Should return:
+
+"Skyscraper"
+If the building contains no ghosts, return the string:
+
+"You just wanted my autograph didn't you?"
+*/
+
+function ghostBusters(building) {
+  return building.split(" ").length == 1 ? "You just wanted my autograph didn't you?" : building.replace(/[\s]/g, "")
+}
+
+console.log(ghostBusters("Factor y")); // "Factory"
+console.log(ghostBusters("O  f fi ce")); // "Office"
+console.log(ghostBusters("BusStation")); // "You just wanted my autograph didn't you?"
