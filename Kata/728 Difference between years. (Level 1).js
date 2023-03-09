d35efcee4993057fa2401e47619ec579a6e16bae
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-09 13:38:23 -0700

    Create 728 Difference between years. (Level 1).js

---
+++ Kata/728 Difference between years. (Level 1).js
@@ -0,0 +1,19 @@
+// Difference between years. (Level 1)
+
+/*
+Write a function that receives two strings as parameter. 
+This strings are in the following format of date: YYYY/MM/DD. 
+Your job is: Take the years and calculate the difference between them.
+
+Examples:
+'1997/10/10' and '2015/10/10' -> 2015 - 1997 = returns 18 
+'2015/10/10' and '1997/10/10' -> 2015 - 1997 = returns 18
+At this level, you don't need validate months and days to calculate the difference.
+*/
+
+var howManyYears = function(date1, date2){
+  return Math.abs(date2.split("/")[0] - date1.split("/")[0])
+}
+
+console.log(howManyYears('1997/10/10', '2015/10/10')); // 18
+console.log(howManyYears('1990/10/10', '2015/10/10')); // 25
