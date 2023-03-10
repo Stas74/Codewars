ce580271f76fc9170702bd32714ca46dfa719245
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-10 14:08:48 -0700

    Update 731 The Pony Express.js

--- Kata/731 The Pony Express.js
+++ Kata/731 The Pony Express.js
@@ -40,3 +40,43 @@ console.log(riders([43, 23, 40, 13])); // 2
 console.log(riders([33, 8, 16, 47, 30, 30, 46])); // 3
 console.log(riders([6, 24, 6, 8, 28, 8, 23, 47, 17, 29, 37, 18, 40, 49])); // 4
 console.log(riders([43, 29, 18, 42, 24, 40, 40, 10, 16, 43])); // 4
+
+
+
+function riders(stations) {  
+  let rider = 1
+  let distance = 0
+  for(s of stations){
+    if(distance + s > 100){
+      rider += 1
+      distance = s
+    }
+    else {
+      distance += s
+    }
+  }
+  return rider
+}
+
+
+
+function riders(stations) {
+  let worker=1, sum=0;
+  for(let i=0; i<stations.length; i++){
+    sum += stations[i];
+    if (sum > 100){
+      sum=stations[i];
+      worker+=1;
+    }
+  }
+  return worker;
+}
+
+
+const riders = stations =>
+  stations.reduce(([dist, rid], val) => val + dist > 100 ? [val, ++rid] : [dist + val, rid], [0, 1])[1];
+
+
+function riders(st, n = 100) {
+  return st.map( el => n+el > 100 ? [100, n = el][0] : [0, n += el]).filter(e => e >= 100).length
+}
