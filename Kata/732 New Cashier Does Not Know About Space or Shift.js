f3496d9a8447356c0dcf871002b3dc77168dbd54
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-10 14:43:10 -0700

    Update 732 New Cashier Does Not Know About Space or Shift.js

--- Kata/732 New Cashier Does Not Know About Space or Shift.js
+++ Kata/732 New Cashier Does Not Know About Space or Shift.js
@@ -53,3 +53,58 @@ console.log(
   getOrder("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza")
 );
 // "Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
+
+
+
+
+const menu = ["Burger", "Fries", "Chicken", "Pizza", "Sandwich", "Onionrings", "Milkshake", "Coke"];
+const capitalize = word => word.slice(0, 1).toUpperCase() + word.slice(1);
+const comparator = (a, b) => menu.indexOf(a) - menu.indexOf(b);
+
+function getOrder(input) {
+  return input.match(new RegExp(menu.join("|"), "ig")).map(capitalize).sort(comparator).join(" ");
+}
+
+
+
+const MENU = {
+  Burger:     1,
+  Fries:      2,
+  Chicken:    3,
+  Pizza:      4,
+  Sandwich:   5,
+  Onionrings: 6,
+  Milkshake:  7,
+  Coke:       8,
+};
+const REG_CMD = new RegExp(Object.keys(MENU).join('|'), 'gi');
+
+function getOrder(cmd) {
+  return cmd.match(REG_CMD)
+            .map(s=>s.charAt(0).toUpperCase()+s.slice(1))
+            .sort((x,y)=>MENU[x]-MENU[y])
+            .join(' ');
+}
+
+
+
+function getOrder(input) {
+  let menu = ['Burger', 'Fries', 'Chicken', 'Pizza', 'Sandwich', 'Onionrings', 'Milkshake', 'Coke'];
+  let result = '';
+
+  menu.forEach(item => {
+    result += (item + ' ').repeat((input.match(new RegExp(item, 'gi')) || []).length);
+  });
+  return result.trim();
+}
+
+
+getOrder = a => ['burger','fries','chicken','pizza','sandwich','onionrings','milkshake','coke'].map(e=>(e+' ')
+                 .repeat(a.split(e).length-1)).join``.split` `.map(e=>e[0]?e[0].toUpperCase()+e.slice(1):'').join` `.trim()
+
+
+
+const getOrder = input =>
+  [`burger`, `fries`, `chicken`, `pizza`, `sandwich`, `onionrings`, `milkshake`, `coke`]
+    .reduce((pre, val) => [...pre, ...input.match(new RegExp(val, `g`)) || []], [])
+    .map(val => val[0].toUpperCase() + val.slice(1)).join(` `);
