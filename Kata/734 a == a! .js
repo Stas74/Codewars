ec7371a3e063b8b699bef7c971c137566737d70d
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-13 12:56:46 -0600

    Create 734 a == a! .js

---
+++ Kata/734 a == a! .js
@@ -0,0 +1,15 @@
+// !a == a ?!
+
+/*
+You task to pass only this tests :
+
+a == false
+!a == false
+a == !a
+*/
+
+const a = [];
+
+console.log(a == false); // true
+console.log(!a == false); // true
+console.log(a == !a); // true
