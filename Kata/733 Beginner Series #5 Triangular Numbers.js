bc71da384a7ad50dbc24946569bf0dce1e77e375
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-13 12:29:46 -0600

    Create 733 Beginner Series #5 Triangular Numbers.js

---
+++ Kata/733 Beginner Series #5 Triangular Numbers.js
@@ -0,0 +1,26 @@
+// Beginner Series #5 Triangular Numbers
+
+/*
+Triangular number is any amount of points that can fill an equilateral triangle.
+
+Example: the number 6 is a triangular number because all sides of a triangle has the same amount of points.
+
+alt text
+
+Hint!
+T(n) = n * (n + 1) / 2,
+n - is the size of one side.
+T(n) - is the triangular number.
+Given a number T from interval [1..2147483646], find if it is triangular number or not.
+*/
+
+function isTriangular(t) {
+  return Number.isInteger((-1 + (1 + 8 * t) ** 0.5) / 2);
+}
+
+console.log(isTriangular(1)); // true
+console.log(isTriangular(3)); // true
+console.log(isTriangular(6)); // true
+console.log(isTriangular(2)); // false
+console.log(isTriangular(7)); // false
+console.log(isTriangular(14)); // false
