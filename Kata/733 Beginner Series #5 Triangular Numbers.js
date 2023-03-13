74513d419275e1dc42cff3f2413e4470d7ffe9f8
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-13 12:39:25 -0600

    Update 733 Beginner Series #5 Triangular Numbers.js

--- Kata/733 Beginner Series #5 Triangular Numbers.js
+++ Kata/733 Beginner Series #5 Triangular Numbers.js
@@ -24,3 +24,32 @@ console.log(isTriangular(6)); // true
 console.log(isTriangular(2)); // false
 console.log(isTriangular(7)); // false
 console.log(isTriangular(14)); // false
+
+
+
+function isTriangular(t) {
+  return Math.sqrt(8*t + 1) % 1 == 0;
+}
+
+
+function isTriangular(t) {
+  for (let i = 1,n = 1;i<=t; n++){
+    i = n*(n+1)/2;
+    if (i==t) return true
+  }
+  return false;
+}
+
+
+
+function isTriangular(t) {
+  var i=1, s=0;
+  while (s < t)
+    s += i++;
+  return s == t;
+}
+
+
+
+const isTriangular = t =>
+  Number.isInteger((1 + (t << 3)) ** .5);
