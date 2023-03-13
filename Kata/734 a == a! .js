5a9a8ec12de370371bdafe73005230bcb62821c8
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-13 12:57:51 -0600

    Update 734 a == a! .js

--- Kata/734 a == a! .js
+++ Kata/734 a == a! .js
@@ -13,3 +13,34 @@ const a = [];
 console.log(a == false); // true
 console.log(!a == false); // true
 console.log(a == !a); // true
+
+
+
+const a = '0';
+
+
+const a = new Boolean();
+
+
+// First
+const a = []
+
+// Second
+// const a = ' '
+
+// Third
+// const a = '0'
+
+// Fourth
+// const a = new Boolean
+
+// Fifth
+// const a = {
+//   num: true,
+//   valueOf: function() {
+//     return !this.num
+//   }
+// }
+
+
+
