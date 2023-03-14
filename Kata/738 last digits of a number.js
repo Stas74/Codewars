50b242e5a71fd75766c2ce0bbf381d4863f966b4
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-14 13:53:31 -0600

    Create 738 last digits of a number.js

---
+++ Kata/738 last digits of a number.js
@@ -0,0 +1,29 @@
+// last digits of a number
+
+/*
+Your job is to implement a function which returns the last D digits of an integer N as a list.
+
+Special cases:
+If D > (the number of digits of N), return all the digits.
+If D <= 0, return an empty list.
+Examples:
+N = 1
+D = 1
+result = [1]
+
+N = 1234
+D = 2
+result = [3, 4]
+
+N = 637547
+D = 6
+result = [6, 3, 7, 5, 4, 7]
+*/
+
+function lastDigit(n, d) {
+  return d <= 0 ? [] : String(n).slice(-d).split("").map(el => +el);
+}
+
+console.log(lastDigit(1, 1)); // [1]
+console.log(lastDigit(123767, 4)); // [3,7,6,7]
+console.log(lastDigit(1234, 0)); // []
