41617c5581393a7b7946d1a92e6d1bf386b207b8
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-14 13:37:23 -0600

    Update 737 Spraying trees.js

--- Kata/737 Spraying trees.js
+++ Kata/737 Spraying trees.js
@@ -33,3 +33,17 @@ console.log(task("Wednesday", 10, 2));
 // 'It is Wednesday today, Robert, you have to work, you must spray 10 trees and you need 20 dollars to buy liquid'
 console.log(task("Monday", 4, 3));
 // 'It is Monday today, James, you have to work, you must spray 4 trees and you need 12 dollars to buy liquid'
+
+
+
+let task = (w,n,c) => {
+  let obj = {'Monday':'James','Tuesday':'John','Wednesday':'Robert','Thursday':'Michael','Friday':'William'};
+  return `It is ${w} today, ${obj[w]}, you have to work, you must spray ${n} trees and you need ${n*c} dollars to buy liquid`;
+};
+
+
+const task = (w, n, c) =>
+  (obj => `It is ${w} today, ${obj[w]}, you have to work, you must spray ${n} trees and you need ${n * c} dollars to buy liquid`)
+  ({Monday: `James`, Tuesday: `John`, Wednesday: `Robert`, Thursday: `Michael`, Friday: `William`});
+
+
