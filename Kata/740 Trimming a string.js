921007bf49bcc4a9ff069c8910e2995ddb03e058
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-15 16:19:48 -0600

    Update 740 Trimming a string.js

--- Kata/740 Trimming a string.js
+++ Kata/740 Trimming a string.js
@@ -34,3 +34,32 @@ console.log(trim("Creating kata is fun", 14)); // "Creating ka..."
 console.log(trim("Creating kata is fun", 2)); // "'Cr..."
 console.log(trim("He", 1)); // "H..."
 console.log(trim("Code Wars is pretty rad", 50)); // "Code Wars is pretty rad"
+
+
+
+function trim ( str, size ) {
+  if ( str.length <= size ) {
+    return str;
+  }  
+  const real = str.slice(0, size);  
+  return `${real.length < 4 ? real : real.slice(0, -3)}...`;
+}
+
+
+
+const trim = (str, size) => {
+  if(str.length <= size) return str
+  if(size <= 3)return str.slice(0,size) + '...'
+  if(size > 3) return str.slice(0,size-3)+'...' 
+}
+
+
+
+function trim(arr, size) {
+  if(arr.length <= size) return arr;
+  if(size > 3) size -= 3;
+  return arr.substr(0, size) + "...";
+}
+
+
+
