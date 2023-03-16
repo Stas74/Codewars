fa1fa8b89758559396ab2fb0c7a722029ad94581
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-16 13:33:57 -0600

    Update 744 Clocky Mc Clock-Face.js

--- Kata/744 Clocky Mc Clock-Face.js
+++ Kata/744 Clocky Mc Clock-Face.js
@@ -52,3 +52,53 @@ console.log(whatTimeIsIt(270)); // "09:00"
 console.log(whatTimeIsIt(360)); // "12:00"
 console.log(whatTimeIsIt(50)); // '01:40'
 console.log(whatTimeIsIt(4)); // '12:08'
+
+
+
+
+var whatTimeIsIt = function(angle) {
+let hour = Math.floor(angle / 30), minutes = Math.floor((angle % 30) * 2);
+if (hour < 10){hour = "0"+ hour;}
+if (hour <= 0){hour = 12;}
+if (minutes < 10){minutes = "0" + minutes;}
+  return hour + ":" + minutes;
+}
+
+
+
+var whatTimeIsIt = function (angle) {
+  let h = ~~(angle / 30),
+    m = ~~((angle % 30) * 2);
+  return `${h == 0 ? 12 : h > 9 ? h : "0" + h}:${m > 9 ? m : "0" + m}`;
+};
+
+
+var whatTimeIsIt = function(angle) {
+  // 1 degree  is 2 minutes
+  
+  var conversion = Math.floor(angle * 2); // Convert degrees to minutes. Round down
+  var hour = Math.floor(conversion / 60); // Calculate hours. Round down 
+  var minute = conversion - (hour * 60); // Calculate minutes
+  
+  
+  if (hour === 00){
+    hour = 12;
+  }
+  
+  if (hour < 10){
+    hour = "0" + hour;
+  }
+  
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+  
+  return hour + ":" + minute;
+}
+
+
+
+const whatTimeIsIt = angle =>
+  [(angle / 30 ^ 0) || 12, angle * 2 % 60 ^ 0].map(val => `${val}`.padStart(2, `0`)).join(`:`);
+
+
