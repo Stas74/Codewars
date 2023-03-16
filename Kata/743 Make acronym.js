d4043ca542f12de0f86af08f70fe937a6c583495
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-16 12:59:45 -0600

    Update 743 Make acronym.js

--- Kata/743 Make acronym.js
+++ Kata/743 Make acronym.js
@@ -23,3 +23,30 @@ console.log(toAcronym("Code Wars")); // "CW"
 console.log(toAcronym("Water Closet")); // "WC"
 console.log(toAcronym("Portable Network Graphics")); // "PNG"
 console.log(toAcronym("hyper text markup language")); // "HTML"
+
+
+
+function toAcronym( input ){
+  return input.replace(/(\w).*?\b[^\w]*/g,"$1").toUpperCase()
+}
+
+
+const toAcronym = inp =>
+  inp.match(/\b\w/g).join(``).toUpperCase();
+
+
+function toAcronym( input ){
+  var re = /\b[a-zA-Z]/g;
+  var result = input.match(re);
+  return result.join('').toUpperCase();
+}
+
+
+
+function toAcronym(inp){
+  let text = "";
+  for (let i = 0; i < inp.split(" ").length; i++) {
+    text += inp.split(" ")[i][0];
+  }
+  return text.toUpperCase();
+}
