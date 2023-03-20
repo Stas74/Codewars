87ac6a3353bc310b80c00c23ea411f7aad2bfa20
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-20 15:03:38 -0600

    Update 747 Number Pairs.js

--- Kata/747 Number Pairs.js
+++ Kata/747 Number Pairs.js
@@ -19,3 +19,29 @@ function getLargerNumbers(a, b) {
 console.log(getLargerNumbers([13, 64, 15, 17, 88], [23, 14, 53, 17, 80])); // [23, 64, 53, 17, 88]
 console.log(getLargerNumbers([34, -64, 15, 17, 88], [23, 14, 53, 17, 80])); // [34, 14, 53, 17, 88]
 
+
+
+
+function getLargerNumbers(a, b) {
+
+  var newArray = [];
+  
+  for ( i=0; i<a.length; i++) {
+    newArray.push( Math.max( a[i], b[i] ) );
+  }
+  
+  return newArray;
+}
+
+
+
+const getLargerNumbers = (a, b) => a.map((v, i) => Math.max(v, b[i]));
+
+
+
+const getLargerNumbers = (a, b) => a.reduce((acc, el, i) => [...acc, Math.max(el, b[i])], [])
+
+
+
+const getLargerNumbers = (a, b) => 
+  a.map((val, idx) => Math.max(val, b[idx]));
