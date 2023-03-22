1bf6766d8807acb00bb5e192cf6765ae3bd44c9a
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-22 12:19:15 -0600

    Update 751 Ch4113ng3.js

--- Kata/751 Ch4113ng3.js
+++ Kata/751 Ch4113ng3.js
@@ -14,3 +14,37 @@ console.log(nerdify("Fund4m3nt41s")); //  "Fund4m3nt41s"
 console.log(nerdify("Fundamentals")); //  "Fund4m3nt41s"
 console.log(nerdify("Seven")); //  "S3v3n"
 console.log(nerdify("Los Angeles")); //  "Los 4ng313s"
+
+
+
+const nerdify = t => t.replace(/[aelAE]/g, a => ({'a': 4, 'e': 3, 'l':1 }[a.toLowerCase()]));
+
+
+function nerdify(txt) {
+  txt = txt.replace(/a|A/g, "4");
+  txt = txt.replace(/e|E/g, "3");
+  txt = txt.replace(/l/g, "1");
+  return txt;
+}
+
+
+
+function nerdify(txt) {
+  return txt.replace(/[AaEel]/g, c => "44331"["AaEel".indexOf(c)])
+}
+
+
+let map = {
+  'a': 4,
+  'A': 4,
+  'e': 3,
+  'E': 3,
+  'l': 1
+}
+
+function nerdify(txt){
+  return txt
+          .split('')
+          .map(x => map[x] ? map[x] : x)
+          .join('');
+}
