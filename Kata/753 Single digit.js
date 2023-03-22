44a1f3070d1a87c4ae0eb9acba5bd50b9f0693cc
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-22 13:44:06 -0600

    Create 753 Single digit.js

---
+++ Kata/753 Single digit.js
@@ -0,0 +1,40 @@
+// Single digit
+
+/*
+The goal of this Kata is to reduce the passed integer to a single digit (if not already) 
+by converting the number to binary, taking the sum of the binary digits, 
+and if that sum is not a single digit then repeat the process.
+
+If the passed integer is already a single digit there is no need to reduce
+n will be an integer such that 0 < n < 10⁹
+
+For example given 5665 the function should return 5:
+
+5665 --> (binary) 1011000100001
+1011000100001 --> (sum of binary digits) 5
+
+Given 123456789 the function should return 1:
+
+123456789 --> (binary) 111010110111100110100010101
+111010110111100110100010101 --> (sum of binary digits) 16
+16 --> (binary) 10000
+10000 --> (sum of binary digits) 1
+*/
+
+function sumBin(arr) {
+  return arr.reduce((prev, cur) => prev + +cur, 0);
+}
+
+function singleDigit(n) {
+  if (n < 10) {
+    return n;
+  }
+  while (String(n).length != 1) {
+    n = sumBin(n.toString(2).split(""));
+  }
+  return n;
+}
+
+console.log(singleDigit(5)); //  5
+console.log(singleDigit(999)); //  8
+console.log(singleDigit(1234444123)); //  1
