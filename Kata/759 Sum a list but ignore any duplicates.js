01648724d59163ddbcb1abb4f4574fa046d6f54f
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-24 13:22:59 -0600

    Update 759 Sum a list but ignore any duplicates.js

--- Kata/759 Sum a list but ignore any duplicates.js
+++ Kata/759 Sum a list but ignore any duplicates.js
@@ -18,3 +18,45 @@ function sumNoDuplicates(numList) {
 console.log(sumNoDuplicates([1, 1, 2, 3])); // 5
 console.log(sumNoDuplicates([1, 1, 2, 2, 3])); // 3
 console.log(sumNoDuplicates([5, 6, 10, 3, 10, 10, 6, 7, 0, 9, 1, 1, 6, 3, 1])); // 21
+
+
+
+function sumNoDuplicates(arr) {
+        let uniqueArr = [];
+        let sum = 0;
+        arr.forEach(num => {
+            if (arr.indexOf(num) === arr.lastIndexOf(num)) {
+                uniqueArr.push(num);
+                sum += num;
+            }
+        });
+        return sum;
+}
+
+
+
+function sumNoDuplicates(numList) {
+  const uniqueArr = numList.filter(
+    (num, index) =>
+      numList.indexOf(num) === index && numList.lastIndexOf(num) === index
+  );
+  return uniqueArr.reduce((x, y) => x + y, 0);
+}
+
+
+
+function sumNoDuplicates(numList) {
+  return numList.filter((i)=> numList.indexOf(i) == numList.lastIndexOf(i)).reduce((a,b)=> a + b, 0)
+}
+
+
+
+function sumNoDuplicates(numList) {
+  const seen = new Set();
+  const duplicate = new Set();
+  numList.forEach(e => {
+    if(seen.has(e)) duplicate.add(e);
+    else seen.add(e);
+  });
+  return [...seen].filter(e => !duplicate.has(e)).reduce((a,b) => a + b, 0);
+}
