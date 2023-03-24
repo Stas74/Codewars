270c106e07a7d091bd89ac40e3ef5919d61791ac
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-24 12:58:13 -0600

    Update 758 Geometric Progression Sequence.js

--- Kata/758 Geometric Progression Sequence.js
+++ Kata/758 Geometric Progression Sequence.js
@@ -22,3 +22,25 @@ function geometricSequenceElements(a, r, n) {
 
 console.log(geometricSequenceElements(2, 3, 5)); // '2, 6, 18, 54, 162'
 console.log(geometricSequenceElements(2, 2, 10)); // '2, 4, 8, 16, 32, 64, 128, 256, 512, 1024'
+
+
+
+function geometricSequenceElements(a, r, n){
+  var res = [a];
+  for (var i = 0; i < n - 1; i++) {
+     res.push(res[i]*r);
+  }
+  return res.join(', ');
+}
+
+
+
+function geometricSequenceElements(a, r, n){
+  return [...Array(n)].map((_,i)=>a*r**i).join(', ')
+}
+
+
+
+let geometricSequenceElements = (a, r, n) => n === 1 ? '' + a : `${geometricSequenceElements(a, r, n - 1)}, ${a * Math.pow(r, n - 1)}`;
+
+
