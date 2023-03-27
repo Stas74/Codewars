84b57d6c21bcd8f18e72b2039579c0a608e9d88a
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-27 11:43:58 -0600

    Update 760 Whose bicycle.js

--- Kata/760 Whose bicycle.js
+++ Kata/760 Whose bicycle.js
@@ -72,3 +72,69 @@ console.log(
     }
   )
 ); // 'I need to buy a bicycle for my second son.'
+
+
+
+
+function whoseBicycle(dairy1, dairy2, dairy3) {
+  const diaries = [dairy1, dairy2, dairy3];
+  const son = {
+    0: 'first',
+    1: 'second',
+    2: 'third'
+  };
+  const scores = diaries.map((diary, index) => {
+    const keys = Object.keys(diary);
+    
+    return keys.reduce((acc, cur) => acc + diary[cur], 0);
+  });
+  
+  const maxValue = Math.max(...scores);
+  
+  const maxDiary = scores.lastIndexOf(maxValue);
+  
+  return `I need to buy a bicycle for my ${son[maxDiary]} son.`;
+}
+
+
+
+function whoseBicycle(a, b, c) {
+  [a, b, c] = [a, b, c].map(x => Object.values(x).reduce((y, z) => y + z, 0));
+  let n = Math.max(a, b, c);
+  return `I need to buy a bicycle for my ${n === c ? "third" : n === b ? "second" : "first"} son.`;
+}
+
+
+
+function whoseBicycle(diary1, diary2, diary3) {
+  const total1 = Object.values(diary1).reduce((total, currentValue) => total + currentValue);
+  const total2 = Object.values(diary2).reduce((total, currentValue) => total + currentValue);
+  const total3 = Object.values(diary3).reduce((total, currentValue) => total + currentValue);
+
+  if (total1 > total2 && total1 > total3) {
+    return 'I need to buy a bicycle for my first son.';
+  } else if (total2 >= total1 && total2 > total3) {
+    return 'I need to buy a bicycle for my second son.';
+  } else if (total3 >= total1 && total3 >= total2) {
+    return 'I need to buy a bicycle for my third son.';
+  }
+}
+
+
+
+class Son {
+  constructor(num, diary) {
+    this.num = num;
+    this.sum = Object.values(diary).reduce((pre, val) => pre + val);
+    this.age = ageTable[`${num}SonAge`];
+  }
+}
+
+const whoseBicycle = (diary1, diary2, diary3) =>
+  `I need to buy a bicycle for my ${
+    [
+      new Son(`first`, diary1),
+      new Son(`second`, diary2),
+      new Son(`third`, diary3),
+    ].sort((a, b) => b.sum - a.sum || a.age - b.age)[0].num
+  } son.`;
