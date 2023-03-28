382ccce71439278b497e4139819a24e8ec915713
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-28 13:35:12 -0600

    Update 765 Word to binary.js

--- Kata/765 Word to binary.js
+++ Kata/765 Word to binary.js
@@ -13,3 +13,17 @@ function wordToBin(str) {
 
 console.log(wordToBin("man")); // [ '01101101', '01100001', '01101110' ]
 console.log(wordToBin("AB")); // ['01000001', '01000010']
+
+
+
+const wordToBin = (x) => [...x].map(el=> el.codePointAt().toString(2).padStart(8,'0'))
+
+
+
+function wordToBin(str) {
+  return Array.from(str, c => "0" + c.charCodeAt(0).toString(2))
+}
+
+
+const wordToBin = str =>
+  [...str].map(val => `0${val.charCodeAt().toString(2)}`);
