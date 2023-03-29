2c927a04a8a68b4f1805373717c1c9aedf73fea1
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-29 13:11:30 -0600

    Update 766 Array comparator.js

--- Kata/766 Array comparator.js
+++ Kata/766 Array comparator.js
@@ -16,3 +16,25 @@ console.log(
   )
 ); // 1
 console.log(matchArrays([1, 2, 3, 4, 5], [2, 3, 4, 5, 6])); // 4
+
+
+
+const matchArrays = (a, b) => a.filter(e => b.includes(e)).length;
+
+
+function matchArrays(a, b) {
+  const s = new Set(a);
+  return b.reduce((a, x) => a + s.has(x), 0);
+}
+
+
+
+function matchArrays(v, r) {
+  return v.filter(el => r.indexOf(el) > -1).length;
+}
+
+
+
+function matchArrays(v, r) {
+  return v.reduce((n, x) => r.indexOf(x) >= 0 ? n + 1 : n, 0)
+}
