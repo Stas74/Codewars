50515e1c57dbf43d16126344daa60a5dbabb6e00
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-29 17:24:44 -0600

    Update 768 Highest Rank Number in an Array.js

--- Kata/768 Highest Rank Number in an Array.js
+++ Kata/768 Highest Rank Number in an Array.js
@@ -29,3 +29,62 @@ function highestRank(arr) {
 
 console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12])); // 12
 console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10])); // 12
+
+
+
+function highestRank(arr) {
+  return arr.sort(
+    (a, b) =>
+      arr.filter((i) => i === b).length - arr.filter((i) => i === a).length
+  )[0];
+}
+
+
+function highestRank(arr){
+  var obj = {};
+  arr.forEach(function(elem){
+     if(obj[elem] === undefined)
+       obj[elem] = 0;
+     obj[elem]++;
+  });
+  var keys = Object.keys(obj), highest = 0, key;
+  keys.forEach(function(elem){
+     if(obj[elem] >= highest)
+     {
+        highest = obj[elem];
+        key = elem;
+     }
+  })
+  return parseInt(key);
+}
+
+
+
+function highestRank(arr) {
+  var high,
+    max = 0,
+    rec = {};
+  for (var x of arr) {
+    rec[x] = (rec[x] || 0) + 1;
+    if (rec[x] > max) {
+      max = rec[x];
+      high = x;
+    }
+  }
+  return high;
+}
+
+
+
+const highestRank = arr =>
+  arr.sort((a, b) => arr.filter(val => val === b).length - arr.filter(val => val === a).length || b - a)[0];
+
+
+
+const highestRank = arr => {
+    let nums = {};
+    arr.forEach(el => {
+        nums[el] ? nums[el]++ : nums[el] = 1
+    })   
+    return +Object.keys(nums).reduce((a, b) => nums[a] > nums[b] ? a : b);
+}
