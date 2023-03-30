72f55f4c20d098b1d6712855d9a3a0c7b9f2b349
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-30 16:10:35 -0600

    Create 771 99 Problems, #1 - last in list.js

---
+++ Kata/771 99 Problems, #1 - last in list.js
@@ -0,0 +1,16 @@
+// 99 Problems, #1: last in list
+
+/*
+Write a function last that accepts a list and returns the last element in the list.
+
+If the list is empty:
+
+In languages that have a built-in option or result type (like OCaml or Haskell), return an empty option
+
+In languages that do not have an empty option, just return null
+*/
+
+const last = (xs) => (xs.length == 0 ? null : xs.pop());
+
+console.log(last([1, 2, 3])); // 3
+console.log(last([])); // null
