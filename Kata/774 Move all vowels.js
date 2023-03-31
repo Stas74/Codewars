db1744160ed470d2f95f7fe5158b4a7390dc077b
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-31 14:52:35 -0600

    Update 774 Move all vowels.js

--- Kata/774 Move all vowels.js
+++ Kata/774 Move all vowels.js
@@ -18,3 +18,42 @@ function moveVowel(input) {
 
 console.log(moveVowel("apple")); // "pplae"
 console.log(moveVowel("peace")); // "pceae"
+
+
+
+function moveVowel(input) 
+{
+  var cs = "";
+  var vs = "";
+  for(var i=0;i<input.length;i++)
+  {
+    if("aeiouAEIOU".indexOf(input.charAt(i)) == -1)
+    {
+      cs += input.charAt(i);
+    }
+    else
+    {
+      vs += input.charAt(i);
+    }
+  }
+  return cs + vs;
+}
+
+
+function moveVowel(input) {
+    var vow = input.match(/[aeiou]/gi) || [];
+    var conso = input.match(/[^aeiou]/gi) || [];
+    return conso.concat(vow).join('')
+}
+
+
+function moveVowel(s) {
+  return (k='',s=s.replace(/[aeuoi]/g,m=>(k+=m,'')),s+k)
+}
+
+
+function moveVowel(input) {
+return (input.match(/[^aeiou]/g)||[]).join("") + (input.match(/[aeiou]/g)||[]).join("")
+}
+
+
