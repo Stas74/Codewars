4c10b4ec1aa3b5586ce15fb57fcbd14f1efc60f6
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-03-31 14:43:36 -0600

    Update 773 Fruit string calculator.js

--- Kata/773 Fruit string calculator.js
+++ Kata/773 Fruit string calculator.js
@@ -28,3 +28,29 @@ function calculate(string) {
 
 console.log(calculate("Panda has 48 apples and loses 4")); // 44
 console.log(calculate("Jerry has 34 apples and gains 6")); // 40
+
+
+
+function calculate(string) {
+  return eval(string.replace('loses','-').replace('gains','+').replace(/[a-zA-Z]/g,''));
+}
+
+
+
+function calculate(string) {
+  return eval(string.match(/\d+|lose|gain/g).join("").replace("lose", "-").replace("gain", "+")) 
+}
+
+
+calculate = string =>
+    string.split(' ')
+        .map(x => parseInt(x))
+        .filter(x => !isNaN(x))
+        .reduce((acc, x) => string.indexOf('loses') !== -1 ? acc - x : acc + x )
+
+
+
+function calculate(string) {
+  var [a, b] = string.match(/\d+/g);
+  return /loses/.test(string) ? a-b : +a+ +b;
+}
