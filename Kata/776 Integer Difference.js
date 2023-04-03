6509dfd65439c2e11f074166d16ae540c8983d5d
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-03 13:23:39 -0600

    Update 776 Integer Difference.js

--- Kata/776 Integer Difference.js
+++ Kata/776 Integer Difference.js
@@ -24,3 +24,34 @@ const intDiff = (arr, n) => {
 
 console.log(intDiff([1, 1, 5, 6, 9, 16, 27], 4)); // 3
 console.log(intDiff([1, 1, 3, 3], 2)); // 4
+
+
+
+const intDiff = (arr, n) => {
+  let count = 0;
+  for(let i=1; i<arr.length; i++)
+    for(let j=0; j<i; j++)
+        if (Math.abs(arr[i]-arr[j]) === n) count++;
+  return count;
+}
+
+
+
+const intDiff = (arr, n) => 
+  arr.reduce((pre, val, idx) => pre + arr.slice(++idx).filter(v => Math.abs(v - val) === n).length, 0);
+
+
+
+const intDiff = function(arr, n){
+  let [ out, k ] = [ [], 0 ]
+  for ( const i of arr ){
+    out = out.concat( arr.slice(++k).filter(el => Math.abs(el - i) === n ))
+  }
+  return out.length;
+}
+
+
+
+const intDiff = ([first, ...rest], n, count = 0) => rest.length ? intDiff(rest, n, count + rest.reduce((a, v) => a + (Math.abs(first - v) == n), 0)) : count;
+
+
