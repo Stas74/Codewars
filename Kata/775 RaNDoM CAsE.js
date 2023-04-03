68c712c241873b29f53c35b60839d7050cf9906b
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-03 12:54:17 -0600

    Create 775 RaNDoM CAsE.js

---
+++ Kata/775 RaNDoM CAsE.js
@@ -0,0 +1,34 @@
+// RaNDoM CAsE
+
+/*
+Write a function that will randomly upper and lower characters in a string - randomCase() (random_case() for Python).
+
+A few examples:
+
+randomCase("Lorem ipsum dolor sit amet, consectetur adipiscing elit") == 
+"lOReM ipSum DOloR SiT AmeT, cOnsEcTEtuR aDiPiSciNG eLIt"
+
+randomCase("Donec eleifend cursus lobortis") == "DONeC ElEifEnD CuRsuS LoBoRTIs"
+Notes:
+
+This function will work within the basic ASCII character set to make this kata easier - 
+so no need to make the function multibyte safe.
+
+The letters MUST be selected randomly - filters are set to make sure there is no cheating!
+*/
+
+function randomCase(x) {
+  return x
+    .split("")
+    .map((el) =>
+      el == " "
+        ? " "
+        : Math.round(Math.random())
+        ? el.toUpperCase()
+        : el.toLowerCase()
+    )
+    .join("");
+}
+
+console.log(randomCase("this is an all lower string")); //
+console.log(randomCase("peace")); //
