bbafad519e19a3f1581f1dd464be95a047d9fe1f
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-04 13:06:11 -0600

    Update 779 Basic JS - Calculating averages.js

--- Kata/779 Basic JS - Calculating averages.js
+++ Kata/779 Basic JS - Calculating averages.js
@@ -22,3 +22,45 @@ var Calculator = {
 
 console.log(Calculator.average(3, 4, 5)); // 4
 console.log(Calculator.average(3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3)); // 5.769230769230769
+
+
+
+var Calculator = {
+ average: function() {
+   
+  return Array.prototype.slice.call(arguments).reduce(function(prev, next) {
+    return prev + next;
+  }, 0) / (arguments.length || 1);
+ }
+};
+
+
+
+var Calculator = {
+ average: function(...args) {
+    return args.length==0?0:args.reduce((a,b)=>b+a)/args.length
+ }
+};
+
+
+
+var Calculator = {
+ average: function() {
+   var average = 0;
+   for (var i = 0; i < arguments.length; i++) {
+     average += arguments[i];
+   }
+   average = average / Math.max(arguments.length, 1);
+   return average;
+ }
+};
+
+
+var Calculator = {
+ average: (...$) => $.reduce((a, b) => a + b, 0) / $.length || 0
+};
+
+
+
+Calculator.average = (...args) => 
+  args.reduce((pre, val) => pre + val, 0) / (args.length || 1);
