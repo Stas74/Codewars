a4c259aa2336d7325fb464fd9866ee3614ff50b1
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-04 11:59:35 -0600

    Create 778 The reject() function.js

---
+++ Kata/778 The reject() function.js
@@ -0,0 +1,17 @@
+// The reject() function
+
+/*
+Implement a function which filters out array values which satisfy the given predicate.
+
+reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)  =>  [1, 3, 5]
+*/
+
+function reject(array, predicate) {
+  return array.filter((el) => !predicate(el));
+}
+
+console.log(
+  reject([1, 2, 3, 4, 5, 6], function (num) {
+    return num % 2 == 0;
+  })
+); // [1, 3, 5]
