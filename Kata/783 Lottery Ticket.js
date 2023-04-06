fba0b068bb0142b114e8f6b2184161a00130c827
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-06 13:46:36 -0600

    Create 783 Lottery Ticket.js

---
+++ Kata/783 Lottery Ticket.js
@@ -0,0 +1,32 @@
+// Lottery Ticket
+
+/*
+Time to win the lottery!
+
+Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
+
+Example ticket:
+
+[ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+
+To do this, you must first count the 'mini-wins' on your ticket. 
+Each subarray has both a string and a number within it. 
+If the character code of any of the characters in the string matches the number, you get a mini win. 
+Note you can only have one mini win per sub array.
+
+Once you have counted all of your mini wins, compare that number to the other input provided (win). 
+If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
+
+All inputs will be in the correct format. Strings on tickets are not always the same length.
+
+*/
+
+function bingo(ticket, win){
+  let total = 0;
+  ticket.map(el => el[0].split("").includes(String.fromCharCode(el[1])) ? total += 1 : total += 0);
+  return total < win ? 'Loser!' : 'Winner!'
+}
+
+console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 2)); // 'Loser!'
+console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 1)); // 'Winner!'
+console.log(bingo([['HGTYRE', 74], ['BE', 66], ['JKTY', 74]], 3)); // 'Loser!'
