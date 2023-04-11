9f1837d3ba3952138455ce732bd48d6f9d250e79
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-11 12:57:30 -0600

    Update 787 Framed Reflection.js

--- Kata/787 Framed Reflection.js
+++ Kata/787 Framed Reflection.js
@@ -30,3 +30,39 @@ function mirror(text) {
 console.log(mirror("Hello World")); // '*********\n* olleH *\n* dlroW *\n*********'
 console.log(mirror("Codewars")); // '************\n* srawedoC *\n************'
 console.log(mirror("Hellooo World")); // '*********\n* olleH *\n* dlroW *\n*********'
+
+
+
+
+function mirror(s) {
+  let a = s.split(" ");
+  let m = Math.max(...a.map((x) => x.length));
+  a = a.map(
+    (x) => "* " + [...x].reverse().join("") + " ".repeat(m - x.length) + " *"
+  );
+  return ["*".repeat(m + 4), ...a, "*".repeat(m + 4)].join("\n");
+}
+
+
+
+function mirror(text) {
+  const words = text.split(' ');
+  const width = Math.max.apply(null, words.map(w => w.length));
+  const tb = '*'.repeat(width + 4);
+  const revs = words.map(w => `* ${Array.from(w).reverse().join('')}${' '.repeat(width - w.length)} *`).join('\n');
+  return `${tb}\n${revs}\n${tb}`;
+}
+
+
+
+mirror = (a) =>
+  (([a, b]) =>
+    [
+      "*".repeat(4 + b),
+      ...a
+        .map((a) => a.split``.reverse().join``)
+        .map((a) => "* " + a + " ".repeat(1 + b - a.length) + "*"),
+      "*".repeat(4 + b),
+    ].join`\n`)(((a) => [a, Math.max(...a.map((a) => a.length))])(a.split` `));
+
+
