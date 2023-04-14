8fc1df4ba933d7a8e622c06afebb628226554961
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-14 13:47:13 -0600

    Update 795 Array Array Array.js

--- Kata/795 Array Array Array.js
+++ Kata/795 Array Array Array.js
@@ -33,3 +33,24 @@ console.log(explode(["a", 3])); // [['a', 3], ['a', 3], ['a', 3]]
 console.log(explode([6, "c"])); // [[6, 'c'], [6, 'c'], [6, 'c'], [6, 'c'], [6, 'c'], [6, 'c']]
 console.log(explode(["a", "b"])); // 'Void!'
 console.log(explode(["a", 0])); // []
+
+
+
+function explode(x){
+  let [a,b] = x;
+  
+  if(typeof a == "number" && typeof b == "number") return new Array(a+b).fill(x);
+  else if(typeof a == "number") return new Array(a).fill(x);
+  else if(typeof b == "number") return new Array(b).fill(x);
+  
+  return 'Void!';
+}
+
+
+const explode= ([x,y]) => x + 0 != x && y + 0 != y ? "Void!" : Array((+x||0) + (+y||0)).fill([x,y])
+
+
+// destructuring
+const explode = ({0:a, 1:b, 0:{0:x}, 1:{0:y}}) => x && y ? 'Void!' : Array((+a | 0) + (+b | 0)).fill([a,b])
+
+
