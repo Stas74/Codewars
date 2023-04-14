6ea6c49e4b3b9fe0eb3e5af5249f03d3bd9c547d
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-14 16:43:53 -0600

    Update 796 String Scramble.js

--- Kata/796 String Scramble.js
+++ Kata/796 String Scramble.js
@@ -35,3 +35,32 @@ function scramble(str, arr) {
 console.log(scramble("abcd", [0, 3, 1, 2])); // "acdb"
 console.log(scramble("sc301s", [4, 0, 3, 1, 5, 2])); // "c0s3s1"
 console.log(scramble("bskl5", [2, 1, 4, 3, 0])); // "5sblk"
+
+
+
+function scramble(str, arr) {
+  for(var r=[],i=0;i<arr.length;i++) r[arr[i]]=str[i]
+  return r.join("")
+};
+
+
+
+const scramble = (s,ar) => s.split('').reduce((a,e,i,arr) => a + arr[ar.indexOf(i)],'');
+
+
+
+function scramble(s, a) {
+  return a.map((_,i)=>s[a.indexOf(i)]).join``
+};
+
+
+function scramble(str, arr) {
+  let output = [];
+  arr.forEach((charIdx, i) => output[charIdx] = str[i])
+  return output.join('');
+};
+
+
+
+const scramble = (str, arr) =>
+  arr.map((_, idx) => str[arr.indexOf(idx)]).join(``);
