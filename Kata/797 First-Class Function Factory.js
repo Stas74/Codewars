d7b14512ecc2c32c51b8f345f5a9a3471639efcc
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-17 12:34:58 -0600

    Create 797 First-Class Function Factory.js

---
+++ Kata/797 First-Class Function Factory.js
@@ -0,0 +1,35 @@
+// First-Class Function Factory
+
+/*
+
+Write a function, factory, that takes a number as its parameter and returns another function.
+
+The returned function should take an array of numbers as its parameter, 
+and return an array of those numbers multiplied by the number that was passed into the first function.
+
+In the example below, 5 is the number passed into the first function. 
+So it returns a function that takes an array and multiplies all elements in it by five.
+
+Translations and comments (and upvotes) welcome!
+
+Example
+var fives = factory(5);       // returns a function - fives
+var myArray = [1, 2, 3];
+fives(myArray);               //returns [5, 10, 15];
+
+*/
+
+// function scramble(str, arr) {
+//   return arr.map((el, idx) => str[arr[el]]).join("");
+// }
+
+function factory(x) {
+  return arr => arr.map( el => el * x );
+}
+
+var myArray = [1, 2, 3];
+
+var threes = factory(3);
+console.log(threes(myArray)); // [3, 6, 9]
+var fives = factory(5);
+console.log(fives(myArray)); // [5, 10, 15]
