1c2ef5462d62529d8a1e56f8469dba57208195e5
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-17 13:36:27 -0600

    Update 798 Total pressure calculation.js

--- Kata/798 Total pressure calculation.js
+++ Kata/798 Total pressure calculation.js
@@ -14,3 +14,18 @@ const solution = (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) 
 
 console.log(solution(44, 30, 3, 2, 5, 50)); // 0.7146511212121212
 console.log(solution(60, 20, 10, 30, 10, 100)); // 5.099716666666667
+
+
+solution=(m1,m2,M1,M2,v,t) => (M1 / m 1+ M2 / m2) * 0.082 * (t + 273.15) / v
+
+
+function solution(M1, M2, m1, m2, V, T) {
+		M1 = m1 * 0.001/M1;
+    M2 = m2 * 0.001/M2;
+    T = T + 273.15;
+	var R = 0.082;
+
+	return (((M1 + M2) * R * T) / V) * 1000;
+}
+
+
