9d9b582127e3645957d5ba6c5ca167c4690779ef
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-18 11:23:37 -0600

    Update 800 Ones' Complement.js

--- Kata/800 Ones' Complement.js
+++ Kata/800 Ones' Complement.js
@@ -20,3 +20,38 @@ console.log(onesComplement("0")); // "1"
 console.log(onesComplement("1")); // "0"
 console.log(onesComplement("01")); // "10"
 console.log(onesComplement("1101")); // "0010"
+
+
+
+function onesComplement(n) {
+  return [...n].map(e => e === '1' ? '0' : '1').join('')
+};
+
+
+function onesComplement(n) {
+  return [...n].map( b => b^1 ).join``
+};
+
+
+const onesComplement = (str) => str.replace(/\d/g, x=> x === '0' ? '1' : '0')
+
+
+const onesComplement = n => n.replace(/./g, i => '10'[i])
+
+
+const onesComplement = n =>
+  n.replace(/./g, val => val ^ 1);
+
+
+
+function onesComplement(n) {
+  var result = ''
+  for (let i of n){
+    if(i == '1'){
+      result += '0'
+    }else{
+      result += '1'
+    }
+  }
+  return result
+}
