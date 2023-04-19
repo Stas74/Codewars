4c90b6159db7889ca91aadff079924212217cbcd
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-19 12:44:43 -0600

    Update 803 Larger Product or Sum.js

--- Kata/803 Larger Product or Sum.js
+++ Kata/803 Larger Product or Sum.js
@@ -39,3 +39,27 @@ function sumOrProduct(array, n) {
 console.log(sumOrProduct([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4)); // "sum"
 console.log(sumOrProduct([10, 41, 8, 16, 20, 36, 9, 13, 20], 3)); // "product"
 console.log(sumOrProduct([10, 20, 3, 30, 5, 4], 3)); // "same"
+
+
+
+
+const sumOrProduct = (arr, n) => {
+  const sorted = arr.sort((a, b) => a - b);
+  const sum = sorted.slice(-n).reduce((acc, i) => acc + i);
+  const product = sorted.slice(0, n).reduce((acc, i) => acc * i, 1);
+  return sum > product ? 'sum' : sum < product ? 'product' : 'same';
+}
+
+
+
+const sumOrProduct = (array, n) =>
+  (([p, s]) => p > s ? `product` : p < s ? `sum` : `same`)
+  (array.sort((a, b) => a - b).reduce(([p, s], val, idx) => [p * (idx < n ? val : 1), s + (idx >= array.length - n && val)], [1, 0]));
+
+
+
+function sumOrProduct(arr, n) {
+    const sum = arr.sort((a, b) => b - a).reduce((s, a, i) => i < n ? s + a : s, 0);
+    const pro = arr.sort((a, b) => a - b).reduce((s, a, i) => i < n ? s * a : s, 1);
+    return pro > sum ? "product" : sum > pro ? "sum" : "same"    
+}
