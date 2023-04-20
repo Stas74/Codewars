47d50683dbffb563bdda4812aa364fe7ef8e46cb
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-20 14:16:58 -0600

    Update 805 Arguments to Binary addition.js

--- Kata/805 Arguments to Binary addition.js
+++ Kata/805 Arguments to Binary addition.js
@@ -31,3 +31,11 @@ console.log(arr2bin([1, 2, 3, 4, 5])); // "1111"
 console.log(arr2bin([1, 10, 100, 1000])); // "10001010111"
 console.log(arr2bin([null])); // "0"
 console.log(arr2bin([true, true, false, 15])); // "1111"
+
+
+
+function arr2bin(arr) {
+  return arr.reduce((sum, cur) => typeof(cur) === 'number' ? sum + cur : sum, 0).toString(2);
+}
+
+
