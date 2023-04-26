71154aba884e850ae2efe53dddf0af5c82376abe
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-26 14:28:44 -0600

    Create 814 Ordering the words!.js

---
+++ Kata/814 Ordering the words!.js
@@ -0,0 +1,27 @@
+// Ordering the words!
+
+/*
+
+Given a string, you need to write a method that order every letter in this string in ascending order.
+
+Also, you should validate that the given string is not empty or null. If so, the method should return:
+
+"Invalid String!"
+Notes
+• the given string can be lowercase and uppercase.
+• the string could include spaces or other special characters like '# ! or ,'. Sort them based on their ASCII codes
+Examples
+"hello world" => " dehllloorw"
+"bobby"       => "bbboy"
+""            => "Invalid String!"
+"!Hi You!"    => " !!HYiou"
+
+*/
+
+function orderWord(s) {
+  return s ? s.split("").sort().join("") : "Invalid String!";
+}
+
+console.log(orderWord("Hello, World!")); //  " !,HWdellloor"
+console.log(orderWord("bobby")); //  "bbboy"
+console.log(orderWord("")); //  "Invalid String!"
