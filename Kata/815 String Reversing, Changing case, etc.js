9622caa273bfb92479f0c96da3ee662134f6de16
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-26 15:31:34 -0600

    Update 815 String Reversing, Changing case, etc.js

--- Kata/815 String Reversing, Changing case, etc.js
+++ Kata/815 String Reversing, Changing case, etc.js
@@ -20,3 +20,31 @@ function reverseAndMirror(s1, s2) {
 s1 = "FizZ";
 s2 = "buZZ";
 console.log(reverseAndMirror(s1,s2)); //  "zzUB@@@zZIffIZz"
+
+
+
+function reverseAndMirror(s1,s2) {
+  s1 = [...s1].map(x => x == x.toLowerCase() ? x.toUpperCase() : x.toLowerCase()).join('')
+  s2 = [...s2].map(x => x == x.toLowerCase() ? x.toUpperCase() : x.toLowerCase()).reverse().join('')
+  return `${s2}@@@${[...s1].reverse().join('')}${s1}`
+}
+
+
+
+function reverseAndMirror(s1,s2) {
+  return s2.split('').reverse().map(symbol => symbol.toLowerCase() != symbol ? symbol.toLowerCase() : symbol.toUpperCase()).join('')
+    + "@@@"
+    + s1.split('').reverse().map(symbol => symbol.toLowerCase() != symbol ? symbol.toLowerCase() : symbol.toUpperCase()).join('')
+    + s1.split('').map(symbol => symbol.toLowerCase() != symbol ? symbol.toLowerCase() : symbol.toUpperCase()).join('');
+}
+
+
+
+const reverseAndMirror = (s1, s2) =>
+  (fn => `${fn(s2)}@@@${fn(s1)}${[...fn(s1)].reverse().join(``)}`)
+  (str => [...str].map(val => val[`to${val < `a` ? `Low` : `Upp`}erCase`]()).reverse().join(``));
+
+
+
+const reverseAndMirror = (x, y) => `${reverse(y)}@@@${reverse(x)}${reverse(x).split``.reverse().join``}`;
+const reverse = s => s.split``.reverse().map(e => e === e.toUpperCase() ? e.toLowerCase() : e.toUpperCase()).join``;
