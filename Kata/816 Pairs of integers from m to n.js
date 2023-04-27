9f3f256fda9b36f8506a322833f865eae81b3029
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-04-27 13:40:12 -0600

    Create 816 Pairs of integers from m to n.js

---
+++ Kata/816 Pairs of integers from m to n.js
@@ -0,0 +1,27 @@
+// Pairs of integers from m to n
+
+/*
+
+Implement a function that receives two integers m and n and generates a sorted list of pairs (a, b) such that m <= a <= b <= n.
+
+The input m will always be smaller than or equal to n (e.g., m <= n)
+
+Example
+m = 2
+n = 4
+
+result = [(2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (4, 4)]
+
+*/
+
+function generatePairs(m, n) {
+  const finArr = [];
+  for (let i = m; i <= n; i++) {
+    for (let j = i; j <= n; j++) {
+      finArr.push([i, j]);
+    }
+  }
+  return finArr;
+}
+
+console.log(generatePairs(2, 4)); // [ [2, 2], [2, 3], [2, 4], [3, 3], [3, 4], [4, 4] ]
