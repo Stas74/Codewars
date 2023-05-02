31f12c4e9dc9a3f622b9414664a2477b45cb5a66
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-02 12:07:15 -0600

    Create 822 Array2Binary addition.js

---
+++ Kata/822 Array2Binary addition.js
@@ -0,0 +1,25 @@
+// Array2Binary addition
+
+/*
+
+Given an array containing only integers, add all the elements and return the binary equivalent of that sum.
+
+If the array contains any non-integer element (e.g. an object, a float, a string and so on), return false.
+
+Note: The sum of an empty array is zero.
+
+arr2bin([1,2]) == '11'
+arr2bin([1,2,'a']) == false
+
+*/
+
+function arr2bin(arr) {
+  return arr.some((el) => typeof el !== "number")
+    ? false
+    : arr.reduce((prev, cur) => prev + cur, 0).toString(2);
+}
+
+console.log(arr2bin([1, 2])); // "11"
+console.log(arr2bin([1, 2, "a"])); // false
+console.log(arr2bin([1, 2, 3, 4, 5])); // "1111"
+console.log(arr2bin([1, 10, 100, 1000])); // "10001010111"
