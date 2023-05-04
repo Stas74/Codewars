a67a36be607c1b871ceb792708490b166438e278
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-04 15:32:08 -0600

    Update 827 Change it up.js

--- Kata/827 Change it up.js
+++ Kata/827 Change it up.js
@@ -34,3 +34,39 @@ function changer(str) {
 }
 
 console.log(changer("Cat30")); // 'dbU30'
+
+
+
+
+const changer = str =>
+  str
+    .toLowerCase()
+    .replace(/[a-z]/g, m => String.fromCharCode((m.charCodeAt(0) - 96) % 26 + 97)) 
+    .replace(/[aeiou]/g, m => m.toUpperCase())
+
+
+
+const letters = "abcdEefghIijklmnOopqrstUuvwxyzA";
+const changer = s => s.replace( /[a-z]/gi, c => letters[ letters.indexOf(c.toLowerCase())+1 ] );
+
+
+function changer(str) { 
+  var a='AbcdEfghIjklmnOpqrstUvwxyz';
+  var s='zabcdefghijklmnopqrstuvwxy';
+  var ans='';
+  str=str.toLowerCase();
+  for (var i=0; i<str.length; ++i)
+    if (str[i]>='a' && str[i]<='z')
+      ans+=a[s.indexOf(str[i])];
+    else
+      ans+=str[i];
+  return ans;
+}
+
+
+
+changer=a=>a.toLowerCase().replace(/[a-z]/g,a=>a<'z'?String.fromCharCode(a.charCodeAt()+1):'a').replace(/[aeioue]/g,a=>a.toUpperCase())
+
+
+
+
