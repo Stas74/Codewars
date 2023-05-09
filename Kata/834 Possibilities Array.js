42819dc37141db031efb746106eaa306ba938b67
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-09 13:23:31 -0600

    Update 834 Possibilities Array.js

--- Kata/834 Possibilities Array.js
+++ Kata/834 Possibilities Array.js
@@ -29,3 +29,24 @@ function isAllPossibilities(x) {
 console.log(isAllPossibilities([0, 1, 2, 3])); // true
 console.log(isAllPossibilities([1, 2, 3, 4])); // false
 console.log(isAllPossibilities([])); // false
+
+
+
+function isAllPossibilities(x){
+	return x.length > 0 ? x.every((a,i) => x.includes(i)) : false;
+}
+
+
+function isAllPossibilities(a) {
+  return !!a.length && a.sort().every((x, i) => x === i);
+}
+
+
+const isAllPossibilities = x =>
+  !!x.length && x.every((_, idx) => x.includes(idx));
+
+
+function isAllPossibilities(array) {
+  const [first, ...rest] = array.slice().sort();
+  return first === 0 && rest.every((n, i) => n === i + 1);
+}
