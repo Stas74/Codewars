8c4cb8dd79b1d76df31c318de48975c25c6d4890
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-09 13:52:50 -0600

    Update 835 Alphabetically ordered.js

--- Kata/835 Alphabetically ordered.js
+++ Kata/835 Alphabetically ordered.js
@@ -20,3 +20,39 @@ console.log(alphabetic("asd")); // false
 console.log(alphabetic("codewars")); // false
 console.log(alphabetic("door")); // true
 console.log(alphabetic("cell")); // true
+
+
+
+function alphabetic(s) {
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] < s[i - 1]) {
+      // If the current character is less than the previous character, the string is not in alphabetical order
+      return false;
+    }
+  }
+  // If we make it through the loop without finding a pair of characters that are out of order, the string is in alphabetical order
+  return true;
+
+}
+
+
+function alphabetic(s) {
+  // Yor code here
+  let newStr = s.toLowerCase().split("")
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[i - 1] > newStr[i]) {
+        return false
+    }
+  }
+  return true
+}
+
+
+
+function alphabetic(s) {
+  return s === s.split("").sort((a, b) => a.localeCompare(b)).join("");
+};
+
+
+
+const alphabetic = (s) => s === [...s].sort().join('');
