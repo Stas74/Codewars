71a0b1c967bbe9fb1bb3d84aedafd5913906be02
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-09 12:09:11 -0600

    Update 833 Capitals first!.js

--- Kata/833 Capitals first!.js
+++ Kata/833 Capitals first!.js
@@ -26,3 +26,34 @@ function capitalsFirst(str) {
 
 console.log(capitalsFirst("hey You, Sort me Already!")); // "You, Sort Already! hey me"
 console.log(capitalsFirst("You 123 Me 123 baby and")); // 'You Me baby and'
+
+
+
+function capitalsFirst(str){
+	let words = str.split(' ');
+  let upper = words.filter(function(x) { return x.charAt(0).match(/[A-Z]/) });
+  let lower = words.filter(function(x) { return x.charAt(0).match(/[a-z]/) });
+  return upper.concat(lower).join(' ');
+}
+
+
+
+function capitalsFirst(str) {
+  return str
+    .split(" ")
+    .filter(([a]) => a >= "a" && a <= "z" || a >= "A" && a <= "Z")
+    .sort(([a], [b]) => a < "a" && b >= "a" ? -1 : a >= "a" && b < "a" ? 1 : 0)
+    .join(" ")
+}
+
+
+const capitalsFirst = str =>
+  (fn => [...fn(/^[A-Z]/), ...fn(/^[a-z]/)].join(` `))
+  (reg => str.split(` `).filter(val => reg.test(val)));
+
+
+function capitalsFirst(str){
+  var s1=str.match(/( |^)[A-Z]([^ ]+)?(?= |$)/g)||[];
+  var s2=str.match(/( |^)[a-z]([^ ]+)?(?= |$)/g)||[];
+  return s1.concat(s2).map(x=>x.trim()).join(" ");
+}
