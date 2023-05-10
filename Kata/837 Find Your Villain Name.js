8e53f53c783fd98f81ffa43d999aa9eb8de9eda7
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-10 16:25:05 -0600

    Update 837 Find Your Villain Name.js

--- Kata/837 Find Your Villain Name.js
+++ Kata/837 Find Your Villain Name.js
@@ -53,3 +53,55 @@ function getVillainName(birthday) {
 
 console.log((getVillainName(new Date("April 21")))); // "The Trashy Pickle"
 console.log((getVillainName(new Date("May 3")))); // "The Despicable Raisin"
+
+
+
+
+function getVillainName(birthday){
+    firstNames = ["The Evil", "The Vile", "The Cruel", "The Trashy", "The Despicable", "The Embarrassing", "The Disreputable", "The Atrocious", "The Twirling", "The Orange", "The Terrifying", "The Awkward"];
+    lastNames = ["Mustache", "Pickle", "Hood Ornament", "Raisin", "Recycling Bin", "Potato", "Tomato", "House Cat", "Teaspoon", "Laundry Basket"];
+    return firstNames[birthday.getMonth()] + ' ' + lastNames[birthday.getDate() % 10]
+}
+
+
+
+var month=[
+  "The Evil",
+  "The Vile",
+  "The Cruel",
+  "The Trashy",
+  "The Despicable",
+  "The Embarrassing",
+  "The Disreputable",
+  "The Atrocious",
+  "The Twirling",
+  "The Orange",
+  "The Terrifying",
+  "The Awkward"
+];
+var date =[
+  "Mustache",
+  "Pickle",
+  "Hood Ornament",
+  "Raisin",
+  "Recycling Bin",
+  "Potato",
+  "Tomato",
+  "House Cat",
+  "Teaspoon",
+  "Laundry Basket"
+];
+
+function getVillainName(birthday) {
+  return month[birthday.getMonth()]+" "+date[birthday.getDate()%10];
+}
+
+
+
+function getVillainName(birthday){
+  return "The " + 
+  ["Evil","Vile","Cruel","Trashy","Despicable","Embarrassing","Disreputable","Atrocious","Twirling","Orange","Terrifying","Awkward"]
+  [birthday.getMonth()] + ' ' + 
+  ["Mustache","Pickle","Hood Ornament","Raisin","Recycling Bin","Potato","Tomato","House Cat","Teaspoon","Laundry Basket"]
+  [(birthday.getDate()+'').substr(-1)];
+}
