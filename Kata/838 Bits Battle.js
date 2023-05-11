6f3adfe5d4814e0ab2d16085af1bb50434648126
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-11 10:58:56 -0600

    Update 838 Bits Battle.js

--- Kata/838 Bits Battle.js
+++ Kata/838 Bits Battle.js
@@ -42,3 +42,45 @@ console.log(bitsBattle([3, 8, 22, 15, 78])); // 'evens win'
 console.log(bitsBattle([1, 13, 16])); // 'tie'
 console.log(bitsBattle([0])); // 'tie'
 console.log(bitsBattle([0, 1, 2])); // 'tie'
+
+
+
+const bitsBattle = arr => {
+  var score = arr.reduce((a, x) => a + (x&1?+1:-1)*(x && x.toString(2).split(x&1).length-1), 0);
+  return score > 0 ? "odds win" : score < 0 ? "evens win" : "tie";
+}
+
+
+
+const countBits = (num, val) => num.toString('2').split(val).length - 1;
+
+function bitsBattle(numbers) {
+  let totals = [ 0, 0 ];  
+  for (let num of numbers)
+    if (num)
+      totals[num % 2] += countBits(num, num % 2);    
+  let diff = totals[0] - totals[1];  
+  return diff > 0 ? 'evens win'
+       : diff < 0 ? 'odds win'
+       : 'tie';       
+}
+
+
+
+function bitsBattle(numbers) {
+  const num = numbers.filter(Boolean);
+  const odd = num.filter(e => e%2).reduce((a,c) => a+(c.toString(2).match(/1/g)||[]).length,0);
+  const even = num.filter(e => e%2==0).reduce((a,c) => a+(c.toString(2).match(/0/g)||[]).length,0);
+  return odd>even?'odds win':(even>odd?'evens win':'tie');
+}
+
+
+
+function bitsBattle(numbers) {
+  return ["odds win","tie","evens win"][Math.sign(numbers.reduce( (battle,n) => battle + fight(n), 0 ))+1]
+}
+
+const fight = x => x==0 ? 0 : x.toString(2).replace( x%2 ? /0/g : /1/g, '' ).length*(x%2 ? -1:1);
+
+
+
