cf5397a07dbe8ed93edfcbaf6b5a52feea573e66
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-11 14:26:09 -0600

    Update 839 Moves in squared strings (III).js

--- Kata/839 Moves in squared strings (III).js
+++ Kata/839 Moves in squared strings (III).js
@@ -91,3 +91,70 @@ console.log(diag1Sym(s)); // "aeim\nbfjn\ncgko\ndhlp"
 console.log(rot90Clock(s)); // "miea\nnjfb\nokgc\nplhd"
 console.log(selfieAndDiag1(s)); // "abcd|aeim\nefgh|bfjn\nijkl|cgko\nmnop|dhlp"
 
+
+
+
+
+const diag1Sym = s => s.map((v, i) => [...v].map((_, j) => s[j][i]).join(''))
+const rot90Clock = s => diag1Sym(s).map(v => [...v].reverse().join(''))
+const selfieAndDiag1 = s => {
+  const r = diag1Sym(s)
+  return s.map((v, i) => v + '|' + r[i])
+}
+const oper = (fct, s) => fct(s.split('\n')).join('\n')
+
+
+
+
+const getFirstCharOfEach = (_, i, arr) => 
+	arr.map(y => y.slice(i, i + 1)).join('')
+  
+const oper = (fct, s) => 
+	fct(s.split('\n')).join('\n')
+
+const rot90Clock = arr => 
+	arr.reverse().map(getFirstCharOfEach)
+  	
+const diag1Sym = arr => 
+	arr.map(getFirstCharOfEach)
+
+const selfieAndDiag1 = arr => 
+	diag1Sym(arr).map((x, i) => `${[...arr][i]}|${x}`)
+
+
+
+
+function rot90Clock(str) {
+  let c = diag1Sym(str);
+  return c.split('\n').map(a=>a.split('').reverse().join('')).join('\n');
+}
+function diag1Sym(str) {
+  let arr = str.split('\n');
+  let s = [], t;
+  for(let i = 0; i < arr.length; i += 1) {
+    s.push(arr.map(a=>a[i]).join(''));
+  }
+  return s.join('\n');
+}
+function selfieAndDiag1(str) {
+  let arr = str.split('\n');
+  let s = diag1Sym(str).split('\n');
+  return arr.map((a,i)=>a + '|' + s[i]).join('\n');
+}
+function oper(fct, s) {
+  return fct(s);
+}
+
+
+
+const diag1Sym=s=>s.reduce((a,_,i,r)=>[...a,r.map(item => item[i]).join``],[]);
+const rot90Clock=s=>diag1Sym(s).map(i=>[...i].reverse().join``)
+const selfieAndDiag1=s=>diag1Sym(s).map((v,i)=>`${s[i]}|${v}`)
+const oper=(c,s)=>c(s.split`\n`).join`\n`
+
+
+
+diag1Sym = a => new Array(a[0].length).fill('').map((v,i) => a.reduce((aa, ac) => aa+ac.split('')[i], ''));
+selfieAndDiag1 = a => a.map((w,i) => w + '|' + diag1Sym(a)[i]);
+rot90Clock = a => diag1Sym(a).map(w => w.split('').reverse().join(''));
+oper = (fct, s) => fct(s.split("\n")).join("\n");
