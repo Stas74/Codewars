0caaa56d38ffbb128a06a2984debe702c4732ce0
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-16 13:05:07 -0600

    Update 845 No ifs no buts.js

--- Kata/845 No ifs no buts.js
+++ Kata/845 No ifs no buts.js
@@ -30,3 +30,28 @@ function noIfsNoButs(a, b) {
 console.log(noIfsNoButs(45, 51)); // "45 is smaller than 51"
 console.log(noIfsNoButs(100, 80)); // "100 is greater than 80"
 console.log(noIfsNoButs(100, 100)); // "100 is equal to 100"
+
+
+
+
+var noIfsNoButs = function (a,b) {
+  switch (true) {
+    case (a <   b) : return a + " is smaller than " + b;
+    case (a >   b) : return a + " is greater than " + b;
+    default        : return a + " is equal to "     + b;
+  }
+}
+
+
+
+const noIfsNoButs = (a, b) =>
+  a < b && `${a} is smaller than ${b}` ||
+  a > b && `${a} is greater than ${b}` ||
+  `${a} is equal to ${b}`
+
+
+
+const noIfsNoButs = (a, b) =>
+  `${a} is ${[`smaller than`, `equal to`, `greater than`][Math.sign(a - b) + 1]} ${b}`;
+
+
