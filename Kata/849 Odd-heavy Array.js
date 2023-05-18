8605d16460311c45cdb12853c52c5957b17a99bf
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-18 13:26:57 -0600

    Update 849 Odd-heavy Array.js

--- Kata/849 Odd-heavy Array.js
+++ Kata/849 Odd-heavy Array.js
@@ -36,3 +36,50 @@ console.log(isOddHeavy([1, -2, -1, 2])); // false
 console.log(isOddHeavy([11, 4, 9, 2, 3, 10])); // false
 console.log(isOddHeavy([0, 0])); // false
 console.log(isOddHeavy([2])); // false
+
+
+
+
+// O(n) solution
+function isOddHeavy(n) {
+
+  let min_odd = Number.MAX_SAFE_INTEGER
+  ,  max_even = Number.MIN_SAFE_INTEGER;
+  
+  for (let m of n)
+    if (m % 2)
+      min_odd = Math.min(min_odd, m);
+    else
+      max_even = Math.max(max_even, m);
+      
+  return min_odd !== Number.MAX_SAFE_INTEGER 
+      && min_odd > max_even;
+  
+}
+
+
+function isOddHeavy(n){  
+  const odds = n.filter(e => e % 2)
+  const evens = n.filter(e => !(e % 2))
+  
+  return !!odds.length && Math.min(...odds) > Math.max(...evens)
+}
+
+
+
+function isOddHeavy(n){
+  let odds = [];
+  let evens = [];
+  n.forEach(num=>num%2===0? evens.push(num) : odds.push(num));
+  if(odds.length>0){
+    return Math.min(...odds)>=Math.max(...evens)
+  }return false
+}
+
+
+
+function isOddHeavy(n){
+  var even = n.filter(y => y%2==0).sort((a,b) => b-a);
+  var odd = n.filter(y => y%2!=0).sort()
+  return even=='' ? (odd=='' ? false : true) : odd[0]>even[0];
+}
