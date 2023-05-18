558cd9880eb2e016175470fc1790dd671c58aafc
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-18 11:56:06 -0600

    Create 848 Get decimal part of the given number.js

---
+++ Kata/848 Get decimal part of the given number.js
@@ -0,0 +1,23 @@
+// Get decimal part of the given number
+
+/*
+
+Write a function that returns only the decimal part of the given number.
+
+You only have to handle valid numbers, not Infinity, NaN, or similar. 
+Always return a positive decimal part.
+
+Examples
+getDecimal(2.4)  === 0.4
+getDecimal(-0.2) === 0.2
+
+*/
+
+function getDecimal(n){
+  return n ? Math.abs(n) - Math.floor(Math.abs(n)) : 0
+}
+
+console.log(getDecimal(10)); // 0
+console.log(getDecimal(-1.2)); // 0.2
+console.log(getDecimal(4.5)); // 0.5
+console.log(getDecimal(NaN)); // 0
