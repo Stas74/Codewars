bbdec2bfb57217eee54dbe8d421ee55a98b77185
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-19 11:31:16 -0600

    Update 850 Colour Association.js

--- Kata/850 Colour Association.js
+++ Kata/850 Colour Association.js
@@ -22,3 +22,31 @@ console.log(colourAssociation([["white", "goodness"], ["blue", "tranquility"]]))
 // [{white:"goodness"},{blue:"tranquility"}]
 console.log(colourAssociation([["red", "energy"],["yellow", "creativity"],["brown" , "friendly"],["green", "growth"]])); 
 // [{red: "energy"},{yellow: "creativity"}, {brown: "friendly"},{green: "growth"}]
+
+
+
+
+const colourAssociation = array => array.map(([colour, association]) => ({[colour]:association}))
+
+
+
+function colourAssociation(array){
+  let newArr = [];
+  for(let i = 0; i < array.length; i++) {
+    let newObj = {};
+    let key = array[i][0];
+    let value = array[i][1];
+    newObj[key] = value;
+    newArr.push(newObj);
+  }
+  return newArr;
+}
+
+
+
+const colourAssociation = array =>
+  array.map(([color, association]) => ({[color]: association}));
+
+
+
+const colourAssociation = (array) => array.map(value => Object.fromEntries([value]));
