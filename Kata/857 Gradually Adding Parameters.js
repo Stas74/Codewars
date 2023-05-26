4174e9b6a591be2bb36d63d099e9119c6d6d8220
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-05-26 11:22:06 -0600

    Update 857 Gradually Adding Parameters.js

--- Kata/857 Gradually Adding Parameters.js
+++ Kata/857 Gradually Adding Parameters.js
@@ -28,3 +28,37 @@ function add() {
 }
 
 console.log(add(100, 200, 300)); // 1400
+
+
+
+function add() {
+  for (var n = 0, i = 0; i < arguments.length; i++) {
+    n += arguments[i] * (i + 1)
+  }  
+  return n
+}
+
+
+
+const add = (...args) => args.reduce((s, v, i) => s + v * (i + 1), 0);
+
+
+
+function add() {
+  return [].reduce.call(arguments, function(sum,v,i) { return sum + v * (i+1); }, 0);
+}
+
+
+
+const add = (...args) =>
+  args.reduce((pre, val, idx) => pre + val * ++idx, 0);
+
+
+
+function add (...num) {
+  let sum = 0;
+  for (let i = 0; i <num.length; i++) {
+    sum += num[i]*(i+1);
+  }
+  return sum;
+}
