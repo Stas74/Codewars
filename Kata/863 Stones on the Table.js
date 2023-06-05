64a77ab12703375e2c29dc02db8fd4745b7af3e8
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-06-05 12:25:07 -0600

    Create 863 Stones on the Table.js

---
+++ Kata/863 Stones on the Table.js
@@ -0,0 +1,29 @@
+// Stones on the Table
+
+/*
+There are some stones on Bob's table in a row, and each of them can be red, green or blue, 
+indicated by the characters R, G, and B.
+
+Help Bob find the minimum number of stones he needs to remove from the table 
+so that the stones in each pair of adjacent stones have different colors.
+
+Examples:
+
+"RGBRGBRGGB"   => 1
+"RGGRGBBRGRR"  => 3
+"RRRRGGGGBBBB" => 9
+*/
+
+function solve(stones) {
+  let count = 0;
+  for (let i = 1; i < stones.length; i++) {
+    if (stones[i] === stones[i - 1]) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(solve("RRGGBB")); // 3
+console.log(solve("RGBRGB")); // 0
+console.log(solve("BGRBBGGBRRR")); // 4
