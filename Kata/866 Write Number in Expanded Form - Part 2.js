4fa096c2f58b92ce7d9a0de7c71bc4661abf7721
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-06-08 11:45:32 -0600

    Create 866 Write Number in Expanded Form - Part 2.js

---
+++ Kata/866 Write Number in Expanded Form - Part 2.js
@@ -0,0 +1,40 @@
+// Write Number in Expanded Form - Part 2
+
+/*
+YYou will be given a number and you will need to return it as a string in expanded form :
+
+expanded form illustration
+
+For example:
+
+expanded_from(807.304); // Should return "800 + 7 + 3/10 + 4/1000"
+expanded_from(1.24); // should return "1 + 2/10 + 4/100"
+expanded_from(7.304); // should return "7 + 3/10 + 4/1000"
+expanded_from(0.04); // should return "4/100"
+*/
+
+function expandedForm(num) {
+  const numArr = String(num).split(".");
+
+  const wholePart = numArr[0]
+    .split("")
+    .map((el, i) =>
+      el == "0" ? "" : el + "0".repeat(numArr[0].length - 1 - i)
+    )
+    .filter((el) => el != "")
+    .join(" + ");
+
+  const fractionPart = numArr[1]
+    .split("")
+    .map((el, i) => (el == "0" ? "" : el + "/" + "1" + "0".repeat(i + 1)))
+    .filter((el) => el != "")
+    .join(" + ");
+
+  return numArr[0] == "0" ? fractionPart : wholePart + " + " + fractionPart;
+}
+
+console.log(expandedForm(807.304)); // '800 + 7 + 3/10 + 4/1000'
+console.log(expandedForm(1.24)); // '1 + 2/10 + 4/100'
+console.log(expandedForm(4.28)); // '4 + 2/10 + 8/100'
+console.log(expandedForm(7.304)); // '7 + 3/10 + 4/1000'
+console.log(expandedForm(0.501)); // '5/10 + 1/1000'
