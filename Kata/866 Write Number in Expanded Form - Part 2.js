5db164f0d8b85e9b550307e6bffa069700fbc63f
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-06-08 12:07:20 -0600

    Update 866 Write Number in Expanded Form - Part 2.js

--- Kata/866 Write Number in Expanded Form - Part 2.js
+++ Kata/866 Write Number in Expanded Form - Part 2.js
@@ -38,3 +38,86 @@ console.log(expandedForm(1.24)); // '1 + 2/10 + 4/100'
 console.log(expandedForm(4.28)); // '4 + 2/10 + 8/100'
 console.log(expandedForm(7.304)); // '7 + 3/10 + 4/1000'
 console.log(expandedForm(0.501)); // '5/10 + 1/1000'
+
+
+
+
+function expandedForm(num) {
+  var [int, dec] = (''+num).split('.')
+
+  return int.split('').map((x, y, a) => x + '0'.repeat(a.length - y - 1))
+    .concat(dec.split('').map((x, y, a) => `${x}/1${'0'.repeat(y + 1)}`))
+    .filter(x => !x.startsWith('0'))
+    .join(' + ')
+}
+
+
+function expandedForm(num) {
+  let digits = String(num).split('.')[0]
+  let decimals = String(num).split('.')[1]
+  let result = ''
+  
+  if (digits !== '0') {
+    result += digits.toString()
+    .split("")
+    .reverse()
+    .map( (a, i) => a * Math.pow(10, i))
+    .filter(a => a > 0)
+    .reverse()
+    .join(" + ");
+    result += ' + '
+  }
+  
+  result += decimals.toString()
+    .split("")
+    .map( (a, i) => a + "/" + Math.pow(10, i + 1))
+    .filter(a => !a.startsWith('0'))
+    .join(" + ");
+  
+  return result
+}
+
+
+
+function expandedForm(num) {
+  let  result = []
+  let [a, b] = String(num).split('.')
+  for (let i = 0; i < a.length; i++) {
+    if (+a[i]) result.push(a[i] + '0'.repeat(a.length - i - 1)) ;
+  }
+  for (let j = 0; j < b.length; j++) {
+    if (+b[j]) result.push(b[j] + '/1' + '0'.repeat(j + 1));
+  }
+  
+  return result.join(' + ')
+}
+
+
+
+function expandedForm(num) {
+  let [int, dec] = `${num}`.split('.')
+  let result = ''
+  int.split('').forEach((n,i,arr)=>{
+    result += Number(n) ? `${n*(10**(arr.length-1-i))}` + ' + ' : ''
+  })
+  dec.split('').forEach((n,i,arr)=>{
+    result += Number(n) ? n + '/' + `${10**(i+1)}` + `${i !== arr.length-1 ? ' + ' : ''}` : ''
+  })
+  return result
+}
+
+
+
+function expandedForm(num) {
+
+  let base = Math.max(0, Math.floor(Math.log(num) / Math.log(10)));
+  return `${num}`
+    .match(/\d/g)
+    .map((d, i) => [+d, Math.pow(10, Math.abs(base - i)), base >= i, i])
+    .filter(p => p[0])
+    .map(p => p[2] ? p[0] * p[1] : p[0] + '/' + p[1])
+    .join(' + ');
+
+}
+
+
