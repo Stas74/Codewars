87a4f40ce6c328b8c47861513d5b9cbc9a985d25
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-06-09 16:55:14 -0600

    Update 867 Most valuable character.js

--- Kata/867 Most valuable character.js
+++ Kata/867 Most valuable character.js
@@ -34,3 +34,51 @@ console.log(solve("aa")); // 'a'
 console.log(solve("bcd")); // 'b'
 console.log(solve("axyzxyz")); // 'x'
 console.log(solve("aabccc")); // 'c'
+
+
+
+function solve(str) {
+  let maxDiff = 0, result = 'z';
+  for (let el of str) {
+    let diff = str.lastIndexOf(el) - str.indexOf(el);
+      if (maxDiff < diff || maxDiff == diff && el <= result) {
+        result = el;
+        maxDiff = diff;
+      }
+    }
+  return result;
+}
+
+
+
+const solve = function( str ) {
+  let arr = [...str].map( el => str.lastIndexOf(el) - str.indexOf(el) ) 
+  arr = arr.map( (el, i)=> el === Math.max(...arr) ? str[i] : '' )
+  return arr.filter(el => el).sort()[0]
+}
+
+
+
+function solve(st) {
+  return [...new Set([...st])].map(c => {
+    return [c, st.lastIndexOf(c) - st.indexOf(c)];
+  }).sort((a, b) => {
+    const c = b[1] - a[1];
+    if (c === 0) return a[0].localeCompare(b[0]);
+    return c;
+  })[0][0];
+}
+
+
+
+function solve(st) {
+  return [...st].reduce((res, ch) => {
+    const max = st.lastIndexOf(ch) - st.indexOf(ch);
+    return (max > res.max || max === res.max && ch < res.ch) ? {max, ch} : res;
+  }, {max : 0, ch: st[0]}).ch;
+}
+
+
+
+const solve = st =>
+  [...new Set(st)].map(val => [val, st.lastIndexOf(val) - st.indexOf(val)]).sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]))[0][0];
