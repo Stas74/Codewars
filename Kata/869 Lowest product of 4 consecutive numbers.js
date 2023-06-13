b9711d63e9b751b6e57b52c6243fc2e5cf01c111
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-06-13 12:22:02 -0600

    Update 869 Lowest product of 4 consecutive numbers.js

--- Kata/869 Lowest product of 4 consecutive numbers.js
+++ Kata/869 Lowest product of 4 consecutive numbers.js
@@ -29,3 +29,51 @@ console.log(lowestProduct("234567899")); // 120
 console.log(lowestProduct("2345611117899")); // 1
 console.log(lowestProduct("333")); // "Number is too small"
 console.log(lowestProduct("1234111")); // 4  "Numbers should be consecutives"
+
+
+
+function lowestProduct(input) {
+  let arr = input.split``.map((x,i,a) => a.length - i < 4 ? Infinity : x * a[i+1] * a[i+2] * a[i+3]);
+  return input < 1000 ? "Number is too small" : Math.min(...arr);
+}
+
+
+
+function compareNumbers(a, b) {
+    return a - b;
+}
+
+function lowestProduct(input) { 
+  let separate = input.split("");
+  if(separate.length < 4) {
+    return "Number is too small";
+  }
+  let cell = [];
+  for(let i = 0; i < separate.length; i++) {
+    cell.push(separate[i] * separate[i + 1] * separate[i + 2] * separate[i + 3]);
+  }
+
+  let order = cell.sort(compareNumbers);
+  return order[0];
+}
+
+
+
+lowestProduct=(a,b=a.length)=>b<4?'Number is too small':Math.min(...[...Array(b-3)].map((_,i)=>a.slice(i,i+4).split``.reduce((a,b)=>a*b)))
+
+
+
+function lowestProduct(input) { 
+  if (input.length < 4) {
+      return 'Number is too small';
+  } else {
+      let obj = {}, arr = [];
+      for (let i = 0; i < input.length - 3; i++) {
+          let s = input.split('').splice(i, 4).reduce((acc, num) => acc * Number(num), 1);
+          arr.push(s);
+      }
+      return Math.min.apply(null, arr);
+  }
+}
+
+
