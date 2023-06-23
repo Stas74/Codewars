587776b1d4b7140e478525b2ee31dd3264219f60
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-06-23 13:10:56 -0600

    Update 877 ASC Week 1 Challenge 5 (Medium #2).js

--- Kata/877 ASC Week 1 Challenge 5 (Medium #2).js
+++ Kata/877 ASC Week 1 Challenge 5 (Medium #2).js
@@ -40,3 +40,32 @@ console.log(avgArray([[1, 2, 3, 4], [5, 6, 7, 8]])); // [3, 4, 5, 6]
 console.log(avgArray([[2, 3, 9, 10, 7], [12, 6, 89, 45, 3], [9, 12, 56, 10, 34], [67, 23, 1, 88, 34]])); 
 // [22.5, 11, 38.75, 38.25, 19.5]
 console.log(avgArray([[1.2, 8.521, 0.4, 3.14, 1.9], [2, 4.5, 3.75, 0.987, 1.0]])); // [1.6, 6.5105, 2.075, 2.0635, 1.45]
+
+
+const avgArray = arr =>
+  arr[0].map((_, idx) => arr.reduce((pre, val) => pre + val[idx], 0) / arr.length);
+
+
+
+function avgArray(arr) {
+  return arr
+    .reduce((acc, cur) => {
+      return acc.map((n, i) => n + cur[i]);
+    })
+    .map(n => n / arr.length);
+}
+
+
+function avgArray(arr) {
+  var result=[];
+  for(var i in arr[0]){
+    var num=0;
+    for(var j in arr){
+      num+=arr[j][i];
+    }
+    result.push(num/arr.length);
+  }
+  return result;
+}
+
+
