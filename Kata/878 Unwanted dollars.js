2d86d034e9d67e3532ca92099ec7118fdb64c345
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-06-26 15:19:54 -0600

    Create 878 Unwanted dollars.js

---
+++ Kata/878 Unwanted dollars.js
@@ -0,0 +1,32 @@
+// Unwanted dollars
+
+/*
+If you're faced with an input box, like this:
+
+                                           +--------------+
+  Enter the price of the item, in dollars: |              |
+                                           +--------------+
+
+do you put the $ sign in, or not? Inevitably, some people will type a $ sign and others will leave it out. 
+The instructions could be made clearer - but that won't help those annoying people who never read instructions anyway.
+
+A better solution is to write code that can handle the input whether it includes a $ sign or not.
+
+So, we need a simple function that converts a string representing a number 
+(possibly with a $ sign in front of it) into the number itself.
+
+Remember to consider negative numbers (the - sign may come either before or after the $ sign, if there is one), 
+and any extraneous space characters (leading, trailing, or around the $ sign) that the users might put in. 
+You do not need to handle input with trailing characters other than spaces (e.g. "1.2 3" or "1$"). 
+If the given string does not represent a number, (either with or without a $ sign), return 0.0 .
+*/
+
+function money_value(s) {
+  return +s.replace(/^0+/, "").replace(/[$\s]/g, "") || 0;
+}
+
+console.log(money_value(" $5.67")); // 5.67
+console.log(money_value("-$ 0.1")); // -0.10
+console.log(money_value("$-2.3456")); // -2.3456
+console.log(money_value("007")); // 7.00
+console.log(money_value(" $ 89")); // 89.0
