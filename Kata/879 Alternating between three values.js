2d2f0c59e75157a5a2ae2e184eeb4a91d5876042
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-06-27 13:54:59 -0600

    Update 879 Alternating between three values.js

--- Kata/879 Alternating between three values.js
+++ Kata/879 Alternating between three values.js
@@ -22,3 +22,50 @@ function f(x, cc) {
 console.log(f(3, { a: 3, b: 4, c: 5 })); // 4
 console.log(f(4, { a: 3, b: 4, c: 5 })); // 5
 console.log(f(5, { a: 3, b: 4, c: 5 })); // 3
+
+
+
+
+function f(x, cc) { 
+  switch (x) {
+    case cc.a: return cc.b;
+    case cc.b: return cc.c;
+    case cc.c: return cc.a;
+  }
+}
+
+
+const f = (x,{a,b,c}) => ({[a]:b,[b]:c,[c]:a})[x] ;
+
+
+function f(x, cc) { 
+  let [C, A, B] = Object.values(cc)
+  const arr = Object.keys(cc)
+  return [A, B, C][arr.reduce((acc, e, i) => acc + (cc[e] === x ? i : 0), 0)]
+}
+
+
+function f(x, cc) {
+  return cc.a === x ? cc.b : cc.b === x ? cc.c : cc.a
+}
+
+
+function f(value, {a, b, c}) { 
+  return {
+    [a]: b,
+    [b]: c,
+    [c]: a,
+  }[value]
+}
+
+
+const f = (x, cc) =>
+  (arr => arr[(arr.indexOf(x) + 1) % arr.length])
+  (Object.values(cc));
+
+
+function f(x, cc) { 
+  // Destructering the object and assigning the values of obj to a,b & c
+  const {a, b, c} = cc
+  return x === a ? b : x === b ? c : a
+}
