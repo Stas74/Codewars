e40cf64ade74f340310f8b2287cd267f5fd30897
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-06-29 12:35:34 -0600

    Create 881 Recursion #2 - Fibonacci.js

---
+++ Kata/881 Recursion #2 - Fibonacci.js
@@ -0,0 +1,34 @@
+// Recursion #2 - Fibonacci
+
+/*
+This is a kata series that you can only solve using recursion.
+##2 - Fibonacci number
+
+In mathematical terms, the sequence f(n) of fibonacci numbers is defined by the recurrence relation
+
+f(n) = f(n-1) + f(n-2)
+
+with seed values
+
+f(1) = 1 and f(2) = 1
+
+#Your task
+
+You have to create the function fibonacci that receives n and returns f(n). You have to use recursion.
+*/
+
+const fibonacci = (n) => {
+  return n < 3 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+// if (n < 3) {
+//   return 1;
+// } else {
+//   return fibonacci(n - 1) + fibonacci(n - 2);
+// }
+
+console.log(fibonacci(1)); // 1
+console.log(fibonacci(2)); // 1
+console.log(fibonacci(3)); // 2
+console.log(fibonacci(4)); // 3
+console.log(fibonacci(5)); // 5
