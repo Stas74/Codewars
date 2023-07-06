7f42a9a5a609393ebdbcad42f7751deb1b6e031e
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-07-06 14:17:42 -0600

    Update 885 Sum of a nested list.js

--- Kata/885 Sum of a nested list.js
+++ Kata/885 Sum of a nested list.js
@@ -21,3 +21,59 @@ const sumNested = (arr) => {
 console.log(sumNested([1, 2, 3, 4])); // 10
 console.log(sumNested([[1, 2, 3, 4]])); // 10
 console.log(sumNested([1, [1], [[1]], [[[1]]]])); // 4
+
+
+
+const sumNested = arr => {
+  if (Array.isArray(arr)) {
+    return arr.reduce(function(accumulator, currentValue) {
+      return accumulator + sumNested(currentValue);
+    }, 0);
+  } else {
+    return arr;
+  }
+};
+
+
+
+const R = require('ramda'), sumNested = R.pipe(R.flatten, R.sum);
+
+
+
+const _ = require('lodash');
+const sumNested = arr => _(arr).flattenDeep().sum();
+
+
+
+const sumNested = (arr) => arr.reduce((acc,e) => {
+  if (e instanceof Array) {
+    return acc + sumNested(e);
+  }
+  return acc + e;
+}, 0);
+
+
+
+const sumNested = (arr) => arr.reduce((acc,e) => {
+  if (e instanceof Array) {
+    return acc + sumNested(e);
+  }
+  return acc + e;
+}, 0);
+
+
+
+const sumNested = arr => {
+  if ( arr.length == 0 ) return 0;
+  var value =  ( Array.isArray( arr[0] )) ? sumNested( arr[0] ) : arr[0];
+  return sumNested( arr.slice( 1, arr.length )) + value;
+};
+
+
+const sumNested = arr => arr.reduce(
+  (a, b) => a + (Array.isArray(b) ? sumNested(b) : b)
+, 0);
+
+
+const sumNested = arr =>
+  arr.reduce((pre, val) => pre + (Array.isArray(val) ? sumNested(val) : val), 0);
