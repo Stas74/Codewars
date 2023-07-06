a585e914319bee5ecbc9d51257b58cd78df89cee
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-07-06 14:03:50 -0600

    Create 885 Sum of a nested list.js

---
+++ Kata/885 Sum of a nested list.js
@@ -0,0 +1,23 @@
+// Sum of a nested list
+
+/*
+Implement a function to calculate the sum of the numerical values in a nested list. For example :
+
+sumNested([1, [2, [3, [4]]]]) => 10
+*/
+
+const sumNested = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      sum += sumNested(arr[i]);
+    } else {
+      sum += arr[i];
+    }
+  }
+  return sum;
+};
+
+console.log(sumNested([1, 2, 3, 4])); // 10
+console.log(sumNested([[1, 2, 3, 4]])); // 10
+console.log(sumNested([1, [1], [[1]], [[[1]]]])); // 4
