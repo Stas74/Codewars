2eae20ea0396d521056ab49145932722464b9983
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-07-11 12:10:04 -0600

    Update 888 Principal Diagonal - VS - Secondary Diagonal.js

--- Kata/888 Principal Diagonal - VS - Secondary Diagonal.js
+++ Kata/888 Principal Diagonal - VS - Secondary Diagonal.js
@@ -61,3 +61,47 @@ console.log(
     [2, 8, 2],
   ])
 ); // "Draw!"
+
+
+
+
+function diagonal(matrix){
+  let pd = 0, sd = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    pd += matrix[i][i];
+    sd += matrix[i][matrix.length-i-1];
+  }
+  if (pd > sd) return "Principal Diagonal win!";
+  else if (sd > pd) return "Secondary Diagonal win!";
+  return "Draw!";
+}
+
+
+const diagonal = matrix => ["Secondary Diagonal win!", "Draw!", "Principal Diagonal win!"][
+    1 + Math.sign(matrix.reduce((a, b, i) => a + b[i] - b[b.length - 1 - i], 0))];
+
+
+
+const sumDiagonals = (sum, row, i) => sum + row[i] - row[row.length - i - 1];
+const diagonal = (matrix) => [
+  'Secondary Diagonal win!',
+  'Draw!',
+  'Principal Diagonal win!'
+][Math.sign(matrix.reduce(sumDiagonals, 0)) + 1];
+
+
+
+const diagonal = matrix =>
+  (val => !val ? `Draw!` : `${val > 0 ? `Principal` : `Secondary`} Diagonal win!`)
+  (matrix.reduce((pre, _, idx) => pre + matrix[idx][idx] - matrix[idx][matrix.length - ++idx], 0));
+
+
+
+function diagonal(matrix) {
+  const p = matrix.map((cur, ind) => cur[ind]).reduce((acc, cur) => acc + cur);
+  const s = matrix.map((cur, ind, arr) => cur[arr.length - ind - 1]).reduce((acc, cur) => acc + cur);
+  
+  return p > s ? 'Principal Diagonal win!' : p < s ? 'Secondary Diagonal win!' : 'Draw!';
+}
+
+
