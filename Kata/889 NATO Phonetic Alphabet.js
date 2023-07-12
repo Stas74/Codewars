79c934ac5d9c7441d7913456e4e555ee8bdae6b8
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-07-12 13:07:42 -0600

    Update 889 NATO Phonetic Alphabet.js

--- Kata/889 NATO Phonetic Alphabet.js
+++ Kata/889 NATO Phonetic Alphabet.js
@@ -52,3 +52,38 @@ var nato = (function () {
 console.log(nato("hi")); // "Hotel India"
 console.log(nato("abc")); // "Alpha Bravo Charlie"
 console.log(nato("babble")); // "Bravo Alpha Bravo Bravo Lima Echo"
+
+
+
+
+var nato = (function() {
+  var letters =  {
+    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",   "H": "Hotel",   "I": "India",
+    "J": "Juliett","K": "Kilo",    "L": "Lima",
+    "M": "Mike",   "N": "November","O": "Oscar",
+    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra", "T": "Tango",   "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu"
+  }
+  
+  return function(word) {
+    return word.split('').map(function(v) {
+      return letters[v.toUpperCase()]
+    }).join(' ');
+  }
+})()
+
+
+
+const nato = word =>
+  [...word].map(val => ({
+    A: `Alpha`, B: `Bravo`, C: `Charlie`, D: `Delta`, E: `Echo`, F: `Foxtrot`, G: `Golf`, H: `Hotel`, I: `India`,
+    J: `Juliett`, K: `Kilo`, L: `Lima`, M: `Mike`, N: `November`, O: `Oscar`, P: `Papa`, Q: `Quebec`, R: `Romeo`,
+    S: `Sierra`, T: `Tango`, U: `Uniform`, V: `Victor`, W: `Whiskey`, X: `X-ray`, Y: `Yankee`, Z: `Zulu`})
+    [val.toUpperCase()]).join(` `);
+
+
+
