bf763cb1d4890c69f88a5670fc10d5cee64e393f
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-07-14 15:03:42 -0600

    Update 891 Tribonacci Sequence.js

--- Kata/891 Tribonacci Sequence.js
+++ Kata/891 Tribonacci Sequence.js
@@ -47,3 +47,51 @@ console.log(tribonacci([1, 1, 1], 10)); // [1,1,1,3,5,9,17,31,57,105]
 console.log(tribonacci([0, 0, 1], 10)); // [0,0,1,1,2,4,7,13,24,44]
 console.log(tribonacci([1, 0, 0], 10)); // [1,0,0,1,1,2,4,7,13,24]
 console.log(tribonacci([300, 200, 100], 0)); // []
+
+
+
+
+function tribonacci(signature,n){  
+  for (var i = 0; i < n-3; i++) { // iterate n times
+    signature.push(signature[i] + signature[i+1] + signature[i+2]); // add last 3 array items and push to trib
+  }
+  return signature.slice(0, n); //return trib - length of n
+}
+
+
+
+function tribonacci(signature,n) {
+  const result = signature.slice(0, n);
+  while (result.length < n) {
+    result[result.length] = result.slice(-3).reduce((p,c) => p + c, 0);
+  }
+  return result;
+}
+
+
+function tribonacci(s,n){
+  var arr = [];
+  for(var i=0; i<n; i++) {
+    arr.push((i<3) ? s[i] : arr[i-1]+arr[i-2]+arr[i-3]);
+  }
+  return arr;
+}
+
+
+
+function tribonacci(signature, n) {
+  while(signature.length < n) {
+    signature.push(signature.slice(-3).reduce(sum));
+  }
+  return signature.slice(0, n);
+}
+function sum(a, b) { return a + b }
+
+
+
+function tribonacci(signature,n){
+  while (signature.length < n) {
+    signature.push(signature.slice(-3).reduce((a, b) => a + b));
+  }
+  return signature.slice(0, n);
+}
