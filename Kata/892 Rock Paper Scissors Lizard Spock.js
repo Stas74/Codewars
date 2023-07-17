5816774f407e81c29913fa8e64f677e128e3b1df
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-07-17 13:01:45 -0600

    Update 892 Rock Paper Scissors Lizard Spock.js

--- Kata/892 Rock Paper Scissors Lizard Spock.js
+++ Kata/892 Rock Paper Scissors Lizard Spock.js
@@ -52,3 +52,91 @@ console.log(rpsls("lizard", "scissors")); // 'Player 2 Won!'
 console.log(rpsls("spock", "lizard")); // 'Player 2 Won!'
 
 console.log(rpsls("rock", "rock")); // 'Draw!'
+
+
+
+
+const MATCH = {
+  scissors: ['paper', 'lizard'],
+  paper: ['rock', 'spock'],
+  rock: ['lizard', 'scissors'],
+  lizard: ['spock', 'paper'],
+  spock: ['scissors', 'rock'],
+};
+
+function rpsls(pl1, pl2) {
+  return MATCH[pl1].indexOf(pl2) >= 0 ? 'Player 1 Won!' :
+         MATCH[pl2].indexOf(pl1) >= 0 ? 'Player 2 Won!' : 'Draw!';
+}
+
+
+
+function rpsls(pl1, pl2) {
+    if (pl1 === pl2) return 'Draw!'
+    let rules = {
+        'paper': ['rock', 'spock'],
+        'rock': ['lizard', 'scissors'],
+        'lizard': ['spock', 'paper'],
+        'spock': ['scissors', 'rock'],
+        'scissors': ['lizard', 'paper']       
+    }
+    if (rules[pl1].includes(pl2))
+        return 'Player 1 Won!'
+    else
+        return 'Player 2 Won!'
+}
+
+
+
+function rpsls(p1,p2){
+  let wins = {
+    scissors:['paper','lizard'],
+    paper:['rock','spock'],
+    rock:['lizard','scissors'],
+    lizard:['spock','paper'],
+    spock:['scissors','rock']
+  }
+  
+  let player1 = wins[p1].includes(p2)
+  let player2 = wins[p2].includes(p1)
+  
+  if (!player1 && !player2) { return 'Draw!' } 
+  return `Player ${ player1?'1':'2'} Won!`
+}
+
+
+
+function rpsls(p1,p2){
+  if ((p1=="rock" && p2=="lizard") 
+      ||(p1=="paper" && p2=="rock") 
+      ||(p1=="scissors" && p2=="lizard") 
+      ||(p1=="lizard" && p2=="paper")
+      ||(p1=="paper" && p2=="spock")
+      ||(p1=="rock" && p2=="scissors")
+      ||(p1=="lizard" && p2=="spock")
+      ||(p1=="scissors" && p2=="paper")
+      ||(p1=="spock" && p2=="scissors")
+      ||(p1=="spock" && p2=="rock"))
+    return "Player 1 Won!";
+  if (p1==p2)
+    return "Draw!"
+  return "Player 2 Won!";
+}
+
+
+
+function rpsls(pl1,pl2){
+	const list = 'rock paper scissors spock lizard'.split(' ');
+  const offset = (list.indexOf(pl2) - list.indexOf(pl1) + 5) % 5;
+  return offset === 0 ? 'Draw!' : (offset % 2 === 0 ? 'Player 1 Won!' : 'Player 2 Won!')
+}
+
+
+
+function rpsls(a,b){
+  var win="ScissorsPaper PaperRock RockLizard LizardSpock SpockScissors ScissorsLizard LizardPaper PaperSpock SpockRock RockScissors".toLowerCase()
+  return win.includes(a+b)?'Player 1 Won!':win.includes(b+a)?'Player 2 Won!':'Draw!'
+}
+
+
+
