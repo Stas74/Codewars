40f6a87469bfc1f4d96ce84a26939ca1cc6b1ae4
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-07-18 13:02:38 -0600

    Update 893 The old switcheroo 2.js

--- Kata/893 The old switcheroo 2.js
+++ Kata/893 The old switcheroo 2.js
@@ -34,3 +34,41 @@ console.log(encode("abc")); // '123'
 console.log(encode("ABCD")); // '1234'
 console.log(encode("ZzzzZ")); // '2626262626'
 console.log(encode("abc-#@5")); // '123-#@5'
+
+
+
+
+const encode = str => {
+  return str.replace(/[a-z]/gi, l => {
+    let cc = l.charCodeAt(0);
+    return cc - (cc < 97 ? 64 : 96);
+  });
+}
+
+
+function encode(str) {
+  return str.replace(/[a-z]/gi, c => c.charCodeAt() - (c < "a" ? 64 : 96))
+}
+
+
+const encode = str => str.toLowerCase().replace(/[a-z]/g, $ => $.codePointAt()-96)
+
+
+
+const encode = str =>
+  str.replace(/[A-Z]/gi, val => val.charCodeAt() & 31);
+
+
+
+function encode(str) {
+  var letters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  return str.split('').map(x => {let index = letters.indexOf(x.toLowerCase());return index == -1 ? x : index+1}).join('')
+}
+
+
+
+let encode = s => s.toLowerCase().split('').map(el => (el.codePointAt() < 123 && el.codePointAt() > 96) ? el.codePointAt() - 96 : el).join('');
+
+
+
+const encode = str => str.replace(/[a-z]/gi,(_,i) => 'abcdefghijklmnopqrstuvwxyz'.indexOf(_.toLowerCase())+1);
