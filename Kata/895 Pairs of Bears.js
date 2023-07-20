b957094a8bffff3d650fdc5b4c8426848b53c1a4
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-07-20 12:50:33 -0600

    Update 895 Pairs of Bears.js

--- Kata/895 Pairs of Bears.js
+++ Kata/895 Pairs of Bears.js
@@ -41,3 +41,46 @@ function bears(x, s) {
 console.log(bears(7, "8j8mBliB8gimjB8B8jlB")); // ["B8B8B8",false]
 console.log(bears(3, "88Bifk8hB8BB8BBBB888chl8BhBfd")); // ["8BB8B8B88B",true]
 console.log(bears(8, "8")); // ["",false]
+
+
+
+function bears(x, s){
+  var pairs = s.match(/(8B)|(B8)/g) || [];
+  return [pairs.join(""), pairs.length >= x];
+}
+
+
+
+const bears=(x,s)=>[ y = (s.match(/B8|8B/g) || [] ).join(''), y.length / 2 >= x ];
+
+
+
+function bears(x, s){
+  var count = 0;
+  var out = '';
+  
+  for (var i = 0; i < s.length; i++){
+    if (s[i] === 'B' && s[i+1] === '8' || s[i] === '8' && s[i+1] === 'B') {
+      out += s[i] + s[i+1];
+      i ++;
+      count ++;
+    }
+  } 
+  var res = [out, count >= x ? true : false];
+  return res;
+}
+
+
+
+function bears(x, s){
+ let pairsOfBears = s.match(/8B|B8/g);
+ pairsOfBears = (pairsOfBears === null?  "":  pairsOfBears.join(""))
+ return [pairsOfBears, pairsOfBears.length/2 >= x];
+}
+
+
+
+bears = (a,b,c = b.match(/(8B)|(B8)/g) | |[] ) => [c.join``, c.length >= a]
+
+
+const bears = (x, s) => (arr => [arr.join``, arr.length >= x])(s.match(/B8|8B/g) || []);
