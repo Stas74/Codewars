e4a116f12741fdb154b82a4e5314f8ab6e2e9227
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-07-21 13:10:59 -0600

    Update 896 Cat and Mouse - Harder Version.js

--- Kata/896 Cat and Mouse - Harder Version.js
+++ Kata/896 Cat and Mouse - Harder Version.js
@@ -43,3 +43,72 @@ console.log(catMouse("..D.....Cm", 13)); // "Caught!"
 console.log(catMouse("............C.............D..m...", 8)); // "Escaped!"
 console.log(catMouse("...m....D....C.......", 10)); // "Protected!"
 console.log(catMouse("m.C...", 5)); // "boring without all three"
+
+
+
+
+function catMouse(x, j){
+	var index = {};
+	for(var l = x.length-1; l >=0; --l){
+  	index[x[l]] = l;
+  }
+  var {C,D,m} = index;
+  if(Number.isNaN(C+D+m)) return 'boring without all three'
+  if(Math.abs(m-C) <= j){
+  	if((D > C && D < m) || (D > m && D < C)) return 'Protected!'
+    return 'Caught!'
+  }
+  return 'Escaped!'
+}
+
+
+
+function catMouse(str, j){
+  const index = {};
+  for (let i = str.length - 1; i >= 0; --i) index[str[i]] = i;
+  
+  let {C, m, D} = index;
+  
+  if (!(m + C + D > 0)) return 'boring without all three';
+  if (C > m) m = [C, C = m][0];
+  if (m - C > j) return 'Escaped!';
+  return D > C && D < m ? 'Protected!' : 'Caught!';
+}
+
+
+
+function catMouse(x, j){
+		var animoC =  RegExp('C','i');
+		var animoD =  RegExp('D','i');
+		var animom =  RegExp('m','i');
+    if(!(x.match(animoC) && x.match(animoD) && x.match(animom))){
+    	return 'boring without all three'
+    }
+    var qq = new RegExp('(m|C).{0,'+(j-1)+'}(C|m)');
+    if(x.match(qq)){
+    	var mm = new RegExp('(m|C)\.*D\.*(C|m)');
+      if(x.match(mm)){
+      return 'Protected!'
+      }
+    	return 'Caught!'
+    }
+    return 'Escaped!'
+}
+
+
+
+const between = (x, a, b) => Math.min(a, b) < x && x < Math.max(a, b);
+function catMouse(x, j) {
+  let c = x.indexOf('C');
+  let m = x.indexOf('m');
+  let d = x.indexOf('D');
+  if (![c, m, d].every(i => i >= 0)) {
+    return 'boring without all three';
+  }
+  if (Math.abs(c - m) - 1 < j) {
+    return between(d, c, m) ? 'Protected!' : 'Caught!';
+  }
+  return 'Escaped!'
+}
+
+
