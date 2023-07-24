e4c886ef6e715bc991d2a2888a8146847317b29f
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-07-24 14:29:34 -0600

    Update 897 Find the area of the rectangle!.js

--- Kata/897 Find the area of the rectangle!.js
+++ Kata/897 Find the area of the rectangle!.js
@@ -15,3 +15,32 @@ console.log(area(10, 6)); // 48
 console.log(area(13, 5)); // 60
 console.log(area(12, 5)); // 54.54
 console.log(area(56, 65)); // "Not a rectangle"
+
+
+
+
+function area(d, l) {
+  return Math.round(l * Math.sqrt(d*d - l*l) * 1e2) / 1e2 || 'Not a rectangle';
+}
+
+
+
+function area(d,l){
+  return d <= l ? "Not a rectangle" : +(l * Math.sqrt((d*d)-(l*l))).toFixed(2);
+}
+
+
+
+const area = (z, a) => a >= z ? "Not a rectangle" : Math.round( a * ( Math.sqrt(z * z - a * a) ) * 100 ) / 100
+
+
+
+const area = (d, l) =>
+  Math.round((d ** 2 - l ** 2) ** .5 * l * 1e2) / 1e2 || `Not a rectangle`;
+
+
+function area(d,l){
+  return (d <= l) ? 'Not a rectangle' : +(Math.sqrt(d**2 - l**2) * l).toFixed(2);
+}
+
+
