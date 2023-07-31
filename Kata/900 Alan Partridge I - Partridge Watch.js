e1ee5387fda178cdc555a36abf4981f36db5c827
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-07-31 14:18:47 -0600

    Update 900 Alan Partridge I - Partridge Watch.js

--- Kata/900 Alan Partridge I - Partridge Watch.js
+++ Kata/900 Alan Partridge I - Partridge Watch.js
@@ -42,3 +42,32 @@ function part(x) {
 
 console.log(part(["Grouse", "Partridge", "Pheasant"])); // 'Mine\'s a Pint!'
 console.log(part(["Pheasant", "Goose", "Starling", "Robin"])); // 'Lynn, I\'ve pierced my foot on a spike!!'
+
+
+
+const part = x =>
+  (val => val ? `Mine's a Pint` + `!`.repeat(val) : `Lynn, I've pierced my foot on a spike!!`)
+  (x.filter(val => [`Partridge`, `PearTree`, `Chat`, `Dan`, `Toblerone`, `Lynn`, `AlphaPapa`, `Nomad`].includes(val)).length);
+
+
+
+function part(x){
+  let arr = ['Partridge','PearTree','Chat','Dan','Toblerone','Lynn','AlphaPapa','Nomad']
+  let count = x.filter(item => arr.includes(item)).length
+  return (count > 0) ? 'Mine\'s a Pint!' + '!'.repeat(count-1) : 'Lynn, I\'ve pierced my foot on a spike!!'
+}
+
+
+
+function part(x){
+  var count = x.filter(e => ['Partridge','PearTree','Chat','Dan','Toblerone','Lynn','AlphaPapa','Nomad'].indexOf(e) != -1).length;
+  return count > 0 ? 'Mine\'s a Pint' + '!'.repeat(count) : 'Lynn, I\'ve pierced my foot on a spike!!';
+}
+
+
+
+const part = ($) => {
+  let list = ['Partridge','PearTree','Chat','Dan','Toblerone','Lynn','AlphaPapa','Nomad']
+  let out = $.filter(el => list.includes(el)) || []
+  return out.length ? 'Mine\'s a Pint' + '!'.repeat(out.length) : 'Lynn, I\'ve pierced my foot on a spike!!'
+}
