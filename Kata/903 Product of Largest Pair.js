1b4bd79734c7c4c9b5183119069969fbc94dfde2
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-08-03 14:55:10 -0600

    Update 903 Product of Largest Pair.js

--- Kata/903 Product of Largest Pair.js
+++ Kata/903 Product of Largest Pair.js
@@ -28,3 +28,71 @@ function maxProduct(a) {
 console.log(maxProduct([56, 335, 195, 443, 6, 494, 252])); // 218842
 console.log(maxProduct([154, 428, 455, 346])); // 194740
 console.log(maxProduct([136, 376, 10, 146, 105, 63, 234])); // 87984
+
+
+
+
+function maxProduct(a) {    
+  var max = a[0];
+  var preMax = a[0];
+  for(var i=1;i<a.length;i++)
+  {
+    if(a[i] > max)
+    {
+      preMax = max;
+      max = a[i];
+    }
+    else
+    {
+      if(a[i] > preMax)
+      {
+        preMax = a[i];
+      }
+    }
+  }
+  return max * preMax;
+}
+
+
+
+function maxProduct(a) {
+    let largestVal = a.splice((a.indexOf(Math.max(...a))),1)
+    let secondVal = a.splice((a.indexOf(Math.max(...a))),1)
+    return largestVal*secondVal
+}
+
+
+
+function maxProduct(a) {
+ return a.splice(a.indexOf(Math.max.apply(Math,a)),1) * Math.max.apply(Math,a);
+}
+
+
+
+maxProduct = a => {
+  let x = 0, y = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > x) {
+      y = x;
+      x = a[i];
+    }
+    else if (a[i] > y) {
+      y = a[i];
+    }
+  }
+  return x * y;
+}
+
+
+
+function maxProduct(a) {
+  const max = Math.max(...a);
+  return max*a.reduce((x,c) => c!==max&&c>x?c:x,0);
+}
+
+
+
+const maxProduct = a => a.splice(a.indexOf(Math.max(...a)), 1) * Math.max(...a);
+
+
+
