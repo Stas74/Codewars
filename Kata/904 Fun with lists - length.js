221e4357d14cf7e37f487d2cb07fee2d1e81f408
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-08-04 13:32:42 -0600

    Update 904 Fun with lists - length.js

--- Kata/904 Fun with lists - length.js
+++ Kata/904 Fun with lists - length.js
@@ -26,3 +26,39 @@ function length(head) {
 }
 
 console.log(length(null)); // 0
+
+
+
+function length(head) {
+	if (head == null) return 0;
+	return 1 + length(head.next);
+}
+
+
+function length(head) {  
+  var currentNode = head;  
+  for (var i=0;currentNode; ++i) {    
+    currentNode = currentNode.next
+  }  
+  return i;
+}
+
+
+
+function length(head) {
+  return !head ? 0 : 1 + length(head.next);
+}
+
+
+
+const length = $ => !$ ? 0 : length($.next) + 1
+
+
+
+function length(head) {
+// first test if head exists
+// if not return 0
+// else return 1 + call function again with next element in list and do the same check
+// so each time we have a pointer to a next elemnt we will add 1 and return the sum in the end
+  return typeof head === 'undefined' || head === null ? 0 : 1 + length(head.next);
+}
