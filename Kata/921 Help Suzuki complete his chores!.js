dcc70064720286d6d0c468094281860f69c63ca0
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-09-22 14:49:02 -0600

    Update 921 Help Suzuki complete his chores!.js

--- Kata/921 Help Suzuki complete his chores!.js
+++ Kata/921 Help Suzuki complete his chores!.js
@@ -27,3 +27,52 @@ function choreAssignment(chores) {
 
 console.log(choreAssignment([5, 2, 1, 6, 4, 4])); // [7, 7, 8]
 console.log(choreAssignment([1, 5, 2, 8, 4, 9, 6, 4, 2, 2, 2, 9])); // [7, 8, 8, 10, 10, 11]
+
+
+
+
+function choreAssignment(chores) {
+ var result = [];
+ chores.sort((a,b)=> a-b);
+ while (chores.length) {
+  result.push(chores.pop() + chores.shift());
+ };
+ return result.sort((a,b)=> a-b);
+}
+
+
+
+
+function choreAssignment(chores) {
+ return chores.sort((a,b) => a-b)
+  .map((cur, idx) => cur + chores[chores.length-idx-1])
+  .splice(chores.length/2, chores.length/2)
+  .sort((a,b) => a-b);
+}
+
+
+
+
+var reduction = A => A.reduce((a,b)=>a+b,0),
+    desc = (a,b)=>b-a,
+    asc = (a,b)=>a-b
+    
+function choreAssignment(chores) {
+ var group = []
+ var chores = chores.sort(desc, 0)
+ // Group into twos
+   for(var i=0; i<chores.length/2; i++){
+     group.push([chores[i], chores[chores.length-i-1]])
+   }
+ // replace with sum of entries
+ return group.map(reduction).sort(asc,0)
+}
+
+
+
+
+const choreAssignment = (chores,length = chores.length) =>
+  chores.sort((a,b) => a - b)
+        .map((element,index) => element + chores[length - 1 - index])
+        .slice(length / 2)
+        .sort((a,b) => a - b)
