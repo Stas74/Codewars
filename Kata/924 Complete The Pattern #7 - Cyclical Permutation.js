be257d5ac0e4bedc1b3a76dcd6db227ff50af0f6
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-10-03 17:24:02 -0600

    Update 924 Complete The Pattern #7 - Cyclical Permutation.js

--- Kata/924 Complete The Pattern #7 - Cyclical Permutation.js
+++ Kata/924 Complete The Pattern #7 - Cyclical Permutation.js
@@ -46,9 +46,48 @@ function pattern(n) {
   }
   return finArr.map((el) => el.join("")).join("\n");
 }
+
 // console.log("slice1 = ", finArr[0].slice(0,1));
 // console.log("slice2 = ", finArr[0].slice(-3));
 // console.log("push = ", finArr.push(finArr[0].slice(-3).concat(finArr[0].slice(0,1))));
 // console.log(finArr);
 
+
 console.log(pattern(4)); // "1234\n2341\n3412\n4123"
+
+
+
+function pattern(n) {
+  for (var output = [], o = 1; o <= n; o += 2) {
+    output.push(Array(o + 1).join(o));
+  }
+  return output.join("\n");
+}
+
+
+function pattern(n){
+ var output = "";
+ for(var i = 1; i <= n; i+=2) {
+   output += new Array(i+1).join(i) + '\n';
+ }
+ return output.trim();
+}
+
+
+
+function pattern(n){
+  return n = n % 2 == 0 ? n - 1 : n, n > 1 ? pattern(n - 2) + '\n' + Array.apply(0, Array(n)).map(function(_, i) {return n;}).join('') : n > 0 ? '1' : '';
+}
+
+
+
+const pattern = n =>
+  [...Array(Math.max(0, ++n))].reduce((pre, _, idx) => idx % 2 ? [...pre, `${idx}`.repeat(idx)] : pre, []).join(`\n`);
+
+
+
+function pattern(n) {
+  for (var lines = [], i=1; i<=n; i+=2) 
+    lines.push(Array(i+1).join(i));
+  return lines.join('\n');
+}
