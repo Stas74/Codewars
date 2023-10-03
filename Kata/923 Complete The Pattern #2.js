909ece346ca3bad31ceeadd48f9991e011117906
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-10-02 18:22:17 -0600

    Update 923 Complete The Pattern #2.js

--- Kata/923 Complete The Pattern #2.js
+++ Kata/923 Complete The Pattern #2.js
@@ -59,3 +59,81 @@ function pattern(n) {
 console.log(pattern(1)); // "1"
 console.log(pattern(2)); // "21\n2"
 console.log(pattern(5)); // "54321\n5432\n543\n54\n5"
+
+
+
+function pattern(n) {
+  if (n < 1) return ''
+  var lines = []
+  
+  for (var i = 0; i < n; i++) {
+    var line = ''
+    
+    for (var j = n; j > i; j--) {
+      line += j
+    }
+    
+    lines.push(line)
+  }  
+  return lines.join('\n')
+}
+
+
+
+function pattern(n) {
+ var number = "", output = "";
+ while (n > 0) {
+   number = number + n;
+   output = number + (output ? "\n" : "") + output;
+   n--;
+ }
+ return output;
+}
+
+
+
+function pattern(n){
+  var output="";
+  for(i=0;i<n;i++){
+    for(j=n;j>i;j--){
+      output+= j;
+    }
+    if(i<n-1)output+="\n"
+  }   
+  return output;
+}
+
+
+
+function pattern(n) {
+  return n > 0 ? Array.apply(n, Array(n)).map(function(v, i) {return Array.apply(0, Array(n - i)).map(function(w, j) {return n - j;}).join('');}).join('\n') : '';
+}
+
+
+
+const pattern = n =>
+  n > 0 ? [...Array(n)].map((_, idx) => [...Array(n - idx)].map((_, idx) => n - idx).join(``)).join(`\n`) : ``;
+
+
+const pattern=(n,s='')=>Array.from({length:n},_=>s=s+n--).reverse().join`\n`;
+/*
+Notes:
+Handling negtive values of n is handled by using Array.from({length:n}) 
+instead of Array.from(Array(n)).
+Array(n) will throw an Uncaught RangeError when n is negative.
+
+Calling .map exlicitly on the array is avoided because a map function
+ can be passed as the second argument to Array.from
+
+The expression s=s+n-- is doing 3 things in one.
+1 - changing the value of s by appending the current value of n
+2 - decrementing n by one
+3 - returning the new value of s as the result of the map function.
+
+join`\n` saves 2 characters by calling .join as a tag function.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#Tagged_templates
+When called as a tag function, the first argument passed to join is an array of string values.
+When the value passed to Array.prototype.join is not a string, it gets converted to a string.
+Arrays get converted to strings as a string of comma separated values.
+An array with just a single string element will get converted to simply the string element.
+*/
