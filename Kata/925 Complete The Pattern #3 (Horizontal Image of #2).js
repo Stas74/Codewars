1db07b8f0e07c5a953d1f823ac3d862166e506e0
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-10-04 17:10:26 -0600

    Update 925 Complete The Pattern #3 (Horizontal Image of #2).js

--- Kata/925 Complete The Pattern #3 (Horizontal Image of #2).js
+++ Kata/925 Complete The Pattern #3 (Horizontal Image of #2).js
@@ -50,9 +50,52 @@ function pattern(n) {
   return finArr.map((el) => el.join("")).join("\n");
 }
 
-// console.log("slice1 = ", finArr[0].slice(0,1));
-// console.log("slice2 = ", finArr[0].slice(-3));
-// console.log("push = ", finArr.push(finArr[0].slice(-3).concat(finArr[0].slice(0,1))));
-// console.log(finArr);
-
 console.log(pattern(5)); // "5\n54\n543\n5432\n54321"
+
+
+
+function pattern(n) {
+  var res = [], line = '', i;
+  for(i = n; i > 0; i--) {
+    line += i;
+    res.push(line);
+  }
+  return res.join('\n');
+}
+
+
+
+function pattern(n){
+  var result = [], last = '';
+  for (var i = n; i > 0; i--) {
+    result.push(last += i);
+  }  
+  return result.join('\n');
+}
+
+
+
+const pattern = n =>
+  [...Array(Math.max(n, 0))].map((_, idx) => [...Array(++idx)].map((_, idx) => n - idx).join(``)).join(`\n`);
+
+
+
+function pattern(n) {
+  return n > 0 ? Array.apply(n, Array(n)).map(function(_, i) {return Array.apply(0, Array(i + 1)).map(function(_, j) {return n - j;}).join('');}).join('\n') : '';
+}
+
+
+const pattern = (n,s='')=> Array.from({length:n}, _=> s=s+n--).join('\n');
+
+
+
+function pattern(n){
+ var number = n, line = n < 1 ? "" : "" + n, output = line;
+ 
+ for(var i = 1; i < n; i++)
+ {
+   line += (--number);
+   output += "\n" + line;
+ }
+ return output;
+}
