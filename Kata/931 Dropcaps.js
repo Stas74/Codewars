7a1b5b55efd1d96064614d255a5fafab714def53
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-10-13 15:56:18 -0600

    Update 931 Dropcaps.js

--- Kata/931 Dropcaps.js
+++ Kata/931 Dropcaps.js
@@ -24,3 +24,35 @@ console.log(dropCap("Apple Banana")); // "Apple Banana"
 console.log(dropCap("more  than    one space between words")); // "More  Than    One Space Between Words"
 console.log(dropCap("rAnDoM CaPs CrAzInEsS")); // "Random Caps Craziness"
 
+
+
+function dropCap(n) {
+  return n.replace(/\S{3,}/g, w => w.slice(0, 1).toUpperCase() + w.slice(1).toLowerCase());
+}
+
+
+
+function dropCap(n) {
+  n = n.split(' ');
+  for(var i = 0; i < n.length; i++) {
+    if(n[i].length > 2) {
+      n[i] = n[i].slice(0, 1).toUpperCase() + n[i].slice(1).toLowerCase();
+    }
+  }
+  return n.join(' ');
+}
+
+
+
+const dropCap = n =>
+  n.replace(/\b(\w)(\w{2,})/g, (_, $1, $2) => $1.toUpperCase() + $2.toLowerCase());
+
+
+
+const dropCap = $ => 
+  $.match(/\s|[,]|\w*/g)
+    .map(el => el.length > 2 && el !== '' ? (el[0].toUpperCase() + el.slice(1).toLowerCase()) : el)
+      .join('')
+
+
+
