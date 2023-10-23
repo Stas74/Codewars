9a494b9140f5570d474e4034eb4240fe80ee7eee
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-10-23 16:48:11 -0600

    Create 936 Linked Lists - Length & Count.js

---
+++ Kata/936 Linked Lists - Length & Count.js
@@ -0,0 +1,47 @@
+// Linked Lists - Length & Count
+
+/*
+Linked Lists - Length & Count
+
+Implement Length() to count the number of nodes in a linked list.
+
+length(null) => 0
+length(1 -> 2 -> 3 -> null) => 3
+
+Implement Count() to count the occurrences of an integer in a linked list.
+
+count(null, 1) => 0
+count(1 -> 2 -> 3 -> null, 1) => 1
+count(1 -> 1 -> 1 -> 2 -> 2 -> 2 -> 2 -> 3 -> 3 -> null, 2) => 4
+
+I've decided to bundle these two functions within the same Kata since they are both very similar.
+
+The push()/Push() and buildOneTwoThree()/BuildOneTwoThree() functions do not need to be redefined.
+*/
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function length(head) {
+  let pointer = head;
+  let totalLength = 0;
+  while(pointer) {
+    totalLength += 1;
+    pointer = pointer.next;
+  }
+  return totalLength;
+}
+
+function count(head, data) {
+  let pointer = head;
+  let countInteger = 0;
+  while(pointer) {
+    if(pointer.data === data) {
+      countInteger += 1;
+    }
+    pointer = pointer.next;
+  }
+  return countInteger;
+}
