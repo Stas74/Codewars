ea7713917e4c69fceccf49c022ecc414d7ec9471
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-10-24 13:45:01 -0600

    Update 937 LinkedList --- Array.js

--- Kata/937 LinkedList --- Array.js
+++ Kata/937 LinkedList --- Array.js
@@ -23,8 +23,35 @@ function listToArray(list) {
   return finArr;
 }
 
+
 var list1 = { value: 1, next: { value: 2, next: { value: 3, next: null } } };
 var list2 = { value: "foo", next: { value: "bar", next: null } };
 
 console.log(listToArray(list1)); // [1, 2, 3]
 console.log(listToArray(list2)); // ["foo", "bar"]
+
+
+
+
+function listToArray(list) {
+  var array = [];
+  for (var node = list; node; node = node.next)
+    array.push(node.value);
+  return array;
+}
+
+
+
+
+function listToArray(list) {
+  return !list ? [] : [list.value].concat(listToArray(list.next));
+}
+
+
+const listToArray = (list) => list == null ? [] : [list.value].concat(listToArray(list.next));
+
+
+const listToArray = list => list === null ? [] : [list.value, ...listToArray(list.next)];
+
+
+
