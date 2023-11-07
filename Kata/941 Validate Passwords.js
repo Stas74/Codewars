67e24395e784cfec7c6e1a67da2fa7232c4797a7
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-11-07 12:04:54 -0700

    Update 941 Validate Passwords.js

--- Kata/941 Validate Passwords.js
+++ Kata/941 Validate Passwords.js
@@ -49,3 +49,66 @@ var mary = signIn("Bubblehead123");
 console.log(logIn("Mr. me")); // true
 console.log(logIn("Bubblehead123")); // true
 console.log(logIn("This isn't a password")); // false
+
+
+
+
+const passwords = new Set()
+
+function signIn(password) {
+  passwords.add(password)
+}
+
+function logIn(password) {
+  return passwords.has(password)
+}
+
+
+
+const passwords = [];
+
+var signIn = function(newPassword) {
+  passwords.push(newPassword);
+};
+var logIn = function(password) {
+  return passwords.includes(password);
+};
+
+
+
+
+const passwords = []
+var signIn = function(newPassword) {
+  return passwords.push(newPassword);
+};
+var logIn = function(password) {
+  return !!passwords.find(v=>v===password)
+};
+
+
+
+
+var signIn = function(newPassword) {
+  log.add(newPassword);
+};
+var logIn = function(password) {
+  return log.has(password) ? true : false; 
+};
+
+var log = new Set();
+
+
+
+
+var storage = [];
+var signIn = function(newPassword) {
+  storage.push(newPassword);
+};
+var logIn = function(password) {
+	for (var i = 0; i < storage.length; i++) {
+		if(storage[i] == password) {
+			return true;
+		}
+	}
+	return false;
+};
