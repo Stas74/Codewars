246646befb8d56fd5d0994d66e682616c21e0bd6
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-11-10 11:50:51 -0700

    Update 944 Remove duplication.js

--- Kata/944 Remove duplication.js
+++ Kata/944 Remove duplication.js
@@ -25,3 +25,40 @@ function removeDuplication(arr) {
 console.log(removeDuplication([1, 2, 3, 2, 4, 6, 2, 6, 7])); // [1,3,4,7]
 console.log(removeDuplication([1, 2, 1, 2, 1, 1, 2, 2])); // []
 console.log(removeDuplication([2, 5, 6, 7, 5, 2, 6])); // [7]
+
+
+
+
+const removeDuplication = a => a.filter( v => a.indexOf(v)===a.lastIndexOf(v) ) ;
+
+
+
+function removeDuplication(arr) {
+  const counts = new Map();
+  for (const x of arr)
+    counts.set(x, (counts.get(x) || 0) + 1);
+  return arr.filter(x => counts.get(x) == 1);
+}
+
+
+
+const removeDuplication = arr =>
+  arr.filter(val => arr.indexOf(val) === arr.lastIndexOf(val));
+
+
+
+function removeDuplication(arr) {
+  var h = arr.reduce((b, n) => ((b[n] = (b[n] || 0) + 1), b), {});
+  return arr.filter((e) => h[e] < 2);
+}
+
+
+
+function removeDuplication(arr) {
+  const counter = {}
+  for(const el of arr) {
+    counter[el] = (counter[el] || 0) + 1
+  }
+  return arr.filter(el => counter[el] === 1)
+}
+
