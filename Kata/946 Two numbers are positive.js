d5056792323d7cd0989aa1c94703b0cc6a8d4458
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-11-16 12:04:57 -0700

    Update 946 Two numbers are positive.js

--- Kata/946 Two numbers are positive.js
+++ Kata/946 Two numbers are positive.js
@@ -21,3 +21,41 @@ console.log(twoArePositive(2, 4, -3)); // true
 console.log(twoArePositive(-4, 6, 8)); // true
 console.log(twoArePositive(-4, 6, 0)); // false
 console.log(twoArePositive(4, 6, 10)); // false
+
+
+
+
+function twoArePositive(a, b, c) {
+  return (a > 0) + (b > 0) + (c > 0) == 2;
+}
+
+
+
+function twoArePositive(a, b, c) {
+  return [a, b, c].filter(val => val > 0).length === 2
+}
+
+
+
+function twoArePositive(a, b, c) {
+  // Happy Coding
+  let num = 0;
+  if(a > 0){
+    num++
+  } if(b > 0){
+    num++
+  } if(c > 0){
+    num++
+  }
+  return num === 2
+}
+
+
+
+function twoArePositive(...arr) {
+  return arr.filter(e => e>0).length==2;
+}
+
+
+
+const twoArePositive = (...a) => a.filter(b => b > 0).length === 2;
