a354dac5bcf4c0199937336b2c6b0c655cf22a6c
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-11-17 16:23:24 -0700

    Update 947 extract portion of file name.js

--- Kata/947 extract portion of file name.js
+++ Kata/947 extract portion of file name.js
@@ -36,5 +36,85 @@ function extractFileName(dirtyFileName) {
 }
 // return dirtyFileName.replace(/(\d)/g, "").slice(1).split(".").slice(0,2).join(".")
 
+class FileNameExtractor {
+    static extractFileName (dirtyFileName) {
+        return dirtyFileName.split("_").slice(1).join("_").split(".").slice(0,2).join(".")
+    }
+}
+
 console.log(extractFileName("1_FILE_NAME.EXTENSION.OTHEREXTENSIONadasdassdassds34")); // "FILE_NAME.EXTENSION"
 console.log(extractFileName("1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION")); // "FILE_NAME.EXTENSION"
+
+
+
+class FileNameExtractor {
+
+  static extractFileName = dirty => dirty.match(/^\d+_([^.]+\.[^.]+)/)[1];
+
+}
+
+
+
+class FileNameExtractor {
+    static extractFileName (dirtyFileName) {
+        var numSlice = dirtyFileName.indexOf('_')
+        var extSlice = dirtyFileName.lastIndexOf('.')
+        dirtyFileName = dirtyFileName.slice(numSlice+1, extSlice)
+        return dirtyFileName
+    }
+}
+
+
+
+
+class FileNameExtractor {
+    static extractFileName (dFN) {
+        // pfft who needs REGEXZzz... :)
+        return dFN.slice(dFN.indexOf('_') + 1, dFN.lastIndexOf('.'));
+    }
+}
+
+
+
+class FileNameExtractor {
+    static extractFileName (s) {
+        return s.replace(/^\d*_(.*)\..*$/,'$1');
+    }
+}
+
+
+
+class FileNameExtractor {
+    static extractFileName (dirtyFileName) {
+        const regex = /\_(.*)\./;
+        return regex.exec(dirtyFileName)[1];
+    }
+}
+
+
+
+var FileNameExtractor={extractFileName(dirtyFileName) {
+    return dirtyFileName.match( /(?<=^\d+_)[\w-]+\.[\w-]+(?=\..+$)/s )[0];
+}}
+
+
+
+var FileNameExtractor={extractFileName(dirtyFileName) {
+    return dirtyFileName.match( /(?<=^\d+_)[\w-]+\.[\w-]+(?=\..+$)/s )[0];
+}}
+
+
+
+class FileNameExtractor {
+	static extractFileName(dirtyFileName) {
+		return dirtyFileName.replace(/^[0-9]+_/, '').replace(/.[A-Za-z0-9]+$/, '');
+	}
+}
+
+
+
+class FileNameExtractor {
+    static extractFileName (dirtyFileName) {
+        return dirtyFileName.match(/\_[^\.]+\.[^\.]+/)[0].slice(1);
+    }
+}
