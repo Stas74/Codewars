1af948338d12c6f1337b81c2d5913c99b30fd302
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-11-20 11:51:27 -0700

    Create 948 You are a Cube!.js

---
+++ Kata/948 You are a Cube!.js
@@ -0,0 +1,22 @@
+// You are a Cube!
+
+/*
+In geometry, a cube is a three-dimensional solid object bounded by six square faces, facets or sides, 
+with three meeting at each vertex.
+The cube is the only regular hexahedron and is one of the five Platonic solids. 
+It has 12 edges, 6 faces and 8 vertices.
+The cube is also a square parallelepiped, an equilateral cuboid and a right rhombohedron. 
+It is a regular square prism in three orientations, and a trigonal trapezohedron in four orientations.
+
+You are given a task of finding a if the provided value is a perfect cube!
+*/
+
+function youAreACube(value) {
+  return Number.isInteger(Math.cbrt(value));
+}
+
+console.log(youAreACube(1)); // true
+console.log(youAreACube(27)); // true
+console.log(youAreACube(64)); // true
+console.log(youAreACube(2)); // false
+console.log(youAreACube(99)); // false
