f89fed6478e244878c3f9b5aea9b93fa3fbf0bcd
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-11-20 11:57:35 -0700

    Update 948 You are a Cube!.js

--- Kata/948 You are a Cube!.js
+++ Kata/948 You are a Cube!.js
@@ -20,3 +20,41 @@ console.log(youAreACube(27)); // true
 console.log(youAreACube(64)); // true
 console.log(youAreACube(2)); // false
 console.log(youAreACube(99)); // false
+
+
+
+function youAreACube (value){
+  return (Math.cbrt(value) % 1 === 0)
+}
+
+
+
+const youAreACube = value =>
+  !(Math.cbrt(value) % 1);
+
+
+
+function youAreACube (value){
+  var i=0;
+  while (value>i*i*i)
+    i++;
+  return value==i*i*i;
+}
+
+
+
+const youAreACube = (v) =>
+  ((a) => a * a * a == v)(Math.round(Math.pow(v, 1 / 3)));
+
+
+
+const youAreACube = value => {
+  const squareCube = value ** (1/3);
+  const rounded = Math.round(squareCube);
+  
+  return Math.abs(squareCube - rounded) < 0.000001;
+};
+
+
+
+
