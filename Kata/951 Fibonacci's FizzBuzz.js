def7eb0d6bf28a210c57a5eadd8849d95fe7f8ba
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-11-23 11:13:06 -0700

    Update 951 Fibonacci's FizzBuzz.js

--- Kata/951 Fibonacci's FizzBuzz.js
+++ Kata/951 Fibonacci's FizzBuzz.js
@@ -56,3 +56,79 @@ function fibsFizzBuzz(n) {
 console.log(fibsFizzBuzz(1)); // [ 1 ]
 console.log(fibsFizzBuzz(2)); // [ 1, 1 ]
 console.log(fibsFizzBuzz(5)); // [ 1, 1, 2, 'Fizz', 'Buzz' ]
+
+
+
+
+function fibsFizzBuzz(n) {
+  let res = [], [a, b] = [0, 1];
+  for (let i = 0; i < n; i++) {
+    [a, b] = [b, a + b];
+    res.push(a % 15 == 0 ? 'FizzBuzz' :
+      a % 3 == 0 ? 'Fizz' : a % 5 == 0 ? 'Buzz' : a);
+  }
+  return res;
+}
+
+
+
+function fibsFizzBuzz(n) {
+    var a = 0;
+    var b = 1;
+    var res = [1];
+    for(var i = 1;i < n; i++) {
+        var b_ = b;
+        var b = a + b;
+        var a = b_;
+        res.push(fizzBuzz(b));
+    }
+    return res;
+}
+
+function fizzBuzz(n) {
+    if(n % 15 == 0) { return "FizzBuzz"; } 
+    if(n % 5 == 0)  { return "Buzz"; }
+    if(n % 3 == 0)  { return "Fizz"; }
+    return n;
+}
+
+
+
+
+const fibsFizzBuzz = n =>
+  n < 2 ? [1] : [...Array(n - 2)].reduce((pre, _, idx) => [...pre, pre[idx] + pre[++idx]], [1, 1]).map(val => !(val % 15) ? `FizzBuzz` : !(val % 5) ? `Buzz` : !(val % 3) ? `Fizz` : val);
+
+
+
+
+var fibsFizzBuzz = function(n) {
+    let arr = [0, 1]
+    for(let i = 2; i <=n;i++){
+      arr.push(arr[i-2] + arr[i-1])
+    }
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i]%15 == 0) arr[i] = "FizzBuzz"
+       if(arr[i]%5 == 0) arr[i] = "Buzz"
+       if(arr[i]%3 == 0) arr[i] = "Fizz"
+    }
+  arr.shift()
+  return arr 
+}
+
+
+
+const fib = function* () {
+  let [prevVal, val] = [0, 1];
+  
+  while (true) {
+    yield val;
+    [prevVal, val] = [val, prevVal + val];
+  }
+}
+const fibsFizzBuzz = (n, fibGen = fib()) => 
+  [...Array(n)].map(() => fibGen.next().value)
+               .map(val => (val % 3 ? '' : 'Fizz') + (val % 5 ? '' : 'Buzz') || val);
+
+
+
+
