3069953d772426a5cdcb321dbaaf21e19a9fe3dc
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-12-01 12:06:04 -0700

    Update 956 Sorting Arrays.js

--- Kata/956 Sorting Arrays.js
+++ Kata/956 Sorting Arrays.js
@@ -26,3 +26,29 @@ var a1 = ["giraffe", "orangutan", "impala", "elephant", "rhino"];
 var a2 = ["rattlesnake", "eagle", "geko", "iguana", "octopus"];
 
 console.log(sortArray(a1, a2)); // ['geko', 'octopus', 'iguana', 'eagle', 'rattlesnake']
+
+
+
+const sortArray = (a1, a2) => a1.map( v => a2.find( w => v[0]===w[0] ) );
+
+
+
+function sortArray(a1, a2) {
+  a1 = a1.map(e => e[0]).join('');
+  return a2.sort((a,b) => a1.indexOf(a[0]) > a1.indexOf(b[0]));
+}
+
+
+
+function sortArray(a1, a2) {
+    let d = a1.reduce((o,w,i)=>{ o[w[0]]=i; return o; }, {});
+    return a2.sort( (a,b) => d[a[0]]-d[b[0]] );
+}
+
+
+
+const sortArray = (a1, a2) =>
+  [...a2].sort(([a0], [b0]) => a1.findIndex(([an0]) => an0 === a0) - a1.findIndex(([an0]) => an0 === b0));
+
+
+
