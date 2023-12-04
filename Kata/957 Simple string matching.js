ebbf3aa2ef8bfc41dee8ce5a34d3ef6b6a621435
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-12-04 15:59:38 -0700

    Create 957 Simple string matching.js

---
+++ Kata/957 Simple string matching.js
@@ -0,0 +1,41 @@
+// Simple string matching
+
+/*
+You will be given two strings a and b consisting of lower case letters, but a will have at most one asterix character. 
+The asterix (if any) can be replaced with an arbitrary sequence (possibly empty) of lowercase letters. 
+No other character of string a can be replaced. 
+If it is possible to replace the asterix in a to obtain string b, then string b matches the pattern.
+
+If the string matches, return true else false.
+
+For example:
+
+solve("code*s","codewars") = true, because we can replace the asterix(*) with "war" to match the second string. 
+solve("codewar*s","codewars") = true, because we can replace the asterix(*) with "" to match the second string. 
+solve("codewars","codewars") = true, because the strings already match.
+solve("a","b") = false
+solve("*", "codewars") = true
+
+More examples in test cases.
+
+Good luck!
+
+*/
+
+function solve(a, b) {
+  return new RegExp(`^${a.replace(/\*/g, '(.)*')}$`, '').test(b);  
+}
+
+// if (/[*]/.test(a)) {
+//     let arrA = a.split("*");
+//     return b.includes(arrA[0]) && b.includes(arrA[1]);
+//   }
+//   return a === b;
+
+
+console.log(solve("code*s", "codewars")); // true
+console.log(solve("codewar*s", "codewars")); // true
+console.log(solve("code*warrior", "codewars")); // false
+console.log(solve("c", "c")); // true
+console.log(solve("*s", "codewars")); // true
+console.log(solve("*s", "s")); // true
