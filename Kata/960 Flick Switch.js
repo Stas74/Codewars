d5845bc3aac70a3b84b61790dec922f2826de5f5
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-12-07 15:29:23 -0700

    Update 960 Flick Switch.js

--- Kata/960 Flick Switch.js
+++ Kata/960 Flick Switch.js
@@ -33,3 +33,65 @@ function flickSwitch(arr) {
 
 console.log(flickSwitch(["codewars", "flick", "code", "wars"])); // [true, false, false, false]
 console.log(flickSwitch(["flick", "chocolate", "adventure", "sunshine"])); // [false, false, false, false]
+
+
+
+function flickSwitch(arr){
+  let returning = true;
+    return arr.map( (v) => {
+      return (v === "flick") ? returning = !returning : returning;
+    })
+}
+
+
+
+
+function flickSwitch(arr){
+  let flick = true  
+   return arr.map(item => item === 'flick'? flick = !flick: flick)
+}
+
+
+
+
+function flickSwitch(arr){
+  let flag = true
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] == 'flick'){
+      flag == false ? flag = true : flag = false
+    } 
+    arr[i] = flag
+  }
+  return arr
+}
+
+
+
+
+function flickSwitch(arr) {
+  return arr.reduce(
+    (a, c, i) =>
+      i == 0
+        ? a.concat(c == "flick" ? false : true)
+        : a.concat(c == "flick" ? !a[a.length - 1] : a[a.length - 1]),
+    []
+  );
+}
+
+
+
+const flickSwitch = (arr) => (flick = true, arr.map(v => v === 'flick' ? flick = !flick : flick));
+
+
+
+
+function flickSwitch(arr) {
+  let True = true
+  
+  return arr.map(item=>{
+    if(item==='flick'){
+      True = !True
+    }
+    return True
+  })
+}
