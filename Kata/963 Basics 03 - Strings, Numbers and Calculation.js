30b013b7b7f551b2c9a58534071ee21b433fbb83
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-12-15 12:36:01 -0700

    Update 963 Basics 03 - Strings, Numbers and Calculation.js

--- Kata/963 Basics 03 - Strings, Numbers and Calculation.js
+++ Kata/963 Basics 03 - Strings, Numbers and Calculation.js
@@ -27,3 +27,80 @@ function calculateString(st) {
 }
 
 console.log(calculateString(";$%§fsdfsd235??df/sdfgf5gh.000kk0000")); // "47"
+
+
+
+
+function calculateString(s){
+    return eval(s.replace(/[^\d+*/.()-]/g,'')).toFixed();
+}
+
+
+
+function calculateString(st){
+    var result = ""
+    var valid_simbols = "0123456789+-/*." 
+    for (var i=0;i<st.length;i++){
+        if (valid_simbols.includes(st[i]))
+            result+=st[i]    
+    }
+    return Math.round(eval(result)).toString();
+}
+
+
+
+function calculateString(s) {
+  return new Function(`return (${s.replace(/[^\d.+*/-]/g, "")}).toFixed()`)();
+}
+
+
+
+const calculateString = st =>
+  (([a, op, b]) => (op === `+` ? +a + +b : op === `-` ? a - b : op === `*` ? a * b : a / b).toFixed())
+  (st.replace(/[^\d./+*-]/g, ``).match(/\d+\.*\d*|[/+*-]/g));
+
+
+
+
+function calculateString(st) {
+  let calculations = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "/": (a, b) => a / b,
+    "*": (a, b) => a * b,
+  };
+
+  let sign = st.search(/[-/+*]/g);
+  let regexGetNumber = /[0-9.]+/gi;
+  let [side1, side2] = st.split(st[sign]);
+  let num1 = parseFloat(side1.match(regexGetNumber).join(""));
+  let num2 = parseFloat(side2.match(regexGetNumber).join(""));
+
+  return Math.round(calculations[st[sign]](num1, num2)).toString();
+}
+
+
+
+const calculateString = (str) => `${Math.round(eval(str.replace(/[^0-9\/\+\-\*\.]/g, '')))}`
+
+
+
+
+function calculateString(st){
+    return Math.round(eval(st.replace(/[A-Za-z;:$%&§?,]/gi, ''))).toString()
+}
+
+
+
+const CALC = {
+  '+' : (a, b) => +a + +b,
+  '-' : (a, b) =>  a -  b,
+  '*' : (a, b) =>  a *  b,
+  '/' : (a, b) =>  a /  b,
+}
+
+const calculateString = str => {
+  const [, op1, opnd, op2] = str.replace(/[^\d+\-*/.]/g, '').match(/(\d*\.?\d+).*?([+\-*/]).*?(\d*\.?\d+)/);
+  
+  return `${Math.round(CALC[opnd](op1, op2))}`;
+}
