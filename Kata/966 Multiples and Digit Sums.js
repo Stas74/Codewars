6a85aeae34a0fd656191d09b65f7b2d9b1413e97
Stas74 <88213143+Stas74@users.noreply.github.com>
2023-12-21 12:12:38 -0700

    Update 966 Multiples and Digit Sums.js

--- Kata/966 Multiples and Digit Sums.js
+++ Kata/966 Multiples and Digit Sums.js
@@ -39,5 +39,86 @@ function procedure(n) {
     .reduce((p, c) => p + c, 0);
 }
 
+
 console.log(procedure(25)); // 25
 console.log(procedure(30)); // 18
+
+
+
+
+function procedure(n){
+  let out = 0, max = 100;
+  
+  while ( max >= n ) {
+    if ( Number.isInteger(max / n) ){
+      const multiple = n * max / n;
+      out += [...String(multiple)].reduce((acc, el)=> acc + +el, 0)
+    }
+    max -= 1;
+  } 
+  return out
+}
+
+
+
+const procedure = n => {
+  const arr = [];  
+  for (let i = 0; i <= 100; i += n) {
+    arr.push(i)
+  }  
+  return arr.join('').split('').reduce((acc, curr) => +acc + +curr);
+}
+
+
+
+function procedure(i) {
+  let arr = [];
+  for (let j = 1; j <= 100; j++) {
+    if (j % i === 0) {
+      arr.push(j);
+    }
+  }
+  let x = arr.map((el) =>
+    el
+      .toString()
+      .split("")
+      .reduce((e, cur) => Number(e) + Number(cur))
+  );
+  let sum = x.reduce((ele, curr) => Number(ele) + Number(curr));
+  return sum;
+}
+
+
+
+function procedure(n) {
+  let [result, num] = [0, n];
+  while ( n <= 100 ) {
+    let min = n;
+    while ( min > 0 ) {
+      result += min % 10;
+      min = min / 10 | 0;
+    }
+    n += num;
+  }
+  return result;
+}
+
+
+
+const procedure = (n) =>
+  Array.from({length: Math.round(100/n)}, (_,i)=> n * (i+1) < 101 ? n * (i+1) : 0)
+  .map(String).map(el => [...el].reduce((ac, e) => ac + Number(e), 0)).reduce((a,b)=> a + b)
+
+
+
+
+function procedure(n){
+  let i = 1;
+  let resultArr = [];
+  while ((100 / n) >= i) {
+    let check = (n * i).toString().split('').reduce((acc, curr) => (+acc) + (+curr), 0);
+    resultArr.push(check);
+    i++;
+  }
+  return resultArr.reduce((acc, curr) => acc + curr, 0);
+}
