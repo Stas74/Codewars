153f66df84a03916b2746c4848e59c16ae899f60
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-01-03 15:05:13 -0700

    Update 969 Sum of Digits - Digital Root.js

--- Kata/969 Sum of Digits - Digital Root.js
+++ Kata/969 Sum of Digits - Digital Root.js
@@ -25,3 +25,44 @@ function digitalRoot(n) {
 
 console.log(digitalRoot(16)); // 7
 console.log(digitalRoot(456)); // 6
+
+
+
+
+function digital_root(n) {
+  return (n - 1) % 9 + 1;
+}
+
+
+
+function digital_root(n) {
+  if (n < 10) return n;
+  
+  return digital_root(
+    n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
+}
+
+
+
+function digital_root(n) {
+  if (n < 10)
+    return n;
+  for (var sum = 0, i = 0, n = String(n); i < n.length; i++)
+    sum += Number(n[i]);   
+  return digital_root(sum);
+}
+
+
+
+function digital_root(n){
+    n = eval(n.toString().split('').join('+'));
+    if (n > 9) {
+        return digital_root(n);
+    }
+    return n;
+}
+
+
+function digital_root(n) {
+  return --n % 9 + 1;
+}
