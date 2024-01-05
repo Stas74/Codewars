1577fcd660046385caec7cf42d2d27d525f5dcae
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-01-05 14:54:44 -0700

    Update 971 String prefix and suffix.js

--- Kata/971 String prefix and suffix.js
+++ Kata/971 String prefix and suffix.js
@@ -37,3 +37,47 @@ console.log(solve("abcda")); // 1
 console.log(solve("abcdabc")); // 3
 console.log(solve("abcabc")); // 3
 console.log(solve("abcabca")); // 1
+
+
+
+
+let solve = s => s.match(/^(.*).*\1$/)[1].length;
+
+
+
+function solve(s){  
+    //input: string
+    // output: number (length of suffix)
+    //mechanical way: suffixes are every letters which are repeated. Check if the suffixes are the same as the beginning
+    // if beginning == ending? if no lenght = 0; if yes, length = suffix.length
+    let maxLen = 0; 
+    let sLen = s.length;
+    for(let i = 0; i <= sLen/2; i++) {
+      maxLen =  s.substring(0, i) == s.substring(sLen-i) ? i : maxLen;
+    }
+    return maxLen;
+}
+
+
+
+function solve(s,i=s.length>>1) { while (i && s.slice(0,i)!=s.slice(-i)) --i; return i; }
+
+
+
+solve = (s) =>
+  [...s].reduce(
+    (x, _, i) =>
+      i * 2 < s.length - 1 && s.slice(0, i + 1) === s.slice(-i - 1) ? i + 1 : x,
+    0
+  );
+
+
+const solve = (s) => {  
+  for (let i = s.length / 2 ^ 0; i > 0; i--)
+    if (s.slice(0, i) === s.slice(-i)) return i;
+  return 0;
+}
+
+
+
+
