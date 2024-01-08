a8bcb35360fc9f690355a428382b9942af77d3f1
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-01-08 16:02:18 -0700

    Create 972 Happy Birthday, Darling!.js

---
+++ Kata/972 Happy Birthday, Darling!.js
@@ -0,0 +1,27 @@
+// Happy Birthday, Darling!
+
+/*
+As you may know, once some people pass their teens, they jokingly only celebrate their 20th or 21st birthday, 
+forever. 
+With some maths skills, that's totally possible - you only need to select the correct number base!
+
+For example, if they turn 32, that's exactly 20 - in base 16... Already 39? That's just 21, in base 19!
+
+Your task is to translate the given age to the much desired 20 (or 21) years, 
+and indicate the number base, in the format specified below.
+
+Note: input will be always > 21
+
+Examples:
+32  -->  "32? That's just 20, in base 16!"
+39  -->  "39? That's just 21, in base 19!"
+Hint: if you don't know (enough) about numeral systems and radix, just observe the pattern!
+*/
+
+function womensAge(n) {
+  return `${n}? That's just ${20 + (n % 2)}, in base ${Math.floor(n / 2)}!`;
+}
+
+console.log(womensAge(32)); // "32? That's just 20, in base 16!"
+console.log(womensAge(39)); // "39? That's just 21, in base 19!"
+console.log(womensAge(22)); // "22? That's just 20, in base 11!"
