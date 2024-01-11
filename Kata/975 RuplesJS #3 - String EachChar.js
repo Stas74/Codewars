89b0588e9d216b11c78eb70986ff80cf714d6a22
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-01-11 12:30:46 -0700

    Create 975 RuplesJS #3 - String EachChar.js

---
+++ Kata/975 RuplesJS #3 - String EachChar.js
@@ -0,0 +1,37 @@
+// RuplesJS #3: String EachChar
+
+/*
+Your team is really excited with all the contributions you've made to the RuplesJS library. 
+They feel the work you're doing will truly help Ruby developers transition to Javascript! 
+They've assigned you another task.
+
+String.eachChar()
+In ruby you can add something after each character in a string like so:
+
+"hello".each_char {|c| print c, ' ' } -> "h e l l o " 
+In the spirit of polymorphism, our eachChar method will allow one of two arguments, a callback function or a string. 
+If a string is presented, it will be added after each character of the original string and return the new string. 
+If a function is presented, it will perform a manipulation of each character in the string.
+
+Example:
+
+"hello".eachChar(" ");
+-> "h e l l o "
+
+"hello all".eachChar(function(char) {
+  if (char == "l") {
+    return char.toUpperCase();
+  } else {
+    return char;
+  }
+});
+-> "heLLo aLL"
+*/
+
+String.prototype.eachChar = function (n) {
+  return this.split("").map(el => typeof n == "function" ? n(el) : el + n).join("");
+};
+
+console.log("hello".eachChar(" ")); // "h e l l o "
+console.log("hello".eachChar("123")); // "h123e123l123l123o123"
+console.log("hello".eachChar(c => c.toUpperCase())); // "h123e123l123l123o123"
