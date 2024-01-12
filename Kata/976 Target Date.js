766f4ae1cd6fd6da525a48055bb3fc5aeb60c888
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-01-12 16:15:13 -0700

    Update 976 Target Date.js

--- Kata/976 Target Date.js
+++ Kata/976 Target Date.js
@@ -42,3 +42,90 @@ function dateNbDays(a0, a, p) {
 
 console.log(dateNbDays(4281, 5087, 2)); // "2024-07-03"
 console.log(dateNbDays(4620, 5188, 2)); // "2021-09-19"
+
+
+
+
+const ln = Math.log;
+function dateNbDays(a0, a, p) {
+	let days = Math.ceil((ln(a) - ln(a0))/ln(1 + p/36000));
+  var date = new Date(2016, 0, 1 + days);
+  return date.toISOString().split('T')[0];
+}
+
+
+
+function dateNbDays(a0, a, p) {
+    // Bankers use 360 day year, so the daily percent is set like this (360 * 100)
+    let dailyRate = p / 36000
+    let days = 0
+    // Determine the number of days it will take to reach "a"
+    while (a0 < a) {
+        a0 = a0 * (1 + dailyRate)
+        days++
+    }
+    // Set Date to January 1, 2016
+    var targetDate = new Date("2016-01-01")
+    // Add the number of days need to set date
+    targetDate.setDate(targetDate.getDate() +  days)
+    // String version of Year
+    var y = targetDate.getFullYear(targetDate).toString();
+    // String version of Month (Added 1 because month index at 0)
+    var m = (targetDate.getMonth(targetDate) + 1).toString();
+    // Add "0" in front of "m" if it is a single digit value
+    m = m.length === 1 ? '0' + m: m
+    // String version of Day
+    var d = targetDate.getDate(targetDate).toString();
+    // Add "0" in front of "d" if it is a single digit value
+    d = d.length === 1 ? '0' + d: d
+    // Add y, m, and d together with "-" in between them
+    return y + "-" + m + "-" + d
+
+}
+
+
+
+const dateNbDays = function(a0, a, p) {
+  const countDay = new Date( 2016, 0, 1 )
+  const profit = p / 36000
+  
+  do{ a0 += a0 * profit
+      countDay.setDate( countDay.getDate() + 1 )
+  } while ( a0 <= a )
+  
+  return countDay.toISOString().split('T')[0]
+}
+
+
+
+const dateNbDays = (a0, a, p) =>
+  new Date(2016, 0, 1 + Math.ceil(Math.log(a / a0) / Math.log(p / 36000 + 1))).toISOString().slice(0, 10);
+
+
+
+const dateNbDays = (a0, a, p) => new Date(2016, 0, Math.ceil(Math.log(a / a0) / Math.log(p / 36000 + 1)) + 1).toISOString().slice(0,10)
+
+
+
+function dateNbDays(a0, a, p) {
+  const startingDate = new Date('2016-01-01')
+  const dailyInterestRate = p / 36000
+  let startingMoney = a0
+  let daysPassed = 0
+  
+  while (startingMoney < a) {
+    daysPassed++
+    startingMoney += startingMoney * dailyInterestRate
+  }
+  startingDate.setDate(startingDate.getDate() + daysPassed)
+  return startingDate.toISOString().split('T')[0]
+}
+
+
+
+const dateNbDays = (a0, a, p) => {
+	const days = Math.ceil(Math.log(a / a0) / Math.log(1 + p / 100 / 360));
+  
+  return new Date( +(new Date('2016-01-01')) + days * 24 * 60 * 60 * 1000 )
+               .toISOString().replace(/T.*/, '');
+}
