e17796e1ad71ffa84b4dee845c8fe38ad85ed87a
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-01-31 11:16:09 -0700

    Create 989 Array plus array.js

---
+++ Kata/989 Array plus array.js
@@ -0,0 +1,20 @@
+// Array plus array
+
+/*
+I'm new to coding and now I want to get the sum of two arrays... 
+Actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+*/
+
+// function arrayPlusArray(arr1, arr2) {
+//   return arr1 + arr2; //something went wrong
+// }
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((prev, cur) => prev + cur);
+}
+
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6])); // 21
+console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6])); // -21
+console.log(arrayPlusArray([100, 200, 300], [400, 500, 600])); // 2100
