9498850fa7c1402d43c97d44eee9c9be76a6f990
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-02-14 14:15:44 -0700

    Create 968 Multiply the number.js

---
+++ Kata/968 Multiply the number.js
@@ -0,0 +1,22 @@
+// Multiply the number
+
+/*
+Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised 
+to the number of digits of each numbers, so, for example:
+
+multiply(3) == 15 // 3 * 5¹
+multiply(10) == 250 // 10 * 5²
+multiply(200) == 25000 // 200 * 5³
+multiply(0) == 0 // 0 * 5¹
+multiply(-3) == -15 // -3 * 5¹
+*/
+
+function multiply(number) {
+  return number * 5 ** String(Math.abs(number)).length;
+}
+
+console.log(multiply(10)); // 250
+console.log(multiply(5)); // 25
+console.log(multiply(200)); // 25000
+console.log(multiply(0)); // 0
+console.log(multiply(-2)); // -10
