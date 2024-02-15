2951b88aa82cd8eeb9a33b302bd24717de63ab40
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-02-15 16:13:06 -0700

    Update 999 Well of Ideas - Easy Version.js

--- Kata/999 Well of Ideas - Easy Version.js
+++ Kata/999 Well of Ideas - Easy Version.js
@@ -17,3 +17,44 @@ console.log(well(["good", "bad", "bad", "bad", "bad"])); // 'Publish!'
 console.log(
   well(["good", "bad", "bad", "bad", "bad", "good", "bad", "bad", "good"])
 ); // 'I smell a series!'
+
+
+
+const well = x => {
+  const good_count = x.filter(x => x == 'good').length;
+  return good_count < 1 ? 'Fail!' : 
+         good_count < 3 ? 'Publish!' : 'I smell a series!';
+}
+
+
+
+function well(x) {
+  switch (x.filter(i => i === 'good').length) {
+    case 0:
+      return 'Fail!'
+    case 1:
+    case 2:
+      return 'Publish!'
+    default:
+      return 'I smell a series!'
+  }
+}
+
+
+
+
+function well(x) {
+  const count = x.reduce((s, v) => s + (v == 'good'), 0);
+  return count ? count > 2 ? 'I smell a series!' : 'Publish!' : 'Fail!';
+}
+
+
+
+
+const well = x =>
+  (val => val ? val > 2 ? `I smell a series!` : `Publish!` : `Fail!`)
+  (x.filter(val => val === `good`).length);
+
+
+
+const well = x => x.includes('good') ? x.filter(s => s == 'good').length < 3 ? 'Publish!' : 'I smell a series!' : 'Fail!';
