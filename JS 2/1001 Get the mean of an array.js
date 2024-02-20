7d10f671dfef03a7782b515278bb048ef64531ac
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-02-20 12:00:44 -0700

    Update 1001 Get the mean of an array.js

--- JS 2/1001 Get the mean of an array.js
+++ JS 2/1001 Get the mean of an array.js
@@ -17,3 +17,32 @@ function getAverage(marks) {
 console.log(getAverage([2, 2, 2, 2])); // 2
 console.log(getAverage([1, 2, 3, 4, 5])); // 3
 console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2])); // 1
+
+
+
+var getAverage = ( m ) => parseInt(m.reduce((a, b)=> a + b)/ m.length)
+
+
+
+function getAverage(marks){
+  // calculates total number of marks
+  var total = 0;
+  for(var i = 0; i < marks.length; i++){
+    total += marks[i];
+  }  
+  /* returns total number of marks divided by number of marks
+     rounded down to nearest integer */
+  return Math.floor(total / marks.length);
+}
+
+
+
+const getAverage = marks =>
+  ~~(marks.reduce((pre, val) => pre + val, 0) / marks.length);
+
+
+
+const getAverage = marks => (marks.reduce((a, b) => a + b) / marks.length) | 0;
+
+
+
