cf2142cf98f3a7bd1a040dd68c61ad65fe6c0939
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-02-27 10:31:44 -0700

    Update 1006 Shifty Closures.js

--- JS 2/1006 Shifty Closures.js
+++ JS 2/1006 Shifty Closures.js
@@ -24,3 +24,58 @@ const greetBen = () => 'Hello, ' + name2 + '!';
 
 console.log(greetAbe()); // 'Hello, Abe!'
 console.log(greetBen()); // 'Hello, Ben!'
+
+
+
+
+var name;
+function greet_abe() {
+  name = 'Abe';
+  return "Hello, " + name + '!';
+};
+
+
+function greet_ben() {
+  name = 'Ben'; 
+  return "Hello, " + name + '!';
+};
+
+
+
+
+var greet_abe = greet('Abe');
+var greet_ben = greet('Ben');
+
+function greet(name) {
+  return function() {
+    return "Hello, " + name + '!';
+  };
+}
+
+
+
+
+const greet_abe = () => 'Hello, Abe!';
+const greet_ben = () => 'Hello, Ben!';
+
+
+
+
+const greet = name =>
+  () => `Hello, ${name}!`;
+const greet_abe = greet(`Abe`);
+const greet_ben = greet(`Ben`);
+
+
+
+
+
+const arr = ['Abe', 'Ben']
+
+const greet_abe = function() {
+  return "Hello, " + arr[0] + '!';
+};
+
+const greet_ben = function() {
+  return "Hello, " + arr[1] + '!';
+};
