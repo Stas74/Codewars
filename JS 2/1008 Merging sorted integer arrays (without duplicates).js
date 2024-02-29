68ce0b3ad3bfb0f7d41ffc84a10f6f33dea60425
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-02-29 11:55:26 -0700

    Update 1008 Merging sorted integer arrays (without duplicates).js

--- JS 2/1008 Merging sorted integer arrays (without duplicates).js
+++ JS 2/1008 Merging sorted integer arrays (without duplicates).js
@@ -11,3 +11,35 @@ function mergeArrays(a, b) {
 
 console.log(mergeArrays([1, 3, 5], [2, 4, 6])); // [1, 2, 3, 4, 5, 6]
 console.log(mergeArrays([2, 4, 8], [2, 4, 6])); // [2, 4, 6, 8]
+
+
+
+function mergeArrays(a, b) {
+  return [...new Set(a.concat(b))].sort((a, b) => a - b);
+}
+
+
+
+function mergeArrays(a, b) {
+    let merge = [...a, ...b].sort()
+    let unique = new Set(merge)
+    return Array.from(unique).sort((a,b)=> a-b)
+}
+
+
+
+function mergeArrays(a, b) {
+  const array = [...a, ...b].sort((a,b) => a-b)
+  const answer = array.filter((item, index) => array.indexOf(item) === index)  
+  return answer
+}
+
+
+
+
+function mergeArrays(a, b) {
+ return [...a,...b].sort((a,b)=> a - b).filter ((elem,i,x)=> i == x.indexOf(elem))
+}
+
+
+
