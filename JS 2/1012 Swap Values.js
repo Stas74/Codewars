d8cf45a41228d1537e3a47e1851ca59919f8adff
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-03-06 11:04:21 -0700

    Update 1012 Swap Values.js

--- JS 2/1012 Swap Values.js
+++ JS 2/1012 Swap Values.js
@@ -20,3 +20,45 @@ function swapValues(args) {
 }
 
 console.log(swapValues([1, 2])); // [2,1]
+
+
+
+
+function swapValues() {
+  return arguments[0].reverse();
+}
+
+
+
+function swapValues(args) {
+  var temp = args[0];
+  args[0] = args[1];
+  args[1] = temp;
+}
+
+
+
+
+function swapValues() {
+    var args = Array.prototype.slice.call(arguments)[0];
+    var temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+}
+
+
+
+const swapValues = (arr) => [arr[0], arr[1]] = [arr[1], arr[0]]
+
+
+
+
+function swapValues() {
+    var args = arguments[0];
+    var temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+}
+
+
+
