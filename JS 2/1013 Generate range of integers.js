36a5b0004ff737cf553709c1eb84de4cac4b12b7
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-03-07 11:42:32 -0700

    Update 1013 Generate range of integers.js

--- JS 2/1013 Generate range of integers.js
+++ JS 2/1013 Generate range of integers.js
@@ -23,3 +23,56 @@ function generateRange(min, max, step) {
 }
 
 console.log(generateRange(2, 10, 2)); // [2,4,6,8,10]
+
+
+
+
+function generateRange(min, max, step){
+  var arr = [];
+  while (min <= max) {
+    arr.push(min);
+    min += step;
+  }
+  return arr
+}
+
+
+
+
+function generateRange(min, max, step){
+  return min > max ? [] : [min, ...generateRange(min + step, max, step)];
+}
+
+
+
+
+
+function generateRange(min, max, step) {
+  return [...rangeGenerator(min, max, step)];
+}
+
+function* rangeGenerator(min, max, step) {
+  while (min <= max) {
+    yield min;
+    min += step;
+  }
+}
+
+
+
+
+
+function generateRange(min, max, step){
+  return Array.from({length:1+(max-min)/step}, (_, i) => min + i*step);
+}
+
+
+
+
+const generateRange = (min, max, step) =>
+  [...Array((max - min) / step + 1 ^ 0)].map((_, idx) => min + step * idx);
+
+
+
+
+const generateRange = (min, max, step) => Array.from(Array((max -min)/step + 1 | 0), (_,i) => min + i * step);
