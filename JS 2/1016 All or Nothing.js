9b00645225b6b073399281c6e755f11cfa500fd3
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-03-12 11:27:57 -0600

    Create 1016 All or Nothing.js

---
+++ JS 2/1016 All or Nothing.js
@@ -0,0 +1,62 @@
+// All or Nothing
+
+/*
+Suppose a student can earn 100% on an exam by getting the answers all correct or all incorrect. 
+Given a potentially incomplete answer key and the student's answers, write a function that determines 
+whether or not a student can still score 100%. Incomplete questions are marked with an underscore, "_".
+
+["A", "_", "C", "_", "B"]   # answer key
+["A", "D", "C", "E", "B"]   # student's solution
+
+➞ True
+
+# Possible for student to get all questions correct.
+
+["B", "_", "B"]   # answer key
+["B", "D", "C"]   # student's solution
+
+➞ False
+
+# First question is correct but third is wrong, so not possible to score 100%.
+
+["T", "_", "F", "F", "F"]   # answer key
+["F", "F", "T", "T", "T"]   # student's solution
+
+➞ True
+
+# Possible for student to get all questions incorrect.
+Examples
+
+(["B", "A", "_", "_"], ["B", "A", "C", "C"]) ➞ True
+
+(["A", "B", "A", "_"], ["B", "A", "C", "C"]) ➞ True
+
+(["A", "B", "C", "_"], ["B", "A", "C", "C"]) ➞ False
+
+(["B", "_"], ["C", "A"]) ➞ True
+
+(["B", "A"], ["C", "A"]) ➞ False
+
+(["B"], ["B"]) ➞ True
+
+(["_"], ["B"]) ➞ True
+
+Notes
+
+Test has at least one question.
+len(key) == len(answers)
+*/
+
+function possiblyPerfect(key, answers) {
+  let counter = 0;
+  for (let i = 0; i < key.length; i++) {
+    if (key[i] == answers[i]) {
+      counter++;
+    }
+  }
+  return counter == 0 || key.filter((el) => el != "_").length == counter;
+}
+
+console.log(possiblyPerfect(["B", "A", "_", "_"], ["B", "A", "C", "C"])); // true
+console.log(possiblyPerfect(["A", "B", "C", "_"], ["B", "A", "C", "C"])); // false
+console.log(possiblyPerfect([..."T_FFF"], [..."FFTTT"])); // true
