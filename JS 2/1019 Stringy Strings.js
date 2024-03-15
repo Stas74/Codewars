4ec01e0928bc0ff58cfcd85c2e3060f1843a84ac
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-03-15 14:44:23 -0600

    Update 1019 Stringy Strings.js

--- JS 2/1019 Stringy Strings.js
+++ JS 2/1019 Stringy Strings.js
@@ -21,3 +21,35 @@ function stringy(size) {
 console.log(stringy(4)); // '1010'
 console.log(stringy(6)); // '101010'
 console.log(stringy(12)); // '101010101010'
+
+
+
+const stringy = x => ''.padStart(x,'10');
+
+
+
+const stringy = size => "10".repeat(size).slice(0,size);
+
+
+
+const stringy = size => Array.from(Array(size), (v, i) => +!(i % 2)).join('');
+
+
+
+const stringy = size =>
+  ``.padStart(size, `10`);
+
+
+
+function stringy(size) {
+  return new Array(size).join('10').substr(0,size)   
+}
+
+
+
+function stringy(size) {
+  return ('1'.repeat(size)).replace(/11/g, '10');
+}
+
+
+
