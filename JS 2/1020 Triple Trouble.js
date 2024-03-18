974f4dac0879317e1b5c9ea9e63373d14cd3420a
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-03-18 15:04:28 -0600

    Create 1020 Triple Trouble.js

---
+++ JS 2/1020 Triple Trouble.js
@@ -0,0 +1,24 @@
+// Triple Trouble
+
+/*
+Triple Trouble
+
+Create a function that will return a string that combines all of the letters of the three inputed strings in groups. 
+Taking the first letter of all of the inputs and grouping them next to each other. 
+Do this for every letter, see example below!
+
+E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+Note: You can expect all of the inputs to be the same length.
+*/
+
+function tripleTrouble(one, two, three) {
+  let finStr = "";
+  for (let i = 0; i < one.length; i++) {
+    finStr += one[i] + two[i] + three[i];
+  }
+  return finStr;
+}
+
+console.log(tripleTrouble("aaa", "bbb", "ccc")); // "abcabcabc"
+console.log(tripleTrouble("Sea", "urn", "pms")); // "Supermans"
