a4659ebbaa72bb8a340bba3732b70383f37345ae
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-03-18 15:16:56 -0600

    Update 1020 Triple Trouble.js

--- JS 2/1020 Triple Trouble.js
+++ JS 2/1020 Triple Trouble.js
@@ -22,3 +22,51 @@ function tripleTrouble(one, two, three) {
 
 console.log(tripleTrouble("aaa", "bbb", "ccc")); // "abcabcabc"
 console.log(tripleTrouble("Sea", "urn", "pms")); // "Supermans"
+
+
+
+
+function tripleTrouble(one, two, three) {
+  var result = "";
+  for (let i = 0; i < one.length; i++) {
+    result += one.charAt(i) + two.charAt(i) + three.charAt(i);
+  }
+  return result;
+}
+
+
+
+const tripleTrouble = (one, two, three) => one.split("").map((letter, index) => letter + two[index] + three[index]).join("");
+
+
+
+function tripleTrouble(one, two, three){
+  return one.replace(/./g,(v,i) => v + two[i] + three[i])
+ }
+
+
+
+
+const tripleTrouble = (one, two, three) =>
+  one.replace(/\w/g, (val, idx) => val + two[idx] + three[idx]);
+
+
+
+
+const tripleTrouble = (one, two, three) => 
+  one.split('').reduce((acc, c, i, array) => 
+    [acc, one[i], two[i], three[i]].join(''), '')
+
+
+
+// Recursive solution
+const tripleTrouble = (...arg) =>
+  !arg[0][0] ? '' : arg.map(i => i[0]).join('') + tripleTrouble(...arg.map(i => i.slice(1)))
+
+
+
+
+function tripleTrouble(one, two, three){
+  let len = [one, two, three].sort((a,b)=> b.length - a.length)[0]
+  return [...len].map((_, i)=> (one[i] || '') + (two[i] || '') + three[i]).join('')
+ }//dupa
