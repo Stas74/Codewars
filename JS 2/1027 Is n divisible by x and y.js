10f51e53e29d29889b4c632bcd04001777e79916
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-04-03 11:42:57 -0600

    Update 1027 Is n divisible by x and y.js

--- JS 2/1027 Is n divisible by x and y.js
+++ JS 2/1027 Is n divisible by x and y.js
@@ -19,3 +19,29 @@ console.log(isDivisible(3, 3, 4)); // false
 console.log(isDivisible(12, 3, 4)); // true
 console.log(isDivisible(8, 3, 4)); // false
 console.log(isDivisible(48, 3, 4)); // true
+
+
+
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0
+}
+
+
+
+const isDivisible = (n, x, y) => n % x == 0 && n % y == 0;
+
+
+
+
+function isDivisible(n, x, y) {
+  return !(n % x || n % y)
+}
+
+
+
+function isDivisible(n, ...dividers) {
+  return dividers.every(divider => n % divider == 0)
+}
+
+
+
