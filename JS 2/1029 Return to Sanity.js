00a56fcb717b34494ae0f402017e21fda0496564
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-04-05 13:54:41 -0600

    Create 1029 Return to Sanity.js

---
+++ JS 2/1029 Return to Sanity.js
@@ -0,0 +1,24 @@
+// Return to Sanity
+
+/*
+This function should return an object, but it's not doing what's intended. What's wrong?
+
+function mystery() {
+  var results =
+    {sanity: 'Hello'};
+  return
+    results;
+}
+
+*/
+
+// function mystery() {
+//   var results = { sanity: "Hello" };
+//   return results;
+// }
+
+function mystery() {
+  return { sanity: "Hello" };
+}
+
+console.log(mystery()); //
