27d108b4ceb6eb77d4fc504537290803a43110d9
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-04-10 14:45:50 -0600

    Update 1032 Incorrect division method.js

--- JS 2/1032 Incorrect division method.js
+++ JS 2/1032 Incorrect division method.js
@@ -11,3 +11,26 @@ const solve = (x, y) => x / y;
 
 console.log(solve(2, 1)); // 2
 console.log(solve(2, 4)); // 0.5
+
+
+
+
+function solve(x, y) {
+  return (y == 0) ? Infinity : (x / y);
+}
+
+
+
+const solve = (x, y) => {
+  let s = x / y;
+  if (y == 0) {
+    return Infinity;
+  }
+  return s;
+};
+
+
+
+const solve = (x, y) => {
+  return x / y == NaN ? Infinity : x / y;
+}
