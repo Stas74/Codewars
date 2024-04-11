5a4d059010a9f754b46a03c6aedf5ad6974afdcc
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-04-11 14:05:07 -0600

    Update 1033 Jenny's secret message.js

--- JS 2/1033 Jenny's secret message.js
+++ JS 2/1033 Jenny's secret message.js
@@ -26,3 +26,26 @@ function greet(name) {
 console.log(greet("Jim")); // "Hello, Jim!"
 console.log(greet("Jane")); // "Hello, Jane!"
 console.log(greet("Johnny")); // "Hello, my love!"
+
+
+
+
+function greet(name){
+  return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
+}
+
+
+
+let greet = name => `Hello, ${name == "Johnny" ? "my love" : name}!`;
+
+
+
+function greet(name){
+  return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
+}
+
+
+
+const greet = name => `Hello, ${name === 'Johnny' ? 'my love' : name}!`;
+
+
