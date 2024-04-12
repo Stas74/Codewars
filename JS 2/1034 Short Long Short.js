a2a345a3c128fac1bf0ed05f742df0c2a692f67d
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-04-12 12:52:34 -0600

    Update 1034 Short Long Short.js

--- JS 2/1034 Short Long Short.js
+++ JS 2/1034 Short Long Short.js
@@ -22,3 +22,21 @@ console.log(solution('45', '1')); // '1451'
 console.log(solution('13', '200')); // '1320013'
 console.log(solution('Soon', 'Me')); // 'MeSoonMe'
 console.log(solution('U', 'False')); // 'UFalseU'
+
+
+
+function solution(a, b) {
+  return a.length < b.length ? a + b + a : b + a + b
+}
+
+
+
+const solution = (a, b) => {
+  return a.length > b.length ? `${b}${a}${b}` : `${a}${b}${a}`
+}
+
+
+var solution = (a, b) => a.length > b.length ? b + a + b : a + b + a;
+
+
+
