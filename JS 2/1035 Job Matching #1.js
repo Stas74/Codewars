b6e132bb96bad3bb7ad7c8793fb2e4ad48ec0419
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-04-15 15:08:13 -0600

    Create 1035 Job Matching #1.js

---
+++ JS 2/1035 Job Matching #1.js
@@ -0,0 +1,46 @@
+// Job Matching #1
+
+/*
+Let's build a matchmaking system that helps discover jobs for developers based on a number of factors.
+
+One of the simplest, yet most important factors is compensation. 
+As developers we know how much money we need to support our lifestyle, 
+so we generally have a rough idea of the minimum salary we would be satisfied with.
+
+Let's give this a try. We'll create a function match (job_matching in Python) which takes a candidate and a job, 
+which will return a Boolean indicating whether the job is a valid match for the candidate.
+
+A candidate will have a minimum salary, so it will look like this:
+
+let candidate = {
+  minSalary: 120000
+}
+A job will have a maximum salary, so it will look like this:
+
+let job = {
+  maxSalary: 140000
+}
+If either the candidate's minimum salary or the job's maximum salary is not present, throw an error.
+
+For a valid match, the candidate's minimum salary must be less than or equal to the job's maximum salary. 
+However, let's also include 10% wiggle room (deducted from the candidate's minimum salary) 
+in case the candidate is a rockstar who enjoys programming on Codewars in their spare time. 
+The company offering the job may be able to work something out.
+*/
+
+function match(candidate, job) {
+  if (!candidate.minSalary || !job.maxSalary) {
+    throw new Error("Parameters not avalaible!");
+  }
+  return candidate.minSalary * 0.9 <= job.maxSalary;
+}
+
+let candidate1 = { minSalary: 120000 },
+  job1 = { maxSalary: 130000 },
+  job2 = { maxSalary: 80000 };
+
+console.log(match(candidate1, job1)); // true
+console.log(match(candidate1, job2)); // false
+
+console.log(match({ minSalary: 190000 }, { maxSalary: 171000 })); // true
+console.log(match({}, { maxSalary: 171000 })); // error
