9ddbb17d4060adf40b7d9407179b960acfb082dd
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-04-23 19:39:10 -0600

    Create 1041 Reversed Words.js

---
+++ JS 2/1041 Reversed Words.js
@@ -0,0 +1,21 @@
+// Reversed Words
+
+/*
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.
+
+Example(Input --> Output):
+
+"The greatest victory is that which requires no battle" --> 
+"battle no requires which that is victory greatest The"
+
+*/
+
+function reverseWords(str) {
+  return str.split(" ").reverse().join(" "); // reverse those words
+}
+
+console.log(reverseWords("hello world!")); // "world! hello"
+console.log(reverseWords("yoda doesn't speak like this")); // "this like speak doesn't yoda"
+console.log(reverseWords("row row row your boat")); // "boat your row row row"
