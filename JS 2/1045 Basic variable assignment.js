39e984bd81e01e3b76476fd17336c3c13b7d3f09
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-04-30 16:28:35 -0600

    Create 1045 Basic variable assignment.js

---
+++ JS 2/1045 Basic variable assignment.js
@@ -0,0 +1,16 @@
+// Basic variable assignment
+
+/*
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+a == "code"
+b == "wa.rs"
+name == a + b
+
+*/
+
+const a = "code";
+const b = "wa.rs";
+let name = a + b;
+
+console.log(name); // "codewa.rs"
