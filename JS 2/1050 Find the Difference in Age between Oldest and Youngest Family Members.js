28b6662dec9a58e163186302ff38025e94223a5a
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-05-07 22:36:43 -0600

    Update 1050 Find the Difference in Age between Oldest and Youngest Family Members.js

--- JS 2/1050 Find the Difference in Age between Oldest and Youngest Family Members.js
+++ JS 2/1050 Find the Difference in Age between Oldest and Youngest Family Members.js
@@ -16,3 +16,45 @@ function differenceInAges(ages){
 
 console.log(differenceInAges([82, 15, 6, 38, 35])); // [6, 82, 76]
 console.log(differenceInAges([57, 99, 14, 32])); // [14, 99, 85]
+
+
+
+
+
+function differenceInAges (ages) {
+
+    let max = Math.max(...ages),
+        min = Math.min(...ages)
+        diff = max - min
+        
+    return [min, max, diff]
+}
+
+
+
+
+function differenceInAges(ages){
+  const min = Math.min(...ages);
+  const max = Math.max(...ages);
+  
+  return [min, max, max-min];
+}
+
+
+
+const differenceInAges = (ages) => {
+  let youngest = Math.min.apply(null, ages);
+  let oldest = Math.max.apply(null, ages);
+  return [youngest, oldest, oldest - youngest];
+};
+
+
+
+
+const differenceInAges = a => (min = Math.min(...a),max = Math.max(...a),[min,max,max-min])
+
+
+
+const differenceInAges = ages =>
+  (val => val.concat(val[1] - val[0]))
+  ([Math.min(...ages), Math.max(...ages)]);
