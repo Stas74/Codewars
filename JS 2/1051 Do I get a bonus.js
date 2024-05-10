d0d18d2045a9ce1d0dd760ca49917fd1d748c48f
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-05-10 08:52:11 -0600

    Create 1051 Do I get a bonus.js

---
+++ JS 2/1051 Do I get a bonus.js
@@ -0,0 +1,24 @@
+// Do I get a bonus?
+
+/*
+It's bonus time in the big city! 
+The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+
+Build a function that takes in two arguments (salary, bonus). 
+Salary will be an integer, and bonus a boolean.
+
+If bonus is true, the salary should be multiplied by 10. If bonus is false, 
+the fatcat did not make enough money and must receive only his stated salary.
+
+Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java, Scala, and Julia), 
+"$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell, and Lua) or "¥" (Rust).
+*/
+
+function bonusTime(salary, bonus) {
+  return `£${bonus ? salary * 10 : salary}`;
+}
+
+console.log(bonusTime(10000, true)); // '£100000'
+console.log(bonusTime(25000, true)); // '£250000'
+console.log(bonusTime(10000, false)); // '£100000'
+console.log(bonusTime(60000, false)); // '£60000'
