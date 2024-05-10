d87c29807471668fd4d87f13486cfbe67ce77c69
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-05-10 08:53:14 -0600

    Update 1051 Do I get a bonus.js

--- JS 2/1051 Do I get a bonus.js
+++ JS 2/1051 Do I get a bonus.js
@@ -22,3 +22,22 @@ console.log(bonusTime(10000, true)); // '£100000'
 console.log(bonusTime(25000, true)); // '£250000'
 console.log(bonusTime(10000, false)); // '£100000'
 console.log(bonusTime(60000, false)); // '£60000'
+
+
+
+function bonusTime(salary, bonus) {
+  return bonus ? `£${10 * salary}` : `£${salary}`;
+}
+
+
+const bonusTime = (salary, bonus) => `£${salary * (bonus ? 10 : 1)}`;
+
+
+
+function bonusTime(salary, bonus) {
+  return '£' + salary * (bonus ? 10 : 1);
+}
+
+
+
+const bonusTime = (salary, bonus) => bonus ? `£${salary * 10}` : `£${salary}`;
