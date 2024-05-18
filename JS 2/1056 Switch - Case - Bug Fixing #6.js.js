70d23f776e842888cd7a964ed851d9898f4cecdb
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-05-17 21:46:23 -0600

    Update 1056 Switch - Case - Bug Fixing #6.js.js

--- JS 2/1056 Switch - Case - Bug Fixing #6.js.js
+++ JS 2/1056 Switch - Case - Bug Fixing #6.js.js
@@ -41,3 +41,57 @@ function evalObject(value) {
 
 console.log(evalObject({ a: 1, b: 1, operation: "+" })); // 2
 console.log(evalObject({ a: 1, b: 1, operation: "-" })); // 0
+
+
+
+
+function evalObject(value){
+  switch(value.operation){
+    case'+': return value.a + value.b;
+    case'-': return value.a - value.b;
+    case'/': return value.a / value.b;
+    case'*': return value.a * value.b;
+    case'%': return value.a % value.b;
+    case'^': return Math.pow(value.a, value.b);
+  }
+}
+
+
+
+
+let ops = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '/': (a, b) => a / b,
+  '*': (a, b) => a * b,
+  '%': (a, b) => a % b,
+  '^': Math.pow,
+}
+
+let evalObject = o => ops[o.operation](o.a, o.b)
+
+
+
+
+const evalObject = value => ({
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '/': (a, b) => a / b,
+  '*': (a, b) => a * b,
+  '%': (a, b) => a % b,
+  '^': Math.pow,
+}[value.operation](value.a, value.b));
+
+
+
+
+const evalObject = value => {
+  return {
+    '+': v => v.a + v.b,
+    '-': v => v.a - v.b,
+    '/': v => v.a / v.b,
+    '*': v => v.a * v.b,
+    '%': v => v.a % v.b,
+    '^': v => v.a ** v.b,
+  }[value.operation](value);
+}
