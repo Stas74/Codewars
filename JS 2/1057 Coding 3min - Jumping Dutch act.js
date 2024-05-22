8bd57b404f7403312015b744ef91a3e3a7a1996b
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-05-22 15:42:07 -0600

    Update 1057 Coding 3min - Jumping Dutch act.js

--- JS 2/1057 Coding 3min - Jumping Dutch act.js
+++ JS 2/1057 Coding 3min - Jumping Dutch act.js
@@ -33,6 +33,8 @@ sc(7) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!"
 sc(10) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!"
 */
 
+// Length should be more 90 symbols
+
 function sc(floor){
   return floor < 2 ? "" : floor < 7 ? "Aa~ ".repeat(floor - 1) + "Pa!" + " Aa!" : "Aa~ ".repeat(floor - 1) + "Pa!";
 }
@@ -42,3 +44,51 @@ console.log(sc(1)); // ""
 console.log(sc(2)); // "Aa~ Pa! Aa!"
 console.log(sc(6)); // "Aa~ Aa~ Aa~ Aa~ Aa~ Pa! Aa!"
 console.log(sc(7)); // "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!"
+
+
+
+
+function sc(floor){
+  if(floor <= 1) return "";
+  
+  return 'Aa~ '.repeat(floor-1) + 'Pa!' + (floor<=6 ? ' Aa!': '');
+}
+
+
+
+function sc(floor) {
+  if (floor <= 1) return "";
+  var SCREAM = "";
+  for (let i = 0; i < floor - 1; i++) {
+    SCREAM += "Aa~ ";
+  }
+  SCREAM += "Pa!";
+  if (floor <= 6) SCREAM += " Aa!";
+  return SCREAM;
+}
+
+
+
+const sc = floor => floor < 2 ? '' : 'Aa~ '.repeat(floor-1) + 'Pa!' + (floor < 7 ? ' Aa!' : '')
+
+
+
+const sc = floor => floor > 1 ? Array.from({length: floor - 1}, () => "Aa~").concat(floor > 6 ? ["Pa!"] : ["Pa!", "Aa!"]).join(" ") : "";
+
+
+
+const sc = floor => 
+  floor >= 7 ? 'Aa~ '.repeat(floor - 1) + 'Pa!' :
+  floor > 1 ?  'Aa~ '.repeat(floor - 1) + 'Pa! Aa!' : ''
+
+
+
+const sc=(floor)=>floor>1?new Array(floor).join('Aa~ ')+'Pa!'+(floor>6?'':' Aa!'):'';
+//or you can use the ES6 new API array.from :)   just like follow:
+const otherSc=(floor)=>floor>1?Array.from({length:floor-1}).map(a=>'Aa~ ').join('')+'Pa!'+(floor>6?'':' Aa!'):'';
+
+
+
+
+const sc = floor =>
+  floor < 2.00000000000000 ? `` : `${`Aa~ `.repeat(--floor)}Pa!${floor < 6 ? ` Aa!` : ``}`;
