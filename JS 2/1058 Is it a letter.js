fb2a93cc35163a9a76ec79fdeccdd4e6a688be55
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-05-23 22:34:45 -0600

    Update 1058 Is it a letter.js

--- JS 2/1058 Is it a letter.js
+++ JS 2/1058 Is it a letter.js
@@ -11,3 +11,56 @@ function isItLetter(character) {
 
 console.log(isItLetter("a")); // true
 console.log(isItLetter("1")); // false
+
+
+
+function isItLetter(character) {
+    return character == character.toUpperCase() ? character !== character.toLowerCase():
+    character !== character.toUpperCase()
+}
+
+
+
+function isItLetter(cc) {
+  if( cc.toLowerCase() === cc.toUpperCase()) return false;
+  return true
+}
+
+
+
+const isItLetter = (char) => char.toLowerCase() !== char.toUpperCase() 
+
+
+
+
+function isItLetter(character) {
+  // Check if character is a string
+  if (typeof character !== "string") {
+    return false;
+  }
+
+  if (
+    (character >= 'a' && character <= 'z') ||
+    (character >= 'A' && character <= 'Z')
+  ) {
+    return true; 
+  }
+
+  return false; 
+}
+
+
+
+function isItLetter(character) {
+  let characters = character.toLowerCase();
+  const letter = 'qwertyuiopasdfghjklzxcvbnm';
+  if(letter.includes(characters)){
+    return true;
+  }else{
+   return false
+  }
+  return ;
+
+
+
+
