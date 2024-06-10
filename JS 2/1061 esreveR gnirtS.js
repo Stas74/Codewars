f986c5ab03f5efab22bc7425ac41a95c916b497c
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-06-10 15:11:59 -0600

    Update 1061 esreveR gnirtS.js

--- JS 2/1061 esreveR gnirtS.js
+++ JS 2/1061 esreveR gnirtS.js
@@ -11,3 +11,67 @@ String.prototype.reverse = function reverse() {
   return this.split("").reverse().join("");
 };
 
+
+
+
+//Create reverse function for the String prototype
+String.prototype.reverse = function () {
+  return [...this].reverse().join``;
+};
+
+
+
+// lets make something different
+String.prototype.reverse = function() {
+  let rev = [];
+  for (let letter of this) {
+    rev.unshift(letter);
+  }
+  return rev.join('');
+}
+
+
+
+String.prototype.reverse = function(){
+  return this.length ? this.substring(1).reverse() + this.charAt(0) : "";
+}
+
+
+
+
+String.prototype.reverse = function() {
+  return this.split('').reduceRight((acc, val) => acc + val,'')
+}
+
+
+
+
+String.prototype.reverse=function(){
+  return [...this.split(' ')].map(e => [...e].reverse().join('')).reverse().join(' ');
+};
+
+
+
+//Create reverse function for the String prototype
+
+String.prototype.reverse=function(...args){
+    let b = args; 
+    
+  try{
+    return  [...this].reverse().join('');
+    }
+   catch(e){
+     return this
+   }
+}
+
+
+
+String.prototype.reverse=function() {
+  return [...this].reverse().join``
+}
+
+
+
+//Create reverse function for the String prototype
+String.prototype.reverse = function() {return this.split``.reverse().join``}
