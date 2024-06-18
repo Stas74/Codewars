9591743cd7c89489173b5e1c9db196d210b8a87f
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-06-17 21:48:25 -0600

    Update 1062 String reverse slicing 101.js

--- JS 2/1062 String reverse slicing 101.js
+++ JS 2/1062 String reverse slicing 101.js
@@ -23,3 +23,41 @@ function reverseSlice(str) {
 
 console.log(reverseSlice("123")); // ['321', '21', '1']
 console.log(reverseSlice("abcde")); // ['edcba', 'dcba', 'cba', 'ba', 'a']
+
+
+
+
+
+function reverseSlice(s) {
+  return Array(s.length).fill(0).map((e, i) => s.split("").reverse().join("").slice(i));
+}
+
+
+
+const reverseSlice = str =>
+  Array(str.length).fill(str.replace(/./g, (_, idx) => str[str.length - ++idx])).map((val, idx) => val.slice(idx));
+
+
+
+const reverseSlice = (str, arr = []) => 
+	str ?
+  	arr.concat(reverseStr(str), reverseSlice(str.slice(0, -1), arr)) :
+    arr
+    
+const reverseStr = str => [...str].reverse().join('')
+
+
+
+reverseSlice = s => (s = s.split('').reverse().join('')).split('').map((e, i) => e = s.slice(i));
+
+
+
+function reverseSlice(str) {
+  return Array(str.length).fill([...str].reverse().join('')).map((e,i) => e.slice(-(str.length-i)));
+}
+
+
+
+function reverseSlice(s) {
+  return [...s].reverse().map((_,i,a)=>a.slice(i).join``);
+}
