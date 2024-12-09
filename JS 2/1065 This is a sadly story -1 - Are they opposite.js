75d52d9617fd166014aaf6e2dbe27e0194714f2a
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-12-09 11:31:13 -0800

    Update 1065 This is a sadly story -1 - Are they opposite.js

--- JS 2/1065 This is a sadly story -1 - Are they opposite.js
+++ JS 2/1065 This is a sadly story -1 - Are they opposite.js
@@ -37,3 +37,58 @@ console.log(isOpposite("aBcde", "AbCD")); // false
 console.log(isOpposite("AB", "Ab")); // false
 console.log(isOpposite("", "")); // false
 console.log(isOpposite("QWXYhhkTqO", "vwGqrQPowG")); // false
+
+
+
+
+
+function isOpposite(s1,s2){
+  if(s1 === s2 || s1.toLowerCase() !== s2.toLowerCase()) return false;
+  
+  for(var i = 0; i < s1.length; i++) {
+    if(s1.charAt(i) === s2.charAt(i)) return false;
+  }
+  
+  return true;  
+}
+
+
+function isOpposite(s1,s2){
+  return s1.split('')
+    .map(c => c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase())
+    .join('') === s2 && s1 !== '';  
+}
+
+
+function isOpposite(s1,s2){
+  return s1!=''&&s1==s2.replace(/./g,ch=>ch['to'+(ch<='Z'?'Lower':'Upper')+'Case']())
+}
+
+
+function isOpposite(s1,s2){
+  return (s1 !== s2) && s1.split("").map(function(el) {
+    return ((/[a-z]/).test(el)) ? el.toUpperCase() : el.toLowerCase();
+  }).join("") == s2;
+}
+
+
+const isOpposite = (s1, s2) =>
+  !!s1 && s2 === s1.replace(/[A-Z]/gi, val => val[`to${val < `a` ? `Low` : `Upp`}erCase`]());
+
+
+
+const isOpposite = (s1, s2) => {
+  if (!s1.length || !s2.length) return false
+  const s3 = s2.replace(/[a-z]/ig, c => c.charCodeAt(0) < 97 ? c.toLowerCase() : c.toUpperCase())
+  return s3 === s1
+}
+
+
+const isOpposite = (a, b) => {
+  if (!a && !b) return false;
+  return (
+    [...a]
+      .map((i) => (i === i.toLowerCase() ? i.toUpperCase() : i.toLowerCase()))
+      .join("") === b
+  );
+};
