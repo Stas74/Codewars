8a645aca9ea65921d1948056f0fa4d9ded9c9e48
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-12-10 17:20:04 -0800

    Create 1066 Get Nth Even Number.js

---
+++ JS 2/1066 Get Nth Even Number.js
@@ -0,0 +1,24 @@
+// Get Nth Even Number
+
+/*
+Return the Nth Even Number
+
+Example(Input --> Output)
+
+1 --> 0 (the first even number is 0)
+3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+100 --> 198
+1298734 --> 2597466
+
+The input will not be 0.
+*/
+
+function nthEven(n) {
+  return n * 2 - 2;
+}
+
+console.log(nthEven(1)); // 0
+console.log(nthEven(2)); // 2
+console.log(nthEven(3)); // 4
+console.log(nthEven(100)); // 198
+console.log(nthEven(1298734)); // 2597466
