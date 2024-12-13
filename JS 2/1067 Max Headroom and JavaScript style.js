8215f5e65c6bd8ee6cfd8122d04e5f9e7c250729
Stas74 <88213143+Stas74@users.noreply.github.com>
2024-12-12 19:49:39 -0800

    Update 1067 Max Headroom and JavaScript style.js

--- JS 2/1067 Max Headroom and JavaScript style.js
+++ JS 2/1067 Max Headroom and JavaScript style.js
@@ -38,3 +38,47 @@ function getMax2() {
 
 console.log(getMax1()); // { name: 'Max Headroom' }
 console.log(getMax2()); // { name: 'Max Headroom' }
+
+
+
+function getMax1()
+{
+  var max = {name: 'Max Headroom'}
+  return max;
+}
+
+function getMax2()
+{
+// Have your return object on same line
+return {name: 'Max Headroom'};
+}
+
+
+
+function getMax1()
+{
+  var max = 
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2()
+{
+  // note Javascript does not require the semicolon but can assume it meaning nothing is returned
+  // instead move the brace up
+  return  {
+    name: 'Max Headroom'
+  }
+}
+
+
+
+const getMax1 = () => {
+  const max = {name: `Max Headroom`};
+  return max;
+};
+
+const getMax2 = () =>
+  ({name: `Max Headroom`});
