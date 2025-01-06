f85858a44ff642790d2edff9fbea4249208c662f
Stas74 <88213143+Stas74@users.noreply.github.com>
2025-01-06 13:34:40 -0800

    Update 1069 Sum and Multiply.js

--- JS 2/1069 Sum and Multiply.js
+++ JS 2/1069 Sum and Multiply.js
@@ -47,3 +47,56 @@ function sumAndMultiply(sum, multiply) {
 console.log(sumAndMultiply(13, 12)); // [1, 12]
 console.log(sumAndMultiply(6, 9)); // [3, 3]
 console.log(sumAndMultiply(50, 0)); // [3, 3]
+
+
+
+
+const sumAndMultiply = (sum, multiply, a = 0, b = sum - a) => a * b == multiply ? [a,b] : b < a ? null : sumAndMultiply(sum, multiply, a+1)
+
+
+const sumAndMultiply = (sum, multiply) => {
+  let res = null;
+  
+  for (let i = 0; i <= sum; i++) {
+    if (multiply / i + i === sum) res = [multiply / i, i]
+  }
+  
+  return res;
+};
+
+
+
+const sumAndMultiply = (sum, multiply) => {
+  let res = null;
+  
+  for (let i = 0; i <= sum; i++) {
+    if (multiply / i + i === sum) res = [multiply / i, i]
+  }
+  
+  return res;
+};
+
+
+
+function sumAndMultiply(sum, mul, max = Math.sqrt(mul)) {
+  for (var a = -1, b; a <= max; a++, b = sum - a)
+  	if (a * b === mul) return [a, b];
+  return null;
+}
+
+
+
+var sumAndMultiply = function(s, m) {
+  const r = Math.sqrt(s * s - 4 * m);
+  
+  return r % 2 === s % 2 
+       ? [ (s - r) / 2, (s + r) / 2 ] 
+       : null;  
+}
+
+
+const sumAndMultiply = (sum, multiply) =>
+  (val =>  sum % 2 === val % 2 ? [sum - val >> 1, sum + val >> 1] : null)
+  ((sum ** 2 - 4 * multiply) ** .5);
+
+
