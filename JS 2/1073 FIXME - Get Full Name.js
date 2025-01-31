be2bfc775fb8951d1808209120357557fa77503c
Stas74 <88213143+Stas74@users.noreply.github.com>
2025-01-30 16:27:03 -0800

    Create 1073 FIXME - Get Full Name.js

---
+++ JS 2/1073 FIXME - Get Full Name.js
@@ -0,0 +1,39 @@
+// FIXME: Get Full Name
+
+/*
+The code provided is supposed return a person's Full Name given their first and last names.
+
+But it's not working properly.
+
+Notes
+The first and/or last names are never null, but may be empty.
+
+Task
+Fix the bug so we can all go home early.
+
+
+class Dinglemouse{
+
+  constructor( firstName, lastName ){
+  }
+  
+  getFullName(){
+    return firstName+" "+lastName
+  }
+  
+}
+
+*/
+
+class Dinglemouse {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`.trim();
+  }
+}
+
+console.log(new Dinglemouse("Clint", "Eastwood").getFullName()); // "Clint Eastwood"
