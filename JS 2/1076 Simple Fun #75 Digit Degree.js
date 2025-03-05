52fda169c72e58b416ffdb8d0e0f414e620f4153
Stas74 <88213143+Stas74@users.noreply.github.com>
2025-03-04 20:24:16 -0800

    Update 1076 Simple Fun #75 Digit Degree.js

--- JS 2/1076 Simple Fun #75 Digit Degree.js
+++ JS 2/1076 Simple Fun #75 Digit Degree.js
@@ -36,3 +36,56 @@ console.log(digitDegree(91)); // 2
 console.log(digitDegree(99)); // 2
 console.log(digitDegree(1234567)); // 3
 console.log(digitDegree(1000000000)); // 1
+
+
+
+const digitDegree = n =>
+  n < 10 ? 0 : 1 + digitDegree([...`${n}`].reduce((pre, val) => pre + +val, 0));
+
+
+function digitDegree(n, count=0) {
+  while(n>9){
+    n = +n.toString().split('').reduce((s, v) => s + +v, 0);
+    count++;
+  }
+  return count;
+}
+
+
+
+function digitDegree(n) {
+  var r=0
+  while(n>9) r++,n=sum(n)
+  return r
+  
+  function sum(n){
+    return (n+"").split("").reduce((a,b)=>a+ +b,0)
+  }
+}
+
+
+// recursion
+
+function digitDegree(n) {
+  if (n < 10) return 0
+  n = [...n+''].reduce((s,i) => s + +i, 0)
+  return 1 + digitDegree(n)
+}
+
+
+function digitDegree(n) {
+  return n < 10 ? 0 : 1 + digitDegree([...(n + "")].reduce((s, n) => s + +n, 0));
+}
+
+
+const digitDegree = num =>
+  num < 10 ? 0 : 1 + digitDegree([...`${num}`].reduce((acc, dgt) => acc + +dgt, 0));
+
+
+function digitDegree(n) {
+  let c=0;
+  while((n+'').length>1){
+    n = +[...n+''].reduce((a,c) => a+ +c,0);
+    c++;
+  } return c;
+}
